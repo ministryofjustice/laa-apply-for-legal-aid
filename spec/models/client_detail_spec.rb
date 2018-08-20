51bceacb8316f56816db165ee299211de428cc83
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-08-20 14:09:53 +0100

    Add test for relationship between application and applicant

--- spec/models/client_detail_spec.rb
+++ spec/models/client_detail_spec.rb
@@ -4,11 +4,12 @@ require 'date'
 RSpec.describe Applicant, type: :model do
   subject { described_class.new }
 
+  let!(:application) { LegalAidApplication.create }
+
   before do
-    LegalAidApplication.create
     subject.name = "John Doe"
     subject.date_of_birth = Date.new(1988, 02, 01)
-    subject.legal_aid_application_id = 1
+    subject.legal_aid_application_id = application.id
   end
 
   it "is valid with all valid attributes" do
@@ -33,6 +34,10 @@ RSpec.describe Applicant, type: :model do
   it "is not valid if the date of birth is before 1900-01-01" do
     subject.date_of_birth = Date.new(1899, 12, 31)
     expect(subject).to_not be_valid
+  end
+
+  it "should belong to a legal aid application" do
+    expect(subject.legal_aid_application_id).to eq(application.id)
   end
 
 end
