b5ca5732a3a53f889645998882d98e725125d000
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-08-21 12:00:02 +0100

    Remove debug code from legalaid_application_spec.rb

--- spec/requests/legalaid_application_spec.rb
+++ spec/requests/legalaid_application_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'Legal aid applications' do
   describe 'POST /v1/applications' do
     it 'returns an application reference' do
       post '/v1/applications'
-      puts response_json
       expect(response_json['data']['attributes']['application_ref']).not_to be_empty
       expect(response.status).to eql(201)
     end
