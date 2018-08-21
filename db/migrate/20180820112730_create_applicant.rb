7545fb947cf0dbd400b2385b82223b9a87d7e129
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-08-21 13:05:53 +0100

    Revert migrations to master

--- db/migrate/20180820112730_create_applicant.rb
+++ db/migrate/20180820112730_create_applicant.rb
@@ -4,6 +4,7 @@ class CreateApplicant < ActiveRecord::Migration[5.2]
       t.string :name
       t.date :date_of_birth
       t.timestamps
+      t.references :legal_aid_application
     end
   end
 end
