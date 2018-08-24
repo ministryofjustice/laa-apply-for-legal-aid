9e626d9036999cd3e0ab31ac58390dbce322e233
Daman <daman.fergus@justice.gov.uk>
2018-08-24 10:01:55 +0100

    WIP changes to connect the provider frontend to the API

---
+++ app/controllers/v1/applicants_controller.rb
@@ -0,0 +1,61 @@
+class V1::ApplicantsController < ApplicationController
+    before_action :set_applicant, only: [:show, :update, :destroy]
+
+    # GET /citizens
+    def index
+      @applicant = Applicant.all
+      render json: @applicant
+    end
+
+    # GET /citizens/1
+    def show
+      render json: @applicant
+    end
+
+    # POST /citizens
+    # def create
+    #   @applicant = Applicant.new(applicant_params)
+    #
+    #   if @applicant.save
+    #     render json: @applicant, status: :created, location: @applicant
+    #   else
+    #     render json: @applicant.errors, status: :unprocessable_entity
+    #   end
+    # end
+
+    def create
+      @applicant = Applicant.new(params[:applicant])
+      if @applicant.save
+        redirect_to @applicant
+      else
+        render json: @applicant.errors, status: :unprocessable_entity
+      end
+    end
+
+
+    # PATCH/PUT /citizens/1
+    def update
+      if @applicant.update(applicant_params)
+        render json: @applicant
+      else
+        render json: @applicant.errors.messages, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /citizens/1
+    def destroy
+      @applicant.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_applicant
+        @applicant = Applicant.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def applicant_params
+        params.require(:applicant).permit(:name, :date_of_birth)
+      end
+
+end
