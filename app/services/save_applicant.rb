e7c28f0f6b85410b75bd5e0c7390b3baca055941
Daman <daman.fergus@justice.gov.uk>
2018-09-04 11:56:28 +0100

    Moved the save applicant service to the services directory

--- app/services/save_applicant.rb
+++ app/services/save_applicant.rb
@@ -1,5 +1,4 @@
 class SaveApplicant
-
   def self.call(name:, date_of_birth:, application_ref:)
     new.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref)
   end
@@ -7,22 +6,22 @@ class SaveApplicant
   def call(name:, date_of_birth:, application_ref:)
     applicant = Applicant.new(name: name, date_of_birth: date_of_birth)
     app = LegalAidApplication.find_by(application_ref: application_ref)
-    
-    if (applicant.valid? && app)
+
+    if applicant.valid? && app
       applicant.save
       app.applicant = applicant
       app.save
       Result.new(nil, true, applicant)
     elsif app.nil?
       Result.new(['invalid application reference'], false)
-    else 
+    else
       Result.new(applicant.errors.full_messages, false)
     end
   end
 
   class Result
     attr_reader :applicant, :errors
-    def initialize(errors, success, applicant=nil)
+    def initialize(errors, success, applicant = nil)
       @errors = errors
       @success = success
       @applicant = applicant
--- spec/services/save_applicant_spec.rb
+++ spec/services/save_applicant_spec.rb
@@ -1,43 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SaveApplicant do
-  let(:name) { 'Rich' }
-  let(:date_of_birth) { '1991-12-01' }
-  let(:existing_application) { LegalAidApplication.create }
-  let(:application_ref) { existing_application.application_ref }
+  let(:valid_name) { 'Rich' }
+  let(:valid_dob) { '1991-12-01' }
 
   context 'When given valid values' do
     it 'should save the model' do
-      result = described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref)
-      expect(result.success?).to eq true
-      expect(existing_application.reload.applicant).to eq result.applicant
-      expect(result.applicant.name).to eq name
-    end
-  end
-
-  context 'When given invalid values' do
-    let(:date_of_birth) { '11-11-11' }
-    it 'should throw an error' do
-      result = described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref)
-      expect(result).not_to be_success
-      expect(existing_application.reload.applicant).to be_nil
-      expect(result.errors).to match(["Date of birth is not valid"])
-    end
-    it 'does not create a new applicant record' do
-      expect { described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref) }.not_to change { Applicant.count }
-    end
-  end
-
-  context 'When given an invalid application reference' do
-    let(:application_ref) { SecureRandom.uuid }
-    it 'should throw an error' do
-      result = described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref)
-      expect(result).not_to be_success
-      expect(result.errors).to match(['invalid application reference'])
-    end
-
-    it 'does not create a new applicant record' do
-      expect { described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref) }.not_to change { Applicant.count } 
+      applicant, success = described_class.call(name: valid_name, date_of_birth: valid_dob)
+      expect(success).to eq true
+      expect(applicant.name).to eq valid_name
     end
   end
 end
