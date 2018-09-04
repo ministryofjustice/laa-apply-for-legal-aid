02f076d37fb7a7ba38f4f752d32c7415eddb13f9
umeshjoshi <umesh.joshi@digital.justice.gov.uk>
2018-09-04 12:13:23 +0100

    added logic and test cases to save proceedings when legal aid application is created

--- db/migrate/20180829150801_create_legal_aid_application_proceedings.rb
+++
@@ -1,9 +0,0 @@
-class CreateLegalAidApplicationProceedings < ActiveRecord::Migration[5.2]
-  def change
-    create_table :application_proceedings do |t|
-      t.references :legal_aid_application
-      t.references :proceeding
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(version: 2018_08_29_150801) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "application_proceedings", force: :cascade do |t|
+  create_table "application_proceeding_types", force: :cascade do |t|
     t.bigint "legal_aid_application_id"
-    t.bigint "proceeding_id"
+    t.bigint "proceeding_type_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["legal_aid_application_id"], name: "index_application_proceedings_on_legal_aid_application_id"
-    t.index ["proceeding_id"], name: "index_application_proceedings_on_proceeding_id"
+    t.index ["legal_aid_application_id"], name: "index_application_proceeding_types_on_legal_aid_application_id"
+    t.index ["proceeding_type_id"], name: "index_application_proceeding_types_on_proceeding_type_id"
   end
 
   create_table "legal_aid_applications", force: :cascade do |t|
@@ -39,14 +39,14 @@ ActiveRecord::Schema.define(version: 2018_08_29_150801) do
     t.index ["applicant_id"], name: "index_legal_aid_applications_on_applicant_id"
   end
 
-  create_table "proceedings", force: :cascade do |t|
+  create_table "proceeding_types", force: :cascade do |t|
     t.string "code"
     t.string "ccms_code"
     t.string "meaning"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["code"], name: "index_proceedings_on_code"
+    t.index ["code"], name: "index_proceeding_types_on_code"
   end
 
 end
