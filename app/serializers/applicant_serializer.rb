210c779c13d01633fdefa8cae34558db0166f5b5
Daman <daman.fergus@justice.gov.uk>
2018-09-06 14:57:06 +0100

    WIP changes to save applicant service and applicant controller

--- app/serializers/applicant_serializer.rb
+++ app/serializers/applicant_serializer.rb
@@ -2,4 +2,5 @@ class ApplicantSerializer
   include FastJsonapi::ObjectSerializer
   attribute :name
   attribute :date_of_birth
+  attribute :legal_aid_application.application_ref
 end
--- spec/services/save_applicant_spec.rb
+++ spec/services/save_applicant_spec.rb
@@ -1,14 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe SaveApplicant do
-  let(:valid_name) { 'Rich' }
-  let(:valid_dob) { '1991-12-01' }
+  let(:name) { 'Rich' }
+  let(:date_of_birth) { '1991-12-01' }
+  let(:existing_application) {LegalAidApplication.create}
+  let(:application_ref) {existing_application.application_ref}
 
   context 'When given valid values' do
     it 'should save the model' do
-      applicant, success = described_class.call(name: valid_name, date_of_birth: valid_dob)
+      applicant, success = described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref )
       expect(success).to eq true
-      expect(applicant.name).to eq valid_name
+      expect(existing_application.reload.applicant).to eq applicant
+      expect(applicant.name).to eq name
+    end
+  end
+
+  context 'When given invalid values' do
+    let(:date_of_birth) {"11-11-11"}
+    it 'should throw an error' do
+      errors, success = described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref )
+      expect(success).to eq false
+      expect(existing_application.reload.applicant).to be_nil
+      expect(errors).to match(['invalid application reference'])
+    end
+    it 'does not create a new applicant record' do
+      expect {described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref )}.not_to change {Applicant.count}
+    end
+  end
+
+  context 'When given an invalid application reference' do
+    let(:application_ref) {SecureRandom.uuid}
+    it 'should throw an error' do
+      errors, success = described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref )
+      expect(success).to eq false
+      expect(errors).to match(['invalid application reference'])
+    end
+
+    it 'does not create a new applicant record' do
+      expect {described_class.call(name: name, date_of_birth: date_of_birth, application_ref: application_ref )}.not_to change {Applicant.count}
     end
   end
 end
