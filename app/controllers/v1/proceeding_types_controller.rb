894bfb9789a9751a044b460bc58ba73883bf277d
umeshjoshi <umesh.joshi@digital.justice.gov.uk>
2018-09-17 13:58:37 +0100

    added /v1 in request path for proceeding types and removed status from v1 name space because (it exist outside v1 name space which is the correct location)

---
+++ app/controllers/v1/proceeding_types_controller.rb
@@ -0,0 +1,8 @@
+module V1
+  class ProceedingTypesController < ApplicationController
+    def index
+      proceeding_types = ProceedingType.all
+      render json: ProceedingTypesSerializer.new(proceeding_types).serialized_json
+    end
+  end
+end
