6af41ea43f8032305ef82d563d5efd3fc2853269
willc-work <william.clarke@justice.gov.uk>
2018-10-17 21:32:15 +0100

    Add pgcrypto extension to support UUID for DB
    The tables currently use sequential IDs as the primary key and ID for
    each table. This extension ensures that further tables are created
    using uuid for the id field.

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,9 @@ Bundler.require(*Rails.groups)
 module LaaApplyForLegalaidApi
   class Application < Rails::Application
     config.load_defaults 5.2
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_143410) do
+ActiveRecord::Schema.define(version: 2018_10_17_202608) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
