29dd8b4719f6489475e775c0268add54b0f51367
willc-work <william.clarke@justice.gov.uk>
2018-10-19 10:35:48 +0100

    Fix rubocop styling errors

--- db/migrate/20181018165405_id_to_uuid_fk.rb
+++ db/migrate/20181018165405_id_to_uuid_fk.rb
@@ -4,7 +4,6 @@ class IdToUuidFk < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       dir.up do
-
         primary_key_and_all_references_to_uuid :applicants
         primary_key_and_all_references_to_uuid :legal_aid_applications
         primary_key_and_all_references_to_uuid :proceeding_types
--- db/migrate/20181018165537_uuid_migration.rb
+++ db/migrate/20181018165537_uuid_migration.rb
@@ -4,7 +4,6 @@ class UuidMigration < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       dir.up do
-
         primary_key_to_uuid :addresses
         primary_key_to_uuid :application_proceeding_types
       end
