d28fa8e41441637eec7f162b1cbcd947375ee262
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-11-06 16:20:31 +0000

    Remove secrets from VCR cassettes
    Previously recorded VCR cassettes contained secrets such as the
    Ordnance Survey API key and Benefit Checker credentials.
    
    This commit will add configuration to spec/spec_helper.rb and
    features/support/vcr.rb to filter secrets out from cassettes. It also
    rerecords any affected cassettes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,11 @@ VCR.configure do |vcr_config|
   }
   vcr_config.configure_rspec_metadata!
   vcr_config.debug_logger = STDOUT if vcr_debug
+  vcr_config.filter_sensitive_data('<GOVUK_NOTIFY_API_KEY>') { ENV['GOVUK_NOTIFY_API_KEY'] }
+  vcr_config.filter_sensitive_data('<ORDNANACE_SURVEY_API_KEY>') { ENV['ORDNANACE_SURVEY_API_KEY'] }
+  vcr_config.filter_sensitive_data('<BC_LSC_SERVICE_NAME>') { ENV['BC_LSC_SERVICE_NAME'] }
+  vcr_config.filter_sensitive_data('<BC_CLIENT_ORG_ID>') { ENV['BC_CLIENT_ORG_ID'] }
+  vcr_config.filter_sensitive_data('<BC_CLIENT_USER_ID>') { ENV['BC_CLIENT_USER_ID'] }
 end
 
 RSpec.configure do |config|
