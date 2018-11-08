252bcf65c033f78067e8c00995eadf6ff6b9ba7a
Rob Nichols <rob@nicholshayes.co.uk>
2018-11-08 17:02:52 +0000

    Add facility to generate secure key from legal_aid_application

--- spec/factories/secure_data.rb
+++ spec/factories/secure_data.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :secure_data, class: 'SecureData' do
-    data { JWT.encode( { data: Faker::Lorem.sentence }, nil, 'none') }
+    data { JWT.encode({ data: Faker::Lorem.sentence }, nil, 'none') }
   end
 end
