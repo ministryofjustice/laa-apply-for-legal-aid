c8da3e78e27823f062b6317e12dc6890515ff9cd
Anthony McKernan <anthonymckernan@gmail.com>
2018-11-14 11:40:48 +0000

    display all the details of accounts that have been shared

---
+++ app/helpers/money_helper.rb
@@ -0,0 +1,12 @@
+module MoneyHelper
+  def currency_symbol(currency)
+    return '£' if currency == 'GBP'
+    return '$' if currency == 'USD'
+    return '€' if currency == 'EUR'
+    currency
+  end
+
+  def display_value_with_symbol(value, currency)
+    number_to_currency(value, unit: currency_symbol(currency))
+  end
+end
--- spec/requests/citizens/accounts_spec.rb
+++ spec/requests/citizens/accounts_spec.rb
@@ -2,7 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'citizen accounts request test', type: :request do
   describe 'GET /citizens/account' do
-    let(:applicant) { create :applicant }
+    let!(:applicant) { create :applicant }
+    let(:addresses) do
+      [{ address: Faker::Address.building_number,
+         city: Faker::Address.city,
+         zip: Faker::Address.zip }]
+    end
+    let!(:applicant_bank_provider) { create(:bank_provider, applicant_id: applicant.id) }
+    let!(:applicant_bank_account_holder) do
+      create(:bank_account_holder, bank_provider_id: applicant_bank_provider.id,
+                                   addresses: addresses)
+    end
+    let!(:applicant_bank_account) do
+      create(:bank_account, bank_provider_id: applicant_bank_provider.id, currency: 'GBP')
+    end
 
     before do
       sign_in applicant
@@ -11,6 +24,31 @@ RSpec.describe 'citizen accounts request test', type: :request do
 
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'display account holder name' do
+      expect(response.body).to include(applicant_bank_account_holder.full_name)
+    end
+
+    it 'display first account holder address' do
+      expect(response.body).to include(addresses.first.values.join(', '))
+    end
+
+    it 'display bank name and bank account type' do
+      bank_name_type = "#{applicant_bank_provider.name} #{applicant_bank_account.name}"
+      expect(response.body).to include(bank_name_type)
+    end
+
+    it 'display account number' do
+      expect(response.body).to include(applicant_bank_account.account_number)
+    end
+
+    it 'display sort code' do
+      expect(response.body).to include(applicant_bank_account.sort_code)
+    end
+
+    it 'display balance with pound symbol' do
+      expect(response.body).to include("£#{applicant_bank_account.balance}")
     end
   end
 end
