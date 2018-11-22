60606c56e28239ea49a5a46f5bef7c676cf6e43f
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-11-22 14:29:59 +0000

    Add content to Check Your Answers page
    Display required proceeding and applicant details
    on the Check Your Answers page.

--- app/controllers/providers/check_your_answers_controller.rb
+++ app/controllers/providers/check_your_answers_controller.rb
@@ -3,13 +3,23 @@ module Providers
     include Providers::ApplicationDependable
     include Steppable
 
-    def index; end
+    def index
+      @proceeding = @legal_aid_application.proceeding_types[0]
+      @applicant = @legal_aid_application.applicant
+      @address = html_address(@applicant.addresses[0])
+    end
 
     def confirm
       legal_aid_application.provider_submit!
       CitizenEmailService.new(legal_aid_application).send_email
       flash[:notice] = 'Application completed. An e-mail will be sent to the citizen.'
       redirect_to next_step_url
+    end
+
+    private
+
+    def html_address(address)
+      [address.organisation, address.address_line_one, address.address_line_two, address.city, address.postcode].compact.reject(&:blank?).join('</br>').html_safe
     end
   end
 end
