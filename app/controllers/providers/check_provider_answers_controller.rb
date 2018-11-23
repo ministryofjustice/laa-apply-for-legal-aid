728932f5a995a94d1a210fb3a9f6b955862a5e18
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-11-23 16:09:23 +0000

    Replace html_friendly with sanitize
    - Move code to produce address with line breaks to a new address_helper.
    - Use sanitize instead of html_safe.
    - Use first instead of [0] to get proceeding and address.

--- app/controllers/providers/check_provider_answers_controller.rb
+++ app/controllers/providers/check_provider_answers_controller.rb
@@ -4,9 +4,9 @@ module Providers
     include Steppable
 
     def index
-      @proceeding = @legal_aid_application.proceeding_types[0]
+      @proceeding = @legal_aid_application.proceeding_types.first
       @applicant = @legal_aid_application.applicant
-      @address = html_address(@applicant.addresses[0])
+      @address = @applicant.addresses.first
     end
 
     def confirm
@@ -14,12 +14,6 @@ module Providers
       CitizenEmailService.new(legal_aid_application).send_email
       flash[:notice] = 'Application completed. An e-mail will be sent to the citizen.'
       redirect_to next_step_url
-    end
-
-    private
-
-    def html_address(address)
-      [address.organisation, address.address_line_one, address.address_line_two, address.city, address.postcode].compact.reject(&:blank?).join('</br>').html_safe
     end
   end
 end
