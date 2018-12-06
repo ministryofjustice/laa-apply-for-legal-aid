11c88c954380f5b8554fbb80b3aefe10bfda3a00
willc-work <william.clarke@justice.gov.uk>
2018-12-06 21:31:57 +0000

    Add extra validation to ensure values greater than 0 Refactor the code to use some of the changes from ap213

--- app/forms/applicants/property_value_form.rb
+++ app/forms/applicants/property_value_form.rb
@@ -6,6 +6,24 @@ module Applicants
 
     attr_accessor :property_value
 
-    validates :property_value, presence: true, numericality: true
+    before_validation :clean_up_input
+    validates(
+      :property_value,
+      presence: true,
+      numericality: { greater_than: 0 }
+    )
+
+    private
+
+    def clean_up_input
+      property_value.delete!(',') if property_value.is_a?(String) && valid_number_pattern =~ property_value
+    end
+
+    # Starts with a digit
+    # Then perhaps any combination of digit, ',' or '_'
+    # Then perhaps dot followed by up to 2 digits
+    def valid_number_pattern
+      /\A\d[,_\d]*(\.\d{,2})?\z/
+    end
   end
 end
