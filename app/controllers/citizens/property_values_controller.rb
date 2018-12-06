719f61bbf2659d00db39ced5ac6c52f1b79e5306
willc-work <william.clarke@justice.gov.uk>
2018-12-06 14:58:38 +0000

    remove unused code and comments

--- app/controllers/citizens/property_values_controller.rb
+++ app/controllers/citizens/property_values_controller.rb
@@ -1,32 +1,20 @@
 module Citizens
   class PropertyValuesController < BaseController
     def show
-      # @legal_aid_application = LegalAidApplication.find(session[:current_application_ref])
       @form = Applicants::PropertyValueForm.new # this is so errors are visible
     end
 
     def create
-      # @legal_aid_application = LegalAidApplication.find(session[:current_application_ref])
       @form = Applicants::PropertyValueForm.new(edit_params)
 
       if @form.save
         render plain: 'Navigate to question 1c; What is the outstanding mortgage?'
       else
-        # @legal_aid_application = LegalAidApplication.find(session[:current_application_ref])
         render :show
       end
     end
 
     private
-
-    # def current_params
-    #   current_applicant.legal_aid_application.attributes.symbolize_keys.slice(:property_value)
-    # end
-
-    # def current_params
-    #   return unless current_applicant.legal_aid_application.property_value
-    #   applicant.property_value.attributes.symbolize_keys.slice(:property_value)
-    # end
 
     def property_value_params
       params.fetch(:legal_aid_application, {}).permit(:property_value)
