8273ff2e2e5b84fa6a2389da1c3afce3f6f53dd7
willc-work <william.clarke@justice.gov.uk>
2018-12-11 12:54:33 +0000

    Remove test that is no longer required

--- spec/requests/citizens/own_home_spec.rb
+++ spec/requests/citizens/own_home_spec.rb
@@ -20,10 +20,6 @@ RSpec.describe 'citizen own home requests', type: :request do
     context 'valid params' do
       let(:params) { { 'legal_aid_application' => { 'own_home' => 'owned_outright' } } }
 
-      it 'returns http_success' do
-        expect(response).to have_http_status(:found)
-      end
-
       it 'updates the record' do
         expect(application.reload.own_home).to eq 'owned_outright'
       end
