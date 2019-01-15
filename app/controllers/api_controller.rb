3830c7a0e000c2fc906e164dad7fe7ca07ea1c79
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-01-15 12:58:58 +0000

    Remove unused API code
    There is some legacy code from when this application was an API. This
    is no longer needed as all functionality has been brought into the app.
    
    This commit removes the controllers and associated tests.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,2 @@
 class ApiController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound do
-    render json: {}, status: :not_found
-  end
-
-  protected
-
-  def render_400(errors)
-    render json: { errors: ErrorsSerializer.new(errors) }, status: :bad_request
-  end
 end
