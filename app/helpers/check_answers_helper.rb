0f8f7fb46c25e17cf2166f9bfbf72d511ca8a9b0
Rob Nichols <rob@nicholshayes.co.uk>
2019-01-16 11:41:48 +0000

    AP-270 Clearer subheading on Check answers pages

--- app/helpers/check_answers_helper.rb
+++ app/helpers/check_answers_helper.rb
@@ -1,13 +1,16 @@
 module CheckAnswersHelper
   def check_answer_link(url:, question:, answer:, name:)
+    partial = case answer
+              when Hash
+                'currency_items'
+              when Array
+                'items'
+              else
+                'item'
+              end
     render(
-      partial: 'shared/check_answers_item',
-      locals: {
-        url: url,
-        question: question,
-        answer: answer,
-        name: name
-      }
+      partial: "shared/check_answers/#{partial}",
+      locals: { url: url, question: question, answer: answer, name: name }
     )
   end
 end
