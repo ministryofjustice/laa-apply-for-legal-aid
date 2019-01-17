31fde9a5532ff7cdad68a96eee5954c84d51e531
Rob Nichols <rob@nicholshayes.co.uk>
2019-01-17 15:15:09 +0000

    AP-291 Use H1 for page title - start of Provider journey

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,15 +1,38 @@
 module ProvidersHelper
-  def provider_back_link(text: t('generic.back'), path: back_step_url, method: :get)
-    link_to text, path, method: method, class: 'govuk-back-link', id: 'back-top'
-  end
-
-  def provider_basic_page(page_title:, back_link: nil, column_width: 'two-thirds', &content)
+  # A wrapper for the main elements needed in a Providers page.
+  # On a simple page, this will wrap the main content:
+  #
+  #   <%= provider_basic_page(page_title: 'Some Page') do %>
+  #     <p>Some content</p>
+  #   <% end %>
+  #
+  # On more complex pages, the method can be called without a block to create
+  # an initial `govuk-grid-row` containing just the main page heading.
+  #
+  #   <%= provider_basic_page(page_title: 'Some Page') %>
+  #
+  # If the page contains a form, pass the form object in, and error and fieldset
+  # markup will be added to match:
+  #
+  #   <%= provider_basic_page(page_title: 'Some Page', form: @form) do %>
+  #     <%= form_with(model: @form) do |form| %>
+  #       ...
+  #     <% end %>
+  #   <% end %>
+  #
+  def provider_basic_page(page_title:, back_link: {}, column_width: 'two-thirds', form: nil, &content)
+    content = capture(&content) if content
+    template = form ? 'form_page_template' : 'basic_page_template'
     render(
-      'shared/providers/basic_page_template',
+      "shared/providers/#{template}",
       page_title: page_title,
       back_link: back_link,
       column_width: column_width,
-      content: capture(&content)
+      content: content
     )
+  end
+
+  def provider_back_link(text: t('generic.back'), path: back_step_url, method: :get)
+    link_to text, path, class: 'govuk-back-link', id: 'back-top', method: method
   end
 end
