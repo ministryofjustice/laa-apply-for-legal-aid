b918af01cb9c0a1bb64b57a174722ba2596bf916
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-01-18 15:34:54 +0000

    AP-275 Fix Back button link method (patch not get) on check_passported_answers_page

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,6 +1,6 @@
 module ProvidersHelper
-  def provider_back_link(text: t('generic.back'), path: back_step_url)
-    link_to text, path, class: 'govuk-back-link', id: 'back-top'
+  def provider_back_link(text: t('generic.back'), path: back_step_url, method: :get)
+    link_to text, path, method: method, class: 'govuk-back-link', id: 'back-top'
   end
 
   def provider_basic_page(page_title:, back_link: nil, column_width: 'two-thirds', &content)
