e5fbb4475e83bc788909b4c0c73ad32001b3caa5
Rob Nichols <rob@nicholshayes.co.uk>
2019-01-18 10:29:52 +0000

    AP-291 Provider journey to does client use online banking

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -11,24 +11,34 @@ module ProvidersHelper
   #
   #   <%= provider_basic_page(page_title: 'Some Page') %>
   #
-  # If the page contains a form, pass the form object in, and error and fieldset
-  # markup will be added to match:
+  # If the page contains a form, you can use the :form template that will put
+  # the content within a fieldset and the heading into a fieldset heading:
   #
-  #   <%= provider_basic_page(page_title: 'Some Page', form: @form) do %>
+  #   <%= provider_basic_page(page_title: 'Some Page', template: :form) do %>
   #     <%= form_with(model: @form) do |form| %>
   #       ...
   #     <% end %>
   #   <% end %>
   #
-  def provider_basic_page(page_title:, back_link: {}, column_width: 'two-thirds', form: nil, &content)
+  def provider_basic_page( # rubocop:disable Metrics/ParameterLists
+        page_title:,
+        back_link: {},
+        column_width: 'two-thirds',
+        template: nil,
+        show_errors_for: @form,
+        &content
+      )
     content = capture(&content) if content
-    template = form ? 'form_page_template' : 'basic_page_template'
+    template = :default unless %i(form basic).include?(template)
+    content_for(:navigation) { provider_back_link(back_link) unless back_link == :none }
+    content_for(:page_title) { page_title }
     render(
-      "shared/providers/#{template}",
+      "shared/providers/#{template}_page_template",
       page_title: page_title,
       back_link: back_link,
       column_width: column_width,
-      content: content
+      content: content,
+      show_errors_for: show_errors_for
     )
   end
 
