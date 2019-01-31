132447171dc46fb30c62bb30efe28adb5b77d34e
Julien Sansot <juliensansot@gmail.com>
2019-01-31 11:23:00 +0000

    AP-327 logout provider when applicant starts journey

--- spec/requests/citizens/legal_aid_applications_spec.rb
+++ spec/requests/citizens/legal_aid_applications_spec.rb
@@ -28,23 +28,36 @@ RSpec.describe 'citizen home requests', type: :request do
   end
 
   describe 'GET citizens/applications' do
-    before do
+    subject do
       get citizens_legal_aid_application_path(secure_id)
       get citizens_legal_aid_applications_path
     end
 
     context 'when there is a legal aid application that matches the secure data' do
       it 'returns http success' do
+        subject
         expect(response).to have_http_status(:ok)
       end
 
       it 'sets the application_ref session variable' do
+        subject
         expect(session[:current_application_ref]).to eq(application_id)
       end
 
       it 'returns the correct application' do
+        subject
         expect(unescaped_response_body).to include(applicant_first_name.html_safe)
         expect(unescaped_response_body).to include(applicant_last_name.html_safe)
+      end
+
+      context 'if a provider is logged in' do
+        let(:provider) { create :provider }
+        before { sign_in provider }
+
+        it 'logs out the provider' do
+          subject
+          expect(unescaped_response_body).not_to include(provider.username)
+        end
       end
     end
   end
