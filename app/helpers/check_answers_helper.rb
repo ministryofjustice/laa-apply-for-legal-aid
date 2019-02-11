8796e3ce0b3a027b6ce8d290d6c25640653c105e
Rob Nichols <rob@nicholshayes.co.uk>
2019-02-11 10:34:08 +0000

    Small tweaks prior to push to UAT

--- app/helpers/check_answers_helper.rb
+++ app/helpers/check_answers_helper.rb
@@ -1,4 +1,8 @@
 module CheckAnswersHelper
+  # Creates one dictionary list item - so needs to be used within a `govuk-summary-list` `dl`:
+  #     <dl class="govuk-summary-list govuk-!-margin-bottom-9">
+  #       <%= check_answer_link ..... %>
+  #     </dl>
   def check_answer_link(url:, question:, answer:, name:)
     render(
       'shared/check_answers/item',
@@ -9,6 +13,7 @@ module CheckAnswersHelper
     )
   end
 
+  # Creates both the outer `dl` and the inner list items
   def check_answer_list(url:, question:, answers:, name:)
     render(
       'shared/check_answers/items',
@@ -19,6 +24,7 @@ module CheckAnswersHelper
     )
   end
 
+  # Creates both the outer `dl` and the inner list items
   def check_answer_currency_list(url:, question:, answer_hash:, name:)
     render(
       'shared/check_answers/currency_items',
