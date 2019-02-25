ccb49f6921c62c00372a9841df17f5b0de80b3cf
Julien Sansot <juliensansot@gmail.com>
2019-02-25 11:21:07 +0000

    AP-396 Disable malware check until it works consistently

--- app/forms/statement_of_cases/statement_of_case_form.rb
+++ app/forms/statement_of_cases/statement_of_case_form.rb
@@ -59,6 +59,8 @@ module StatementOfCases
       errors.add(:original_file, original_file_error_for(:content_type_invalid))
     end
 
+    # TODO: re-enable when clamdscan's bug is fixed
+    # :nocov:
     def original_file_malware_scan
       return unless file_present?
 
@@ -66,6 +68,7 @@ module StatementOfCases
 
       errors.add(:original_file, original_file_error_for(:file_virus))
     end
+    # :nocov:
 
     def file_present?
       original_file.present?
