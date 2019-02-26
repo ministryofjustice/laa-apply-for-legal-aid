c0e860c9411ca33480707bf000e8c3323f9de7ca
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-02-26 16:39:45 +0000

    Remove 'user-info' span tags from header
    Malcolm has identified that the 'user-info' span that contains the
    provider's profile link and logout buttons should not be there. He has
    made the necessary changes so that the page appears correctly without
    that element. This commit removes the line from the helper class which
    generates the tags.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,8 +34,7 @@ module ApplicationHelper
     html << content_tag(:li, link_to(current_provider.username.truncate(20), providers_provider_path, class: 'govuk-header__link'), class: 'govuk-header__navigation-item')
     html << content_tag(:li, link_to('Sign out', destroy_provider_session_path, method: :delete, class: 'govuk-header__link'), class: 'govuk-header__navigation-item')
     html = sanitize html, tags: %w[a li], attributes: %w[href class rel data-method]
-    html = content_tag :ul, html, id: 'navigation', class: 'govuk-header__navigation', 'aria-label': 'Top Level Navigation'
-    content_tag :span, html, class: 'user-info'
+    content_tag :ul, html, id: 'navigation', class: 'govuk-header__navigation', 'aria-label': 'Top Level Navigation'
   end
 
   def list_from_translation_path(translation_path)
