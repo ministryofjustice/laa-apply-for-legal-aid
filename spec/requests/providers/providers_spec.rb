1884557f3b94dc458ec0015a3e0507640241c301
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-03-22 10:14:08 +0000

    Add styling to wrap text on provider profile
    The 'role' field on the provider profile page can contain a long list
    of roles which does not currently wrap.
    
    This commit adds a css class to force the text to wrap. It also changes
    the way the comma-separated list of roles is displayed by adding a
    space after each comma, which will prevent the list wrapping mid-word.

--- spec/requests/providers/providers_spec.rb
+++ spec/requests/providers/providers_spec.rb
@@ -20,4 +20,11 @@ RSpec.describe Providers::ProvidersController, type: :request do
   it 'displays provider data' do
     expect(response.body).to include(provider.username)
   end
+
+  context 'with unspaced roles' do
+    let(:provider) { create :provider, roles: 'CWA,PUI' }
+    it 'displays roles with spaces' do
+      expect(response.body).to include('CWA, PUI')
+    end
+  end
 end
