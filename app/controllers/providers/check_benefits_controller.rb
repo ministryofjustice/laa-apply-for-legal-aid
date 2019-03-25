7b8d8897022cc51a24c79f43be0a5001ef9457c9
Rob Nichols <rob@nicholshayes.co.uk>
2019-03-25 16:22:24 +0000

    AP-451 Handle benefit checker known issue

--- app/controllers/providers/check_benefits_controller.rb
+++ app/controllers/providers/check_benefits_controller.rb
@@ -1,11 +1,24 @@
 module Providers
   class CheckBenefitsController < ProviderBaseController
     def index
+      return set_negative_result_and_go_forward if know_issue_prevents_benefit_check?
+
       legal_aid_application.add_benefit_check_result if legal_aid_application.benefit_check_result_needs_updating?
     end
 
     def update
       continue_or_draft
+    end
+
+    private
+
+    def know_issue_prevents_benefit_check?
+      applicant.last_name.length == 1
+    end
+
+    def set_negative_result_and_go_forward
+      legal_aid_application.create_benefit_check_result(result: 'skipped:known_issue')
+      go_forward
     end
   end
 end
