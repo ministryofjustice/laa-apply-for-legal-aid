877e7ebe0ef303a94589583327600204afacec54
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-03-25 16:37:31 +0000

    AP-409 Monitor all three queues

--- lib/prometheus_collectors/sidekiq_queue_collector.rb
+++ lib/prometheus_collectors/sidekiq_queue_collector.rb
@@ -1,19 +1,32 @@
 module PrometheusCollector
   class SidekiqQueueCollector < PrometheusExporter::Server::TypeCollector
+
+    QUEUE_NAMES = {
+      default_queue_size: 'Default Sidekiq queue size',
+      mailers_queue_size: 'Mailers Sidekiq queue size',
+      sidekiq_alive_queue_size: 'Sidekiq-Alive Sidekiq queue size'
+    }
+
     def initialize
-      @queue_size = PrometheusExporter::Metric::Gauge.new('sidekiq_queue_size', 'Sidekiq Queue Size')
+      @collectors = []
+      QUEUE_NAMES.each do |queue_name, desription|
+        @collectors << PrometheusExporter::Metric::Gauge.new(queue_name, description)
+      end
     end
 
     def type
-      'sidekiq_queue_size'
+      'sidekiq_queue_sizes'
     end
 
     def collect(_obj)
-      @queue_size.observe(rand(5))
+      @collectors.each do |collector|
+        size = Sidekiq::Queue.new(collector.name).size
+        collector.observe(size)
+      end
     end
 
     def metrics
-      [@queue_size]
+      QUEUE_NAMES.keys
     end
   end
 end
