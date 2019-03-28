4c85ab631330674205b59c29bb132b4b72c02594
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-03-28 11:01:28 +0000

    ap-409 revert to juliens branch

--- lib/prometheus_collectors/sidekiq_queue_collector.rb
+++ lib/prometheus_collectors/sidekiq_queue_collector.rb
@@ -1,37 +1,19 @@
 module PrometheusCollector
   class SidekiqQueueCollector < PrometheusExporter::Server::TypeCollector
-    GAUGE_NAMES = {
-      sidekiq_queue_default: 'Default Sidekiq queue size',
-      sidekiq_queue_mailers: 'Mailers Sidekiq queue size',
-      sidekiq_queue_sidekiq_alive: 'Sidekiq-Alive Sidekiq queue size'
-    }.freeze
-
     def initialize
-      @gauges = []
-      GAUGE_NAMES.each do |gauge_name, description|
-        @gauges << PrometheusExporter::Metric::Gauge.new(gauge_name.to_s, description)
-      end
+      @queue_size = PrometheusExporter::Metric::Gauge.new('sidekiq_queue_size', 'Sidekiq Queue Size')
     end
 
     def type
-      'sidekiq_queue_sizes'
+      'sidekiq_queue_size'
     end
 
     def collect(_obj)
-      @gauges.each do |gauge|
-        size = Sidekiq::Queue.new(queue_name(gauge)).size
-        gauge.observe(size)
-      end
+      @queue_size.observe(rand(5))
     end
 
     def metrics
-      @gauges
-    end
-
-    private
-
-    def queue_name(gauge)
-      gauge.name.sub(/^sidekiq_queue_/, '')
+      [@queue_size]
     end
   end
 end
--- lib/tasks/prometheus.rake
+++
@@ -1,6 +0,0 @@
-namespace :prometheus do
-  desc 'Measure sidekiq queue sizes and send to prometheus'
-  task sidekiq_queues: :environment do
-    PrometheusExporter::Client.default.send_json(type: 'sidekiq_queue_sizes')
-  end
-end
