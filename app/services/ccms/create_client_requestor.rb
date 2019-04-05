a4f73eca32682f7b91ffe5ff3a396e7d900a0e9f
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-04-05 15:47:25 +0100

    Add requestor and parser for create client

---
+++ app/services/ccms/create_client_requestor.rb
@@ -0,0 +1,130 @@
+module CCMS
+  class CreateClientRequestor
+    NAMESPACES = {
+      'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+      'xmlns:ns2' => 'http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIM',
+      'xmlns:ns3' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Header',
+      'xmlns:ns4' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Common',
+      'xmlns:ns5' => 'http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIO'
+    }.freeze
+
+    WSDL_LOCATION = "#{File.dirname(__FILE__)}/wsdls/ClientProxyServiceWsdl.xml".freeze
+
+    def initialize
+      @soap_client = Savon.client(
+        env_namespace: :soap,
+        wsdl: WSDL_LOCATION,
+        namespaces: NAMESPACES,
+        pretty_print_xml: true,
+        convert_request_keys_to: :none,
+        namespace_identifier: 'ns2',
+        log: true
+      )
+      @transaction_request_id = nil
+    end
+
+    def call
+      @soap_client.call(:create_client, soap_header: header_message, message: body_message)
+    end
+
+    def request_xml
+      request.body
+    end
+
+    def formatted_xml
+      result = ''
+      formatter = REXML::Formatters::Pretty.new
+      formatter.compact = true
+      formatter.write(REXML::Document.new(request_xml), result)
+      result
+    end
+
+    def transaction_request_id
+      @transaction_request_id ||= Time.now.strftime('%Y%m%d%H%M%S%6N')
+    end
+
+    private
+
+    def request
+      @request ||= @soap_client.build_request(:create_client, soap_header: header_message, message: body_message)
+    end
+
+    def header_message
+      {
+        'ns1:Security' => {
+          'ns1:UsernameToken' => {
+            'ns1:Username' => ENV['SOAP_CLIENT_USERNAME'],
+            'ns1:Password' => {
+              '@Type' => ENV['SOAP_CLIENT_PASSWORD_TYPE'],
+              content!: ENV['SOAP_CLIENT_PASSWORD']
+            }
+          }
+        }
+      }
+    end
+
+    def body_message
+      {
+        'ns3:HeaderRQ' => header_request,
+        'ns2:Client' => client
+      }
+    end
+
+    def header_request
+      {
+        'ns3:TransactionRequestID' => transaction_request_id,
+        'ns3:Language' => 'ENG',
+        'ns3:UserLoginID' => ENV['USER_LOGIN'],
+        'ns3:UserRole' => ENV['USER_ROLE']
+      }
+    end
+
+    def client
+      {
+        'ns5:Name' => name,
+        'ns5:PersonalInformation' => personal_information,
+        'ns5:Contacts' => contacts,
+        'ns5:NoFixedAbode' => false,        # this is mandatory, but we don't hold this data
+        'ns5:Address' => address,
+        'ns5:EthnicMonitoring' => 0         # this is mandatory, but we don't hold this data
+      }
+    end
+
+    def name
+      {
+        'ns4:Surname' => 'Hurlock',
+        'ns4:FirstName' => 'lenovo'
+      }
+    end
+
+    # this is all mandatory: we don't hold any of this data except date of birth
+    def personal_information
+      {
+        'ns5:DateOfBirth' => '1969-01-01',
+        'ns5:Gender' => 'FEMALE',
+        'ns5:MaritalStatus' => 'U',
+        'ns5:VulnerableClient' => 'false',
+        'ns5:HighProfileClient' => 'false',
+        'ns5:VexatiousLitigant' => 'false',
+        'ns5:CountryOfOrigin' => 'GBR',
+        'ns5:MentalCapacityInd' => 'false'
+      }
+    end
+
+    # this is the only mandatory item of contact data. i'm not sure what it's for...
+    def contacts
+      {
+        'ns5:Password' => 'Testing'
+      }
+    end
+
+    def address
+      {
+        'ns4:AddressLine1' => '102 Petty France',
+        'ns4:City' => 'London',
+        'ns4:Country' => 'GBR', # country is mandatory; we don't hold this
+        'ns4:PostalCode' => 'SW1H 9AJ'
+      }
+    end
+  end
+end
---
+++ spec/services/ccms/create_client_requestor_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+module CCMS
+  RSpec.describe CreateClientRequestor do
+    describe 'XML request' do
+      it 'generates the expected XML' do
+        with_modified_env(modified_environment_vars) do
+          requestor = described_class.new
+          allow(requestor).to receive(:transaction_request_id).and_return('20190101121530123456')
+          expect(requestor.formatted_xml).to eq expected_xml.chomp
+        end
+      end
+    end
+
+    describe '#transaction_request_id' do
+      it 'returns the id based on current time' do
+        Timecop.freeze(2019, 1, 2, 3, 4, 5.123456) do
+          requestor = described_class.new
+          expect(requestor.transaction_request_id).to eq '20190102030405123456'
+        end
+      end
+    end
+
+    def modified_environment_vars
+      {
+        'SOAP_CLIENT_USERNAME' => 'my_soap_client_username',
+        'SOAP_CLIENT_PASSWORD' => 'xxxxx',
+        'USER_LOGIN' => 'my_login',
+        'USER_ROLE' => 'my_role'
+      }
+    end
+
+    def expected_xml
+      File.read("#{File.dirname(__FILE__)}/data/expected_create_client_request.xml")
+    end
+  end
+end
