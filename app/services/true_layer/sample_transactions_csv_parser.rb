e5059b07bd5ffabca0b7baf9051ed6795369f184
Julien Sansot <juliensansot@gmail.com>
2019-04-05 11:50:59 +0100

    AP-502 Set sample bank transactions in a CSV

---
+++ app/services/true_layer/sample_transactions_csv_parser.rb
@@ -0,0 +1,48 @@
+require 'csv'
+module TrueLayer
+  class SampleTransactionsCsvParser
+    TRANSACTIONS_CSV = 'db/sample_data/bank_transactions.csv'.freeze
+
+    def self.call
+      new.call
+    end
+
+    def call
+      @transaction_ids = []
+      CSV.read(Rails.root.join(TRANSACTIONS_CSV), headers: true).map do |row|
+        parse_transaction_csv_row(row.to_h.symbolize_keys)
+      end
+    end
+
+    private
+
+    def parse_transaction_csv_row(row)
+      transaction_type = row[:amount_in].present? ? :credit : :debit
+      amount = transaction_type == :credit ? parse_amount(row[:amount_in]) : - parse_amount(row[:amount_out])
+      {
+        transaction_id: unique_but_not_random_id(row),
+        description: row[:description],
+        currency: 'GBP',
+        amount: amount.to_f,
+        timestamp: row[:date].to_time.to_s,
+        transaction_type: transaction_type.to_s
+      }
+    end
+
+    def parse_amount(amount_string)
+      amount_string.tr('^0-9.', '').to_d
+    end
+
+    def unique_but_not_random_id(row)
+      counter = 0
+      transaction_id = ''
+      loop do
+        counter += 1
+        transaction_id = JWT.encode("#{row.values.join}#{counter}", 'secret!', 'HS256').last(40)
+        break unless transaction_id.in?(@transaction_ids)
+      end
+      @transaction_ids << transaction_id
+      transaction_id
+    end
+  end
+end
