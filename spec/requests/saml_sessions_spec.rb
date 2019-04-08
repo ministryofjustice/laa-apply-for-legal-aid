bf78dff0732a6327c94287e39c994c8b087feb49
Julien Sansot <juliensansot@gmail.com>
2019-04-08 14:59:48 +0100

    AP-488 Log out of portal when logging out

--- spec/requests/saml_sessions_spec.rb
+++ spec/requests/saml_sessions_spec.rb
@@ -11,9 +11,20 @@ RSpec.describe 'SamlSessionsController', type: :request do
       expect(controller.current_provider).to be_nil
     end
 
-    it 'redirects to providers root' do
+    it 'redirects to the SAML sign_out URL' do
       subject
-      expect(response).to redirect_to(providers_root_url)
+      expect(response).to redirect_to(idp_sign_out_provider_session_url(SAMLRequest: 1))
+    end
+
+    context 'mock_saml' do
+      before do
+        allow(Rails.configuration.x.laa_portal).to receive(:mock_saml).and_return('true')
+      end
+
+      it 'redirects to providers root' do
+        subject
+        expect(response).to redirect_to(providers_root_url)
+      end
     end
   end
 end
