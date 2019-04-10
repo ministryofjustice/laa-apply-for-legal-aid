20c705eae190249236c4a01f053d38b01316fcfb
Rob Nichols <rob@nicholshayes.co.uk>
2019-04-10 11:05:10 +0100

    AP-457 Fix issue with wrong transaction types being deleted by update

--- app/controllers/citizens/identify_types_of_outgoings_controller.rb
+++ app/controllers/citizens/identify_types_of_outgoings_controller.rb
@@ -8,7 +8,8 @@ module Citizens
     end
 
     def update
-      legal_aid_application.update!(legal_aid_application_params)
+      legal_aid_application.transaction_types.debits.destroy_all
+      legal_aid_application.transaction_types << transaction_types
       go_forward
     end
 
@@ -16,6 +17,10 @@ module Citizens
 
     def legal_aid_application_params
       params.require(:legal_aid_application).permit(transaction_type_ids: [])
+    end
+
+    def transaction_types
+      TransactionType.debits.where(id: legal_aid_application_params[:transaction_type_ids])
     end
   end
 end
