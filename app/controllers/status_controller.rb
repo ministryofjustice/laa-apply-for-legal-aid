f91a979fcbfc46155df1994bb8b002e6e628ad1f
Julien Sansot <juliensansot@gmail.com>
2019-04-16 16:46:45 +0100

    AP-415 Improving container's health status. Add Prometheus rule

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,45 @@
 class StatusController < ApiController
   def index
-    render json: { status: 'ok' }
+    render json: {
+      redis: redis_connection,
+      db: db,
+      malware_scanner: {
+        positive: malware_scanner_positive,
+        negative: malware_scanner_negative
+      }
+    }
+  end
+
+  private
+
+  def db
+    LegalAidApplication.select(:id).first
+    true
+  end
+
+  def redis_connection
+    Sidekiq.redis(&:ping) == 'PONG'
+  end
+
+  def malware_scanner_positive
+    virus_found = MalwareScanner.call(
+      file_path: Rails.root.join('spec/fixtures/files/malware.doc'),
+      save_result: false
+    ).virus_found
+
+    throw 'MalwareScanner fails to identify virus' unless virus_found
+
+    true
+  end
+
+  def malware_scanner_negative
+    virus_found = MalwareScanner.call(
+      file_path: Rails.root.join('spec/fixtures/files/documents/hello_world.pdf'),
+      save_result: false
+    ).virus_found
+
+    throw 'MalwareScanner wrongly flags safe file' if virus_found
+
+    true
   end
 end
