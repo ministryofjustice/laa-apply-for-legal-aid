69648a4cafcc5452acefd96ef09964971cafbc58
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-04-23 16:48:02 +0100

    AP-499 Convert ClientSearchRequestor to use XML:Builder

--- app/services/ccms/client_search_requestor.rb
+++ app/services/ccms/client_search_requestor.rb
@@ -1,8 +1,11 @@
 module CCMS
   class ClientSearchRequestor < BaseRequestor
     NAMESPACES = {
-      'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+      'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema',
+      'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
       'xmlns:ns2' => 'http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIM',
+      'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/',
+      'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
       'xmlns:ns3' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Header',
       'xmlns:ns4' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Common',
       'xmlns:ns5' => 'http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIO'
@@ -17,49 +20,60 @@ module CCMS
     # temporarily ignore this until connectivity with ccms is working
     # :nocov:
     def call
-      @soap_client.call(:get_client_details, soap_header: header_message, message: body_message)
+      @soap_client.call(:get_client_details, xml: request_xml)
     end
     # :nocov:
 
     private
-
-    def request
-      @request ||= @soap_client.build_request(:get_client_details, soap_header: header_message, message: body_message)
+    def request_xml
+      body_message.to_xml
     end
 
     def body_message
-      {
-        'ns3:HeaderRQ' => header_request,
-        'ns2:RecordCount' => record_count,
-        'ns2:SearchCriteria' => search_criteria
-      }
+      Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+        xml.__send__('soap:Envelope', NAMESPACES) do
+          xml.__send__('soap:Header') { soap_header(xml) }
+          xml.__send__('soap:Body') { soap_body(xml) }
+        end
+      end
     end
 
-    def header_request
-      {
-        'ns3:TransactionRequestID' => transaction_request_id,
-        'ns3:Language' => 'ENG',
-        'ns3:UserLoginID' => ENV['USER_LOGIN'],
-        'ns3:UserRole' => ENV['USER_ROLE']
-      }
+    def soap_header(xml)
+      xml.__send__('ns1:Security') do
+        xml.__send__('ns1:UsernameToken') do
+          xml.__send__('ns1:Username', ENV['SOAP_CLIENT_USERNAME'])
+          xml.__send__('ns1:Password', 'Type' => ENV['SOAP_CLIENT_PASSWORD_TYPE'] ) { xml.text ENV['SOAP_CLIENT_PASSWORD'] }
+        end
+      end
     end
 
-    def record_count
-      {
-        'ns4:MaxRecordsToFetch' => 200,
-        'ns4:RetriveDataOnMaxCount' => false
-      }
+    def soap_body(xml)
+      xml.__send__('ns2:ClientInqRQ') do
+        xml.__send__('ns3:HeaderRQ') { header_request(xml) }
+        xml.__send__('ns2:RecordCount') { record_count(xml) }
+        xml.__send__('ns2:SearchCriteria') { search_criteria(xml) }
+      end
+    end
+
+    def header_request(xml)
+      xml.__send__('ns3:TransactionRequestID', transaction_request_id)
+      xml.__send__('ns3:Language', 'ENG')
+      xml.__send__('ns3:UserLoginID', ENV['USER_LOGIN'])
+      xml.__send__('ns3:UserRole', ENV['USER_ROLE'])
+    end
+
+    def record_count(xml)
+      xml.__send__('ns4:MaxRecordsToFetch', 200)
+      xml.__send__('ns4:RetriveDataOnMaxCount', false)
     end
 
     # this is the minimum criteria for a search. nino is also a valid field
-    def search_criteria
-      {
-        'ns2:ClientInfo' => {
-          'ns5:FirstName' => 'lenovo',
-          'ns5:Surname' => 'hurlock',
-          'ns5:DateOfBirth' => '1969-01-01'
-        }
-      }
+    def search_criteria(xml)
+      xml.__send__('ns2:ClientInfo') do
+        xml.__send__('ns5:FirstName', 'lenovo')
+        xml.__send__('ns5:Surname', 'hurlock')
+        xml.__send__('ns5:DateOfBirth', '1969-01-01')
+      end
     end
   end
 end
