18d4d6e1f6a66ba282ab6ac41a88a434c7ea9567
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-04-24 10:05:18 +0100

    AP-499 Convert ReferenceDataRequestor to use XMl Builder

--- app/services/ccms/reference_data_requestor.rb
+++ app/services/ccms/reference_data_requestor.rb
@@ -1,11 +1,14 @@
 module CCMS
   class ReferenceDataRequestor < BaseRequestor
     NAMESPACES = {
-      'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
-      'xmlns:ns2' => 'http://legalservices.gov.uk/CCMS/Common/ReferenceData/1.0/ReferenceDataBIM',
-      'xmlns:ns3' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Header',
-      'xmlns:ns4' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Common',
-      'xmlns:ns5' => 'http://legalservices.gov.uk/CCMS/Common/ReferenceData/1.0/ReferenceDataBIO'
+    'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema',
+    'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+    'xmlns:ns2' => 'http://legalservices.gov.uk/CCMS/Common/ReferenceData/1.0/ReferenceDataBIM',
+    'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/',
+    'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+    'xmlns:ns3' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Header',
+    'xmlns:ns4' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Common',
+    'xmlns:ns5' => 'http://legalservices.gov.uk/CCMS/Common/ReferenceData/1.0/ReferenceDataBIO'
     }.freeze
 
     WSDL_LOCATION = "#{File.dirname(__FILE__)}/wsdls/GetReferenceDataWsdl.xml".freeze
@@ -17,39 +20,40 @@ module CCMS
     # temporarily ignore this until connectivity with ccms is working
     # :nocov:
     def call
-      @soap_client.call(:process, soap_header: header_message, message: body_message)
+      @soap_client.call(:process, xml: message)
     end
     # :nocov:
 
     private
 
-    def request
-      @request ||= @soap_client.build_request(:process, soap_header: header_message, message: body_message)
+    def message
+      Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+        xml.__send__('soap:Envelope', NAMESPACES) do
+          xml.__send__('soap:Header') { soap_header(xml) }
+          xml.__send__('soap:Body') { soap_body(xml) }
+        end
+      end
     end
 
-    def body_message
-      {
-        'ns3:HeaderRQ' => header_request,
-        'ns2:SearchCriteria' => search_criteria
-      }
+    def soap_body(xml)
+      xml.__send__('ns2:ReferenceDataInqRQ') do
+        xml.__send__('ns3:HeaderRQ') { header_request(xml) }
+        xml.__send__('ns2:SearchCriteria') { search_criteria(xml) }
+      end
     end
 
-    def header_request
-      {
-        'ns3:TransactionRequestID' => transaction_request_id,
-        'ns3:Language' => 'ENG',
-        'ns3:UserLoginID' => ENV['USER_LOGIN'],
-        'ns3:UserRole' => ENV['USER_ROLE']
-      }
+    def header_request(xml)
+      xml.__send__('ns3:TransactionRequestID', transaction_request_id)
+      xml.__send__('ns3:Language', 'ENG')
+      xml.__send__('ns3:UserLoginID', ENV['USER_LOGIN'])
+      xml.__send__('ns3:UserRole', ENV['USER_ROLE'])
     end
 
-    def search_criteria
-      {
-        'ns5:ContextKey' => 'CaseReferenceNumber',
-        'ns5:SearchKey' => {
-          'ns5:Key' => 'CaseReferenceNumber'
-        }
-      }
+    def search_criteria(xml)
+      xml.__send__('ns5:ContextKey', 'CaseReferenceNumber')
+      xml.__send__('ns5:SearchKey') do
+        xml.__send__('ns5:Key', 'CaseReferenceNumber')
+      end
     end
   end
 end
