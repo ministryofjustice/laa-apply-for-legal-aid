97e12ee5a38742aabf42e9f7a0c8b95a1549354f
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-04-24 10:26:39 +0100

    Refactor create_client_status_requestor.rb

--- app/services/ccms/create_client_status_requestor.rb
+++ app/services/ccms/create_client_status_requestor.rb
@@ -1,8 +1,11 @@
 module CCMS
   class CreateClientStatusRequestor < BaseRequestor
     NAMESPACES = {
-      'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+      'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema',
+      'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
       'xmlns:ns2' => 'http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIM',
+      'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/',
+      'xmlns:ns1' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
       'xmlns:ns3' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Header',
       'xmlns:ns4' => 'http://legalservices.gov.uk/Enterprise/Common/1.0/Common',
       'xmlns:ns5' => 'http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIO'
@@ -23,24 +26,27 @@ module CCMS
 
     private
 
-    def request
-      @request ||= @soap_client.build_request(:get_client_txn_status, soap_header: header_message, message: body_message)
+    def message
+      Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+        xml.__send__('soap:Envelope', NAMESPACES) do
+          xml.__send__('soap:Header') { soap_header(xml) }
+          xml.__send__('soap:Body') { soap_body(xml) }
+        end
+      end
     end
 
-    def body_message
-      {
-        'ns3:HeaderRQ' => header_request,
-        'ns2:TransactionID' => '20190101121530123456' # this needs to be the transaction id of the create client request
-      }
+    def soap_body(xml)
+      xml.__send__('ns2:ClientAddUpdtStatusRQ') do
+        xml.__send__('ns3:HeaderRQ') { header_request(xml) }
+        xml.__send__('ns2:TransactionID', '20190101121530123456') # this needs to be the transaction id of the create client request
+      end
     end
 
-    def header_request
-      {
-        'ns3:TransactionRequestID' => transaction_request_id,
-        'ns3:Language' => 'ENG',
-        'ns3:UserLoginID' => ENV['USER_LOGIN'],
-        'ns3:UserRole' => ENV['USER_ROLE']
-      }
+    def header_request(xml)
+      xml.__send__('ns3:TransactionRequestID', transaction_request_id)
+      xml.__send__('ns3:Language', 'ENG')
+      xml.__send__('ns3:UserLoginID', ENV['USER_LOGIN'])
+      xml.__send__('ns3:UserRole', ENV['USER_ROLE'])
     end
   end
 end
