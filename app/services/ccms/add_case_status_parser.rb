25127f52ddb4615a385f70c5e3b20ef167c75dd8
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-04-24 16:33:54 +0100

    Add requestor and parser classes for add case status updates

---
+++ app/services/ccms/add_case_status_parser.rb
@@ -0,0 +1,22 @@
+module CCMS
+  class AddCaseStatusParser < BaseParser
+    TRANSACTION_ID_PATH = '//Body//CaseAddUpdtStatusRS//HeaderRS//TransactionID'.freeze
+    STATUS_FREE_TEXT_PATH = '//Body//CaseAddUpdtStatusRS//HeaderRS//Status//StatusFreeText'.freeze
+
+    def parse
+      raise 'Invalid transaction request id' if extracted_transaction_request_id != @transaction_request_id
+
+      extracted_status_free_text
+    end
+
+    private
+
+    def extracted_transaction_request_id
+      @doc.xpath(TRANSACTION_ID_PATH).text
+    end
+
+    def extracted_status_free_text
+      @doc.xpath(STATUS_FREE_TEXT_PATH).text
+    end
+  end
+end
