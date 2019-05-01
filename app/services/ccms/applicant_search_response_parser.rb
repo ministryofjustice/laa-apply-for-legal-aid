0af87cdb629a159c004bd4727d3093069f4d20f7
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-05-01 11:12:25 +0100

    Improve applicant search response parser
    Add functionality to extract an applicant_ccms_reference from a response.
    
    Add extra test data, to cover scenarios where one applicant or multiple
    applicants are returned by CCMS.

--- app/services/ccms/applicant_search_response_parser.rb
+++ app/services/ccms/applicant_search_response_parser.rb
@@ -2,11 +2,16 @@ module CCMS
   class ApplicantSearchResponseParser < BaseResponseParser
     TRANSACTION_ID_PATH = '//Body//ClientInqRS//HeaderRS//TransactionID'.freeze
     RECORD_COUNT_PATH = '//Body//ClientInqRS//RecordCount//RecordsFetched'.freeze
+    APPLICANT_CCMS_REFERENCE_PATH = '//Body//ClientInqRS//Client//ClientReferenceNumber'.freeze
 
     def parse
       raise 'Invalid transaction request id' if extracted_transaction_request_id != @transaction_request_id
 
       extracted_record_count
+    end
+
+    def applicant_ccms_reference
+      @applicant_ccms_reference ||= extracted_applicant_ccms_reference
     end
 
     private
@@ -17,6 +22,10 @@ module CCMS
 
     def extracted_record_count
       @doc.xpath(RECORD_COUNT_PATH).text
+    end
+
+    def extracted_applicant_ccms_reference
+      @doc.xpath(APPLICANT_CCMS_REFERENCE_PATH).first&.text
     end
   end
 end
--- spec/services/ccms/applicant_search_response_parser_spec.rb
+++ spec/services/ccms/applicant_search_response_parser_spec.rb
@@ -2,19 +2,52 @@ require 'rails_helper'
 
 module CCMS
   RSpec.describe ApplicantSearchResponseParser do
-    describe '#parse' do
-      let(:response_xml) { File.read("#{File.dirname(__FILE__)}/data/applicant_search_response_no_results.xml") }
-
-      it 'extracts the number of records fetched' do
-        parser = described_class.new('20190301030405123456', response_xml)
-        expect(parser.parse).to eq '0'
-      end
+    describe 'parsing applicant search responses' do
+      let(:no_results_response_xml) { File.read("#{File.dirname(__FILE__)}/data/applicant_search_response_no_results.xml") }
+      let(:one_result_response_xml) { File.read("#{File.dirname(__FILE__)}/data/applicant_search_response_one_result.xml") }
+      let(:multiple_results_response_xml) { File.read("#{File.dirname(__FILE__)}/data/applicant_search_response_multiple_results.xml") }
 
       it 'raises if the transaction_request_ids dont match' do
         expect {
-          parser = described_class.new('20190301030405987654', response_xml)
+          parser = described_class.new('20190301030405987654', no_results_response_xml)
           parser.parse
         }.to raise_error RuntimeError, 'Invalid transaction request id'
+      end
+
+      context 'there are no applicants returned' do
+        let(:parser) { described_class.new('20190301030405123456', no_results_response_xml) }
+
+        it 'extracts the number of records fetched' do
+          expect(parser.parse).to eq '0'
+        end
+
+        it 'does not return an applicant_ccms_reference' do
+          expect(parser.applicant_ccms_reference).to be_nil
+        end
+      end
+
+      context 'there is one applicant returned' do
+        let(:parser) { described_class.new('20190301030405123456', one_result_response_xml) }
+
+        it 'extracts the number of records fetched' do
+          expect(parser.parse).to eq '1'
+        end
+
+        it 'returns the applicant_ccms_reference' do
+          expect(parser.applicant_ccms_reference).to eq '4390016'
+        end
+      end
+
+      context 'there are multiple applicants returned' do
+        let(:parser) { described_class.new('20190301030405123456', multiple_results_response_xml) }
+
+        it 'extracts the number of records fetched' do
+          expect(parser.parse).to eq '2'
+        end
+
+        it 'returns the first applicant_ccms_reference' do
+          expect(parser.applicant_ccms_reference).to eq '4390017'
+        end
       end
     end
   end
