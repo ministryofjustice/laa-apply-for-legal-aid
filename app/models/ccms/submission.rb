672ed60ac71953bee8f34a567425c0d029fd960e
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-05-03 11:32:48 +0100

    Move CCMS state machine into concern

--- app/models/ccms/submission.rb
+++ app/models/ccms/submission.rb
@@ -1,51 +1,12 @@
 module CCMS
   class Submission < ApplicationRecord # rubocop:disable Metrics/ClassLength
-    include AASM
+    include CCMSSubmissionStateMachine
 
     self.table_name = :ccms_submissions
 
     belongs_to :legal_aid_application
 
     validates :legal_aid_application_id, presence: true
-
-    aasm do
-      state :initialised, initial: true
-      state :case_ref_obtained
-      state :applicant_submitted
-      state :applicant_ref_obtained
-      state :case_submitted
-      state :case_created
-      state :failed
-
-      event :obtain_case_ref do
-        transitions from: :initialised, to: :case_ref_obtained
-      end
-
-      event :submit_applicant do
-        transitions from: :case_ref_obtained, to: :applicant_submitted
-      end
-
-      event :obtain_applicant_ref do
-        transitions from: :applicant_submitted, to: :applicant_ref_obtained
-        transitions from: :case_ref_obtained, to: :applicant_ref_obtained
-      end
-
-      event :submit_case do
-        transitions from: :applicant_ref_obtained, to: :case_submitted
-      end
-
-      event :confirm_case_created do
-        transitions from: :case_submitted, to: :case_created
-      end
-
-      event :fail do
-        transitions from: :initialised, to: :failed
-        transitions from: :case_ref_obtained, to: :failed
-        transitions from: :applicant_submitted, to: :failed
-        transitions from: :applicant_ref_obtained, to: :failed
-        transitions from: :case_submitted, to: :failed
-      end
-    end
 
     def process!
       case aasm_state
