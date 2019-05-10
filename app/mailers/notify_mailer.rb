4049afde87270ddea9432c21aec63bcf005756de
Rob Nichols <rob@nicholshayes.co.uk>
2019-05-10 16:30:38 +0100

    AP-587 Fix error where included module not present when Sidekiq retries

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,4 +1,7 @@
 class NotifyMailer < GovukNotifyRails::Mailer
+
+  # Require relative statement required as concern not found when loaded from sidekiq on retry
+  require_relative 'concerns/notify_template_methods'
   include NotifyTemplateMethods
 
   def citizen_start_email(app_id, email, application_url, client_name)
