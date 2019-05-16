5e16b2f4ea1100fdaafa0efa1fbf721638b993e7
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-05-16 11:35:30 +0100

    Remove unnecessary method argument
    The applicant_add_response_parser method in AddApplicantService was
    being passed response as an argument. Remove it and use an instance
    variable instead

--- app/services/ccms/add_applicant_service.rb
+++ app/services/ccms/add_applicant_service.rb
@@ -1,13 +1,13 @@
 module CCMS
   class AddApplicantService < BaseSubmissionService
     def call
-      response = applicant_add_requestor.call
-      if applicant_add_response_parser(response).success?
+      @response = applicant_add_requestor.call
+      if applicant_add_response_parser.success?
         submission.applicant_add_transaction_id = applicant_add_requestor.transaction_request_id
         create_history(submission.aasm_state, :applicant_submitted)
         submission.submit_applicant!
       else
-        handle_failure(response)
+        handle_failure(@response)
       end
     rescue CcmsError, StandardError => e # TODO: Replace `StandardError` with list of known expected errors
       handle_failure(e)
@@ -19,8 +19,8 @@ module CCMS
       @applicant_add_requestor ||= ApplicantAddRequestor.new(submission.legal_aid_application.applicant)
     end
 
-    def applicant_add_response_parser(response)
-      @applicant_add_response_parser ||= ApplicantAddResponseParser.new(applicant_add_requestor.transaction_request_id, response)
+    def applicant_add_response_parser
+      @applicant_add_response_parser ||= ApplicantAddResponseParser.new(applicant_add_requestor.transaction_request_id, @response)
     end
   end
 end
