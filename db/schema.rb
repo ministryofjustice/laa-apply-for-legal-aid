e415183366805f5db21247f201f628f05fdddf38
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-05-20 12:11:46 +0100

    Revert "AP584 WIP"
    This reverts commit a514ed53fe7bdb32a71be9be72cfcff6b7243996.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_111339) do
-
+ActiveRecord::Schema.define(version: 2019_05_14_212618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -296,7 +295,9 @@ ActiveRecord::Schema.define(version: 2019_05_16_111339) do
     t.decimal "second_home_percentage", precision: 14, scale: 2
     t.decimal "timeshare_value", precision: 14, scale: 2
     t.decimal "land_value", precision: 14, scale: 2
-    t.decimal "valuable_items_value", precision: 14, scale: 2
+    t.decimal "jewellery_value", precision: 14, scale: 2
+    t.decimal "vehicle_value", precision: 14, scale: 2
+    t.decimal "classic_car_value", precision: 14, scale: 2
     t.decimal "money_assets_value", precision: 14, scale: 2
     t.decimal "money_owed_value", precision: 14, scale: 2
     t.decimal "trust_value", precision: 14, scale: 2
