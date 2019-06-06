a4ac7c3759019bcf993b5946fb0f28bcca801404
Malcolm Butler <32877315+MoJ-Longbeard@users.noreply.github.com>
2019-06-06 16:47:36 +0100

    Removed Ruby truncate and added new test case email (a long one)

--- config/initializers/mock_saml.rb
+++ config/initializers/mock_saml.rb
@@ -1,7 +1,8 @@
 Rails.configuration.x.application.mock_saml = OpenStruct.new(
   usernames: [
     'test1@example.com'.freeze,
-    'test2@example.com'.freeze
+    'test2@example.com'.freeze,
+    'really-really-long-email-address@example.com'.freeze
   ],
   password: 'password'.freeze
 )
