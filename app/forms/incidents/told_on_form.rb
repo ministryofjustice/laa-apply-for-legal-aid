274f627c43a9370ae971be0b1e45ac622ace5a32
willc-work <william.clarke@justice.gov.uk>
2019-06-21 11:49:01 +0100

    Refactor to use DateFieldBuilder

--- app/forms/incidents/told_on_form.rb
+++ app/forms/incidents/told_on_form.rb
@@ -1,10 +1,7 @@
 module Incidents
   class ToldOnForm
     include BaseForm
-
     form_for Incident
-
-    DATE_PARTS = %i[told_year told_month told_day occurred_year occurred_month occurred_day].freeze
 
     attr_accessor :told_year, :told_month, :told_day
     attr_writer :told_on
@@ -12,147 +9,76 @@ module Incidents
     attr_accessor :occurred_year, :occurred_month, :occurred_day
     attr_writer :occurred_on
 
-    validates :told_on, presence: true, unless: :draft_and_not_incomplete_date?
+    validates :told_on, presence: true, unless: :draft_and_not_partially_complete_date?
     validates :told_on, date: { not_in_the_future: true }, allow_nil: true
 
-    validates :occurred_on, presence: true, unless: :draft_and_not_incomplete_date?
+    validates :occurred_on, presence: true, unless: :draft_and_not_partially_complete_date?
     validates :occurred_on, date: { not_in_the_future: true }, allow_nil: true
 
-    validate :occurred_before_told
+    validate :occurred_on_before_told_on
 
     def initialize(*args)
       super
       set_instance_variables_for_attributes_if_not_set_but_in_model(
-        # attrs: told_on_fields,
-        # attrs: DATE_PARTS,
-        # model_attributes: told_on_from_model,
-        # attrs: (occurred_on_fields + told_on_fields),
-        # model_attributes: (occurred_on_from_model + told_on_from_model)
-        attrs: all_on_fields,
-        model_attributes: all_on_from_model
+        attrs: (told_on_date_fields.fields + occurred_on_date_fields.fields),
+        model_attributes:
+        [
+          told_on_date_fields.model_attributes,
+          occurred_on_date_fields.model_attributes
+        ].compact.reduce(&:merge)
       )
     end
 
-    # def initialize(*args)
-    #   super
-    #   set_instance_variables_for_attributes_if_not_set_but_in_model(
-    #     attrs: (told_on_fields + occurred_on_fields),
-    #     model_attributes: (told_on_from_model && occurred_on_from_model)
-    #     )
-    # end
-
     def told_on
       return @told_on if @told_on.present?
-      return incomplete_date if told_on_methods.any?(&:blank?)
+      return if told_on_date_fields.blank?
+      return :invalid if told_on_date_fields.partially_complete? || told_on_date_fields.form_date_invalid?
 
-      @told_on = attributes[:told_on] = Date.new(*told_on_methods.map(&:to_i))
-    rescue ArgumentError
-      :invalid
+      @told_on = attributes[:told_on] = told_on_date_fields.form_date
     end
 
     def occurred_on
       return @occurred_on if @occurred_on.present?
-      return incomplete_date if occurred_on_methods.any?(&:blank?)
+      return if occurred_on_date_fields.blank?
+      return :invalid if occurred_on_date_fields.partially_complete? || occurred_on_date_fields.form_date_invalid?
 
-      @occurred_on = attributes[:occurred_on] = Date.new(*occurred_on_methods.map(&:to_i))
-    rescue ArgumentError
-      :invalid
+      @occurred_on = attributes[:occurred_on] = occurred_on_date_fields.form_date
     end
 
     private
 
-
-    def occurred_before_told
+    def occurred_on_before_told_on
       return if occurred_on.blank? || told_on.blank?
 
-      if told_on < occurred_on
-        errors.add(:occurred_on, :invalid_timeline)
-      end
+      errors.add(:occurred_on, :invalid_timeline) if told_on < occurred_on
     rescue ArgumentError
       :invalid
     end
 
     def exclude_from_model
-      %i[told_year told_month told_day occurred_year occurred_month occurred_day]
-      # %i[told_on_fields incident_occurred_fields]
-      # how do i just reference the 2 below instead of listing all 6
-      # told_on_fields
-      # occurred_on_fields
+      (told_on_date_fields.fields + occurred_on_date_fields.fields)
     end
 
-    def told_on_fields
-      %i[told_year told_month told_day]
+    def draft_and_not_partially_complete_date?
+      draft? && !told_on_date_fields.partially_complete?
     end
 
-    def occurred_on_fields
-      %i[occurred_year occurred_month occurred_day]
+    def told_on_date_fields
+      @told_on_date_fields ||= DateFieldBuilder.new(
+        form: self,
+        model: model,
+        method: :told_on,
+        prefix: :told_
+      )
     end
 
-    def all_on_fields
-      %i[occurred_year occurred_month occurred_day told_year told_month told_day]
-    end
-
-    def told_on_methods
-      @told_on_methods ||= told_on_fields.map { |f| __send__(f) }
-    end
-
-    def occurred_on_methods
-      @occurred_on_methods ||= occurred_on_fields.map { |f| __send__(f) }
-    end
-
-    def told_on_from_model
-      return unless model.told_on?
-
-      {
-        told_year: model.told_on.year,
-        told_month: model.told_on.month,
-        told_day: model.told_on.day
-      }
-    end
-
-    def occurred_on_from_model
-      return unless model.occurred_on?
-
-      {
-        occurred_year: model.occurred_on.year,
-        occurred_month: model.occurred_on.month,
-        occurred_day: model.occurred_on.day
-      }
-    end
-
-    def all_on_from_model
-      return unless model.occurred_on?
-
-      {
-        told_year: model.told_on.year,
-        told_month: model.told_on.month,
-        told_day: model.told_on.day,
-        occurred_year: model.occurred_on.year,
-        occurred_month: model.occurred_on.month,
-        occurred_day: model.occurred_on.day
-      }
-    end
-
-    def incomplete_date
-      return nil if told_on_methods.all?(&:blank?) || occurred_on_methods.all?(&:blank?)
-
-      @incomplete_date = true
-      :invalid
-    end
-
-    # def incomplete_incident_date
-    #   return nil if occurred_on_methods.all?(&:blank?)
-    #
-    #   @incomplete_incident_date = true
-    #   :invalid
-    # end
-
-    def incomplete_date?
-      @incomplete_date
-    end
-
-    def draft_and_not_incomplete_date?
-      draft? && !incomplete_date?
+    def occurred_on_date_fields
+      @occurred_on_date_fields ||= DateFieldBuilder.new(
+        form: self,
+        model: model,
+        method: :occurred_on,
+        prefix: :occurred_
+      )
     end
   end
 end
