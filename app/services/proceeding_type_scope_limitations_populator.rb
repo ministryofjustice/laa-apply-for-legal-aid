bb1f291e857a35e941765b85423583420e40ee11
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-07-11 08:42:15 +0100

    AP-758 improve seeding of join table

--- app/services/proceeding_type_scope_limitations_populator.rb
+++ app/services/proceeding_type_scope_limitations_populator.rb
@@ -9,19 +9,29 @@ class ProceedingTypeScopeLimitationsPopulator
 
   def call
     CSV.read(file_path, headers: true, header_converters: :symbol).each do |row|
-      proceeding_type_scope_limitation = ProceedingTypeScopeLimitation.new
+      proceeding_type_scope_limitation =
+        ProceedingTypeScopeLimitation.where(proceeding_type_id: proceeding_type_id(row),
+                                            scope_limitation_id: scope_limitation_id(row)).first_or_initialize
       proceeding_type_scope_limitation.update! attributes(row)
-    rescue ActiveRecord::RecordInvalid => e
-      Rails.logger.error e.message
     end
   end
 
   def attributes(row)
     {
-      proceeding_type_id: ProceedingType.find_by(ccms_code: row[:proceeding_type_code]).id,
-      scope_limitation_id: ScopeLimitation.find_by(code: row[:scope_limitation_code]).id,
+      proceeding_type_id: proceeding_type_id(row),
+      scope_limitation_id: scope_limitation_id(row),
       substantive_default: row[:substantive_default],
       delegated_functions_default: row[:delegated_functions_default]
     }
+  end
+
+  private
+
+  def proceeding_type_id(row)
+    ProceedingType.find_by(ccms_code: row[:proceeding_type_code]).id
+  end
+
+  def scope_limitation_id(row)
+    ScopeLimitation.find_by(code: row[:scope_limitation_code]).id
   end
 end
