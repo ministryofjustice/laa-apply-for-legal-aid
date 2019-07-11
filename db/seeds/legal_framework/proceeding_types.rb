d3a3a3aef77f386dcfbc854a419953b70afbedbd
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-07-11 08:42:15 +0100

    Ap-756 Add new proceeding data to seed
    Add data for the following fields to the proceeding_types seed file:
    
    default_level_of_service_id
    default_level_of_service_name
    default_cost_limitation_delegated_functions
    default_cost_limitation_substantive

--- db/seeds/legal_framework/proceeding_types.rb
+++ db/seeds/legal_framework/proceeding_types.rb
@@ -6,7 +6,7 @@ data = CSV.read(file_path)
 data.shift
 
 data.each do |row|
-  code, ccms_code, ccms_category_of_law_code, ccms_category_of_law, ccms_matter_type_code, ccms_matter_type, _current, meaning, description = row
+  code, ccms_code, ccms_category_of_law_code, ccms_category_of_law, ccms_matter_type_code, ccms_matter_type, _current, meaning, description, default_level_of_service_id, default_level_of_service_name, default_cost_limitation_delegated_functions, default_cost_limitation_substantive = row
   proceeding_type = ProceedingType.where(code: code).first_or_initialize
   attrs = {
     ccms_code: ccms_code,
@@ -15,7 +15,11 @@ data.each do |row|
     ccms_category_law: ccms_category_of_law,
     ccms_category_law_code: ccms_category_of_law_code,
     ccms_matter: ccms_matter_type,
-    ccms_matter_code: ccms_matter_type_code
+    ccms_matter_code: ccms_matter_type_code,
+    default_level_of_service_id: default_level_of_service_id,
+    default_level_of_service_name: default_level_of_service_name,
+    default_cost_limitation_delegated_functions: default_cost_limitation_delegated_functions,
+    default_cost_limitation_substantive: default_cost_limitation_substantive
   }
   proceeding_type.update_attributes(attrs)
 end
