1e1bbebe2ab6604197cec4f9ca6f68e2e47b485b
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-08-09 20:23:58 +0100

    Update respondant form and locales

--- app/forms/respondents/respondent_form.rb
+++ app/forms/respondents/respondent_form.rb
@@ -9,6 +9,8 @@ module Respondents
                   :police_notified, :police_notified_details,
                   :bail_conditions_set, :bail_conditions_set_details
 
+    before_validation :clear_fields
+
     validates :understands_terms_of_court_order, presence: true, unless: :draft?
     validates(
       :understands_terms_of_court_order_details,
@@ -30,6 +32,25 @@ module Respondents
       if: proc { |form| !form.draft? && form.police_notified.to_s == 'false' }
     )
 
-    validates :bail_conditions_set, :bail_conditions_set_details, presence: true, unless: :draft?
+    validates :bail_conditions_set, presence: true, unless: :draft?
+    validates(
+      :bail_conditions_set_details,
+      presence: true,
+      if: proc { |form| !form.draft? && form.bail_conditions_set.to_s == 'true' }
+    )
+
+    private
+
+    def clear_fields
+      %i[
+        understands_terms_of_court_order
+        warning_letter_sent
+        police_notified
+      ].each do |attr|
+        attribute_call = method("#{attr}_details").call
+        attribute_call&.clear unless ActiveModel::Type::Boolean.new.cast(attr.to_s)
+      end
+      # bail_conditions_set_details&.clear
+    end
   end
 end
