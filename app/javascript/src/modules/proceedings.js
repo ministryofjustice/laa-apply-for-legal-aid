9a0c96a67b92515f9ffba98ae381f4187ed2b774
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-08-09 12:03:22 +0100

    Refactor Proceeding Types to use modular functions

---
+++ app/javascript/src/modules/proceedings.js
@@ -0,0 +1,99 @@
+import $ from 'jquery'
+import axios from 'axios'
+import * as JsSearch from 'js-search'
+
+async function getAll(){
+  const response = await axios.get('/v1/proceeding_types')
+  return response.data
+}
+
+function filterSearch(proceedings_data) {
+  $("#proceeding-list .proceeding-item").hide()
+  $(".no-proceeding-items").hide()
+
+  let searchObject = search(proceedings_data)
+
+  $("#proceeding-search-input").keyup((event) => searchCallback(event, searchObject))
+
+  $('#clear-proceeding-search').on("click", () => $("#proceeding-search-input").val("").trigger("keyup"))
+
+  $('.proceeding-item').on('mouseover', function (e) { $(this).addClass('hover') })
+    .on('mouseout', function (e) { $(this).removeClass('hover') })
+    .on('click', function (e) { return submitForm(this) })
+    .on('keydown', function (e) {
+      if (e.which == 13) {
+        return submitForm(this)
+      }
+    })
+}
+
+function search(proceedings_data) {
+  // For docs see: https://github.com/bvaughn/js-search
+  const search = new JsSearch.Search("code")
+  search.indexStrategy = new JsSearch.AllSubstringsIndexStrategy()
+
+  search.tokenizer = {
+    tokenize(text) {
+      return text.replace(/[^a-zA-Z\d]/gi, '').split()
+    }
+  }
+
+  search.addIndex("meaning")
+  search.addIndex("description")
+  search.addIndex("category_law")
+  search.addIndex("matter")
+  search.addIndex("additional_search_terms")
+  search.addDocuments(proceedings_data)
+  return search
+}
+
+function searchCallback(event, search) {
+  $("#proceeding-list .proceeding-item").hide()
+  $(".no-proceeding-items").hide()
+
+  // Get user input and filter on it.
+  const inputText = $(event.target).val()
+  filterData(inputText, search)
+}
+
+function filterData(inputText, search) {
+  if (inputText.length > 2) {
+    const codes = search.search(inputText).map(obj => obj["code"])
+
+    if (codes.length > 0) {
+      // Iterate through each code, find the matching element, move it to the
+      // top and display it
+      $.each(codes.reverse(), function (_i, code) {
+        const element = $('#' + code)
+
+        // We want to highlight anything text in <h3> or <span> tags that
+        // matches the user's search criteria
+        const span = element.find("span")
+        const h3 = element.find("h3")
+
+        // Remove any existing highlighting
+        h3.html(h3.html().replace(/<mark class="highlight">(.*)<\/mark>/, '$1'))
+        span.html(span.html().replace(/<mark class="highlight">(.*)<\/mark>/, '$1'))
+
+        // Highlight any text that matches the user's input
+        const regExp = RegExp(inputText, 'gi')
+        h3.html(h3.html().replace(regExp, '<mark class="highlight">$&</mark>'))
+        span.html(span.html().replace(regExp, '<mark class="highlight">$&</mark>'))
+
+        const parent = $('#proceeding-list')
+        element.detach().prependTo(parent)
+        element.show()
+      })
+    }
+    else {
+      $(".no-proceeding-items").show()
+    }
+  }
+}
+
+let submitForm = proceedingItem => {
+  $(proceedingItem).find('form').submit()
+  return false
+}
+
+export { getAll, filterSearch }
--- app/javascript/src/proceeding_types.js
+++ app/javascript/src/proceeding_types.js
@@ -1,100 +1,12 @@
-import * as JsSearch from 'js-search';
+import $ from 'jquery'
+import * as ProceedingTypes from './modules/proceedings'
 
 $(function() {
-  if ($("#proceeding-search-input").length){ searchProceedingTypes() };
-});
+  if ($("#proceeding-search-input").length){ searchProceedingTypes() }
+})
 
-function searchProceedingTypes() {
-  var submitForm = proceedingItem => {
-    $(proceedingItem).find('form').submit();
-    return false;
-  }
-
-  $.getJSON("/v1/proceeding_types", function (proceedings_data) {
-
-    $("#proceeding-list .proceeding-item").hide();
-    $(".no-proceeding-items").hide();
-
-    // For docs see: https://github.com/bvaughn/js-search
-    const search = new JsSearch.Search("code");
-    search.indexStrategy = new JsSearch.AllSubstringsIndexStrategy();
-
-    search.tokenizer = {
-      tokenize(text) {
-        return text.replace(/[^a-zA-Z\d]/gi, '').split()
-      }
-    };
-
-    search.addIndex("meaning");
-    search.addIndex("description");
-    search.addIndex("category_law");
-    search.addIndex("matter");
-    search.addIndex("additional_search_terms");
-    search.addDocuments(proceedings_data);
-
-    $("#proceeding-search-input").keyup(function(){
-      $("#proceeding-list .proceeding-item").hide();
-      $(".no-proceeding-items").hide();
-
-      // Get user input and filter on it.
-      const inputText = $(this).val();
-
-      if (inputText.length > 2) {
-        const codes = search.search(inputText).map(function(obj) {return obj["code"];});
-
-        if (codes.length > 0) {
-          // Iterate through each code, find the matching element, move it to the
-          // top and display it
-          $.each(codes.reverse(), function(_i, code){
-            const element = $('#' + code);
-
-            // We want to highlight anything text in <h3> or <span> tags that
-            // matches the user's search criteria
-            const span = element.find("span")
-            const h3 = element.find("h3")
-
-            // Remove any existing highlighting
-            h3.html(h3.html().replace(/<mark class="highlight">(.*)<\/mark>/, '$1'));
-            span.html(span.html().replace(/<mark class="highlight">(.*)<\/mark>/, '$1'));
-
-            // Highlight any text that matches the user's input
-            const regExp = RegExp(inputText, 'gi');
-            h3.html(h3.html().replace(regExp, '<mark class="highlight">$&</mark>'));
-            span.html(span.html().replace(regExp, '<mark class="highlight">$&</mark>'));
-
-            const parent = $('#proceeding-list');
-            element.detach().prependTo(parent);
-            element.show();
-          });
-        }
-        else {
-          $(".no-proceeding-items").show();
-        }
-      }
-    });
-
-    $('#clear-proceeding-search').on("click", function() {
-      $("#proceeding-search-input").val("").trigger("keyup");
-    });
-
-    $('.proceeding-item').on('mouseover', function(e){
-      $(this).addClass('hover');
-    });
-
-    $('.proceeding-item').on('mouseout', function(e){
-      $(this).removeClass('hover');
-    });
-
-    $('.proceeding-item').on('click', function(e){
-      return submitForm(this);
-    });
-
-    $('.proceeding-item').on('keydown', function(e){
-      if (e.which == 13) {
-        return submitForm(this);
-      }
-    });
-  });
+function searchProceedingTypes(){
+  ProceedingTypes.getAll().then(proceedings_data => ProceedingTypes.filterSearch(proceedings_data))
 }
 
-module.exports = searchProceedingTypes
+export { searchProceedingTypes }
