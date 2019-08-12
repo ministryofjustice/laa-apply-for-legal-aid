37e32928aec85f41dcf98387ba93ae948d01b89c
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-08-12 15:56:45 +0100

    Update views and forms
    Update respondants view to show text area when yes is selected
    Update forms to clear details when no is selected
    Update validation for empty bail conditions details

--- app/forms/respondents/respondent_form.rb
+++ app/forms/respondents/respondent_form.rb
@@ -9,7 +9,11 @@ module Respondents
                   :police_notified, :police_notified_details,
                   :bail_conditions_set, :bail_conditions_set_details
 
+<<<<<<< HEAD
     before_validation :clear_fields
+=======
+    before_validation :clear_details, :clear_bail_details
+>>>>>>> bdb7c5f2... Update views and forms
 
     validates :understands_terms_of_court_order, presence: true, unless: :draft?
     validates(
@@ -41,16 +45,31 @@ module Respondents
 
     private
 
+<<<<<<< HEAD
     def clear_fields
+=======
+    def clear_details
+>>>>>>> bdb7c5f2... Update views and forms
       %i[
         understands_terms_of_court_order
         warning_letter_sent
         police_notified
       ].each do |attr|
+<<<<<<< HEAD
         attribute_call = method("#{attr}_details").call
         attribute_call&.clear unless ActiveModel::Type::Boolean.new.cast(attr.to_s)
       end
       # bail_conditions_set_details&.clear
+=======
+        details = "#{attr}_details".to_sym
+        attr_value = __send__(attr)
+        __send__(details)&.clear if attr_value.to_s == 'true'
+      end
+    end
+
+    def clear_bail_details
+      bail_conditions_set_details&.clear if bail_conditions_set.to_s == 'false'
+>>>>>>> bdb7c5f2... Update views and forms
     end
   end
 end
