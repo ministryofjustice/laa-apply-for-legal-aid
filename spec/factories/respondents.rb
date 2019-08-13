b3e6187a6e142f059f5d24f2ee2071eff0c3b0ae
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-08-13 15:43:07 +0100

    Update respondents factory
    Update respondents factory, hardcoding value of 'true' and 'false' where relevant.
    This is because for example bail conditions details are cleared if the answer is set to 'no'/'false'
    This clearing details functionality is something I added into this feature.
    This broke the positive tests and thus it uses hardcoded values to allow details to be inputted and saved.

--- spec/factories/respondents.rb
+++ spec/factories/respondents.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :respondent do
     legal_aid_application
-    understands_terms_of_court_order { Faker::Boolean.boolean }
+    understands_terms_of_court_order { 'false' }
     understands_terms_of_court_order_details { Faker::Lorem.paragraph }
-    warning_letter_sent { Faker::Boolean.boolean }
+    warning_letter_sent { 'false' }
     warning_letter_sent_details { Faker::Lorem.paragraph }
-    police_notified { Faker::Boolean.boolean }
+    police_notified { 'false' }
     police_notified_details { Faker::Lorem.paragraph }
-    bail_conditions_set { Faker::Boolean.boolean }
+    bail_conditions_set { 'true' }
     bail_conditions_set_details { Faker::Lorem.paragraph }
   end
 end
