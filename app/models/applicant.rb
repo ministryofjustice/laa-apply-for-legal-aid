1ca3b711b78099c2a05b0f56b10069d5ffe63bd0
Julien Sansot <juliensansot@gmail.com>
2019-08-13 14:49:51 +0100

    AP-812 Merits report page

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -39,4 +39,9 @@ class Applicant < ApplicationRecord
   def true_layer_token_data
     @true_layer_token_data = true_layer_secure_data.retrieve
   end
+
+  def age
+    now = legal_aid_application.submission_date
+    now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -172,7 +172,8 @@ Rails.application.routes.draw do
       resource :used_delegated_functions, only: %i[show update]
       resource :substantive_application, only: %i[show update]
       resource :end_of_application, only: %i[show update]
-      resource :submitted_application, only: %i[show]
+      resource :submitted_application, only: :show
+      resource :merits_report, only: :show
     end
   end
 
