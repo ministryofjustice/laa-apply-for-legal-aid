a82a4cac13829a594613ef6b430bcab4e3c9b9bd
Julien Sansot <juliensansot@gmail.com>
2019-08-13 15:38:35 +0100

    AP-812 Generate merits_report.pdf when application is submitted

---
+++ config/initializers/wicked_pdf.rb
@@ -0,0 +1,13 @@
+# WickedPDF Global Configuration
+#
+# Use this to set up shared configuration options for your entire application.
+# Any of the configuration options shown here can also be applied to single
+# models by passing arguments to the `render :pdf` or `render_to_string` call.
+#
+# To learn more, check out the README:
+#
+# https://github.com/mileszs/wicked_pdf/blob/master/README.md
+
+WickedPdf.config = {
+  print_media_type: true
+}
