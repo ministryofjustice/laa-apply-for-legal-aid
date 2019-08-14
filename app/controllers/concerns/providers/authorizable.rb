bb78d225f1bed596f6d962b80eb9bd416ba6688c
Rob Nichols <rob@undervale.co.uk>
2019-08-14 15:34:07 +0100

    AP-832 Move update denied message to translation file

--- app/controllers/concerns/providers/authorizable.rb
+++ app/controllers/concerns/providers/authorizable.rb
@@ -32,7 +32,7 @@ module Providers
             if current_policy.show_submitted_application?
               redirect_to(
                 providers_legal_aid_application_submitted_application_path(legal_aid_application),
-                notice: 'Update denied: redirecting to submitted application'
+                notice: I18n.t('errors.messages.update_denied')
               )
             else
               redirect_to error_path(:access_denied)
