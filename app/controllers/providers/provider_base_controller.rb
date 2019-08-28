138e39378e9cbcf6ba51cb381ac6a94626b28b6b
Rob Nichols <rob@undervale.co.uk>
2019-08-28 10:46:58 +0100

    AP-831a Fix post rebase for method duplication in controller and mixin

--- app/controllers/providers/provider_base_controller.rb
+++ app/controllers/providers/provider_base_controller.rb
@@ -5,17 +5,5 @@ module Providers
     include ApplicationDependable
     include Draftable
     include Authorizable
-
-    def pundit_user
-      current_provider
-    end
-
-    private
-
-    def provider_not_authorized
-      respond_to do |format|
-        format.html { render 'shared/access_denied', status: :forbidden }
-      end
-    end
   end
 end
