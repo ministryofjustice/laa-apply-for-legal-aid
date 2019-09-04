3f490d5efa1af3fe77bf685f9123e1fb1c563178
Rob Nichols <rob@undervale.co.uk>
2019-09-04 16:45:41 +0100

    AP-512 Refactor td cells in sortable table

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -1,6 +1,7 @@
 module TagHelper
-  def gov_uk_tag(text:, type:)
-    type_class = "#{type.to_s.dasherize}-tag"
-    content_tag :strong, text, class: "govuk-tag #{type_class}"
+  def gov_uk_tag(text:, type: nil, classes: [])
+    classes << 'govuk-tag'
+    classes << "#{type.to_s.dasherize}-tag" if type
+    content_tag :strong, text, class: classes
   end
 end
