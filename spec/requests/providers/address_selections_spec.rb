06501d676f8f81f8f475f7ed3a0452315e536b44
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2019-09-11 15:23:43 +0100

    failing postcode

--- spec/requests/providers/address_selections_spec.rb
+++ spec/requests/providers/address_selections_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe 'address selections requests', type: :request do
         end
 
         context 'but the lookup does not return any valid results' do
-          let(:postcode) { 'IM1H 3QQ' } # NOTE: test account does not return any results for this postcode
+          let(:postcode) { 'SW1H 9AJ' } # NOTE: test account does not return any results for this postcode
           let(:form_heading) { "Enter your client's correspondence address" }
           let(:error_message) { 'We could not find any addresses for that postcode. Enter the address manually.' }
 
