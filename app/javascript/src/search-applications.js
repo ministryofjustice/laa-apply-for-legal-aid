854faccb54a104a22eb58d44f2dcb49ffbf772a8
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2019-09-17 15:22:37 +0100

    Tidied up JS - added high row limit

--- app/javascript/src/search-applications.js
+++ app/javascript/src/search-applications.js
@@ -1,23 +1,12 @@
 $("#search-for-applications").ready(function(){ //only adds code when the search box is loaded on the page
-
   $("#search-for-applications").on("keyup paste click", function(){ //every time a new letter is entered
-
     $("tbody tr").css("display","none"); //hides all rows - we re-display applicable rows later
-
     var searchPhrase = $(this).val().toUpperCase().replace(/[.,\/\s\\#£!$%\^&\*;:{}=\-_`~()]/g,""); //gets the search phrase without any punctuation or spaces
-
     $("tbody tr").each(function(){ //for each row in the table
-
       var rowcontent = $(this).text().toUpperCase().replace(/[.,\/\s\\#£!$%\^&\*;:{}=\-_`~()]/g,""); //gets the content phrase without any punctuation or spaces
-
       if (rowcontent.indexOf(searchPhrase) > -1 ) { //if the search phrase is found
-
         $(this).css("display","table-row"); //display this row
-
       }
-
     });
-
   });
-
 });
