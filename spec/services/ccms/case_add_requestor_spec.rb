c055d1a47aecc0e78bfedbc071f3b7771ecfd659
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2019-09-19 20:57:23 +0100

    Populate category of law attributes from database
    Three CategoryOfLaw attributes in the CCMS create case payload are
    currently hardcoded. We hold the relevant data in the database so
    the values should be retrieved from there.
    
    Amend case_add_requestor so that CategoryOfLawCode,
    CategoryOfLawDescription and RequestedAmount are populated correctly.

--- spec/services/ccms/case_add_requestor_spec.rb
+++ spec/services/ccms/case_add_requestor_spec.rb
@@ -263,7 +263,8 @@ module CCMS # rubocop:disable Metrics/ModuleLength
                open_banking_consent_choice_at: Date.new(2019, 6, 1),
                lead_proceeding_type: proceeding_type_1,
                benefit_check_result: true,
-               merits_assessment: merits_assessment
+               merits_assessment: merits_assessment,
+               default_cost_limitation: 25_000.0
       end
 
       let(:other_party_1) { create :opponent, :child }
