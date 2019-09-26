0618611c8230604bf572f191b7d3afea3a8506c3
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-09-26 13:53:33 +0100

    AP-961 undo change to load order in application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module LaaApplyForLegalAid
   class Application < Rails::Application
-    config.load_defaults = 6.0
+    config.load_defaults 5.2
 
     config.time_zone = 'London'
 
