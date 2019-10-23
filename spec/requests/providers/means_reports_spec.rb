4606368ad802abf718e57e026e7db13bfdbf98ed
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2019-10-23 20:56:25 +0100

    AP-914 Add CCMS case reference to means report
    The CCMS case reference needs to be displayed on the generated means
    report PDF so that caseworkers are able to track applications.
    
    There is already a pattern for doing this, as it has been implemented
    previously on the merits report.
    
    This commit adds code to the means_reports_controller to get the case
    reference from CCMS if it does not already exist, and amends tests
    appropriately.
    
    The shared partial to display the reference has already been amended in
    the commit that added the case reference to the merits report, so no
    change is necessary.

--- spec/requests/providers/means_reports_spec.rb
+++ spec/requests/providers/means_reports_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe Providers::MeansReportsController, type: :request do
   let(:legal_aid_application) { create :legal_aid_application, :with_proceeding_types, :with_everything, :assessment_submitted }
   let(:login_provider) { login_as legal_aid_application.provider }
+  let!(:submission) { create :submission, legal_aid_application: legal_aid_application }
+  let(:before_subject) { nil }
 
   describe 'GET /providers/applications/:legal_aid_application_id/means_report' do
     subject { get providers_legal_aid_application_means_report_path(legal_aid_application, debug: true) }
 
     before do
       login_provider
+      before_subject
       subject
     end
 
@@ -18,6 +21,23 @@ RSpec.describe Providers::MeansReportsController, type: :request do
 
     it 'displays the application ref number' do
       expect(unescaped_response_body).to include(legal_aid_application.application_ref)
+    end
+
+    it 'displays the CCMS case reference' do
+      expect(unescaped_response_body).to include(submission.case_ccms_reference)
+    end
+
+    context 'when no CCMS case reference present' do
+      let!(:submission) { create :submission, legal_aid_application: legal_aid_application, case_ccms_reference: nil }
+      let(:case_ccms_reference) { Faker::Number.number(digits: 6).to_s }
+      let(:before_subject) do
+        allow_any_instance_of(CCMS::ObtainCaseReferenceService).to receive(:reference_id).and_return(case_ccms_reference)
+        allow_any_instance_of(CCMS::ObtainCaseReferenceService).to receive(:response).and_return('dummy response')
+      end
+
+      it 'obtains the case reference remotely' do
+        expect(unescaped_response_body).to include(case_ccms_reference)
+      end
     end
 
     context 'when not authenticated' do
