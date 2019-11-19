aa286e6c6384819859e66c07bafdf3d8c0af7b96
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2019-11-19 11:45:11 +0000

    AP-1078 Refactor CCMS ref on means/merits reports
    The CCMS case reference is not appearing on means and merits reports.
    This was working but has stopped, due to a change in the CCMS submission
    process. Previously the call to CCMS to get the case reference always
    took place before the reports were generated but this is no longer the
    case.
    
    To fix this, move the ensure_case_ccms_reference_exists functionality
    from the means/merits_report_controllers (where it wasn't being called)
    into the means/merits_report_creator services, and ensure a CCMS case
    reference is always present before creating the reports.
    
    This includes a refactor of the report_creators, creating a
    base_report_creator to provide common functionality.

--- app/controllers/providers/means_reports_controller.rb
+++ app/controllers/providers/means_reports_controller.rb
@@ -1,21 +1,11 @@
 module Providers
   class MeansReportsController < ProviderBaseController
     authorize_with_policy :show_submitted_application?
-    before_action :ensure_case_ccms_reference_exists
 
     def show
       render pdf: 'Means report',
              layout: 'pdf',
              show_as_html: params.key?(:debug)
-    end
-
-    private
-
-    def ensure_case_ccms_reference_exists
-      return if legal_aid_application.case_ccms_reference
-
-      legal_aid_application.create_ccms_submission unless legal_aid_application.ccms_submission
-      legal_aid_application.ccms_submission.process!
     end
   end
 end
---
+++ app/services/reports/base_report_creator.rb
@@ -0,0 +1,28 @@
+module Reports
+  class BaseReportCreator
+    def self.call(legal_aid_application)
+      new(legal_aid_application).call
+    end
+
+    attr_reader :legal_aid_application
+
+    def initialize(legal_aid_application)
+      @legal_aid_application = legal_aid_application
+    end
+
+    private
+
+    def pdf_report
+      WickedPdf.new.pdf_from_string(html_report)
+    end
+
+    def ensure_case_ccms_reference_exists
+      return if legal_aid_application.case_ccms_reference
+
+      legal_aid_application.create_ccms_submission unless legal_aid_application.ccms_submission
+      legal_aid_application.ccms_submission.process!
+    end
+
+    def html_report; end
+  end
+end
