efa09f73be4bf59076ccb092fb0fe006c78c3966
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-11-22 11:46:49 +0000

    Add conditional to whitelisted_users initialiser for testing purposes

--- config/initializers/whitelisted_users.rb
+++ config/initializers/whitelisted_users.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
-Rails.configuration.x.application.whitelisted_users = YAML.load_file(File.join(Rails.root, 'helm_deploy', 'apply-for-legal-aid', 'whitelisted_users.yaml')) if Rails.env.production?
+white_listed_users = if Rails.env.production?
+                       YAML.load_file(File.join(Rails.root, 'helm_deploy', 'apply-for-legal-aid', 'whitelisted_users.yaml'))
+                     else
+                       ['test1']
+                     end
+
+Rails.configuration.x.application.whitelisted_users = white_listed_users
