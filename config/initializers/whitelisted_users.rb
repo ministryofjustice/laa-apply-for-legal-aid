344d69b72097c2bac1d7dea49791746b1d1879bb
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-11-22 12:03:00 +0000

    Fix test

--- config/initializers/whitelisted_users.rb
+++ config/initializers/whitelisted_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 white_listed_users = if Rails.env.production?
-                       YAML.load_file(File.join(Rails.root, 'helm_deploy', 'apply-for-legal-aid', 'whitelisted_users.yaml'))
+                       YAML.load_file(File.join(Rails.root, "helm_deploy", "apply-for-legal-aid", "whitelisted_users.yaml"))
                      else
                        ['test1']
                      end
