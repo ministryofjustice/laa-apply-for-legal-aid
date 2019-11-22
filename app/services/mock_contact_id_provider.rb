a265e8d129b9c19bd4d217193cdbd894fd50de57
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-11-22 15:42:50 +0000

    AP-1089 Add list of username and contact ids

---
+++ app/services/mock_contact_id_provider.rb
@@ -0,0 +1,17 @@
+class MockContactIdProvider
+
+  # If providers username is in the list of usernames, then use the contact id from there,
+  # otherwise use it from the Provider record
+  def self.call(provider)
+    if private_beta_users.key?(provider.username)
+      private_beta_users[provider.username]
+    else
+      provider.user_login_id
+    end
+  end
+
+  def provider_beta_users
+    @provider_beta_users ||= YAML.load_file
+  end
+
+end
