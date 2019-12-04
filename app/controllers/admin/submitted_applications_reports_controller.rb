82240a087b392fee84d2b7f55f4e4a0b4609a4e0
Abdirizak Obsiye <mrabdirizak@gmail.com>
2019-12-04 14:16:02 +0000

    Requested PR changes

--- app/controllers/admin/submitted_applications_reports_controller.rb
+++ app/controllers/admin/submitted_applications_reports_controller.rb
@@ -1,17 +1,18 @@
 module Admin
   class SubmittedApplicationsReportsController < ApplicationController
+    include Pagy::Backend
     before_action :authenticate_admin_user!
     layout 'admin'.freeze
 
+    DEFAULT_PAGE_SIZE = 10
+
     # GET /admin/submitted_applications_report
     def show
-      @applications = submitted_applications
-    end
-
-    private
-
-    def submitted_applications
-      LegalAidApplication.submitted_applications
+      @pagy, @applications = pagy(
+        LegalAidApplication.submitted_applications,
+        items: params.fetch(:page_size, DEFAULT_PAGE_SIZE),
+        size: [1, 1, 1, 1]
+      )
     end
   end
 end
