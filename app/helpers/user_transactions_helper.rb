ce97ba263bae200a0ed013a05590496908434946
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-01-06 11:25:42 +0000

    Refactor IncomingsHelper
    The new name makes more sense for the next steps,
    it now uses the passed in values to check against

--- app/helpers/user_transactions_helper.rb
+++ app/helpers/user_transactions_helper.rb
@@ -1,10 +1,10 @@
-module IncomingsHelper
+module UserTransactionsHelper
   def incomings_list(incomings, locale_namespace:)
     items = TransactionType.credits&.map do |income_type|
       OpenStruct.new(
         label: t("#{locale_namespace}.#{income_type.name}"),
         name: income_type.name,
-        amount_text: yes_no(@legal_aid_application.transaction_types.credits.pluck(:name).include?(income_type.name))
+        amount_text: yes_no(incomings.pluck(:name).include?(income_type.name))
       )
     end
 
