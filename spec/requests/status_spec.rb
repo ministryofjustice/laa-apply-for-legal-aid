7847d92d6395385217146ac343958e3ac3d9a15f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-01-10 15:14:28 +0000

    Implement new status controller setup
    This provides:
    
    * a simple `/ping` endpoint that will display static values
    (separate PR required to configure values in K8S and CircleCI)
    
    * a dynamic `/healthcheck` endpoint that displays values indicating
    up-state of required dependencies
    
    By default the `/status` endpoint will still return the dynamic data
    this means that monitoring & K8S should not be impacted.
    
    There will be a subsequent commit to change it to see if K8S deploys
    are affected when deployed to UAT

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -1,28 +1,175 @@
 require 'rails_helper'
 
-RSpec.describe 'status requests' do
-  let(:response_json) { JSON.parse(response.body) }
-  let(:expected_json) do
-    {
-      'redis' => true,
-      'db' => true,
-      'malware_scanner' => {
-        'positive' => true,
-        'negative' => true
-      }
-    }
-  end
-
-  describe 'GET /status' do
-    subject { get('/status') }
-    before { subject }
-
-    it 'is successful' do
-      expect(response).to be_successful
+RSpec.describe StatusController, type: :request do
+  describe '#healthcheck' do
+    before do
+      allow(Sidekiq::ProcessSet).to receive(:new).and_return(instance_double(Sidekiq::ProcessSet, size: 1))
+      allow(Sidekiq::RetrySet).to receive(:new).and_return(instance_double(Sidekiq::RetrySet, size: 0))
+      allow(Sidekiq::DeadSet).to receive(:new).and_return(instance_double(Sidekiq::DeadSet, size: 0))
+      connection = double('connection')
+      allow(connection).to receive(:info).and_return(redis_version: '5.0.0')
+      allow(Sidekiq).to receive(:redis).and_yield(connection)
     end
 
-    it 'checks external connections' do
-      expect(response_json).to eq(expected_json)
+    context 'when failed Sidekiq jobs exist' do
+      let(:failed_job_healthcheck) do
+        {
+          checks: {
+            database: true,
+            redis: true,
+            sidekiq: true,
+            sidekiq_queue: false,
+            'malware_scanner': {
+              'positive': true,
+              'negative': true
+            }
+          }
+        }.to_json
+      end
+
+      context 'dead set exists' do
+        before do
+          allow(Sidekiq::DeadSet).to receive(:new).and_return(instance_double(Sidekiq::DeadSet, size: 1))
+          get '/healthcheck'
+        end
+
+        it 'returns ok http status' do
+          expect(response).to have_http_status :ok
+        end
+
+        it 'returns the expected response report' do
+          expect(response.body).to eq(failed_job_healthcheck)
+        end
+      end
+
+      context 'retry set exists' do
+        before do
+          allow(Sidekiq::RetrySet).to receive(:new).and_return(instance_double(Sidekiq::RetrySet, size: 1))
+          get '/healthcheck'
+        end
+
+        it 'returns ok http status' do
+          expect(response).to have_http_status :ok
+        end
+
+        it 'returns the expected response report' do
+          expect(response.body).to eq(failed_job_healthcheck)
+        end
+      end
+    end
+
+    context 'when an infrastructure problem exists' do
+      before do
+        allow(ActiveRecord::Base.connection).to receive(:active?).and_raise(PG::ConnectionBad)
+        allow(Sidekiq::ProcessSet).to receive(:new).and_return(instance_double(Sidekiq::ProcessSet, size: 0))
+
+        connection = double('connection')
+        allow(connection).to receive(:info).and_raise(Redis::CannotConnectError)
+        allow(Sidekiq).to receive(:redis).and_yield(connection)
+
+        get '/healthcheck'
+      end
+
+      let(:failed_healthcheck) do
+        {
+          'checks': {
+            'database': false,
+            'redis': false,
+            'sidekiq': false,
+            'sidekiq_queue': true,
+            'malware_scanner': {
+              'positive': true,
+              'negative': true
+            }
+          }
+        }.to_json
+      end
+
+      it 'returns status bad gateway' do
+        expect(response).to have_http_status :bad_gateway
+      end
+
+      it 'returns the expected response report' do
+        expect(response.body).to eq(failed_healthcheck)
+      end
+    end
+
+    context 'when everything is ok' do
+      before do
+        allow(ActiveRecord::Base.connection).to receive(:active?).and_return(true)
+
+        connection = double('connection', info: {})
+        allow(Sidekiq).to receive(:redis).and_yield(connection)
+
+        get '/healthcheck'
+      end
+
+      let(:expected_response) do
+        {
+          'checks': {
+            'database': true,
+            'redis': true,
+            'sidekiq': true,
+            'sidekiq_queue': true,
+            'malware_scanner': {
+              'positive': true,
+              'negative': true
+            }
+          }
+        }.to_json
+      end
+
+      it 'returns HTTP success' do
+        get '/healthcheck'
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns the expected response report' do
+        get '/healthcheck'
+        expect(response.body).to eq(expected_response)
+      end
+    end
+  end
+
+  describe '#ping' do
+    context 'when environment variables set' do
+      let(:expected_json) do
+        {
+          'build_date' => '20150721',
+          'commit_id' => 'afb12cb3',
+          'build_tag' => 'test',
+          'app_branch' => 'test_branch'
+        }
+      end
+
+      before do
+        ENV['BUILD_DATE']       = '20150721'
+        ENV['COMMIT_ID']        = 'afb12cb3'
+        ENV['BUILD_TAG']        = 'test'
+        ENV['APP_BRANCH']       = 'test_branch'
+
+        get('/ping')
+      end
+
+      it 'returns JSON with app information' do
+        expect(JSON.parse(response.body)).to eq(expected_json)
+      end
+    end
+
+    context 'when environment variables not set' do
+      before do
+        ENV['VERSION_NUMBER']   = nil
+        ENV['BUILD_DATE']       = nil
+        ENV['COMMIT_ID']        = nil
+        ENV['BUILD_TAG']        = nil
+        ENV['APP_BRANCH']       = nil
+
+        get '/ping'
+      end
+
+      it 'returns "Not Available"' do
+        expect(JSON.parse(response.body).values).to be_all('Not Available')
+      end
     end
   end
 end
