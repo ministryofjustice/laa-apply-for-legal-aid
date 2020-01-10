163bcb275515560594508e99ebed3167b6bf2c9f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-01-10 15:14:28 +0000

    Implement new status controller setup
    This provides:
    
    * a simple `/ping` endpoint that will display static values
    (separate PR required to configure values in K8S and CircleCI)
    
    * a dynamic `/healthcheck` endpoint that displays values indicating
    up-state of required dependencies
    
    By default the `/status` endpoint will still return the dynamic data
    this means that monitoring & K8S should not be impacted.
    
    There will be a subsequent commit to change it to see if K8S deploys
    are affected when deployed to UAT

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,24 +1,58 @@
 class StatusController < ApiController
-  def index
-    render json: {
-      redis: redis_connection,
-      db: db,
+  def status
+    checks = {
+      database: database_alive?,
+      redis: redis_alive?,
+      sidekiq: sidekiq_alive?,
+      sidekiq_queue: sidekiq_queue_healthy?,
+
       malware_scanner: {
         positive: malware_scanner_positive,
         negative: malware_scanner_negative
       }
     }
+
+    status = :bad_gateway unless checks.except(:sidekiq_queue).values.all?
+    render status: status, json: { checks: checks }
+  end
+
+  def ping
+    render json: {
+      'build_date' => ENV['BUILD_DATE'] || 'Not Available',
+      'commit_id' => ENV['COMMIT_ID'] || 'Not Available',
+      'build_tag' => ENV['BUILD_TAG'] || 'Not Available',
+      'app_branch' => ENV['APP_BRANCH'] || 'Not Available'
+    }
   end
 
   private
 
-  def db
-    LegalAidApplication.select(:id).first
+  def redis_alive?
+    Sidekiq.redis(&:info)
     true
+  rescue StandardError
+    false
   end
 
-  def redis_connection
-    Sidekiq.redis(&:ping) == 'PONG'
+  def sidekiq_alive?
+    ps = Sidekiq::ProcessSet.new
+    !ps.size.zero?
+  rescue StandardError
+    false
+  end
+
+  def sidekiq_queue_healthy?
+    dead = Sidekiq::DeadSet.new
+    retries = Sidekiq::RetrySet.new
+    dead.size.zero? && retries.size.zero?
+  rescue StandardError
+    false
+  end
+
+  def database_alive?
+    ActiveRecord::Base.connection.active?
+  rescue PG::ConnectionBad
+    false
   end
 
   def malware_scanner_positive
