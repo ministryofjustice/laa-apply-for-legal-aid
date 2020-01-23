92af9aac6b1ac9f9cbdc30c5f8b6d3f7ffc49fc4
willc-work <william.clarke@justice.gov.uk>
2020-01-23 12:06:44 +0000

    increase frequency for testing

--- app/services/dashboard_event_handler.rb
+++ app/services/dashboard_event_handler.rb
@@ -38,6 +38,7 @@ class DashboardEventHandler
 
   def application_created
     Dashboard::UpdaterJob.perform_later('Applications') if payload[:state] == 'initiated'
+    Dashboard::UpdaterJob.perform_later('DailyApplications') if payload[:state] == 'initiated'
   end
 
   def provider_created
