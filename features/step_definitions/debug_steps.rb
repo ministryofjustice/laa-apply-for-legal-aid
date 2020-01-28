48760f0c4cccd5ebf8922cb01ee2bbdd1e976dd8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-01-28 11:25:57 +0000

    fixup! Add debug feature steps

--- features/step_definitions/debug_steps.rb
+++ features/step_definitions/debug_steps.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Lint/Debugger
 When('I bind and pry') do
   binding.pry
 end
@@ -5,6 +6,7 @@ end
 When(/^I save and open page$/) do
   save_and_open_page
 end
+# rubocop:enable Lint/Debugger
 
 When(/^I save and open screenshot$/) do
   screenshot_and_open_image
