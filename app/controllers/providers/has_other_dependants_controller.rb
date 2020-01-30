953240cf664c2a4ddfe6a48c3d8866d5a7974ef3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-01-30 17:09:14 +0000

    Implement provider/has_other_dependants

---
+++ app/controllers/providers/has_other_dependants_controller.rb
@@ -0,0 +1,14 @@
+module Providers
+  class HasOtherDependantsController < ProviderBaseController
+    def show; end
+
+    def update
+      if params[:other_dependant].in?(%w[yes no])
+        go_forward(params[:other_dependant] == 'yes')
+      else
+        @error = I18n.t('providers.has_other_dependants.show.error')
+        render :show
+      end
+    end
+  end
+end
