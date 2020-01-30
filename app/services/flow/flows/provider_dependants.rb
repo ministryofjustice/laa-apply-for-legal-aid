83a7b9d171f02b97a1c62480447d647a7da2ce69
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-01-30 11:32:41 +0000

    Implement provider_dependants flow

---
+++ app/services/flow/flows/provider_dependants.rb
@@ -0,0 +1,45 @@
+module Flow
+  module Flows
+    class ProviderDependants < FlowSteps
+      STEPS = {
+        has_dependants: {
+          path: ->(application) { urls.providers_legal_aid_application_has_dependants_path(application) },
+          forward: ->(application) { application.has_dependants? ? :dependants : :identify_types_of_outgoings }
+        },
+        dependants: {
+          path: ->(application) { urls.providers_legal_aid_application_dependants_path(application) },
+          forward: ->(_, dependant) { dependant.over_fifteen? ? :dependants_relationships : :has_other_dependants }
+        },
+        dependants_details: {
+          forward: ->(_, dependant) { dependant.over_fifteen? ? :dependants_relationships : :has_other_dependants }
+        },
+        dependants_relationships: {
+          path: ->(application, params) do
+            urls.providers_legal_aid_application_dependant_relationship_path(application, params.is_a?(Dependant) ? params.id : params.slice(:dependant_id))
+          end,
+          forward: ->(_, dependant) do
+            dependant.adult_relative? || dependant.eighteen_or_less? ? :dependants_monthly_incomes : :dependants_full_time_educations
+          end
+        },
+        dependants_assets_values: {
+          path: ->(application, dependant) { urls.providers_legal_aid_application_dependant_assets_value_path(application, dependant) },
+          forward: :has_other_dependants
+        },
+        dependants_monthly_incomes: {
+          path: ->(application, dependant) { urls.providers_legal_aid_application_dependant_monthly_income_path(application, dependant) },
+          forward: ->(_, dependant) do
+            dependant.in_full_time_education? || dependant.eighteen_or_less? ? :has_other_dependants : :dependants_assets_values
+          end
+        },
+        has_other_dependants: {
+          path: ->(application) { urls.providers_legal_aid_application_has_other_dependant_path(application) },
+          forward: ->(_, has_other_dependant) { has_other_dependant ? :dependants : :identify_types_of_outgoings }
+        },
+        dependants_full_time_educations: {
+          path: ->(application, dependant) { urls.providers_legal_aid_application_dependant_full_time_education_path(application, dependant) },
+          forward: :dependants_monthly_incomes
+        }
+      }.freeze
+    end
+  end
+end
--- app/services/flow/provider_flow_service.rb
+++ app/services/flow/provider_flow_service.rb
@@ -1,6 +1,7 @@
 module Flow
   class ProviderFlowService < BaseFlowService
     steps = {}.deep_merge(Flows::ProviderStart::STEPS)
+              .deep_merge(Flows::ProviderDependants::STEPS)
               .deep_merge(Flows::ProviderCapital::STEPS)
               .deep_merge(Flows::ProviderVehicle::STEPS)
               .deep_merge(Flows::ProviderMerits::STEPS)
