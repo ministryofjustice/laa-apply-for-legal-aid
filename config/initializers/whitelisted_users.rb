ee761f7ceb705cadd85e756052dcbb8b4e4a5da3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-02-06 13:22:19 +0000

    Update whitelisted_users to allow exclusion of test accounts

--- config/initializers/whitelisted_users.rb
+++ config/initializers/whitelisted_users.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
-file_name = File.join(Rails.root, 'helm_deploy', 'apply-for-legal-aid', 'whitelisted_users.yaml')
-Rails.configuration.x.application.whitelisted_users = YAML.load_file(file_name) unless Rails.env.development?
+include_file = File.join(Rails.root, 'helm_deploy', 'apply-for-legal-aid', 'whitelisted_users.yaml')
+exclude_file = File.join(Rails.root, 'helm_deploy', 'apply-for-legal-aid', 'whitelisted_users_exclude.yaml')
+unless Rails.env.development?
+  Rails.configuration.x.application.whitelisted_users = YAML.load_file(include_file)
+  Rails.configuration.x.application.whitelisted_users_excluding_test = Rails.configuration.x.application.whitelisted_users - YAML.load_file(exclude_file)
+end
