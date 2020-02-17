ee16346ff2958b6e8be8e93d5b1a30a33e51b7a6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-02-17 15:51:29 +0000

    Clear up previous capybara screenshots before a new run

--- features/support/env.rb
+++ features/support/env.rb
@@ -102,6 +102,8 @@ Cucumber::Rails::Database.javascript_strategy = :truncation
 
 Before do |_scenario|
   load Rails.root.join('db/seeds.rb')
+  # Delete previous screenshots from filesystem that were generated during previous feature runs
+  FileUtils.rm_rf("#{Rails.root}/tmp/capybara/**.*")
 end
 
 Around do |_scenario, block|
