3550e01786bcf66ea5e42b40e8d0595581b4d50d
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-03-11 10:00:42 +0000

    AP-1270 Add spec for failed response from CaseAdd

--- spec/services/ccms/parsers/case_add_response_parser_spec.rb
+++ spec/services/ccms/parsers/case_add_response_parser_spec.rb
@@ -41,6 +41,21 @@ module CCMS
 
       context 'unsuccessful response' do
         let(:response_xml) { ccms_data_from_file 'case_add_response_failure.xml' }
+
+        describe '#success?' do
+          it 'extracts the status' do
+            parser = described_class.new(expected_tx_id, response_xml)
+            expect(parser.success?).to eq false
+          end
+        end
+
+        describe '#message' do
+          it 'returns status concatenated with status free text' do
+            parser = described_class.new(expected_tx_id, response_xml)
+            parser.success?
+            expect(parser.message).to eq 'Fail: '
+          end
+        end
       end
     end
   end
