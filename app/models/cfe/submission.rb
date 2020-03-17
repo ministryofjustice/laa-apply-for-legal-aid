2c92e9bca50fb112bd23727710182d6be753ece8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-03-17 09:21:30 +0000

    Move has_many from incorrect data type to the correct object

--- app/models/cfe/submission.rb
+++ app/models/cfe/submission.rb
@@ -4,7 +4,6 @@ module CFE
 
     belongs_to :legal_aid_application
     has_many :submission_histories, -> { order(created_at: :asc) }
-    has_many :submission_documents, -> { order(created_at: :asc) }
     has_one :result, class_name: 'BaseResult'
   end
 end
