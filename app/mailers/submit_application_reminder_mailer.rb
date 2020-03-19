2d3f264ad0936d7d238c50b6b6e4ff905bc53c96
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-03-19 07:50:19 +0000

    AP-1237: Update email date display
    Format the Date in accordance with GDS guidance

--- app/mailers/submit_application_reminder_mailer.rb
+++ app/mailers/submit_application_reminder_mailer.rb
@@ -11,8 +11,8 @@ class SubmitApplicationReminderMailer < GovukNotifyRails::Mailer
       provider_name: name,
       ref_number: application['application_ref'],
       client_name: application.applicant.full_name,
-      delegated_functions_date: application['used_delegated_functions_on'],
-      deadline_date: application['substantive_application_deadline_on']
+      delegated_functions_date: application['used_delegated_functions_on'].strftime('%d %B %y'),
+      deadline_date: application['substantive_application_deadline_on'].strftime('%d %B %y')
     )
     mail to: to
   end
