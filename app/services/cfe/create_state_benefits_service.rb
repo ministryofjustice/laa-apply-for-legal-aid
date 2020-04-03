f287e35a5d141d54284769e1878d1cfcdf4c2b61
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-03 12:08:57 +0100

    Update the CreateStateBenefitsService

--- app/services/cfe/create_state_benefits_service.rb
+++ app/services/cfe/create_state_benefits_service.rb
@@ -6,8 +6,32 @@ module CFE
 
     def request_body
       {
-
+        "state_benefits": build_transactions
       }.to_json
+    end
+
+    private
+
+    def build_transactions
+      result = []
+      benefit_types = legal_aid_application.bank_transactions.for_type(:benefits).pluck(:meta_data).uniq
+
+      raise CFE::SubmissionError, 'Benefit transactions un-coded' if benefit_types.any?(nil)
+      raise CFE::SubmissionError, 'Benefit transaction cannot be identified' if benefit_types.map { |n| n.match?(/Unknown code/) }.any?
+
+      benefit_types.each do |type|
+        type_hash = { "name": type, "payments": transactions(type) }
+        result << type_hash
+      end
+      result
+    end
+
+    def transactions(type)
+      result = []
+      legal_aid_application.bank_transactions.for_type(:benefits).where(meta_data: type).each do |transaction|
+        result << { date: transaction.happened_at.strftime('%Y-%m-%d'), amount: transaction.amount.to_f }
+      end
+      result
     end
   end
 end
--- spec/services/cfe/create_state_benefits_service_spec.rb
+++ spec/services/cfe/create_state_benefits_service_spec.rb
@@ -3,17 +3,28 @@ require 'rails_helper'
 module CFE
   RSpec.describe CreateStateBenefitsService do
     subject(:service) { described_class.new(submission) }
-    let(:application) { create :legal_aid_application, :with_positive_benefit_check_result, transaction_period_finish_on: Date.today, transaction_types: [benefit] }
+
+    DAY_SEQUENCE = [10, 40, 70].freeze
+
+    let(:application) { create :legal_aid_application, :with_positive_benefit_check_result, transaction_period_finish_on: Date.today }
     let!(:applicant) { create :applicant, legal_aid_application: application }
     let(:submission) { create :cfe_submission, aasm_state: 'assessment_created', legal_aid_application: application }
     let(:bank_account) { create :bank_account, bank_provider: bank_provider }
     let(:bank_provider) { create :bank_provider, applicant: applicant }
     let(:bank_account) { create :bank_account, bank_provider: bank_provider }
-    let!(:benefits_bank_transaction) { create :bank_transaction, :credit, transaction_type: benefit, bank_account: bank_account }
+    let!(:non_benefits_bank_transactions) { create_list :bank_transaction, 2, :friends_or_family, bank_account: bank_account }
     let(:benefit) { create :transaction_type, :credit, :benefits }
 
-    it { expect(application.bank_transactions.count).to be_positive }
-    it { binding.pry }
+    before do
+      DAY_SEQUENCE.each do |days|
+        create :bank_transaction, :benefits, bank_account: bank_account, amount: '123.45', happened_at: days.days.ago
+      end
+    end
+
+    # TODO: Delete this placeholder
+    describe 'basic test -- DELETE' do
+      it { expect(application.bank_transactions.count).to eq 5 }
+    end
 
     describe '#cfe_url' do
       it 'contains the submission assessment id' do
@@ -24,47 +35,66 @@ module CFE
 
     describe '#request_body' do
       it 'creates the expected payload from the values in bank_transactions' do
-        expect(service.request_body).to eq expected_payload_hash.to_json
+        expect(service.request_body).to eq build_expected_payload.to_json
+      end
+
+      context 'when a disregarded benefit is recorded' do
+        before { create :bank_transaction, :disregarded_benefits, bank_account: bank_account, amount: '123.45', happened_at: DAY_SEQUENCE[0].days.ago }
+
+        it { expect(service.request_body).to include(with_disregarded_hash.to_json) }
+        it { expect(application.bank_transactions.count).to eq 6 }
+      end
+
+      context 'when a benefit transaction is returned without meta-data' do
+        before { create :bank_transaction, :unassigned_benefits, bank_account: bank_account, amount: '123.45', happened_at: DAY_SEQUENCE[0].days.ago }
+
+        it 'raises an error' do
+          expect { subject.call }.to raise_error(CFE::SubmissionError, 'Benefit transactions un-coded')
+        end
+      end
+
+      context 'when a benefit transaction is returned with an unknown meta-data tag' do
+        before { create :bank_transaction, :unknown_benefits, bank_account: bank_account, amount: '123.45', happened_at: DAY_SEQUENCE[0].days.ago }
+
+        it 'raises an error' do
+          expect { subject.call }.to raise_error(CFE::SubmissionError, 'Benefit transaction cannot be identified')
+        end
       end
     end
 
-    def expected_payload_hash # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def build_expected_payload(with_disregarded = false)
+      base = { "state_benefits": [basic_payload_hash] }
+      base.merge!(with_disregarded_hash) if with_disregarded
+      base
+    end
+
+    def with_disregarded_hash
       {
-        "state_benefits": [
+        "name": 'grenfell_payments',
+        "payments": [
           {
-            "name": 'benefit_type_3',
-            "payments": [
-              {
-                "date": '2019-11-01',
-                "amount": 1046.44
-              },
-              {
-                "date": '2019-10-01',
-                "amount": 1034.33
-              },
-              {
-                "date": '2019-09-01',
-                "amount": 1033.44
-              }
-            ]
+            "date": DAY_SEQUENCE[0].days.ago.strftime('%Y-%m-%d'),
+            "amount": 123.45
+          }
+        ]
+      }
+    end
+
+    def basic_payload_hash # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      {
+        "name": 'child_maintenance',
+        "payments": [
+          {
+            "date": DAY_SEQUENCE[2].days.ago.strftime('%Y-%m-%d'),
+            "amount": 123.45
           },
           {
-            "name": 'benefit_type_4',
-            "payments":
-            [
-              {
-                "date": '2019-11-01',
-                "amount": 250.0
-              },
-              {
-                "date": '2019-10-01',
-                "amount": 266.02
-              },
-              {
-                "date": '2019-09-01',
-                "amount": 250.0
-              }
-            ]
+            "date": DAY_SEQUENCE[1].days.ago.strftime('%Y-%m-%d'),
+            "amount": 123.45
+          },
+          {
+            "date": DAY_SEQUENCE[0].days.ago.strftime('%Y-%m-%d'),
+            "amount": 123.45
           }
         ]
       }
