0feb252f7dd14738057be5c9a7279c08cb598de2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-03 09:46:07 +0100

    Delete the BenefitType factory
    The model for this was deleted in December

--- spec/factories/benefit_types.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :benefit_type do
-    sequence :label do |n|
-      "benefit_type_#{n}"
-    end
-    description { Faker::Lorem.paragraph }
-    exclude_from_gross_income { [true, false].sample }
-  end
-end
