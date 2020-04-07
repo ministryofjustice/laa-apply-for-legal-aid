a5e2f8a22e25ab7eb4a82c88073c47128f1294f4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-07 15:26:24 +0100

    Update MeansSummariesController
    To call and handle CFE::SubmissionManager requests

--- app/controllers/providers/means_summaries_controller.rb
+++ app/controllers/providers/means_summaries_controller.rb
@@ -6,8 +6,17 @@ module Providers
     end
 
     def update
-      legal_aid_application.provider_checked_citizens_means_answers! unless draft_selected? || legal_aid_application.provider_checked_citizens_means_answers?
+      unless draft_selected? || legal_aid_application.provider_checked_citizens_means_answers?
+        redirect_to(problem_index_path) && return unless check_financial_eligibility
+        legal_aid_application.provider_checked_citizens_means_answers!
+      end
       continue_or_draft
+    end
+
+    private
+
+    def check_financial_eligibility
+      CFE::SubmissionManager.call(legal_aid_application.id)
     end
   end
 end
