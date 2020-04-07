400adad34531034d21d99bc26158cdb5027d1d19
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-07 07:51:29 +0100

    Update the tests for the now plurlised urls

--- spec/services/cfe/create_state_benefits_service_spec.rb
+++ spec/services/cfe/create_state_benefits_service_spec.rb
@@ -25,7 +25,7 @@ module CFE
     describe '.cfe_url' do
       it 'contains the submission assessment id' do
         expect(service.cfe_url)
-          .to eq "#{Rails.configuration.x.check_finanical_eligibility_host}/assessments/#{submission.assessment_id}/state_benefit"
+          .to eq "#{Rails.configuration.x.check_finanical_eligibility_host}/assessments/#{submission.assessment_id}/state_benefits"
       end
     end
 
