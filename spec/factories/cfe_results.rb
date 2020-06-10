616060681e4fd798694a8d22284cf903743dcc52
willc-work <william.clarke@digital.justice.gov.uk>
2020-06-10 10:39:16 +0100

    remove v1 references and factories

--- spec/factories/cfe_results.rb
+++ spec/factories/cfe_results.rb
@@ -1,34 +1,4 @@
 FactoryBot.define do
-  factory :cfe_v1_result, class: CFE::V1::Result do
-    submission { create :cfe_submission }
-    legal_aid_application { submission.legal_aid_application }
-    result { CFEResults::V1::MockResults.eligible.to_json }
-
-    trait :eligible do
-      result { CFEResults::V1::MockResults.eligible.to_json }
-    end
-
-    trait :not_eligible do
-      result { CFEResults::V1::MockResults.not_eligible.to_json }
-    end
-
-    trait :contribution_required do
-      result { CFEResults::V1::MockResults.contribution_required.to_json }
-    end
-
-    trait :no_capital do
-      result { CFEResults::V1::MockResults.no_capital.to_json }
-    end
-
-    trait :no_additional_properties do
-      result { CFEResults::V1::MockResults.no_additional_properties.to_json }
-    end
-
-    trait :with_additional_properties do
-      result { CFEResults::V1::MockResults.with_additional_properties.to_json }
-    end
-  end
-
   factory :cfe_v2_result, class: CFE::V2::Result do
     submission { create :cfe_submission }
     legal_aid_application { submission.legal_aid_application }
--- spec/factories/legal_aid_applications.rb
+++ spec/factories/legal_aid_applications.rb
@@ -375,13 +375,6 @@ FactoryBot.define do
       end
     end
 
-    trait :with_cfe_v1_result do
-      after :create do |application|
-        cfe_submission = create :cfe_submission, legal_aid_application: application
-        create :cfe_v1_result, submission: cfe_submission
-      end
-    end
-
     trait :with_cfe_v2_result do
       after :create do |application|
         cfe_submission = create :cfe_submission, legal_aid_application: application
