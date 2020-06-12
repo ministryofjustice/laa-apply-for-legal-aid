18de3f0c29b2d3c40a9d85bd6ecc2ba998445b49
willc-work <william.clarke@digital.justice.gov.uk>
2020-06-12 09:31:42 +0100

    improve validation

--- app/models/irregular_income.rb
+++ app/models/irregular_income.rb
@@ -1,9 +1,6 @@
 class IrregularIncome < ApplicationRecord
   belongs_to :legal_aid_application
 
-  PERMITTED_TYPES = /\Astudent_loan\z/.freeze
-  PERMITTED_FREQUENCY = /\Aannual\z/.freeze
-
-  validates :income_type, format: { with: PERMITTED_TYPES }
-  validates :frequency, format: { with: PERMITTED_FREQUENCY }
+  validates :income_type, inclusion: %w[student_loan]
+  validates :frequency, inclusion: %w[annual]
 end
