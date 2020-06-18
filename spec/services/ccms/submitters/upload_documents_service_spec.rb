9aa5ecb85238098645fae41c526fd5c77bf11333
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-06-18 09:41:37 +0100

    Update UploadDocumentsService
    * Reduce the complexity of `call`
    * Record histories for each document attempt
    * Enable the bank_transaction_report by sending the document_type
    
    Update the tests
    Provide different history definitions for success and failure
    We are checking for different values and the target is now
    quite different

--- spec/services/ccms/submitters/upload_documents_service_spec.rb
+++ spec/services/ccms/submitters/upload_documents_service_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
            :with_transaction_period,
            :with_means_report,
            :with_merits_report,
+           :with_bank_transaction_report,
            state: :submitting_assessment
   end
 
@@ -17,9 +18,14 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
   let(:statement_of_case_attachment) { statement_of_case.original_attachments.first }
   let(:means_report_attachment) { legal_aid_application.means_report }
   let(:merits_report_attachment) { legal_aid_application.merits_report }
+  let(:bank_transaction_report_attachment) { legal_aid_application.bank_transaction_report }
   let!(:statement_of_case_submission_document) { create :submission_document, :id_obtained, submission: submission, attachment_id: statement_of_case_attachment.id }
   let!(:means_report_document) { create :submission_document, :id_obtained, submission: submission, document_type: :means_report, attachment_id: means_report_attachment.id }
   let!(:merits_report_document) { create :submission_document, :id_obtained, submission: submission, document_type: :merits_report, attachment_id: merits_report_attachment.id }
+  let!(:bank_transaction_report_document) do
+    create :submission_document, :id_obtained, submission: submission, document_type: :bank_transaction_report, attachment_id: bank_transaction_report_attachment.id
+  end
+  let(:expected_response) { ccms_data_from_file 'case_add_status_response.xml' }
 
   let(:submission) do
     create :submission,
@@ -28,12 +34,14 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
            case_ccms_reference: Faker::Number.number(digits: 9)
   end
 
-  let(:history) { CCMS::SubmissionHistory.find_by(submission_id: submission.id) }
+  let(:histories) { CCMS::SubmissionHistory.order(:created_at).where(submission_id: submission.id) }
+  let(:first_history) { histories.first }
   let(:document_upload_requestor) do
     double CCMS::Requestors::DocumentUploadRequestor.new(submission.case_ccms_reference,
                                                          statement_of_case_attachment.id,
                                                          'base64encodedpdf',
-                                                         'my_login')
+                                                         'my_login',
+                                                         nil)
   end
   let(:document_upload_response) { ccms_data_from_file 'document_upload_response.xml' }
   let(:transaction_request_id_in_example_response) { '20190301030405123456' }
@@ -42,9 +50,12 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
   before do
     allow(CCMS::Requestors::DocumentUploadRequestor).to receive(:new).and_return(document_upload_requestor)
     allow(document_upload_requestor).to receive(:transaction_request_id).and_return(transaction_request_id_in_example_response)
+    allow(document_upload_requestor).to receive(:formatted_xml).and_return(expected_response)
   end
 
   context 'operation successful' do
+    let(:history) { histories.where(to_state: 'completed').last }
+
     before do
       allow(document_upload_requestor).to receive(:call).and_return(document_upload_response)
     end
@@ -70,8 +81,12 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
       expect { subject.call }.to change { submission.aasm_state }.to 'completed'
     end
 
-    it 'writes a history record' do
-      expect { subject.call }.to change { CCMS::SubmissionHistory.count }.by(1)
+    it 'writes a history record for each document and on completion' do
+      expect { subject.call }.to change { CCMS::SubmissionHistory.count }.by(5)
+    end
+
+    it 'writes a history record on completion that updates the state' do
+      subject.call
       expect(history.from_state).to eq 'case_created'
       expect(history.to_state).to eq 'completed'
       expect(history.success).to be true
@@ -80,9 +95,10 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
   end
 
   context 'operation unsuccessful' do
+    let(:history) { histories.where(request: nil, response: nil, to_state: 'failed').last }
     context 'operation fails due to an exception' do
       before do
-        expect(document_upload_requestor).to receive(:call).and_raise(CCMS::CcmsError, 'failure uploading documents')
+        allow(document_upload_requestor).to receive(:call).and_raise(CCMS::CcmsError, 'failed to upload')
       end
 
       it 'changes the submission state to failed' do
@@ -94,13 +110,26 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
         expect(submission.submission_documents.first.status).to eq 'failed'
       end
 
-      it 'writes a history record' do
-        expect { subject.call }.to change { CCMS::SubmissionHistory.count }.by(1)
+      it 'writes a history record for each document and on completion' do
+        expect { subject.call }.to change { CCMS::SubmissionHistory.count }.by(5)
+      end
+
+      it 'writes a history record on completion that updates the state' do
+        subject.call
+        expect(first_history.from_state).to eq 'case_created'
+        expect(first_history.to_state).to eq 'failed'
+        expect(first_history.success).to be false
+        expect(first_history.details).to match(/CCMS::CcmsError/)
+        expect(first_history.details).to match(/failed to upload/)
+      end
+
+      it 'writes a history record on completion that updates the state' do
+        subject.call
         expect(history.from_state).to eq 'case_created'
         expect(history.to_state).to eq 'failed'
         expect(history.success).to be false
         expect(history.details).to match(/CCMS::CcmsError/)
-        expect(history.details).to match(/failure uploading documents/)
+        expect(history.details).to match(/failed to upload/)
       end
     end
 
@@ -114,13 +143,12 @@ RSpec.describe CCMS::Submitters::UploadDocumentsService do
         expect { subject.call }.to change { submission.aasm_state }.to 'failed'
       end
 
-      it 'changes the document state to failed' do
-        subject.call
-        expect(submission.submission_documents.first.status).to eq 'failed'
+      it 'writes a history record for each document and on completion' do
+        expect { subject.call }.to change { CCMS::SubmissionHistory.count }.by(5)
       end
 
-      it 'writes a history record' do
-        expect { subject.call }.to change { CCMS::SubmissionHistory.count }.by(1)
+      it 'writes a history record on completion that updates the state' do
+        subject.call
         expect(history.from_state).to eq 'case_created'
         expect(history.to_state).to eq 'failed'
         expect(history.success).to be false
