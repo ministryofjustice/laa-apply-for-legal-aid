7486d3be7e8e00cb5e7aeb8b5ce7bac19e091f17
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-06-18 09:28:51 +0100

    Update the DocumentIdRequestor
    This needed to be adapted to generate different XML values
    for the Bank Statement documents

--- spec/services/ccms/requestors/document_id_requestor_spec.rb
+++ spec/services/ccms/requestors/document_id_requestor_spec.rb
@@ -6,21 +6,42 @@ module CCMS
       let(:expected_xml) { ccms_data_from_file 'document_id_request.xml' }
       let(:expected_tx_id) { '20190101121530123456' }
       let(:case_ccms_reference) { '1234567890' }
-      let(:requestor) { described_class.new(case_ccms_reference, 'my_login') }
+      let(:requestor) { described_class.new(case_ccms_reference, 'my_login', type) }
+      let(:type) { 'means_report' }
 
       describe 'XML request' do
-        include_context 'ccms soa configuration'
+        context 'when the attachment is a means report' do
+          include_context 'ccms soa configuration'
 
-        it 'generates the expected XML' do
-          allow(requestor).to receive(:transaction_request_id).and_return(expected_tx_id)
-          expect(requestor.formatted_xml).to be_soap_envelope_with(
-            command: 'ns2:DocumentUploadRQ',
-            transaction_id: expected_tx_id,
-            matchers: [
-              '<ns4:DocumentType>ADMIN1</ns4:DocumentType>',
-              '<ns4:Channel>E</ns4:Channel>'
-            ]
-          )
+          it 'generates the expected XML' do
+            allow(requestor).to receive(:transaction_request_id).and_return(expected_tx_id)
+            expect(requestor.formatted_xml).to be_soap_envelope_with(
+              command: 'ns2:DocumentUploadRQ',
+              transaction_id: expected_tx_id,
+              matching: %w[
+                <ns4:DocumentType>ADMIN1</ns4:DocumentType>
+                <ns4:Channel>E</ns4:Channel>
+              ]
+            )
+          end
+        end
+
+        context 'when the attachment is a bank transaction report' do
+          let(:type) { 'bank_transaction_report' }
+
+          include_context 'ccms soa configuration'
+
+          it 'generates the expected XML' do
+            allow(requestor).to receive(:transaction_request_id).and_return(expected_tx_id)
+            expect(requestor.formatted_xml).to be_soap_envelope_with(
+              command: 'ns2:DocumentUploadRQ',
+              transaction_id: expected_tx_id,
+              matching: %w[
+                <ns4:DocumentType>BSTMT</ns4:DocumentType>
+                <ns4:Channel>E</ns4:Channel>
+              ]
+            )
+          end
         end
       end
 
