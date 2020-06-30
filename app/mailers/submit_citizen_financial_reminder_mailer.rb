e1e960956c1d144e9a47586137ace45006d844c3
willc-work <william.clarke@digital.justice.gov.uk>
2020-06-30 12:01:20 +0100

    mailers should derive from BaseMailer

--- app/mailers/submit_citizen_financial_reminder_mailer.rb
+++ app/mailers/submit_citizen_financial_reminder_mailer.rb
@@ -1,7 +1,11 @@
-class SubmitCitizenFinancialReminderMailer < GovukNotifyRails::Mailer
+class SubmitCitizenFinancialReminderMailer < BaseApplyMailer
   self.delivery_job = GovukNotifyMailerJob
   require_relative 'concerns/notify_template_methods'
   include NotifyTemplateMethods
+
+  def self.eligible_for_delivery?(scheduled_mailing)
+    scheduled_mailing.legal_aid_application.provider_step == 'check_provider_answers'
+  end
 
   def notify_citizen(application_id, email, application_url, client_name)
     application = LegalAidApplication.find(application_id)
