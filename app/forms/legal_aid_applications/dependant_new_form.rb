5ff42edf76a6283443b643df1283565e2bf07a15
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-07-01 14:40:43 +0100

    Create new Dependant Form
    This replaces the broken down sub forms

---
+++ app/forms/legal_aid_applications/dependant_new_form.rb
@@ -0,0 +1,129 @@
+module LegalAidApplications
+  class DependantNewForm
+    include BaseForm
+    form_for Dependant
+
+    before_validation :clear_value_fields
+
+    ATTRIBUTES = %i[name dob_year dob_month dob_day relationship
+                    in_full_time_education has_income monthly_income
+                    has_assets_more_than_threshold assets_value].freeze
+    SCOPE = 'activemodel.errors.models.dependant.attributes'.freeze
+
+    attr_accessor(*ATTRIBUTES)
+    attr_writer :date_of_birth
+
+    validates :name, presence: true
+    validates :date_of_birth, presence: true
+    validates(
+      :date_of_birth,
+      date: {
+        not_in_the_future: true,
+        earliest_allowed_date: { date: '1900-01-01' }
+      },
+      allow_nil: true
+    )
+    validate :relationship_presence
+    validate :full_time_education_presence
+    validate :validate_has_income_presence
+    validates :monthly_income, presence: true, if: proc { |form| form.has_income.to_s == 'true' }
+    validates :monthly_income, allow_blank: true, currency: { greater_than: 0.0 }
+    validates(
+      :has_assets_more_than_threshold,
+      presence: { message: ->(form, _) { form.blank_option_error_message } },
+      unless: :draft?
+    )
+
+    validates(
+      :assets_value,
+      currency: { allow_blank: true },
+      presence: { unless: :draft? },
+      if: :assets_more_than_threshold?
+    )
+
+    validates(
+      :assets_value,
+      numericality: {
+        greater_than_or_equal_to: 8_000,
+        message: ->(form, _) { form.less_than_threshold_error_message }
+      },
+      if: :assets_more_than_threshold?
+    )
+
+    def initialize(*args)
+      super
+      set_instance_variables_for_attributes_if_not_set_but_in_model(
+        attrs: date_fields.fields,
+        model_attributes: date_fields.model_attributes
+      )
+    end
+
+    # Note that this method is first called by `validates`.
+    # Without that validation, the functionality in this method will not be called before save
+    def date_of_birth
+      return @date_of_birth if @date_of_birth.present?
+      return if date_fields.blank?
+      return :invalid if date_fields.partially_complete? || date_fields.form_date_invalid?
+
+      @date_of_birth = attributes[:date_of_birth] = date_fields.form_date
+    end
+
+    def save
+      attributes[:assets_value] = nil if valid? && !assets_more_than_threshold?
+      super
+    end
+
+    def blank_option_error_message
+      I18n.t('.has_assets_more_than_threshold.blank_message', scope: SCOPE, name: model.name)
+    end
+
+    def assets_more_than_threshold?
+      has_assets_more_than_threshold&.to_s == 'true'
+    end
+
+    def less_than_threshold_error_message
+      I18n.t(
+        'activemodel.errors.models.dependant.attributes.assets_value.less_than_threshold',
+        name: model.name
+      )
+    end
+
+    private
+
+    def exclude_from_model
+      date_fields.fields
+    end
+
+    def date_fields
+      @date_fields ||= DateFieldBuilder.new(
+        form: self,
+        model: model,
+        method: :date_of_birth,
+        prefix: :dob_
+      )
+    end
+
+    def relationship_presence
+      return if relationship.present?
+
+      errors.add(:relationship, I18n.t('.relationship.blank', scope: SCOPE, name: model.name))
+    end
+
+    def full_time_education_presence
+      return if in_full_time_education.present?
+
+      errors.add(:in_full_time_education, I18n.t('.in_full_time_education.blank_message', scope: SCOPE, name: model.name))
+    end
+
+    def validate_has_income_presence
+      return if has_income.present?
+
+      errors.add(:has_income, I18n.t('.has_income.blank_message', scope: SCOPE, name: model.name))
+    end
+
+    def clear_value_fields
+      monthly_income&.clear unless ActiveModel::Type::Boolean.new.cast(has_income.to_s)
+      assets_value&.clear unless ActiveModel::Type::Boolean.new.cast(has_assets_more_than_threshold.to_s)
+    end
+  end
+end
---
+++ spec/forms/legal_aid_applications/dependant_new_form_spec.rb
@@ -0,0 +1,177 @@
+require 'rails_helper'
+
+RSpec.describe LegalAidApplications::DependantNewForm do
+  let(:dependant) { create :dependant, date_of_birth: nil }
+  let(:date) { Faker::Date.birthday }
+  let(:day) { date.strftime('%d') } # Two number day
+  let(:month) { date.strftime('%m') } # Two number month
+  let(:year) { date.strftime('%Y') } # Four number year
+  let(:turn_of_century) { Time.local 2000, 1, 1 }
+  let(:has_assets) { 'false' }
+  let(:assets_value) { '' }
+  let(:has_income) { 'false' }
+  let(:monthly_income) { '' }
+
+  let(:params) do
+    {
+      name: Faker::Lorem.word,
+      dob_day: day,
+      dob_month: month,
+      dob_year: year,
+      relationship: 'child_relationship',
+      monthly_income: monthly_income,
+      has_income: has_income,
+      in_full_time_education: 'false',
+      has_assets_more_than_threshold: has_assets,
+      assets_value: assets_value
+    }
+  end
+
+  subject { described_class.new(params) }
+
+  describe 'valid?' do
+    it 'returns true when validations pass' do
+      expect(subject).to be_valid
+    end
+
+    context 'when invalid' do
+      let(:year) { 2.years.from_now.year }
+
+      it 'returns false when validations fail' do
+        expect(subject).not_to be_valid
+      end
+    end
+  end
+
+  describe 'date_of_birth' do
+    it 'matches the input' do
+      expect(subject.date_of_birth).to eq(date)
+    end
+
+    context 'with two character year' do
+      let(:year) { date.strftime('%y') }
+
+      context 'in 20th century' do
+        let(:date) { Faker::Date.between from: 99.years.ago, to: turn_of_century }
+
+        it 'constructs correct date' do
+          expect(subject.date_of_birth).to eq(date)
+        end
+      end
+
+      context 'in 21st century' do
+        let(:date) { Faker::Date.between from: turn_of_century, to: Date.today }
+
+        it 'constructs correct date' do
+          expect(subject.date_of_birth).to eq(date)
+        end
+      end
+    end
+
+    shared_examples 'invalid date' do
+      let(:invalid_date_message) { I18n.t('activemodel.errors.models.dependant.attributes.date_of_birth.date_not_valid') }
+
+      it 'invalidates form' do
+        expect(subject).to be_invalid
+      end
+
+      it 'has invalid date of birth error message' do
+        subject.valid?
+        expect(subject.errors[:date_of_birth]).to include(invalid_date_message)
+      end
+    end
+
+    context 'with three digit year' do
+      let(:year) { date.year.to_s[1, 3] }
+
+      it_behaves_like 'invalid date'
+    end
+
+    context 'with one digit year' do
+      let(:year) { (0..9).to_a.sample }
+
+      it_behaves_like 'invalid date'
+    end
+
+    context 'with three digit month' do
+      let(:month) { Faker::Number.number(digits: 3) }
+
+      it_behaves_like 'invalid date'
+    end
+
+    context 'with three digit day' do
+      let(:day) { Faker::Number.number(digits: 3) }
+
+      it_behaves_like 'invalid date'
+    end
+  end
+
+  describe 'assets' do
+    before { subject.valid? }
+
+    context 'when the dependant has assets' do
+      let(:has_assets) { 'true' }
+
+      context 'with no value specified' do
+        let(:expected_array) do
+          [
+            I18n.t('activemodel.errors.models.dependant.attributes.assets_value.blank'),
+            I18n.t('activemodel.errors.models.dependant.attributes.assets_value.not_a_number')
+          ]
+        end
+
+        it 'raises the expected error' do
+          expect(subject.errors[:assets_value]).to eq expected_array
+        end
+      end
+
+      context 'with a value below the threshold' do
+        let(:assets_value) { '5000' }
+        let(:expected_array) { [I18n.t('activemodel.errors.models.dependant.attributes.assets_value.less_than_threshold')] }
+
+        it 'raises the expected error' do
+          expect(subject.errors[:assets_value]).to eq expected_array
+        end
+      end
+    end
+
+    context 'when the assets question is blank' do
+      let(:has_assets) { '' }
+      let(:assets_value) { '' }
+      let(:expected_array) do
+        [
+          I18n.t('activemodel.errors.models.dependant.attributes.has_assets_more_than_threshold.blank_message')
+        ]
+      end
+
+      it 'raises the expected error' do
+        expect(subject.errors[:has_assets_more_than_threshold]).to eq expected_array
+      end
+    end
+  end
+
+  describe 'income' do
+    before { subject.valid? }
+
+    context 'when the dependant has income' do
+      let(:has_income) { 'true' }
+
+      context 'with no value specified' do
+        let(:expected_array) { [I18n.t('activemodel.errors.models.dependant.attributes.monthly_income.blank')] }
+
+        it 'raises the expected error' do
+          expect(subject.errors[:monthly_income]).to eq expected_array
+        end
+      end
+    end
+
+    context 'when the income question is blank' do
+      let(:has_income) { '' }
+      let(:expected_array) { [I18n.t('activemodel.errors.models.dependant.attributes.has_income.blank_message')] }
+
+      it 'raises the expected error' do
+        expect(subject.errors[:has_income]).to eq expected_array
+      end
+    end
+  end
+end
