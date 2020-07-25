39d6595dc5af301e455d9d93d2dbff81f284a8bc
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-07-25 16:40:14 +0100

    AP=1461 remove passported/non-passported permissions check on Addresses controller

--- app/controllers/providers/addresses_controller.rb
+++ app/controllers/providers/addresses_controller.rb
@@ -1,7 +1,7 @@
 module Providers
   class AddressesController < ProviderBaseController
     include PreDWPCheckVisible
-    
+
     def show
       @form = Addresses::AddressForm.new(model: address)
     end
