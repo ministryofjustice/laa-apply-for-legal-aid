83178473a20213d99eadb69d90cdc5b4c49dd058
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-08-26 11:14:48 +0100

    WIP

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,27 +4,6 @@ class AuthController < ApplicationController
   def failure
     # redirect to consents page if it was an applicant failing to login at his bank
     #
-    if auth_error_during_bank_login?
-      begin
-        raise AuthorizationError, "Redirecting to access denied page - unexpected origin: '#{origin}'"
-      rescue StandardError => e
-        Raven.capture_exception(e)
-      end
-      redirect_to error_path(:access_denied)
-    else
-      redirect_to citizens_consent_path(auth_failure: true)
-    end
-  end
-
-  private
-
-  def origin
-    @origin ||= params[:origin]
-  end
-
-  def auth_error_during_bank_login?
-    return true if origin.nil?
-
-    URI(origin).path != '/citizens/banks'
+    redirect_to citizens_consent_path(auth_failure: true)
   end
 end
--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -18,20 +18,5 @@ RSpec.describe 'AuthController', type: :request do
         expect(response).to redirect_to citizens_consent_path(auth_failure: true)
       end
     end
-
-    context 'origin from elsewhere' do
-      let(:origin_path) { root_path }
-      it 'redirects to access denied' do
-        subject
-        expect(response).to redirect_to error_path(:access_denied)
-      end
-    end
-
-    context 'no origin' do
-      it 'redirects to access denied' do
-        get '/auth/failure'
-        expect(response).to redirect_to error_path(:access_denied)
-      end
-    end
   end
 end
