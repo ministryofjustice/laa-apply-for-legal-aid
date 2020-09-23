bca25cc2c25fab64da473b372c3af019fcfe0a78
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-09-23 17:51:36 +0100

    AP-1545 remove unused lines and add comments

--- app/controllers/test/sessions_controller.rb
+++ app/controllers/test/sessions_controller.rb
@@ -1,4 +1,9 @@
 module Test
+  # This controller is used purely for setting session variables for RSpec request spec.
+  # Use with the #set_session method defined in spec/rails_helper.rb
+  #
+  # The test_session_path route is defined in test environment only
+  #
   class SessionsController < ApplicationController
     def create
       json_vars = params.permit(:session_vars)
