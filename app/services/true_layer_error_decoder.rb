c8bd5d2298efaac2655541f59c9537ef5c6bbcfe
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-10-07 17:06:30 +0100

    AP-1721 Clarify TrueLayer errors and retry path

---
+++ app/services/true_layer_error_decoder.rb
@@ -0,0 +1,41 @@
+class TrueLayerErrorDecoder
+  # expects an array, the third item of which is a JSON-encoded hash
+  #
+  #  [
+  #    "bank_data_import",
+  #    "import_account_holders",
+  #    "{\"TrueLayerError\" : {\"error_description\":\"<details of the error>\",\"error\":\"provider_error\",\"error_details\":{}}}"
+  # ]
+  #
+  #
+  ERROR_CODES = %w[
+    provider_error
+    account_permanently_locked
+    account_temporarily_locked
+    internal_server_error
+    user_input_required
+    wrong_credentials
+  ].freeze
+
+  def initialize(error_details)
+    @error_hash = JSON.parse(error_details[2])
+    @error_code = @error_hash.dig('TrueLayerError', 'error')
+    return if @error_code.in?(ERROR_CODES)
+
+    error_description = @error_hash.dig('TrueLayerError', 'error_description')
+    Raven.capture_message("Unknown error code received from TrueLayer: #{@error_code} :: #{error_description}")
+    @error_code = 'unknown'
+  end
+
+  def error_heading
+    I18n.t("true_layer_errors.headings.#{@error_code}")
+  end
+
+  def error_detail
+    I18n.t("true_layer_errors.detail.#{@error_code}")
+  end
+
+  def show_link?
+    @error_code != 'account_permanently_locked'
+  end
+end
