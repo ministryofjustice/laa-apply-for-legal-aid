fab68dcd89284c81c8d64a6d1a372b75d82c6572
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-10-12 18:27:54 +0100

    AP-1737 fix coverage\

--- app/services/reports/mis/non_passported_applications_report.rb
+++ app/services/reports/mis/non_passported_applications_report.rb
@@ -3,11 +3,11 @@ module Reports
     class NonPassportedApplicationsReport
       EXCLUDED_STATES = %w[use_ccms].freeze
       START_DATE = Time.new(2020, 9, 21, 0, 0, 0)
+      END_TIME = Date.today.end_of_day
 
       def run
         csv_string = CSV.generate do |csv|
           csv << NonPassportedApplicationCsvLine.header_row
-
           legal_aid_applications.find_each(batch_size: 100) do |legal_aid_application|
             csv << NonPassportedApplicationCsvLine.call(legal_aid_application)
           end
@@ -19,7 +19,7 @@ module Reports
 
       def legal_aid_applications
         LegalAidApplication.joins(:state_machine)
-                           .where(created_at: [START_DATE..Time.now])
+                           .where(created_at: [START_DATE..END_TIME])
                            .where(state_machine_proxies: { type: 'NonPassportedStateMachine' })
                            .where.not(state_machine_proxies: { aasm_state: EXCLUDED_STATES })
                            .order(:created_at)
