71691edb3f0d1c79d1b6818b22f0aff96274d92f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-10-14 11:22:41 +0100

    Remove legacy sort table helper
    Ensure all uses point to the same helper

--- app/helpers/table_sort_helper.rb
+++ app/helpers/table_sort_helper.rb
@@ -57,27 +57,7 @@ module TableSortHelper
   #   where at: is the width below which the column will be combined (options are 470 and 555)
   #   and append: is the contented that will be appended to the main content when the columns are combined
   #
-  # TODO: Depending on outcome of second DAC review this version may be deprecatable
-  # it adds a lot of noise to the aria tags and the simple_sortable_th was designed to
-  # be more screen reader friendly. Apparently, this version gives _too much_ detail
-  # to the users!
-  # It was left in place in Oct 2020 because it is still in use on the transaction pages
-  # and we wanted feedback on the new version before replacing it everywhere
   def sort_column_th(type:, content:, combine_right: {}, currently_sorted: nil)
-    combine_right_at = combine_right[:at]
-    klasses = %w[govuk-table__header sort]
-    klasses += ['table-combine_right_if_narrow', "narrow_#{combine_right_at}"] if combine_right_at
-    klasses << 'govuk-table__header--numeric' if type == :numeric
-    klasses << "header-sort-#{currently_sorted}" if currently_sorted
-    content_tag(:th, class: klasses, scope: 'col', 'data-sort-type' => type) do
-      sort_span_by +
-        sort_span_content(content) +
-        sort_span_combine_right(combine_right) +
-        sort_span_sort_indicator(type, currently_sorted)
-    end
-  end
-
-  def simple_sortable_th(type:, content:, combine_right: {}, currently_sorted: nil)
     combine_right_at = combine_right[:at]
     classes = %w[govuk-table__header sort] # sort enables function and wrapping th content in 'sortable-column' span
     classes += ['table-combine_right_if_narrow', "narrow_#{combine_right_at}"] if combine_right_at
@@ -100,55 +80,9 @@ module TableSortHelper
     )
   end
 
-  def sort_span_by
-    content_tag :span, "#{t('sorting.sort_by')} ", class: 'govuk-visually-hidden'
-  end
-
-  def sort_span_content(content)
-    content_tag :span, content, class: 'aria-sort-description'
-  end
-
-  def sort_span_sort_indicator(type = nil, currently_sorted = nil)
-    content_tag(
-      :span,
-      sort_span_screen_reader_sort_indicator(type, currently_sorted),
-      class: 'sort-direction-indicator'
-    )
-  end
-
-  def sort_span_screen_reader_sort_indicator(type, currently_sorted)
-    screen_reader_hint = sort_hint(type: type, direction: currently_sorted)
-    content = screen_reader_hint && "(#{screen_reader_hint})".html_safe
-    content_tag(
-      :span,
-      content,
-      class: 'screen-reader-sort-indicator govuk-visually-hidden'
-    )
-  end
-
   def sort_span_combine_right(options = {})
     return '' if options.empty?
 
     content_tag :span, " #{options[:append]}", class: 'table-hidden_right_title'
-  end
-
-  def sort_hint(type: :numeric, direction: :desc)
-    return unless direction
-
-    labels = sort_hint_labels[type]
-    return I18n.t("sorted_#{direction}_html") unless labels
-
-    labels.reverse! if direction == :desc
-    labels.map! { |label| I18n.t("sorting.#{label}") }
-    from, to = labels
-    I18n.t('sorting.currently_sorted_html', from: from, to: to)
-  end
-
-  def sort_hint_labels
-    {
-      numeric: %i[smallest largest],
-      alphabetic: %i[a z],
-      date: %i[newest oldest]
-    }
   end
 end
