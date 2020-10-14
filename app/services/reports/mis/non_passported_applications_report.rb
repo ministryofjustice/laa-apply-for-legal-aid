332d24ae436869897cd0008a27f5e30b1c2ac35d
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-10-14 11:45:12 +0100

    AP=1737 UAT debug

--- app/services/reports/mis/non_passported_applications_report.rb
+++ app/services/reports/mis/non_passported_applications_report.rb
@@ -9,20 +9,23 @@ module Reports
         csv_string = CSV.generate do |csv|
           csv << NonPassportedApplicationCsvLine.header_row
           legal_aid_applications.find_each(batch_size: 100) do |legal_aid_application|
-            csv << NonPassportedApplicationCsvLine.call(legal_aid_application)
+              csv << NonPassportedApplicationCsvLine.call(legal_aid_application)
           end
         end
+        Raven.capture_message("CSV STRING: #{csv_string}")
         csv_string
       end
 
       private
 
       def legal_aid_applications
-        LegalAidApplication.joins(:state_machine)
+        apps = LegalAidApplication.joins(:state_machine)
                            .where(created_at: [START_DATE..END_TIME])
                            .where(state_machine_proxies: { type: 'NonPassportedStateMachine' })
                            .where.not(state_machine_proxies: { aasm_state: EXCLUDED_STATES })
                            .order(:created_at)
+        Raven.capture_message("NO OF NON PASSPORTED APPS: #{apps.size}")
+        apps
       end
     end
   end
