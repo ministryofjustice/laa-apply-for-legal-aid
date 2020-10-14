273f061bf415c340b03eb8df465a53c29d76d8be
willc-work <william.clarke@digital.justice.gov.uk>
2020-10-14 17:53:10 +0100

    Add session_store for secure cookies

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# config/initializers/session_store.rb
+# set secure: true, optionally only do this for certain Rails environments (e.g., Staging / Production
+Rails.application.config.session_store :cookie_store, key: '_laa_apply_for_legal_aid_session', secure: Rails.env.production? ? true : false
