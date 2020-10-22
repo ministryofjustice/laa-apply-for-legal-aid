fd0baf2c25b19c8a01262c6f2b49e2dbbc6b7f92
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-10-22 09:59:54 +0100

    AP-1674 remove typo

--- spec/requests/providers/outgoings_summary_spec.rb
+++ spec/requests/providers/outgoings_summary_spec.rb
@@ -104,7 +104,7 @@ RSpec.describe Providers::OutgoingsSummaryController, type: :request do
     before { subject }
 
     it 'redirects to the next page' do
-      expect(response).to redirect_to(providers_legal_aid_application_own_home_path(legal_aid_application))features/providers/civil_application_journey.feature:721
+      expect(response).to redirect_to(providers_legal_aid_application_own_home_path(legal_aid_application))
     end
 
     context 'when the provider is not authenticated' do
