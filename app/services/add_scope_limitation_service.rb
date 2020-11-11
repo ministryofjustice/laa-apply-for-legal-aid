d7d5f570ad1533386a72ee67f5f6ccd3e0479732
Eloise Tait <eloisetait@yahoo.com>
2020-11-11 18:41:50 +0000

    Created service object for scope limitation

---
+++ app/services/add_scope_limitation_service.rb
@@ -0,0 +1,36 @@
+class AddScopeLimitationService
+  def initialize(legal_aid_application, delegate)
+    @legal_aid_application = legal_aid_application
+    @delegate = delegate
+  end
+
+  def self.call(legal_aid_application, delegate)
+    new(legal_aid_application, delegate).call
+  end
+
+  def call
+    if @delegate
+      add_default_delegated_functions_scope_limitation!
+    else
+      add_default_substantive_scope_limitation!
+    end
+  end
+
+  def add_default_substantive_scope_limitation!
+    ApplicationScopeLimitation.create!(
+      legal_aid_application: @legal_aid_application,
+      scope_limitation: @legal_aid_application.lead_proceeding_type.default_substantive_scope_limitation,
+      substantive: true
+    )
+  end
+
+  def add_default_delegated_functions_scope_limitation!
+    ApplicationScopeLimitation.create!(
+      legal_aid_application: @legal_aid_application,
+      scope_limitation: @legal_aid_application.lead_proceeding_type.default_delegated_functions_scope_limitation,
+      substantive: false
+    )
+  end
+end
+
+
---
+++ spec/services/add_scope_limitation_service_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe AddScopeLimitationService do
+  let(:legal_aid_application) { create :legal_aid_application, proceeding_types: [proceeding_type] }
+  let!(:proceeding_type) { create :proceeding_type }
+  let!(:sl_substantive_default) { create :scope_limitation, :substantive_default, joined_proceeding_type: proceeding_type, meaning: 'Default substantive SL' }
+  let!(:sl_delegated_default) { create :scope_limitation, :delegated_functions_default, joined_proceeding_type: proceeding_type, meaning: 'Default delegated functions SL' }
+  let!(:sl_non_default) { create :scope_limitation }
+  let(:delegate) { true }
+
+  subject(:service) { described_class.call(legal_aid_application, delegate) }
+
+
+  describe 'default_scope_limitation finding and adding' do
+
+    before { subject }
+
+    context 'substantive application' do
+
+      context '#substantive scope limitations' do
+        let(:delegate) { false }
+
+        describe 'substantive scope limitation' do
+          it 'returns the substantive scope limitation' do
+            expect(legal_aid_application.scope_limitations).to match_array [sl_substantive_default]
+            expect(legal_aid_application.substantive_scope_limitation).to eq sl_substantive_default
+          end
+        end
+
+      end
+    end
+
+    context 'delegated functions scope limitation' do
+      describe 'delegated functions scope limitation' do
+        it 'returns the delegated functions scope limitation' do
+          expect(legal_aid_application.scope_limitations).to match_array [sl_delegated_default]
+          expect(legal_aid_application.delegated_functions_scope_limitation).to eq sl_delegated_default
+        end
+      end
+    end
+  end
+end
