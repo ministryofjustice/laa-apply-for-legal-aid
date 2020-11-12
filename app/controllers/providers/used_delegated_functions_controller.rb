b0f299698f5bc983ea1e76215769f3d273dfb5cb
Eloise Tait <eloisetait@yahoo.com>
2020-11-12 17:51:40 +0000

    lint checks

--- app/controllers/providers/used_delegated_functions_controller.rb
+++ app/controllers/providers/used_delegated_functions_controller.rb
@@ -27,6 +27,7 @@ module Providers
 
     def save_continue_or_draft_and_update_scope_limitations
       return false unless save_continue_or_draft(@form)
+
       AddScopeLimitationService.call(legal_aid_application, :delegated) if @form.model.used_delegated_functions?
       true
     end
