da9093aed730163ef74df915d186897c20d1c4e6
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-11-18 16:13:10 +0000

    AP-1550 Unit specs for saml_idp_controller

---
+++ spec/requests/saml_idp_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe SamlIdpController, type: :request do
+  describe 'POST /saml/auth' do
+    let(:saml_request) { encode_saml_request }
+    let(:password) { 'password' }
+
+    context 'valid username and password in config/initializers/mock_saml.rb' do
+      let(:email) { 'test1@example.com' }
+      let(:username) { 'test1' }
+
+      context 'Record for that provider exists in the database' do
+        before { create :provider, email: email, username: username }
+
+        it 'renders the saml auth form with the encoded request' do
+          post saml_auth_path, params: login_params
+          expect(response).to be_successful
+          expect(response.body).to include('<form action="http://test/providers/saml/auth"')
+          expect(response.body).to include('input type="hidden" name="SAMLResponse" id="SAMLResponse"')
+        end
+      end
+
+      context 'no record exists for that user in database' do
+        it 'renders the saml auth form with the encoded request' do
+          post saml_auth_path, params: login_params
+          expect(response).to be_successful
+          expect(response.body).to include('Incorrect email or password.')
+        end
+      end
+    end
+
+    context 'user not in config/initializers/mock_saml.rb' do
+      let(:email) { 'nobody@example.com' }
+      let(:username) { 'test1' }
+      it 'renders the saml auth form with the encoded request' do
+        post saml_auth_path, params: login_params
+        expect(response).to be_successful
+        expect(response.body).to include('Incorrect email or password.')
+      end
+    end
+
+    context 'password not as specified in config/initializers/mock_saml.rb' do
+      let(:email) { 'test1@example.com' }
+      let(:username) { 'test1' }
+      let(:password) { 'forgotten' }
+      it 'renders the saml auth form with the encoded request' do
+        post saml_auth_path, params: login_params
+        expect(response).to be_successful
+        expect(response.body).to include('Incorrect email or password.')
+      end
+    end
+
+    def encode_saml_request
+      # call providers/saml/sign_in to get encoded saml request
+      get '/providers/saml/sign_in', params: new_params
+      expect(response.status).to eq 302
+      redirect_uri = URI(response.header['Location'])
+      expect(redirect_uri.path).to eq '/saml/auth'
+      request_string = redirect_uri.query.sub(/^SAMLRequest=/, '')
+      CGI.unescape(request_string)
+    end
+
+    def new_params
+      HashWithIndifferentAccess.new(
+        {
+          'locale' => 'en',
+          'controller' => 'saml_sessions',
+          'action' => 'new'
+        }
+      )
+    end
+
+    def login_params
+      HashWithIndifferentAccess.new(
+        {
+          'SAMLRequest' => saml_request,
+          'email' => email,
+          'password' => password,
+          'commit' => 'Sign in',
+          'locale' => 'en',
+          'controller' => 'saml_idp',
+          'action' => 'create'
+        }
+      )
+    end
+  end
+end
