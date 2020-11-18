87b30f1508e665de88fbfc36b6beb3ba07e03811
willc-work <william.clarke@digital.justice.gov.uk>
2020-11-18 11:46:29 +0000

    update validation

--- app/forms/applicants/email_form.rb
+++ app/forms/applicants/email_form.rb
@@ -9,7 +9,7 @@ module Applicants
     before_validation :strip_email
 
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
-    validates :email, presence: true
+    validates :email, presence: true, unless: :draft?
 
     def strip_email
       email.strip!
