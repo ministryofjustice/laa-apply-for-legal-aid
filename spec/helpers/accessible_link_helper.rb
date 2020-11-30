53275090036d738fff370399db072de1db4dd519
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2020-11-30 21:34:20 +0000

    AP-1496 Accessible Statement of Case delete links
    The links used to delete uploaded statements of case are all read out as
    'Delete' by screen readers. This can be improved by including the
    attachment name for the screen reader.
    
    These links are actually `button-as-link`s. To add the desired
    functionality replicate the existing `link_to_with_hidden_suffix` helper
    method for buttons, creating a `button_to_with_hidden_suffix` method
    that appends the attachment name with a `govuk-visually-hidden` class
    so that it is read out by screen readers but does not appear onscreen.

--- spec/helpers/accessible_link_helper.rb
+++ spec/helpers/accessible_link_helper.rb
@@ -19,4 +19,27 @@ RSpec.describe AccessibleLinkHelper, type: :helper do
       end
     end
   end
+
+  describe '#button_to_with_hidden_suffix' do
+    let(:path) { 'test_path' }
+    let(:klass) { 'test-class' }
+    let(:visible_text) { 'visible link text' }
+    let(:hidden_text) { 'hidden link text' }
+    let(:method) { 'post' }
+    let(:params) { { params: 'params' } }
+
+    it 'returns the html for a styled button partly hidden from screen readers' do
+      button = button_to_with_hidden_suffix(path: path, klass: klass, text: visible_text, method: method, params: params, suffix: hidden_text)
+      expect(button).to eq '<form class="button_to" method="post" action="test_path"><button class="test-class" type="submit">visible link text' \
+                           '<span class=\'govuk-visually-hidden\'> hidden link text</span></button><input type="hidden" name="params" value="params" /></form>'
+    end
+
+    context 'with no class provided' do
+      it 'returns the html for an unstyled button partly hidden from screen readers' do
+        button = button_to_with_hidden_suffix(path: path, text: visible_text, method: method, params: params, suffix: hidden_text)
+        expect(button).to eq '<form class="button_to" method="post" action="test_path"><button type="submit">visible link text' \
+                             '<span class=\'govuk-visually-hidden\'> hidden link text</span></button><input type="hidden" name="params" value="params" /></form>'
+      end
+    end
+  end
 end
