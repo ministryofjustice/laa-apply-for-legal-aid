1473438d61038012b4d71ce3a0b49aefb73713cd
willc-work <william.clarke@digital.justice.gov.uk>
2020-12-02 16:35:07 +0000

    add tests for new mailer

---
+++ spec/mailers/citizen_confirmation_mailer_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe CitizenConfirmationMailer, type: :mailer do
+  let(:app_id) { SecureRandom.uuid }
+  let(:email) { Faker::Internet.safe_email }
+  let(:client_name) { Faker::Name.name }
+  let(:citizen_completed_application_template) { Rails.configuration.govuk_notify_templates[:citizen_completed_application] }
+
+  it 'uses GovukNotifyMailerJob' do
+    expect(described_class.delivery_job).to eq(GovukNotifyMailerJob)
+  end
+
+  describe '#citizen_start_email' do
+    let(:mail) { described_class.citizen_complete_email(app_id, email, client_name) }
+
+    it 'sends an email to the correct address' do
+      expect(mail.to).to eq([email])
+    end
+
+    it 'is a govuk_notify delivery' do
+      expect(mail.delivery_method).to be_a(GovukNotifyRails::Delivery)
+    end
+
+    it 'sets the correct template' do
+      expect(mail.govuk_notify_template).to eq(citizen_completed_application_template)
+    end
+
+    it 'has the right personalisation' do
+      expect(mail.govuk_notify_personalisation).to eq(
+        client_name: client_name,
+        ref_number: app_id
+      )
+    end
+  end
+end
