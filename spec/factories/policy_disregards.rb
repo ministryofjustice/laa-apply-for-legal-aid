9eab19895b3827cd1145a7390f7529a31d0652d0
Eloise Tait <eloisetait@yahoo.com>
2020-12-18 17:19:02 +0000

    policy page views, yml and forms added

---
+++ spec/factories/policy_disregards.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :policy_disregards do
+    legal_aid_application
+    england_infected_blood_support { false }
+    vaccine_damage_payments { false }
+    variant_creutzfeldt_jakob_disease { false }
+    criminal_injuries_compensation_scheme { false }
+    national_emergencies_trust { false }
+    we_love_manchester_emergency_fund { false }
+    london_emergencies_trust { false }
+  end
+end
