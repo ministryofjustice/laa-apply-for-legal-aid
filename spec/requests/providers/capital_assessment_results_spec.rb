eac63e1dbef43219004778622ccc3bcc07a7196c
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-12-24 12:30:53 +0000

    AP-1903 controller specs to take policy disregrrds into account

--- spec/requests/providers/capital_assessment_results_spec.rb
+++ spec/requests/providers/capital_assessment_results_spec.rb
@@ -9,8 +9,11 @@ RSpec.describe Providers::CapitalAssessmentResultsController, type: :request do
     let!(:applicant) { create :applicant, with_bank_accounts: 2, legal_aid_application: legal_aid_application }
     let(:applicant_name) { legal_aid_application.applicant_full_name }
     let(:locale_scope) { 'shared.assessment_results' }
+    let(:add_policy_disregards?) { false }
 
     let(:before_tasks) do
+      create(:policy_disregards, :with_selected_value, legal_aid_application: legal_aid_application) if add_policy_disregards?
+
       Setting.setting.update!(manually_review_all_cases: false)
       login_provider
       subject
@@ -21,43 +24,84 @@ RSpec.describe Providers::CapitalAssessmentResultsController, type: :request do
     before { before_tasks }
 
     context 'no restrictions' do
-      context 'eligible' do
-        it 'returns http success' do
-          expect(response).to have_http_status(:ok)
+      context 'no policy disregards' do
+        context 'eligible' do
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('eligible.heading', name: applicant_name, scope: locale_scope))
+          end
         end
 
-        it 'displays the correct result' do
-          expect(unescaped_response_body).to include(I18n.t('eligible.heading', name: applicant_name, scope: locale_scope))
+        context 'when not eligible' do
+          let(:cfe_result) { create :cfe_v2_result, :not_eligible }
+
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('not_eligible.heading', name: applicant_name, scope: locale_scope))
+          end
+        end
+
+        context 'when capital contribution required' do
+          let(:cfe_result) { create :cfe_v2_result, :with_capital_contribution_required }
+
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('capital_contribution_required.heading', name: applicant_name, scope: locale_scope))
+          end
         end
       end
 
-      context 'when not eligible' do
-        let(:cfe_result) { create :cfe_v2_result, :not_eligible }
+      context 'with policy disregards' do
+        let(:add_policy_disregards?) { true }
+        context 'eligible' do
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
 
-        it 'returns http success' do
-          expect(response).to have_http_status(:ok)
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('eligible.heading', name: applicant_name, scope: locale_scope))
+          end
         end
 
-        it 'displays the correct result' do
-          expect(unescaped_response_body).to include(I18n.t('not_eligible.heading', name: applicant_name, scope: locale_scope))
+        context 'when not eligible' do
+          let(:cfe_result) { create :cfe_v2_result, :not_eligible }
+
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('not_eligible.heading', name: applicant_name, scope: locale_scope))
+          end
         end
-      end
 
-      context 'when capital contribution required' do
-        let(:cfe_result) { create :cfe_v2_result, :with_capital_contribution_required }
+        context 'when capital contribution required' do
+          let(:cfe_result) { create :cfe_v2_result, :with_capital_contribution_required }
 
-        it 'returns http success' do
-          expect(response).to have_http_status(:ok)
-        end
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
 
-        it 'displays the correct result' do
-          expect(unescaped_response_body).to include(I18n.t('capital_contribution_required.heading', name: applicant_name, scope: locale_scope))
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('manual_check_disregards.heading', name: applicant_name, scope: locale_scope))
+          end
         end
       end
     end
 
     context 'with restrictions' do
       let(:before_tasks) do
+        create(:policy_disregards, :with_selected_value, legal_aid_application: legal_aid_application) if add_policy_disregards?
+
         Setting.setting.update!(manually_review_all_cases: false)
         create :applicant, legal_aid_application: legal_aid_application, first_name: 'Stepriponikas', last_name: 'Bonstart'
         legal_aid_application.update has_restrictions: true, restrictions_details: 'Blah blah'
@@ -65,26 +109,54 @@ RSpec.describe Providers::CapitalAssessmentResultsController, type: :request do
         subject
       end
 
-      context 'eligible' do
-        let(:cfe_result) { create :cfe_v2_result, :eligible }
-        it 'returns http success' do
-          expect(response).to have_http_status(:ok)
+      context 'no policy disregards' do
+        let(:add_policy_disregards?) { true }
+        context 'eligible' do
+          let(:cfe_result) { create :cfe_v2_result, :eligible }
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('eligible.heading', name: applicant_name, scope: locale_scope))
+          end
         end
 
-        it 'displays the correct result' do
-          expect(unescaped_response_body).to include(I18n.t('eligible.heading', name: applicant_name, scope: locale_scope))
+        context 'when contribution required' do
+          let(:cfe_result) { create :cfe_v2_result, :with_capital_contribution_required }
+
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays manual check required' do
+            expect(unescaped_response_body).to include(I18n.t('manual_check_disregards_restrictions.heading', name: applicant_name, scope: locale_scope))
+          end
         end
       end
 
-      context 'when contribution required' do
-        let(:cfe_result) { create :cfe_v2_result, :with_capital_contribution_required }
+      context 'with policy disregards' do
+        context 'eligible' do
+          let(:cfe_result) { create :cfe_v2_result, :eligible }
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
 
-        it 'returns http success' do
-          expect(response).to have_http_status(:ok)
+          it 'displays the correct result' do
+            expect(unescaped_response_body).to include(I18n.t('eligible.heading', name: applicant_name, scope: locale_scope))
+          end
         end
 
-        it 'displays manual check required' do
-          expect(unescaped_response_body).to include(I18n.t('manual_check_required.heading', name: applicant_name, scope: locale_scope))
+        context 'when contribution required' do
+          let(:cfe_result) { create :cfe_v2_result, :with_capital_contribution_required }
+
+          it 'returns http success' do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'displays manual check required' do
+            expect(unescaped_response_body).to include(I18n.t('manual_check_required.heading', name: applicant_name, scope: locale_scope))
+          end
         end
       end
     end
