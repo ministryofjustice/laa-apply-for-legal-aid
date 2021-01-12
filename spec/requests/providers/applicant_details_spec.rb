33b114a39c30e5d81fb5f1fadbc0e2b266d6d115
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-01-12 16:43:56 +0000

    fix statement of case spec

--- spec/requests/providers/applicant_details_spec.rb
+++ spec/requests/providers/applicant_details_spec.rb
@@ -123,9 +123,9 @@ RSpec.describe Providers::ApplicantDetailsController, type: :request do
                 first_name: '',
                 last_name: 'Doe',
                 national_insurance_number: 'AA 12 34 56 C',
-                dob_year: '1981',
-                dob_month: '07',
-                dob_day: '11',
+                date_of_birth_1i: '1981',
+                date_of_birth_2i: '07',
+                date_of_birth_3i: '11',
                 email: Faker::Internet.safe_email
               }
             }
@@ -168,9 +168,9 @@ RSpec.describe Providers::ApplicantDetailsController, type: :request do
               first_name: '',
               last_name: 'Doe',
               national_insurance_number: 'AA 12 34 56 C',
-              dob_year: '1981',
-              dob_month: '6s',
-              dob_day: '11sa',
+              date_of_birth_1i: '1981',
+              date_of_birth_2i: '6s',
+              date_of_birth_3i: '11sa',
               email: Faker::Internet.safe_email
             }
           }
