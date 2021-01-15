39134582c6e6b8fc6aba22020b3c4d5cd5b06138
willc-work <william.clarke@digital.justice.gov.uk>
2021-01-15 14:08:13 +0000

    add transactions to the views and translations

--- app/controllers/providers/income_summary_controller.rb
+++ app/controllers/providers/income_summary_controller.rb
@@ -2,6 +2,7 @@ module Providers
   class IncomeSummaryController < ProviderBaseController
     def index
       @bank_transactions = bank_transactions
+      @cash_transactions = cash_transactions
     end
 
     def create
@@ -9,6 +10,7 @@ module Providers
 
       if uncategorized_transactions?
         @bank_transactions = bank_transactions
+        @cash_transactions = cash_transactions
         render :index
       else
         go_forward
@@ -26,6 +28,15 @@ module Providers
                             .credit
                             .order(happened_at: :desc)
                             .by_type
+    end
+
+    def cash_transactions
+      cash_transaction_types
+      @legal_aid_application.cash_transactions.credits.order(transaction_date: :desc)
+    end
+
+    def cash_transaction_types
+      @cash_transaction_types ||= @legal_aid_application.cash_transactions.credits.pluck(:name).uniq
     end
   end
 end
--- app/controllers/providers/outgoings_summary_controller.rb
+++ app/controllers/providers/outgoings_summary_controller.rb
@@ -2,6 +2,7 @@ module Providers
   class OutgoingsSummaryController < ProviderBaseController
     def index
       @bank_transactions = bank_transactions
+      @cash_transactions = cash_transactions
     end
 
     def create
@@ -9,6 +10,7 @@ module Providers
 
       if uncategorized_transactions?
         @bank_transactions = bank_transactions
+        @cash_transactions = cash_transactions
         render :index
       else
         go_forward
@@ -26,6 +28,15 @@ module Providers
                             .debit
                             .order(happened_at: :desc)
                             .by_type
+    end
+
+    def cash_transactions
+      cash_transaction_types
+      @legal_aid_application.cash_transactions.debits.order(transaction_date: :desc)
+    end
+
+    def cash_transaction_types
+      @cash_transaction_types ||= @legal_aid_application.cash_transactions.debits.pluck(:name).uniq
     end
   end
 end
