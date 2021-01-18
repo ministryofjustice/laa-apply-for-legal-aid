8aa0a4817e943a1f46f0bbe27d1eb1eca96d5795
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-01-18 16:03:00 +0000

    Update the proceeding types service test
    As per PR review feedback, test multiple proceedings are being added. Also, test the legacy way of replacing existing proceeding type with the new one on
    the application.

--- spec/services/proceeding_types_service_spec.rb
+++ spec/services/proceeding_types_service_spec.rb
@@ -22,6 +22,53 @@ RSpec.describe ProceedingTypesService do
         expect(legal_aid_application.proceeding_types).to include(proceeding_type)
       end
 
+      context 'proceeding types already exist' do
+        context 'Setting.allow_multiple_proceedings? returns true' do
+          let(:legal_aid_application) { create :legal_aid_application, :with_applicant, proceeding_types: [proceeding_type] }
+          let(:proceeding_type2) { create :proceeding_type }
+          let(:params) do
+            {
+              proceeding_type_id: proceeding_type2.id,
+              scope_limitation: scope_limitation
+            }
+          end
+
+          before do
+            allow(Setting).to receive(:allow_multiple_proceedings?).and_return(true)
+          end
+
+          it 'adds another proceeding type' do
+            subject.add(**params)
+            expect(legal_aid_application.proceeding_types).to eq([proceeding_type, proceeding_type2])
+          end
+        end
+
+        context 'Setting.allow_multiple_proceedings? returns false' do
+          let(:legal_aid_application) { create :legal_aid_application, :with_applicant, proceeding_types: [proceeding_type1] }
+          let(:proceeding_type1) { create :proceeding_type }
+          let(:proceeding_type2) { create :proceeding_type }
+          let!(:default_substantive_scope_limitation) do
+            create :scope_limitation, :substantive_default, joined_proceeding_type: proceeding_type2, meaning: 'Default substantive SL'
+          end
+          let(:params) do
+            {
+              proceeding_type_id: proceeding_type2.id,
+              scope_limitation: scope_limitation
+            }
+          end
+
+          before do
+            allow(Setting).to receive(:allow_multiple_proceedings?).and_return(false)
+          end
+
+          it 'replaces the application proceeding type' do
+            expect(legal_aid_application.proceeding_types).to eq([proceeding_type1])
+            subject.add(**params)
+            expect(legal_aid_application.proceeding_types).to eq([proceeding_type2])
+          end
+        end
+      end
+
       it 'calls AddScopeLimitationService' do
         expect(AddScopeLimitationService).to receive(:call).with(legal_aid_application, scope_limitation)
         subject.add(**params)
