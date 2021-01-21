c2c39a60b387e953347e4efddb91c02d8296057d
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-01-21 17:09:29 +0000

    Add transaction period helper spec
    This ensures we know what date format this helper produces

--- spec/helpers/transaction_period_helper_spec.rb
+++ spec/helpers/transaction_period_helper_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe TransactionPeriodHelper, type: :helper do
-  let(:application) { create :legal_aid_application, :with_transaction_period }
+  let(:application) { create :legal_aid_application, transaction_period_start_on: 'Thu, 21 Oct 2020', transaction_period_finish_on: 'Thu, 21 Jan 2021' }
 
   describe '#date_from(application)' do
     it 'returns a valid date' do
-      expect(helper.date_from(application)).to eq '29 09 2020'
+      expect(helper.date_from(application)).to eq 'October 21, 2020'
     end
   end
 
   describe '#date_to(application)' do
     it 'returns a valid date' do
-      expect(helper.date_to(application)).to eq '29 09 2020'
+      expect(helper.date_to(application)).to eq 'January 21, 2021'
     end
   end
 end
