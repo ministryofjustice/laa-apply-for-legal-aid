3f798a41278ef824e7797febef341e91076dcedb
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-01-21 10:16:53 +0000

    Shift the citizen completed means mailer to a later stage
    We send the citizen completed means to the provider before the appliction has completed submission as in completed the bank transaction analyser job
    
    This means the provider won't think to continue on with an application with the wrong state

--- spec/jobs/bank_transactions_analyser_job_spec.rb
+++ spec/jobs/bank_transactions_analyser_job_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe BankTransactionsAnalyserJob, type: :job do
   let(:legal_aid_application) { create :legal_aid_application, :with_non_passported_state_machine, :analysing_bank_transactions }
+  let(:provider_email_service) { double(ProviderEmailService, send_email: true) }
+
+  before do
+    allow(StateBenefitAnalyserService).to receive(:call).with(legal_aid_application)
+    allow(ProviderEmailService).to receive(:new).with(legal_aid_application).and_return(provider_email_service)
+  end
+
   subject { described_class.perform_now(legal_aid_application) }
 
   describe '#perform' do
@@ -14,6 +21,25 @@ RSpec.describe BankTransactionsAnalyserJob, type: :job do
       allow(StateBenefitAnalyserService).to receive(:call).with(legal_aid_application)
       subject
       expect(legal_aid_application.reload.state).to eq('provider_assessing_means')
+    end
+
+    context 'transactions analysis' do
+      context 'pending' do
+        before do
+          allow_any_instance_of(NonPassportedStateMachine).to receive(:aasm_state).and_return('analysing_bank_transactions')
+        end
+
+        it 'does not call ProviderEmailService' do
+          expect(provider_email_service).not_to receive(:send_email)
+          subject
+        end
+      end
+      context 'complete' do
+        it 'calls ProviderEmailService' do
+          expect(provider_email_service).to receive(:send_email)
+          subject
+        end
+      end
     end
   end
 end
