af316847bf1be7d341488a83b8c5bda0f2a292bf
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-01-22 14:14:07 +0000

    Fix rubocop-rails Rails/ShortI18n: Use t instead of translate
    Use t instead of translate for I18n

--- app/lib/govuk_elements_form_builder/form_builder.rb
+++ app/lib/govuk_elements_form_builder/form_builder.rb
@@ -246,7 +246,7 @@ module GovukElementsFormBuilder
     def hint_message(attribute, options)
       return nil if options[:collection]
 
-      options[:hint].presence || I18n.translate("helpers.hint.#{@object_name}.#{attribute}", default: nil)
+      options[:hint].presence || I18n.t("helpers.hint.#{@object_name}.#{attribute}", default: nil)
     end
 
     def hint_tag(attribute, options)
@@ -266,7 +266,7 @@ module GovukElementsFormBuilder
       return if message.blank?
 
       content_tag(:span, class: 'govuk-error-message', id: "#{attribute}-error") do
-        content_tag(:span, I18n.translate('helpers.accessibility.error'), class: 'govuk-visually-hidden') + message
+        content_tag(:span, I18n.t('helpers.accessibility.error'), class: 'govuk-visually-hidden') + message
       end
     end
 
