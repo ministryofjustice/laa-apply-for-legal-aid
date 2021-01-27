844d45837c5ecc75a60daa19ab9f542c682c22ad
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-01-27 10:44:51 +0000

    Use seperate form for the confirmation page
    Place the used delegated functions email reminder into the update method of the confirm delegated functions date controller
    
    Update tests

--- app/controllers/providers/delegated_functions_dates_controller.rb
+++ app/controllers/providers/delegated_functions_dates_controller.rb
@@ -3,44 +3,37 @@ module Providers
     include PreDWPCheckVisible
 
     def show
-      @form = LegalAidApplications::UsedDelegatedFunctionsForm.new(model: legal_aid_application)
+      @form = LegalAidApplications::ConfirmDelegatedFunctionsDateForm.new(model: legal_aid_application)
     end
 
-    # def update
-    #   @form = LegalAidApplications::UsedDelegatedFunctionsForm.new(form_params)
-    #   if save_continue_or_draft_and_update_scope_limitations
-    #     submit_application_reminder
-    #   else
-    #     render :show
-    #   end
-    # end
+    def update
+      @form = LegalAidApplications::ConfirmDelegatedFunctionsDateForm.new(form_params)
+      if save_continue_or_draft(@form)
+        submit_application_reminder
+      else
+        render :show
+      end
+    end
 
-    # private
+    private
 
-    # def submit_application_reminder
-    #   return if legal_aid_application.awaiting_applicant?
-    #   return if legal_aid_application.applicant_entering_means?
-    #   return unless @form.model.used_delegated_functions?
+    def submit_application_reminder
+      return if legal_aid_application.awaiting_applicant?
+      return if legal_aid_application.applicant_entering_means?
+      return unless @form.model.used_delegated_functions?
 
-    #   SubmitApplicationReminderService.new(legal_aid_application).send_email
-    # end
+      SubmitApplicationReminderService.new(legal_aid_application).send_email
+    end
 
-    # def save_continue_or_draft_and_update_scope_limitations
-    #   return false unless save_continue_or_draft(@form)
-
-    #   AddScopeLimitationService.call(legal_aid_application, :delegated) if @form.model.used_delegated_functions?
-    #   true
-    # end
-
-    # def form_params
-    #   merge_with_model(legal_aid_application) do
-    #     params.require(:legal_aid_application).permit(
-    #       :used_delegated_functions_year,
-    #       :used_delegated_functions_month,
-    #       :used_delegated_functions_day,
-    #       :used_delegated_functions
-    #     )
-    #   end
-    # end
+    def form_params
+      merge_with_model(legal_aid_application) do
+        params.require(:legal_aid_application).permit(
+          :used_delegated_functions_year,
+          :used_delegated_functions_month,
+          :used_delegated_functions_day,
+          :confirm_delegated_functions_date
+        )
+      end
+    end
   end
 end
