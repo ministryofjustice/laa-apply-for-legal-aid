0588d06ead6e7b37f6896a0adac49fe4ff24058f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-02-02 12:05:13 +0000

    Update error view handling
    We were passing through the params which could be exploited
    We now use strong params to get the ID and pass it as a variable

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,17 @@
 class ErrorsController < ApplicationController
   before_action :update_locale
-  def show; end
+  def show
+    @error = error_param
+  end
 
   def page_not_found
     update_locale
     redirect_to error_path(:page_not_found, default_url_options)
+  end
+
+  private
+
+  def error_param
+    params.require(:id)
   end
 end
