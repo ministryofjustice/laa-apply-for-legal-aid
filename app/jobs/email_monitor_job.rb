6690d651ccbf208444fa248ba48a0d0255143287
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-03-01 13:15:55 +0000

    AP-2011 remove unused constant

--- app/jobs/email_monitor_job.rb
+++ app/jobs/email_monitor_job.rb
@@ -2,7 +2,6 @@ class EmailMonitorJob < ApplicationJob
   queue_as :default
 
   DEFAULT_DELAY = 2.minutes.freeze
-  END_OF_TIME = Time.zone.local(9999, 12, 31, 23, 59, 59)
 
   def perform
     ScheduledMailing.monitored.each { |mail| GovukEmails::Monitor.call(mail.id) }
