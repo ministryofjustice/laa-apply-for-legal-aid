be4b32bea561b5d7bdab65580a416f02c9df58af
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-03-02 12:46:10 +0000

    formatting and linting

--- app/webpack/src/modules/proceedings.js
+++ app/webpack/src/modules/proceedings.js
@@ -1,44 +1,43 @@
 import axios from 'axios';
 import { hide, show } from '../helpers';
-var typingTimer;                //timer identifier
-var doneTypingInterval = 400;  //time in ms, 1.5 second for example
-var ariaText;
+const doneTypingInterval = 400; // time in ms, 1.5 second for example
+let typingTimer; // timer identifier
+let ariaText;
 
-async function searchResults(search_term){
-  let url = '/v1/proceeding_types?search_term=' + search_term
-  const response = await axios.get(url)
-  return response.data
+async function searchResults (searchTerm) {
+  const url = '/v1/proceeding_types?search_term=' + searchTerm;
+  const response = await axios.get(url);
+  return response.data;
 }
 
 function doneTyping () {
-  document.querySelector('#screen-reader-messages').innerHTML = ariaText
-  const inputText = document.querySelector("#proceeding-search-input").value.trim()
+  document.querySelector('#screen-reader-messages').innerHTML = ariaText;
+  const inputText = document.querySelector('#proceeding-search-input').value.trim();
   if (inputText.length > 2) {
-    hideProceeedingsItems()
+    hideProceeedingsItems();
     searchResults(inputText).then(results => {
-      showResults(results, inputText)
-    })
+      showResults(results, inputText);
+    });
   }
 }
 
-function searchOnUserInput(searchInputBox) {
-
-  searchInputBox.addEventListener("keyup", (event) => {
+function searchOnUserInput (searchInputBox) {
+  searchInputBox.addEventListener('keyup', (event) => {
     clearTimeout(typingTimer);
     typingTimer = setTimeout(doneTyping, doneTypingInterval);
   })
 
-  searchInputBox.addEventListener("keydown", () => {
+  searchInputBox.addEventListener('keydown', () => {
     clearTimeout(typingTimer);
   })
 
   document.querySelector('#clear-proceeding-search').addEventListener('click', () => {
-    searchInputBox.value = ""
-    hideProceeedingsItems()
+    searchInputBox.value = '';
+    hideProceeedingsItems();
   })
 
   // TODO: remove this when the multiple proceedings feature flag is removed
-  let proceedingItems = document.querySelectorAll('.proceeding-item')
+  const proceedingItems = document.querySelectorAll('.proceeding-item')
   if (proceedingItems.length) {
     proceedingItems.forEach((item) => {
       item.addEventListener('mouseover', () => { item.classList.add('hover') })
@@ -46,71 +45,70 @@ function searchOnUserInput(searchInputBox) {
       item.addEventListener('click', () => { return submitForm(item) })
       item.addEventListener('keydown', (e) => {
         if (e.which === 13) {
-          return submitForm(item)
+          return submitForm(item);
         }
       })
     })
   }
 }
 
-function showResults(results, inputText) {
+function showResults (results, inputText) {
   if (results.length > 0) {
-    const codes = results.map(obj => obj["code"])
+    const codes = results.map(obj => obj.code);
     codes.forEach(code => {
       // const element = $('#' + code)
-      var element = document.getElementById(code)
+      const element = document.getElementById(code);
 
       // We want to highlight anything text in <main> or <span> tags that
       // matches the user's search criteria
-      const span = element.querySelector("span")
+      const span = element.querySelector('span');
       // remove h3 when the multiple proceedings feature flag is removed
-      const main = element.querySelectorAll("h3, label")[0]
+      const main = element.querySelectorAll('h3, label')[0];
 
       // Remove any existing highlighting
-      main.innerHTML = main.innerHTML.replace(/<mark class="highlight">(.*)<\/mark>/, '$1')
-      span.innerHTML = span.innerHTML.replace(/<mark class="highlight">(.*)<\/mark>/, '$1')
+      main.innerHTML = main.innerHTML.replace(/<mark class="highlight">(.*)<\/mark>/, '$1');
+      span.innerHTML = span.innerHTML.replace(/<mark class="highlight">(.*)<\/mark>/, '$1');
 
       // Highlight any text that matches the user's input
-      const regExp = RegExp(inputText, 'gi')
-      main.innerHTML = main.innerHTML.replace(regExp, '<mark class="highlight">$&</mark>')
-      span.innerHTML = span.innerHTML.replace(regExp, '<mark class="highlight">$&</mark>')
+      const regExp = RegExp(inputText, 'gi');
+      main.innerHTML = main.innerHTML.replace(regExp, '<mark class="highlight">$&</mark>');
+      span.innerHTML = span.innerHTML.replace(regExp, '<mark class="highlight">$&</mark>');
 
       // show hidden proceedings item
-      show(element)
-      hide(document.querySelector(".no-proceeding-items"))
+      show(element);
+      hide(document.querySelector('.no-proceeding-items'));
       // the below alerts screen reader users that results appeared on the page
       ariaText = codes.length + ' ' + pluralize(codes.length, 'match', 'matches') + ' found for ' + inputText + ', use tab to move through options';
-      })
-    }
-  else {
-    show(document.querySelector(".no-proceeding-items"))
-    ariaText = 'No results found matching ' + inputText
+    })
+  } else {
+    show(document.querySelector('.no-proceeding-items'));
+    ariaText = 'No results found matching ' + inputText;
   }
 }
 
-function hideProceeedingsItems() {
-  document.querySelectorAll(".proceeding-item").forEach((item) => {
-    hide(item)
+function hideProceeedingsItems () {
+  document.querySelectorAll('.proceeding-item').forEach((item) => {
+    hide(item);
   })
-  hide(document.querySelector(".no-proceeding-items"))
+  hide(document.querySelector('.no-proceeding-items'));
 }
 
 const pluralize = (val, word, plural = word + 's') => {
   const _pluralize = (num, word, plural = word + 's') =>
-      [1, -1].includes(Number(num)) ? word : plural;
+    [1, -1].includes(Number(num)) ? word : plural;
   if (typeof val === 'object') return (num, word) => _pluralize(num, word, val[word]);
   return _pluralize(val, word, plural);
-};
+}
 
-let submitForm = proceedingItem => {
-  let form = proceedingItem.querySelector('form')
+const submitForm = proceedingItem => {
+  const form = proceedingItem.querySelector('form');
   if (form) { form.submit() }
   return false
 }
 
 document.addEventListener('DOMContentLoaded', event => {
-  const searchInputBox = document.querySelector("#proceeding-search-input")
-  if (searchInputBox) {searchOnUserInput(searchInputBox)}
-});
+  const searchInputBox = document.querySelector('#proceeding-search-input');
+  if (searchInputBox) { searchOnUserInput(searchInputBox) }
+})
 
-export {searchResults, searchOnUserInput, showResults};
+export { searchResults, searchOnUserInput, showResults }
--- spec/javascript/modules/proceedings.spec.js
+++ spec/javascript/modules/proceedings.spec.js
@@ -3,35 +3,35 @@ import * as ProceedingsTypes from 'proceedings';
 
 jest.mock('axios');
 
-beforeEach(() => jest.resetAllMocks() );
+beforeEach(() => jest.resetAllMocks());
 
 afterEach(() => {
   jest.restoreAllMocks();
-});
+})
 
 describe('ProceedingsTypes.searchResults', () => {
-  let searchTerm = 'family'
+  const searchTerm = 'family';
 
   beforeEach(() => {
     axios.get.mockResolvedValueOnce({ data: { code: 'data' } })
-    ProceedingsTypes.searchResults(searchTerm)
+    ProceedingsTypes.searchResults(searchTerm);
   })
 
   it('calls axios.get once', done => {
-    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledTimes(1);
     done();
   })
 
   it('polls the correct endpoint', done => {
-    let endpoint = '/v1/proceeding_types?search_term=' + searchTerm
-    expect(axios.get.mock.calls).toEqual([[endpoint]])
-    done()
+    let endpoint = '/v1/proceeding_types?search_term=' + searchTerm;
+    expect(axios.get.mock.calls).toEqual([[endpoint]]);
+    done();
   })
 
   it('returns correct values', () => {
     axios.get.mockResolvedValueOnce({ data: { code: 'data' } })
     return ProceedingsTypes.searchResults().then((result) => {
-      expect(result).toEqual({ code: 'data' })
+      expect(result).toEqual({ code: 'data' });
     })
   })
 })
