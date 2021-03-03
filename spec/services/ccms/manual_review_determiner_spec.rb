cb84b524ee9344b56286fbe75add18d4ecbb79d4
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2021-03-03 21:13:17 +0000

    AP-2035 Remove cash payments flag - misc specs
    Remove references to V2 CFE submissions from a few specs that were
    missed originally.

--- spec/services/ccms/manual_review_determiner_spec.rb
+++ spec/services/ccms/manual_review_determiner_spec.rb
@@ -8,190 +8,93 @@ module CCMS
     subject { described_class.call(legal_aid_application) }
 
     describe '.call' do
-      context 'cfe result v2' do
-        context 'assessment not yet carried out on legal aid application' do
-          let(:legal_aid_application) { create :legal_aid_application }
-          it 'raises an error' do
-            expect { subject }.to raise_error RuntimeError, 'Unable to determine whether Manual review is required before means assessment'
+      context 'assessment not yet carried out on legal aid application' do
+        let(:legal_aid_application) { create :legal_aid_application }
+        it 'raises an error' do
+          expect { subject }.to raise_error RuntimeError, 'Unable to determine whether Manual review is required before means assessment'
+        end
+      end
+
+      context 'manual review setting true' do
+        before { setting.update! manually_review_all_cases: true }
+
+        context 'passported, no contrib, no_restrictions' do
+          let(:legal_aid_application) { create :legal_aid_application, :with_positive_benefit_check_result }
+          let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
+
+          it 'returns false' do
+            expect(subject).to be false
           end
         end
 
-        context 'manual review setting true' do
-          before { setting.update! manually_review_all_cases: true }
+        context 'non-passported, no contrib' do
+          let(:legal_aid_application) { create :legal_aid_application, :with_negative_benefit_check_result }
+          let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
 
-          context 'passported, no contrib, no_restrictions' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_positive_benefit_check_result }
-            let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
-
-            it 'returns false' do
-              expect(subject).to be false
-            end
-          end
-
-          context 'non-passported, no contrib' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_negative_benefit_check_result }
-            let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
-
-            it 'returns true' do
-              expect(subject).to be true
-            end
-          end
-        end
-
-        context 'manual review setting false' do
-          before { setting.update! manually_review_all_cases: false }
-
-          context 'passported' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_positive_benefit_check_result, has_restrictions: true }
-
-            context 'contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, :with_capital_contribution_required, submission: cfe_submission }
-
-              it 'returns true' do
-                expect(subject).to be true
-              end
-            end
-
-            context 'no contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
-
-              context 'restrictions' do
-                it 'returns false' do
-                  expect(subject).to be true
-                end
-              end
-
-              context 'no restrictions' do
-                before { legal_aid_application.update! has_restrictions: false }
-
-                it 'returns false' do
-                  expect(subject).to be false
-                end
-              end
-            end
-          end
-
-          context 'non-passported' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_negative_benefit_check_result, has_restrictions: true }
-
-            context 'contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, :with_capital_contribution_required, submission: cfe_submission }
-
-              it 'returns true' do
-                expect(subject).to be true
-              end
-            end
-
-            context 'no contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
-
-              context 'restrictions' do
-                it 'returns false' do
-                  expect(subject).to be true
-                end
-              end
-
-              context 'no restrictions' do
-                before { legal_aid_application.update! has_restrictions: false }
-
-                it 'returns false' do
-                  expect(subject).to be false
-                end
-              end
-            end
+          it 'returns true' do
+            expect(subject).to be true
           end
         end
       end
 
-      context 'cfe result v3' do
-        context 'assessment not yet carried out on legal aid application' do
-          let(:legal_aid_application) { create :legal_aid_application }
-          it 'raises an error' do
-            expect { subject }.to raise_error RuntimeError, 'Unable to determine whether Manual review is required before means assessment'
-          end
-        end
+      context 'manual review setting false' do
+        before { setting.update! manually_review_all_cases: false }
 
-        context 'manual review setting true' do
-          before { setting.update! manually_review_all_cases: true }
+        context 'passported' do
+          let(:legal_aid_application) { create :legal_aid_application, :with_positive_benefit_check_result, has_restrictions: true }
 
-          context 'passported, no contrib, no_restrictions' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_positive_benefit_check_result }
-            let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
-
-            it 'returns false' do
-              expect(subject).to be false
-            end
-          end
-
-          context 'non-passported, no contrib' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_negative_benefit_check_result }
-            let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
+          context 'contribution' do
+            let!(:cfe_result) { create :cfe_v3_result, :with_capital_contribution_required, submission: cfe_submission }
 
             it 'returns true' do
               expect(subject).to be true
             end
           end
-        end
 
-        context 'manual review setting false' do
-          before { setting.update! manually_review_all_cases: false }
+          context 'no contribution' do
+            let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
 
-          context 'passported' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_positive_benefit_check_result, has_restrictions: true }
-
-            context 'contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, :with_capital_contribution_required, submission: cfe_submission }
-
-              it 'returns true' do
+            context 'restrictions' do
+              it 'returns false' do
                 expect(subject).to be true
               end
             end
 
-            context 'no contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
+            context 'no restrictions' do
+              before { legal_aid_application.update! has_restrictions: false }
 
-              context 'restrictions' do
-                it 'returns false' do
-                  expect(subject).to be true
-                end
-              end
-
-              context 'no restrictions' do
-                before { legal_aid_application.update! has_restrictions: false }
-
-                it 'returns false' do
-                  expect(subject).to be false
-                end
+              it 'returns false' do
+                expect(subject).to be false
               end
             end
           end
+        end
 
-          context 'non-passported' do
-            let(:legal_aid_application) { create :legal_aid_application, :with_negative_benefit_check_result, has_restrictions: true }
+        context 'non-passported' do
+          let(:legal_aid_application) { create :legal_aid_application, :with_negative_benefit_check_result, has_restrictions: true }
 
-            context 'contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, :with_capital_contribution_required, submission: cfe_submission }
+          context 'contribution' do
+            let!(:cfe_result) { create :cfe_v3_result, :with_capital_contribution_required, submission: cfe_submission }
 
-              it 'returns true' do
+            it 'returns true' do
+              expect(subject).to be true
+            end
+          end
+
+          context 'no contribution' do
+            let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
+
+            context 'restrictions' do
+              it 'returns false' do
                 expect(subject).to be true
               end
             end
 
-            context 'no contribution' do
-              let!(:cfe_result) { create :cfe_v3_result, submission: cfe_submission }
+            context 'no restrictions' do
+              before { legal_aid_application.update! has_restrictions: false }
 
-              context 'restrictions' do
-                it 'returns false' do
-                  expect(subject).to be true
-                end
-              end
-
-              context 'no restrictions' do
-                before { legal_aid_application.update! has_restrictions: false }
-
-                it 'returns false' do
-                  expect(subject).to be false
-                end
+              it 'returns false' do
+                expect(subject).to be false
               end
             end
           end
