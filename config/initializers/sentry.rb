71204acb3f073f432afb875beea4b998571b4d51
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-03-04 09:34:50 +0000

    Reimplement sentry data scrubbing
    This removes PII (personal identifiable information) before sending to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -9,6 +9,10 @@ if %w[production].include?(Rails.env) && ENV['SENTRY_DSN'].present?
     filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters.map(&:to_s))
 
     config.before_send = ->(event, _hint) {
+      event.extra[:sidekiq][:job]['args'].first['arguments'] = [] if event.extra.dig(:sidekiq, :job, 'args')
+
+      event.extra[:sidekiq][:jobstr] = {} if event.extra.dig(:sidekiq, :jobstr)
+
       event.request.data = filter.filter(event.request&.data || []) if event.request&.data
 
       event
