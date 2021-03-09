1e1f2c1481246e022cd853a1e0ed94cbabfeb24d
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-03-09 20:22:01 +0000

    AP-2046 rubocop

--- db/migrate/20210309195130_add_unique_idex_to_application_proceeding_types_scope_limitations.rb
+++ db/migrate/20210309195130_add_unique_idex_to_application_proceeding_types_scope_limitations.rb
@@ -2,7 +2,7 @@ class AddUniqueIdexToApplicationProceedingTypesScopeLimitations < ActiveRecord::
   def up
     remove_index :application_proceeding_types_scope_limitations, name: :index_application_proceeding_scope_limitation
     add_index :application_proceeding_types_scope_limitations,
-              [:application_proceeding_type_id, :scope_limitation_id],
+              %i[application_proceeding_type_id scope_limitation_id],
               name: :index_application_proceeding_scope_limitation,
               unique: true
   end
@@ -10,7 +10,7 @@ class AddUniqueIdexToApplicationProceedingTypesScopeLimitations < ActiveRecord::
   def down
     remove_index :application_proceeding_types_scope_limitations, name: :index_application_proceeding_scope_limitation
     add_index :application_proceeding_types_scope_limitations,
-              [:application_proceeding_type_id, :scope_limitation_id],
+              %i[application_proceeding_type_id scope_limitation_id],
               name: :index_application_proceeding_scope_limitation
   end
 end
