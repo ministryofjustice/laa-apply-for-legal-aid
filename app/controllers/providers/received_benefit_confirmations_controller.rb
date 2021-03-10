e728d23d59f6e830ef3376fb1bad074f7ad9fc43
Eloise Tait <eloisetait@yahoo.com>
2021-03-10 17:51:55 +0000

    WIP

--- app/controllers/providers/received_benefit_confirmations_controller.rb
+++ app/controllers/providers/received_benefit_confirmations_controller.rb
@@ -1,5 +1,50 @@
 module Providers
   class ReceivedBenefitConfirmationsController < ProviderBaseController
-    def show; end
+    def show
+      @form = Providers::ReceivedBenefitConfirmationForm.new(model: dwp_override)
+    end
+
+    def update
+      @form = Providers::ReceivedBenefitConfirmationForm.new(form_params)
+      render :show unless save_continue_or_draft(@form)
+    end
+
+    private
+
+    def form_params
+      merge_with_model(dwp_override) do
+        params.require(:dwp_override).permit(:passporting_benefit)
+      end
+    end
+
+    def dwp_override
+      @dwp_override ||= legal_aid_application.dwp_override || legal_aid_application.create_dwp_override!
+    end
   end
 end
+
+# module Providers
+#   class PolicyDisregardsController < ProviderBaseController
+#     def show
+#       @form = Providers::PolicyDisregardsForm.new(model: policy_disregards)
+#     end
+
+#     def update
+#       @form = Providers::PolicyDisregardsForm.new(form_params)
+#       render :show unless save_continue_or_draft(@form)
+#     end
+
+#     private
+
+#     def policy_disregards
+#       @policy_disregards ||= legal_aid_application.policy_disregards || legal_aid_application.create_policy_disregards!
+#     end
+
+#     def form_params
+#       merge_with_model(policy_disregards) do
+#         attrs = Providers::PolicyDisregardsForm::CHECK_BOXES_ATTRIBUTES
+#         params[:policy_disregards].permit(*attrs)
+#       end
+#     end
+#   end
+# end
--- app/services/flow/flows/provider_dwp_override.rb
+++ app/services/flow/flows/provider_dwp_override.rb
@@ -4,6 +4,7 @@ module Flow
       STEPS = {
         confirm_dwp_non_passported_applications: {
           path: ->(application) { urls.providers_legal_aid_application_confirm_dwp_non_passported_applications_path(application) },
+<<<<<<< HEAD
           forward: ->(application, confirm_dwp_non_passported) do
             if confirm_dwp_non_passported
               application.change_state_machine_type('NonPassportedStateMachine')
@@ -16,6 +17,19 @@ module Flow
         },
         check_client_details: {
           path: ->(application) { urls.providers_legal_aid_application_check_client_details_path(application) }
+=======
+          forward: :received_benefit_confirmation
+          # uncomment the below to redirect to next pages being developed and remove line 7
+          # forward: ->(_application, confirm_dwp_non_passported) { confirm_dwp_non_passported ? :check_benefits : :check_client_details }
+        },
+        # check_client_details: {
+        #   path: ->(application) { urls.providers_legal_aid_application_check_client_details_path(application) },
+        #   forward: :received_benefit_confirmation
+        # },
+        received_benefit_confirmation: {
+          path: ->(application) { urls.providers_legal_aid_application_received_benefit_confirmation_path(application) },
+          forward: :check_benefits
+>>>>>>> 44869594 (WIP)
         }
       }.freeze
     end
