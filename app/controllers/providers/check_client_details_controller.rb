cd837079e3637b1bca1420cb217e1ca50d201d62
Alexander Swann <alex_swann@hotmail.co.uk>
2021-03-12 19:55:29 +0000

    AP-2083: Add the confirm client details page for the DWP Override flow

--- app/controllers/providers/check_client_details_controller.rb
+++ app/controllers/providers/check_client_details_controller.rb
@@ -1,5 +1,31 @@
 module Providers
   class CheckClientDetailsController < ProviderBaseController
-    def show; end
+    def show
+      applicant
+      @form = Providers::CheckClientDetailsForm.new
+    end
+
+    def update
+      return continue_or_draft if draft_selected?
+
+      applicant
+      @form = Providers::CheckClientDetailsForm.new(form_params)
+
+      if @form.valid?
+        go_forward(form_params[:check_client_details] == 'true')
+      else
+        render :show
+      end
+    end
+
+    private
+
+    def applicant
+      @applicant ||= legal_aid_application.applicant || legal_aid_application.build_applicant
+    end
+
+    def form_params
+      params.require(:providers_check_client_details_form).permit(:check_client_details)
+    end
   end
 end
--- spec/requests/providers/check_client_details_spec.rb
+++ spec/requests/providers/check_client_details_spec.rb
@@ -22,8 +22,68 @@ RSpec.describe Providers::CheckClientDetailsController, type: :request do
         expect(response).to be_successful
       end
 
-      it 'displays the correct page' do
-        expect(unescaped_response_body).to include('check_client_detail page under construction')
+      it 'displays the applicant\'s full name' do
+        full_name = "#{application.applicant.first_name} #{application.applicant.last_name}"
+        expect(unescaped_response_body).to include(full_name)
+      end
+
+      it 'displays the applicant date of birth in the required format' do
+        dob_formatted = application.applicant.date_of_birth.strftime('%e %B %Y')
+        expect(unescaped_response_body).to include(dob_formatted)
+      end
+
+      it 'displays the applicant national insurance number with 2 digit spacing' do
+        ni_number = application.applicant.national_insurance_number.gsub(/(.{2})(?=.)/, '\1 \2')
+        expect(unescaped_response_body).to include(ni_number)
+      end
+    end
+  end
+
+  describe 'PATCH /providers/applications/:legal_aid_application_id/check_client_details' do
+    subject { patch "/providers/applications/#{application_id}/check_client_details", params: params }
+
+    before do
+      login_as application.provider
+      subject
+    end
+
+    context 'correct client details' do
+      let(:params) { { providers_check_client_details_form: { check_client_details: 'true' } } }
+
+      context 'delegated functions' do
+        let(:application) do
+          create :legal_aid_application,
+                 :with_proceeding_types,
+                 :with_applicant_and_address,
+                 used_delegated_functions: true
+        end
+
+        it 'continue to the substantive applications page' do
+          expect(response).to redirect_to(providers_legal_aid_application_substantive_application_path(application))
+        end
+      end
+
+      context 'no delegated functions' do
+        it 'continue to the capital introductions page' do
+          expect(response).to redirect_to(providers_legal_aid_application_capital_introduction_path(application))
+        end
+      end
+    end
+
+    context 'incorrect client details' do
+      let(:params) { { providers_check_client_details_form: { check_client_details: 'false' } } }
+
+      it 'continue to the applicant details page' do
+        expect(response).to redirect_to(providers_legal_aid_application_applicant_details_path(application))
+      end
+    end
+
+    context 'validation error' do
+      let(:params) { { providers_check_client_details_form: { check_client_details: nil } } }
+
+      it 'displays an error if nothing selected' do
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include('Select if your client&#39;s details are correct or not')
       end
     end
   end
