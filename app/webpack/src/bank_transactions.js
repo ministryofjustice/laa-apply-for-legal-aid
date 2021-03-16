d686af359a5e89526e26993654788b8697b5bd98
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-03-16 17:20:07 +0000

    remove jquery from bank_trasactions.js
    remove jquery, replace slide animation with css classes, add slideUp helper function

--- app/webpack/src/bank_transactions.js
+++ app/webpack/src/bank_transactions.js
@@ -1,21 +1,26 @@
-$(document).ready(function(){
-    document.body.addEventListener('ajax:success', function(event) {
-      if (!event.srcElement.classList.contains('remote-remove-transaction')) return
-      var row = $(event.srcElement).parents("tr");
-      row.css("height",row.height()); //we fix the row height to stop it collapsing straight away
-      row.children("td").css("display","none"); //we remove the row innards to allow the row to collapse to zero height
-      row.slideUp(); //invoke collapse
+import { slideUp } from './helpers'
 
-      if (!row.siblings("tr").filter(function() {return $(this).css('display') !== 'none';}).length) { //there are no remaining siblings - all rows have been removed
-        row.parents("table").css("height",row.parents("table").height());
-        row.parents("table").children().css("display","none");
-        row.parents("table").slideUp();
+document.addEventListener('DOMContentLoaded', event => {
+    document.body.addEventListener('ajax:success', function(event) {
+      event.preventDefault()
+      const elem = event.target
+      if (!elem.classList.contains('remote-remove-transaction')) return
+      const row = elem.closest('tr');
+      slideUp(row)
+
+      const table = row.closest('table')
+      const rowsRemaining = Array.prototype.filter.call(row.parentElement.querySelectorAll("tr"), (row) => {
+          return row.style.display !== 'none' || !row.classList.contains('slide');
+      }).length
+
+      if (rowsRemaining === 1) { // all rows have been removed except just hidden
+        slideUp(table);
       }
     })
 
     document.body.addEventListener('ajax:error', function(event){
-      if (!event.srcElement.classList.contains('remote-remove-transaction')) return
-      console.log("Remove failed for unknown reason.  Page reloading.") //placeholder for any error message
-      location.reload(); //reloads page - because if it has failed, the row might've been removed behind the scenes
+      const elem = event.target;
+      if (!elem.classList.contains('remote-remove-transaction')) return
+      location.reload(); // reloads page - because if it has failed, the row may have been removed behind the scenes
     });
 });
--- app/webpack/src/helpers.js
+++ app/webpack/src/helpers.js
@@ -1,6 +1,12 @@
 export function hide(elem) { elem.style.display = "none" }
 export function show(elem) { elem.style.display = "block" }
 
+export function slideUp(elem) {
+  elem.classList.add('target')
+  elem.classList.add('slide')
+  setTimeout( () =>{ hide(elem)}, 500)
+}
+
 export function pluralize(val, word, plural = word + 's') {
     const _pluralize = (num, word, plural = word + 's') =>
         [1, -1].includes(Number(num)) ? word : plural;
