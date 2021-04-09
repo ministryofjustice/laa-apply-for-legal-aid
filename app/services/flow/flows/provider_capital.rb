75a67d0457555c49a1ea17c4e1f6de946041b226
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-04-09 14:10:44 +0100

    Update provider_capital_flow
    Ensure the non-passported route honours the multi-proc
    flow

--- app/services/flow/flows/provider_capital.rb
+++ app/services/flow/flows/provider_capital.rb
@@ -115,7 +115,7 @@ module Flow
         },
         capital_income_assessment_results: {
           path: ->(application) { urls.providers_legal_aid_application_capital_income_assessment_result_path(application) },
-          forward: :start_chances_of_successes
+          forward: ->(_) { Setting.allow_multiple_proceedings? ? :merits_task_list : :start_chances_of_successes }
         },
         means_reports: {
           path: ->(application) { urls.providers_legal_aid_application_means_report_path(application) }
