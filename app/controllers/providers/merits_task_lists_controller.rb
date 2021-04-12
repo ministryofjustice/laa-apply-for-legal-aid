5f913682968ef68171eed9b09971d7afcedba63e
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-04-12 16:05:54 +0100

    get merits task list data from Legal Framework service
    replace mock data with results from legal framework API

--- app/controllers/providers/merits_task_lists_controller.rb
+++ app/controllers/providers/merits_task_lists_controller.rb
@@ -1,39 +1,13 @@
 module Providers
   class MeritsTaskListsController < ProviderBaseController
     def show
-      @application_stages = application_stages
-      @proceeding_stages = proceeding_stages
+      @merits_tasks = merits_tasks
     end
 
     private
 
-    # TODO: replace these with response from LFA
-    def application_stages
-      { title: 'Case details', stages: [
-        { name: 'Latest incident details', status: 'Completed' },
-        { name: 'Opponent details', status: 'Completed' },
-        { name: 'Children involved in this application', status: 'Not started' },
-        { name: 'Statement of case', status: 'Not started' }
-      ] }
-    end
-
-    def proceeding_stages  # rubocop:disable Metrics/MethodLength:
-      [
-        { title: 'Non-molestation order', stages: [
-          { name: 'Chances of success', status: 'Not started' }
-        ] },
-        { title: 'Prohibited steps order',
-          stages: [
-            { name: 'Children covered by proceeding', status: 'Cannot start yet' },
-            { name: 'Attempts to settle', status: 'Not started' },
-            { name: 'Chances of success', status: 'Not started' }
-          ] },
-        { title: 'Child arrangements order (residence)', stages: [
-          { name: 'Children covered by proceeding', status: 'Cannot start yet' },
-          { name: 'Attempts to settle', status: 'Not started' },
-          { name: 'Chances of success', status: 'Not started' }
-        ] }
-      ]
+    def merits_tasks
+      YAML.safe_load(LegalFramework::MeritsTasksService.call(@legal_aid_application).serialized_data).tasks
     end
   end
 end
