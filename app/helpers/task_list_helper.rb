3156d37ba8456b6bb2d6afd9403dec8bb0fdc6a7
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-04-12 17:04:15 +0100

    add urls
    generate urls for merits pages

--- app/helpers/task_list_helper.rb
+++ app/helpers/task_list_helper.rb
@@ -5,8 +5,15 @@ module TaskListHelper
       'providers/merits_task_lists/task_list_item',
       name: name,
       status: status,
-      url: url,
+      url: _task_url(name),
       tag_class: tag_class
     )
+  end
+
+  private
+
+  def _task_url(name)
+    url_fragment = I18n.t("providers.merits_task_lists.task_list_item.urls.#{name}")
+    "providers_legal_aid_application_#{url_fragment}".to_sym
   end
 end
