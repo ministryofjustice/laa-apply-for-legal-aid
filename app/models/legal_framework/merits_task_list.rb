1d9b47a4b8a1b01d5c60b571b05930ec0608ec72
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-04-15 10:39:43 +0100

    get merits task list from db
    update merits task list controller to get the merits task lists from the db, or cal the merits task list service if the record doesnt exist

--- app/models/legal_framework/merits_task_list.rb
+++ app/models/legal_framework/merits_task_list.rb
@@ -1,5 +1,9 @@
 module LegalFramework
   class MeritsTaskList < ApplicationRecord
     belongs_to :legal_aid_application
+
+    def task_list
+      SerializableMeritsTaskList.new_from_serialized(serialized_data)
+    end
   end
 end
