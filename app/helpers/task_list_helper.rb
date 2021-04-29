7dfc11010b6aded00894b9552105e2f9ac293811
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-04-29 14:26:11 +0100

    Update the task list helper to return proceeding merits task url
    Update the task list helper to take in an application and ccms_code, from that it
    finds the appropriate application proceeding type to be used in the proceeding merits task url path
    
    e.g. chances of success path

--- app/helpers/task_list_helper.rb
+++ app/helpers/task_list_helper.rb
@@ -1,11 +1,11 @@
 module TaskListHelper
-  def task_list_item(name:, status:, ccms_code: nil)
+  def task_list_item(name:, status:, legal_aid_application: nil, ccms_code: nil)
     tag_class = status == 'completed' ? nil : 'govuk-tag--grey'
     render(
       'providers/merits_task_lists/task_list_item',
       name: name,
       status: status,
-      url: ccms_code ? proceeding_task_url(name, ccms_code) : _task_url(name),
+      url: ccms_code ? proceeding_task_url(name, legal_aid_application, ccms_code) : _task_url(name),
       proceeding_merits_task: ccms_code.present?,
       tag_class: tag_class
     )
@@ -17,20 +17,23 @@ module TaskListHelper
     "providers_legal_aid_application_#{url_fragment(name)}".to_sym
   end
 
-  def proceeding_task_url(name, ccms_code)
+  def proceeding_task_url(name, application, ccms_code)
     url = "providers_merits_task_list_#{url_fragment(name)}_path".to_sym
 
-    __send__(url, application_proceeding_type_id(ccms_code))
+    __send__(url, application_proceeding_type_id(application, ccms_code))
   end
 
   def url_fragment(name)
     I18n.t("providers.merits_task_lists.task_list_item.urls.#{name}")
   end
 
-  def application_proceeding_type_id(ccms_code)
-    return nil unless ccms_code
+  def application_proceeding_type_id(application, ccms_code)
+    return nil unless ccms_code && application
 
-    pt = ProceedingType.where(ccms_code: ccms_code).first
-    pt.application_proceeding_types.first.id
+    apt = application.application_proceeding_types.find do |x|
+      x.proceeding_type.ccms_code == ccms_code.to_s
+    end
+
+    apt.id
   end
 end
