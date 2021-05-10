9e0d22b202db8eca729ed49e9a1b962677224d83
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-05-10 16:55:16 +0100

    AP-2289 duplicate DF dates on application_proceeding_type

--- app/controllers/providers/limitations_controller.rb
+++ app/controllers/providers/limitations_controller.rb
@@ -3,11 +3,26 @@ module Providers
     include PreDWPCheckVisible
 
     def show
+      update_df_dates
       legal_aid_application.enter_applicant_details! unless legal_aid_application.entering_applicant_details?
     end
 
     def update
       continue_or_draft
+    end
+
+    private
+
+    # This is a little hack to ensure that df dates are copied to the application proceeding type
+    # in the lead-up to switching over to multiple proceedings.  It's done here so that it's after
+    # all the checks that are carried out on older df dates
+
+    # TODO: remove once multiple proceedings has been implemented
+
+    def update_df_dates
+      apt = legal_aid_application.application_proceeding_types.first
+      apt.update!(used_delegated_functions_on: legal_aid_application.used_delegated_functions_on,
+                  used_delegated_functions_reported_on: legal_aid_application.used_delegated_functions_reported_on)
     end
   end
 end
