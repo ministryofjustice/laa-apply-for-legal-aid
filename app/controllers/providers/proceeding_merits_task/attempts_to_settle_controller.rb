eafbffc54ada5c99004afb062f7138d7b57c2395
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-05-12 12:12:40 +0100

    AP-2226: WIP: Update ProceedingMeritsTask::AttemptsToSettleController
    Set the task to complet after update

--- app/controllers/providers/proceeding_merits_task/attempts_to_settle_controller.rb
+++ app/controllers/providers/proceeding_merits_task/attempts_to_settle_controller.rb
@@ -9,7 +9,10 @@ module Providers
       def update
         @application_proceeding_type = application_proceeding_type
         @form = Providers::ProceedingMeritsTask::AttemptsToSettleForm.new(form_params)
-
+        # TODO: Remove SafeNavigators after MultiProceeding Feature flag is turned on
+        # Until then, some applications will not have a legal_framework_merits_task_list
+        # Afterwards - everything should have one!
+        legal_aid_application&.legal_framework_merits_task_list&.mark_as_complete!(proceeding_type.ccms_code.to_sym, :attempts_to_settle)
         render :show unless save_continue_or_draft(@form)
       end
 
@@ -19,12 +22,16 @@ module Providers
         @legal_aid_application ||= application_proceeding_type.legal_aid_application
       end
 
-      def attempts_to_settle
-        @attempts_to_settle ||= @application_proceeding_type.attempts_to_settle || @application_proceeding_type.build_attempts_to_settle
+      def proceeding_type
+        @proceeding_type ||= application_proceeding_type.proceeding_type
       end
 
       def application_proceeding_type
-        ApplicationProceedingType.find(merits_task_list_id)
+        @application_proceeding_type ||= ApplicationProceedingType.find(merits_task_list_id)
+      end
+
+      def attempts_to_settle
+        @attempts_to_settle ||= @application_proceeding_type.attempts_to_settle || @application_proceeding_type.build_attempts_to_settle
       end
 
       def merits_task_list_id
