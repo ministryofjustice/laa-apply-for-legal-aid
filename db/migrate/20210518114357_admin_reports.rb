842411ce77e3a1b5a803da6c18d2e596ecb45bf4
Abdirizak Obsiye <mrabdirizak@gmail.com>
2021-05-21 12:45:19 +0100

    Update admin reports with uuid

--- db/migrate/20210518114357_admin_reports.rb
+++ db/migrate/20210518114357_admin_reports.rb
@@ -1,5 +1,7 @@
 class AdminReports < ActiveRecord::Migration[6.1]
   def change
-    create_table :admin_reports, id: :uuid, &:timestamps
+    create_table :admin_reports, id: :uuid do |t|
+      t.timestamps
+    end
   end
 end
