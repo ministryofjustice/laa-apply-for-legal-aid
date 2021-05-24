6d65f60ce48999ac5584690ea7fbac4ceafeb980
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-05-24 14:06:06 +0100

    Modify to ony process existing APT records without DF dates

--- bin/df_dates_updater.rb
+++ bin/df_dates_updater.rb
@@ -1,18 +1,26 @@
 class DFDatesUpdater
+
   def run
-    laa_ids = LegalAidApplication.where.not(substantive_application_deadline_on: nil).pluck(:id)
-    laa_ids.each do |laa_id|
-      laa = LegalAidApplication.find laa_id
-      df_date = WorkingDayCalculator.call(working_days: -20, from: laa.substantive_application_deadline_on)
-      reminder_mail = ScheduledMailing.where(mailer_klass: 'SubmitApplicationReminderMailer', legal_aid_application_id: laa_id).first
-      reported_date = reminder_mail&.created_at || df_date
-      laa.application_proceeding_types.each do |apt|
-        apt.update!(used_delegated_functions_on: df_date, used_delegated_functions_reported_on: reported_date)
-      end
-      puts "Legal Aid Application id: #{laa_id}  deadline: #{laa.substantive_application_deadline_on.strftime('%F')} DF dates updated to: #{df_date.strftime('%F')} (used), #{reported_date.strftime('%F')} (reported)"
-    end
+    apts = ApplicationProceedingType.where(used_delegated_functions_on: nil)
+    apts.each { |apt| process_apt(apt) }
+  end
+
+  private
+
+  def process_apt(apt)
+    laa = apt.legal_aid_application
+    return if laa.substantive_application_deadline_on.nil?
+
+    update_apt(apt, laa)
+  end
+
+  def update_apt(apt, laa)
+    df_date = WorkingDayCalculator.call(working_days: -20, from: laa.substantive_application_deadline_on)
+    reminder_mail = ScheduledMailing.where(mailer_klass: 'SubmitApplicationReminderMailer', legal_aid_application_id: laa.id).first
+    reported_date = reminder_mail&.created_at || df_date
+    apt.update!(used_delegated_functions_on: df_date, used_delegated_functions_reported_on: reported_date)
+    puts "Legal Aid Application id: #{laa.id}  deadline: #{laa.substantive_application_deadline_on.strftime('%F')} DF dates updated to: #{df_date.strftime('%F')} (used), #{reported_date.strftime('%F')} (reported)"
   end
 end
-
 
 DFDatesUpdater.new.run
