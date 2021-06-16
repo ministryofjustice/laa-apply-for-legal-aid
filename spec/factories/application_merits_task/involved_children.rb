d108bedb817a2a1988a1742072df9facd8e799db
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-06-16 16:35:22 +0100

    AP-2300 split full names of involved children

--- spec/factories/application_merits_task/involved_children.rb
+++ spec/factories/application_merits_task/involved_children.rb
@@ -2,7 +2,7 @@ module ApplicationMeritsTask
   FactoryBot.define do
     factory :involved_child, class: ApplicationMeritsTask::InvolvedChild do
       legal_aid_application
-      full_name { Faker::Name.name }
+      full_name { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
       date_of_birth { Faker::Date.between(from: Date.new(2000, 1, 1), to: Date.current) }
     end
   end
---
+++ spec/models/application_merits_task/involved_child_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+module ApplicationMeritsTask
+  RSpec.describe InvolvedChild do
+    let(:involved_child) { build :involved_child, full_name: full_name }
+
+    subject{ involved_child.split_full_name }
+
+    describe '#split_full_name' do
+      context 'first name and last name' do
+        let(:full_name) { 'John Smith' }
+        it 'separates out first and last name' do
+          expect(subject).to eq ['John', 'Smith']
+        end
+      end
+
+      context 'with  multiple embedded spaces' do
+        let(:full_name) { 'Michael      Winner' }
+        it 'separates out first and last name' do
+          expect(subject).to eq ['Michael', 'Winner']
+        end
+      end
+
+      context 'first name, middle name, last name' do
+        let(:full_name) { 'Philip   Stephen    Richards' }
+        it 'separates out first and last name' do
+          expect(subject).to eq ['Philip Stephen', 'Richards']
+        end
+      end
+
+      context 'just last name' do
+        let(:full_name) { 'Prince' }
+        it 'returns unspecified as first name' do
+          expect(subject).to eq ['unspecified', 'Prince']
+        end
+      end
+
+      context 'double-barrelled names' do
+        let(:full_name) { "Jacob Rees-Mogg" }
+        it 'is not phased by the hyphen' do
+          expect(subject).to eq ['Jacob', "Rees-Mogg"]
+        end
+      end
+
+      context 'irish names' do
+        let(:full_name) { "Daira O'Brien" }
+        it 'is not phased by the apostrophe' do
+          expect(subject).to eq ['Daira', "O'Brien"]
+        end
+      end
+    end
+
+  end
+end
