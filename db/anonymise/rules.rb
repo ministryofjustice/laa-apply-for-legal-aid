71689fb50ee0a452cde2c7d6ac1c2b372c0c62da
willc-work <william.clarke@digital.justice.gov.uk>
2021-07-15 10:38:10 +0100

    test clever pg

--- db/anonymise/rules.rb
+++ db/anonymise/rules.rb
@@ -123,8 +123,7 @@ NINO_REGEXP = /^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]{1}$/.fr
     understands_terms_of_court_order_details: -> { Faker::Lorem.sentence },
     warning_letter_sent_details: -> { Faker::Lorem.sentence },
     police_notified_details: -> { Faker::Lorem.sentence },
-    bail_conditions_set_details: -> { Faker::Lorem.sentence },
-    ccms_opponent_id: -> { Faker::Base.regexify(/^[0-9]{8}$/) }
+    bail_conditions_set_details: -> { Faker::Lorem.sentence }
   },
   other_assets_declarations: {},
   permissions: {},
