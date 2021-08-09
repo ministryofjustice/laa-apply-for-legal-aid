67dd02a3af448b3b9962380c5ef8df641b42f298
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-08-09 10:56:13 +0100

    AP-2416: Handle accented characters
    This was impossible to test, I wrote explicit Rspec and cucumber
    tests and they all passed, including `BROWSER=chrome cucumber`
    
    Then I found this unmerged PR to fix it but in the meantime I
    used the fix from the original issue here and tested in browser
    
    Because the headers donot seem to be generated when running tests
    the encode_header block needs :nocov: because the tests never hit it :/

--- app/controllers/concerns/providers/application_dependable.rb
+++ app/controllers/concerns/providers/application_dependable.rb
@@ -13,6 +13,7 @@ module Providers
     end
 
     included do
+      before_action :encode_upload_header
       before_action :set_legal_aid_application
 
       def legal_aid_application
@@ -40,6 +41,29 @@ module Providers
       def provider_step
         respond_to?(:current_step) ? __send__(:current_step) : controller_name
       end
+
+      def encode_upload_header(x_params = nil)
+        return unless request.content_mime_type&.symbol == :multipart_form
+
+        x_params ||= params
+
+        x_params.each do |_k, v|
+          case v
+          when ActionController::Parameters
+            encode_upload_header(v)
+          when ActionDispatch::Http::UploadedFile
+            encode_header(v.headers)
+          end
+        end
+      end
+
+      # :nocov:
+      def encode_header(headers)
+        headers.encode!(Encoding::UTF_8)
+      rescue EncodingError
+        headers.force_encoding(Encoding::UTF_8)
+      end
+      # :nocov:
     end
   end
 end
