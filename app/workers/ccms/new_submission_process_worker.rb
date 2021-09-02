71da85f30622ab1e463d581e93053a4afb00aa47
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-09-02 14:47:00 +0100

    AP-2472: Add NewSubmissionProcessWorker
    This uses a pure Sidekiq approach--
    If a submission.process! call succeeds and the state is changed
    Call a new worker for the new state
    
    If the stat eis unchnaged, or another error occurs, it fails and
    sidekiq will auto retry after a progressively longer wait
    
    No errors will be logged until 6 attempts have been made or the job
    has tried a maximum of time times (takes approx 4 hours)
    
    This _should_ ensure that each step will try to execute for a maximum of 4 hours, this should allow CCMS to recover from the issues blocking it
    and allow the process to be re-tried via the sidekiq console
    and not require mannual command line interference

---
+++ app/workers/ccms/new_submission_process_worker.rb
@@ -0,0 +1,58 @@
+module CCMS
+  class SubmissionStateUnchanged < StandardError; end
+
+  class SentryIgnoreThisSidekiqFailError < StandardError; end
+
+  class NewSubmissionProcessWorker
+    include Sidekiq::Worker
+    include Sidekiq::Status::Worker
+    attr_accessor :retry_count, :submission
+
+    MAX_RETRIES = 10
+    sidekiq_options retry: MAX_RETRIES
+    sidekiq_retries_exhausted do |msg, _ex|
+      Sentry.capture_message <<~ERROR
+        CCMS submission id: #{msg['args'].first} failed
+        Moving #{msg['class']} to dead set, it failed with: #{msg['error_class']}/#{msg['error_message']}
+      ERROR
+    end
+
+    def perform(submission_id, start_state)
+      @submission = Submission.find(submission_id)
+      Sentry.capture_message in_progress_error if should_warn?
+
+      @submission.process!
+
+      # raise an error if the current attempt has not changed the state
+      raise SubmissionStateUnchanged if state_unchanged?(start_state)
+
+      # if state has changed then create new worker for next state and successfully exit!
+      NewSubmissionProcessWorker.perform_async(submission_id, @submission.aasm_state)
+    rescue StandardError => e
+      raise if @retry_count.eql? MAX_RETRIES
+
+      raise SentryIgnoreThisSidekiqFailError, "Submission `#{@submission.id}` failed at `#{start_state}` on retry #{@retry_count.to_i} with error #{e.message}"
+    end
+
+    private
+
+    def should_warn?
+      @retry_count == MAX_RETRIES / 2 + 1
+    end
+
+    def state_unchanged?(start_state)
+      @submission.aasm_state == start_state
+    end
+
+    def in_progress_error
+      <<~ERROR
+        CCMS submission id: #{@submission.id} is failing
+        #{job_stuck}
+      ERROR
+    end
+
+    def job_stuck
+      "job stuck at state: #{@submission.aasm_state} with retry count at #{@retry_count}"
+    end
+  end
+end
---
+++ spec/workers/ccms/new_submission_process_worker_spec.rb
@@ -0,0 +1,140 @@
+require 'rails_helper'
+
+RSpec.describe CCMS::NewSubmissionProcessWorker do
+  let(:state) { :initialised }
+  let(:submission) { create :submission, aasm_state: state }
+  let(:worker) { described_class.new }
+
+  before do
+    allow(CCMS::Submission).to receive(:find).with(submission.id).and_return(submission)
+    allow(submission).to receive(:process!).and_return(true)
+    worker.retry_count = 1
+  end
+
+  describe '.perform' do
+    subject(:perform) { worker.perform(submission.id, state) }
+
+    it 'calls process! on the submission' do
+      expect(submission).to receive(:process!)
+      subject
+    end
+
+    context 'when an error occurs' do
+      context 'when @retry_count is' do
+        context 'below the halfway point' do
+          before { worker.retry_count = 4 }
+
+          it 'does not raise a sentry error' do
+            expect(Sentry).not_to receive(:capture_message)
+            subject
+          end
+
+          context 'raises a not tracked error' do
+            before do
+              # this should trigger state_unchanged? and therefore SubmissionStateUnchanged
+              allow(submission).to receive(:aasm_state).and_return(state)
+            end
+
+            it do
+              expect { subject }.to raise_error CCMS::SentryIgnoreThisSidekiqFailError
+            end
+          end
+        end
+
+        context 'on the halfway point' do
+          before { worker.retry_count = 5 }
+
+          it 'does not raise a sentry error' do
+            expect(Sentry).not_to receive(:capture_message)
+            subject
+          end
+
+          context 'raises a not tracked error' do
+            before do
+              # this should trigger state_unchanged? and therefore SubmissionStateUnchanged
+              allow(submission).to receive(:aasm_state).and_return(state)
+            end
+
+            it do
+              expect { subject }.to raise_error CCMS::SentryIgnoreThisSidekiqFailError
+            end
+          end
+        end
+
+        context 'one above the halfway point' do
+          before { worker.retry_count = 6 }
+          let(:expected_error) do
+            <<~MESSAGE
+              CCMS submission id: #{submission.id} is failing
+              job stuck at state: #{submission.aasm_state} with retry count at #{worker.retry_count}
+            MESSAGE
+          end
+
+          it 'raises a sentry error' do
+            expect(Sentry).to receive(:capture_message).with(expected_error)
+            subject
+          end
+
+          context 'raises a not tracked error' do
+            before do
+              # this should trigger state_unchanged? and therefore SubmissionStateUnchanged
+              allow(submission).to receive(:aasm_state).and_return(state)
+            end
+
+            it do
+              expect { subject }.to raise_error CCMS::SentryIgnoreThisSidekiqFailError
+            end
+          end
+        end
+
+        context 'above the halfway point' do
+          before { worker.retry_count = 7 }
+
+          it 'does not raise a sentry error' do
+            expect(Sentry).not_to receive(:capture_message)
+            subject
+          end
+
+          context 'raises a not tracked error' do
+            before do
+              # this should trigger state_unchanged? and therefore SubmissionStateUnchanged
+              allow(submission).to receive(:aasm_state).and_return(state)
+            end
+
+            it do
+              expect { subject }.to raise_error CCMS::SentryIgnoreThisSidekiqFailError
+            end
+          end
+        end
+
+        context 'at MAX_RETRIES' do
+          before { worker.retry_count = 10 }
+          let(:expected_error) do
+            <<~MESSAGE
+              CCMS submission id:  failed
+              Moving CCMS::NewSubmissionProcessWorker to dead set, it failed with: /An error occured
+            MESSAGE
+          end
+
+          it 'raises a sentry error' do
+            CCMS::NewSubmissionProcessWorker.within_sidekiq_retries_exhausted_block do
+              expect(Sentry).to receive(:capture_message).with(expected_error)
+            end
+            subject
+          end
+
+          context 'raises a tracked error' do
+            before do
+              # this should trigger state_unchanged? and therefore SubmissionStateUnchanged
+              allow(submission).to receive(:aasm_state).and_return(state)
+            end
+
+            it do
+              expect { subject }.to raise_error CCMS::SubmissionStateUnchanged
+            end
+          end
+        end
+      end
+    end
+  end
+end
