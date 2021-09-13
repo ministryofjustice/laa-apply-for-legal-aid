4efce8b0f883743b1f94b52a581ad7456d4ec2bd
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-09-13 15:58:05 +0100

    Add seed rake task to migration

--- db/migrate/20210913110123_drop_cost_limitations_from_proceeding_types.rb
+++ db/migrate/20210913110123_drop_cost_limitations_from_proceeding_types.rb
@@ -2,5 +2,7 @@ class DropCostLimitationsFromProceedingTypes < ActiveRecord::Migration[6.1]
   def change
     remove_column :proceeding_types, :default_cost_limitation_delegated_functions, :decimal
     remove_column :proceeding_types, :default_cost_limitation_substantive, :decimal
+
+    Rake::Task['db:seed'].invoke
   end
 end
