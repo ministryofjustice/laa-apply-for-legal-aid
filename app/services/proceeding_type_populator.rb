1418d407f819f78e0a650f1f718b485312e23319
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-09-28 12:54:15 +0100

    xx

--- app/services/proceeding_type_populator.rb
+++ app/services/proceeding_type_populator.rb
@@ -25,14 +25,15 @@ class ProceedingTypePopulator
   end
 
   def create_this_row?(row)
-    return true
+    true
+  end
 
     # flag_to_check = FEATURE_FLAGS[row[:ccms_matter_code].to_sym]
     # return true unless flag_to_check
     # return true unless Setting.respond_to?(flag_to_check)
     #
     # Setting.send(flag_to_check)
-  end
+
 
   def populate_default_cost_limitations
     pts = ProceedingType.all
