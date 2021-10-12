f5dde206e2f8c9cf00701c958c7213836333cac8
willc-work <william.clarke@digital.justice.gov.uk>
2021-10-12 16:31:15 +0100

    refactor tests for multi proc

--- spec/services/ccms/requestors/case_add_requestor_xml_blocks/passported_attributes_spec.rb
+++ spec/services/ccms/requestors/case_add_requestor_xml_blocks/passported_attributes_spec.rb
@@ -1296,78 +1296,14 @@ module CCMS
             expect(block).to have_text_response 'UNKNOWN'
           end
 
-          context 'there is one scope limitation' do
-            let(:legal_aid_application) do
-              create :legal_aid_application,
-                     :with_proceeding_types,
-                     :with_everything,
-                     :with_applicant_and_address,
-                     :with_positive_benefit_check_result,
-                     populate_vehicle: true,
-                     with_bank_accounts: 2,
-                     provider: provider,
-                     office: office
-            end
-
-            # before do
-            #   # the :with_proceeding_types trait sets up both substantive and df scope limitations for the application proceeding type, so
-            #   # we have to delete it here because we don't want it
-            #   application_proceeding_type.delegated_functions_scope_limitation.destroy!
-            # end
-
-            it 'REQUESTED_SCOPE should be hard be populated with the scope limitation code' do
-              attributes = [
-                [:proceeding, 'REQUESTED_SCOPE'],
-                [:proceeding_merits, 'REQUESTED_SCOPE']
-              ]
-              attributes.each do |entity_attribute_pair|
-                entity, attribute = entity_attribute_pair
-                block = XmlExtractor.call(xml, entity, attribute)
-                expect(block).to have_text_response legal_aid_application.application_proceeding_types.first.assigned_scope_limitations.first.code
-              end
-            end
+          it 'REQUESTED_SCOPE should be hard be populated with MULTIPLE in proceedings section' do
+            block = XmlExtractor.call(xml, :proceeding, 'REQUESTED_SCOPE')
+            expect(block).to have_text_response 'MULTIPLE'
           end
 
-          context 'there are multiple scope limitations' do
-            let(:legal_aid_application) do
-              create :legal_aid_application,
-                     :with_proceeding_types,
-                     :with_everything,
-                     :with_applicant_and_address,
-                     :with_positive_benefit_check_result,
-                     proceeding_types_count: 2,
-                     populate_vehicle: true,
-                     with_bank_accounts: 2,
-                     provider: provider,
-                     office: office
-            end
-
-            it 'REQUESTED_SCOPE should be hard be populated with MULTIPLE' do
-              skip 'Will be fixed when multiple proceeding CCMS generation is tested'
-              # Currently is producing two attribute blocks as follows:
-              # <Attribute>
-              #     <Attribute>REQUESTED_SCOPE</Attribute>
-              #     <ResponseType>text</ResponseType>
-              #     <ResponseValue>AA003</ResponseValue>
-              #     <UserDefinedInd>true</UserDefinedInd>
-              # </Attribute>
-              # <Attribute>
-              #     <Attribute>REQUESTED_SCOPE</Attribute>
-              #     <ResponseType>text</ResponseType>
-              #     <ResponseValue>AA001</ResponseValue>
-              #     <UserDefinedInd>true</UserDefinedInd>
-              # </Attribute>
-              #
-              attributes = [
-                [:proceeding, 'REQUESTED_SCOPE'],
-                [:proceeding_merits, 'REQUESTED_SCOPE']
-              ]
-              attributes.each do |entity_attribute_pair|
-                entity, attribute = entity_attribute_pair
-                block = XmlExtractor.call(xml, entity, attribute)
-                expect(block).to have_text_response 'MULTIPLE'
-              end
-            end
+          it 'REQUESTED_SCOPE should be hard be populated with a text string in proceeding_merits' do
+            block = XmlExtractor.call(xml, :proceeding_merits, 'REQUESTED_SCOPE')
+            expect(block).to have_text_response 'Dummy requested scope pending legal framework enhancements'
           end
 
           it 'NEW_OR_EXISTING should be hard coded to NEW' do
