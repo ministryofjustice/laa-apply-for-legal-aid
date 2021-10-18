5c6398a9776fb245bd43f18dddd75c78cd303baf
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-10-18 12:00:04 +0100

    AP-2556: Add config values for hmrc-interface

--- config/application.rb
+++ config/application.rb
@@ -104,6 +104,10 @@ module LaaApplyForLegalAid
     config.x.google_oauth2.client_id      = ENV['GOOGLE_CLIENT_ID']
     config.x.google_oauth2.client_secret = ENV['GOOGLE_CLIENT_SECRET']
 
+    config.x.hmrc_interface.host = ENV['HMRC_API_HOST']
+    config.x.hmrc_interface.client_id = ENV['HMRC_API_UID']
+    config.x.hmrc_interface.client_secret = ENV['HMRC_API_SECRET']
+
     config.x.db_url = Rails.env.production? ? "postgres://#{ENV['POSTGRES_USER']}:#{ENV['POSTGRES_PASSWORD']}@#{ENV['POSTGRES_HOST']}:5432/#{ENV['POSTGRES_DATABASE']}" : 'postgres://localhost:5432/apply_for_legal_aid_dev' # rubocop:disable Layout/LineLength
 
     config.active_job.queue_adapter = :sidekiq
