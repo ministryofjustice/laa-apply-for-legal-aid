1eec8322b5885919b0972007bec0c55c75a9a939
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-10-19 15:30:47 +0100

    WIP

--- spec/models/env_spec.rb
+++ spec/models/env_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Env' do
   it 'does' do
-    puts ">>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<".yellow
-    pp ENV['APPLICATION_DEPLOY_NAME']
+    puts ">>>>>>>>>>>> XLOG #{__FILE__}:#{__LINE__} <<<<<<<<<<<<".yellow
+    var = "LF69UUm#{ENV['APPLICATION_DEPLOY_NAME']}BH28"
+    puts var
   end
 end
