796795a73ee6d10746f1ce18759275a6e2097989
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-10-25 16:00:34 +0100

    more debugging

--- app/controllers/providers/submitted_applications_controller.rb
+++ app/controllers/providers/submitted_applications_controller.rb
@@ -3,6 +3,7 @@ module Providers
     authorize_with_policy_method :show_submitted_application?
 
     def show
+      puts ">>>>>>>>>>>> legal aid application  #{legal_aid_application.id} #{__FILE__}:#{__LINE__} <<<<<<<<<<<<".yellow
       @application_proceeding_type = legal_aid_application.lead_application_proceeding_type
     end
   end
