1b4a3f378949d654e62a473aa704e90272cd9763
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-10-25 16:27:24 +0100

    rubocop for debugging code

--- app/controllers/providers/submitted_applications_controller.rb
+++ app/controllers/providers/submitted_applications_controller.rb
@@ -3,8 +3,10 @@ module Providers
     authorize_with_policy_method :show_submitted_application?
 
     def show
+      # rubocop:disable Rails/Output
       puts ">>>>>>>>>>>> legal aid application  #{legal_aid_application.id} #{__FILE__}:#{__LINE__} <<<<<<<<<<<<".yellow
       @application_proceeding_type = legal_aid_application.lead_application_proceeding_type
+      # rubocop:enable Rails/Output
     end
   end
 end
