27a58348f0aaccfa17aa41ad1944b7f809953a72
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2021-11-02 08:17:45 +0000

    ap-2619: Use proceeding_id in merits_task_list controller urls
    instead of applcation_proceeding_type id

--- spec/helpers/task_list_helper_spec.rb
+++ spec/helpers/task_list_helper_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe TaskListHelper, type: :helper do
         <<~RESULT
           <li class="app-task-list__item">
             <span class="app-task-list__task-name">
-                <a aria_describedby="children_application-status" title="Children involved in this application" aria-label="Children involved in this application" href="/providers/applications/#{legal_aid_application.id}/has_other_involved_children?locale=en">Children involved in this application</a>
+                <a aria_describedby="children_application-status" aria-label="Children involved in this application" href="/providers/applications/#{legal_aid_application.id}/has_other_involved_children?locale=en">Children involved in this application</a>
             </span>
             <strong class="govuk-tag  app-task-list__tag" id="children_application_status">Completed</strong>
           </li>
@@ -30,7 +30,7 @@ RSpec.describe TaskListHelper, type: :helper do
         <<~RESULT
           <li class="app-task-list__item">
             <span class="app-task-list__task-name">
-                <a aria_describedby="children_application-status" title="Children involved in this application" aria-label="Children involved in this application" href="/providers/applications/#{legal_aid_application.id}/involved_children/new?locale=en">Children involved in this application</a>
+                <a aria_describedby="children_application-status" aria-label="Children involved in this application" href="/providers/applications/#{legal_aid_application.id}/involved_children/new?locale=en">Children involved in this application</a>
             </span>
             <strong class="govuk-tag govuk-tag--grey app-task-list__tag" id="children_application_status">Not started</strong>
           </li>
@@ -42,6 +42,28 @@ RSpec.describe TaskListHelper, type: :helper do
                                      status: :not_started,
                                      legal_aid_application: legal_aid_application,
                                      ccms_code: nil)).to eq expected
+      end
+    end
+
+    context 'chances of success' do
+      let!(:proceeding) { create :proceeding, :da001, legal_aid_application: legal_aid_application }
+
+      let(:expected) do
+        <<~RESULT
+          <li class="app-task-list__item">
+            <span class="app-task-list__task-name">
+                <a aria_describedby="chances_of_success-status" aria-label="Chances of success" href="/providers/merits_task_list/#{proceeding.id}/chances_of_success?locale=en">Chances of success</a>
+            </span>
+            <strong class="govuk-tag  app-task-list__tag" id="chances_of_success_status">Completed</strong>
+          </li>
+        RESULT
+      end
+
+      it 'returns a link' do
+        expect(helper.task_list_item(name: :chances_of_success,
+                                     status: :complete,
+                                     legal_aid_application: legal_aid_application,
+                                     ccms_code: :DA001)).to eq expected
       end
     end
   end
