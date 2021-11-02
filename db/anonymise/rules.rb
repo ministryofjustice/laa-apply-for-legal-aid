e6f4e2a051fbd300c45d881fe5f80413db230e08
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-11-02 16:35:43 +0000

    AP-2605 add applciation_digests to the anonymise rules

--- db/anonymise/rules.rb
+++ db/anonymise/rules.rb
@@ -33,6 +33,7 @@ NINO_REGEXP = /^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]{1}$/.fr
   application_proceeding_types: {},
   application_proceeding_types_linked_children: {},
   application_proceeding_types_scope_limitations: {},
+  application_digests: {},
   attachments: {},
   attempts_to_settles: {
     attempts_made: -> { Faker::Lorem.sentence }
