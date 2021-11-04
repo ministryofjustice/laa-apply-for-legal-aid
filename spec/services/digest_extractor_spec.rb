6eeaf367f399130f8cc338905d0ee90e53fd748a
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-11-04 12:35:46 +0000

    Add cronjob and extractor

---
+++ spec/services/digest_extractor_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe DigestExtractor do
+  describe '.call' do
+    let!(:laa1) { create :legal_aid_application, updated_at: 5.days.ago }
+    let!(:laa2) { create :legal_aid_application, updated_at: 3.days.ago }
+    let!(:laa3) { create :legal_aid_application, updated_at: 1.minute.ago }
+
+    before { Setting.setting.update!(digest_extracted_at: 4.days.ago) }
+
+    it 'calls ApplicationDigest for each record updated since last extraction date' do
+      expect(ApplicationDigest).to receive(:create_or_update!).with(laa2.id)
+      expect(ApplicationDigest).to receive(:create_or_update!).with(laa3.id)
+      expect(ApplicationDigest).not_to receive(:create_or_update!).with(laa1.id)
+
+      described_class.call
+    end
+
+    it 'updates the settings table with current time' do
+      freeze_time do
+        DigestExtractor.call
+      end
+      expect(Setting.setting.digest_extracted_at >= 2.seconds.ago).to be true
+    end
+  end
+end
