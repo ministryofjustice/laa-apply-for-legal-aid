1283f036d6e617b9938f144130cca797b0c92f50
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-11-05 10:43:36 +0000

    Convert purgeable boolean to purgeable_on date

--- spec/services/mark_purgeable_service_spec.rb
+++ spec/services/mark_purgeable_service_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe MarkPurgeableService do
   describe '.call' do
-    let!(:old1) { create :legal_aid_application, updated_at: 24.months.ago }
-    let!(:old2) { create :legal_aid_application, updated_at: 23.months.ago - 1.day }
+    let(:old_date1) { 24.months.ago }
+    let(:old_date2) { 23.months.ago - 1.day }
+    let!(:old1) { create :legal_aid_application, updated_at: old_date1 }
+    let!(:old2) { create :legal_aid_application, updated_at: old_date2 }
     let!(:new1) { create :legal_aid_application, updated_at: 23.months.ago + 1.day }
     let!(:new2) { create :legal_aid_application, updated_at: 6.months.ago }
 
     it 'marks all records not updated for 23 months as purgeable' do
       described_class.call
-      expect(old1.reload.purgeable?).to be true
-      expect(old2.reload.purgeable?).to be true
-      expect(new1.reload.purgeable?).to be false
-      expect(new2.reload.purgeable?).to be false
+      expect(old1.reload.purgeable_on).to eq (old_date1 + 730.days).to_date
+      expect(old2.reload.purgeable_on).to eq (old_date2 + 730.days).to_date
+      expect(new1.reload.purgeable_on).to be_nil
+      expect(new2.reload.purgeable_on).to be_nil
     end
   end
 end
