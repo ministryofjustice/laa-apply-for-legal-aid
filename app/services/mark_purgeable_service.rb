67cda41915e0192aacbc8b5e25fe33f773f77ac3
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-11-05 10:53:37 +0000

    Fix rubocop warning

--- app/services/mark_purgeable_service.rb
+++ app/services/mark_purgeable_service.rb
@@ -1,14 +1,16 @@
 class MarkPurgeableService
-  THRESHOLD_DATE = 23.months.ago.freeze
-
   def self.call
     new.call
+  end
+
+  def initialize
+    @threshold_date = 23.months.ago
   end
 
   def call
     # update all records whose updated date is more than 23 months ago and set the purgeable_on date to the updated date + 2 years.
     connection = ActiveRecord::Base.connection.raw_connection
     connection.prepare('mark_recs_purgeable', "UPDATE legal_aid_applications SET purgeable_on = date_trunc('day', updated_at) + interval '730' day WHERE updated_at < $1")
-    connection.exec_prepared('mark_recs_purgeable', [THRESHOLD_DATE])
+    connection.exec_prepared('mark_recs_purgeable', [@threshold_date])
   end
 end
