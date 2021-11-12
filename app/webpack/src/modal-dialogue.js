488eed9e77bd2ef03e5946e422db8e3119cfc08d
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-11-12 13:57:22 +0000

    modal tabbing behaviour
    create keyboard trap for modal when open

--- app/webpack/src/modal-dialogue.js
+++ app/webpack/src/modal-dialogue.js
@@ -1,153 +1,118 @@
-// import forEach from 'lodash/forEach';
-import { hide, show } from './helpers'
-// import { searchOnUserInput } from './modules/proceedings'
-//
-// function ModalDialogue () {}
-//
-// ModalDialogue.prototype.start = function ($module) {
-//   this.$module = $module;
-//   this.$dialogBox = this.$module.querySelector('.js-modal-dialog');
-//   this.$closeButton = this.$module.querySelector(
-//     '.js-modal-dialog-close-button--main'
-//   );
-//   this.$allCloseButtons = this.$module.querySelectorAll(
-//     '.js-modal-dialog-close-button'
-//   );
-//   this.$body = document.querySelector('body');
-//
-//   this.$module.open = this.handleOpen.bind(this);
-//   this.$module.close = this.handleClose.bind(this);
-//   this.$module.focusDialog = this.handleFocusDialog.bind(this);
-//   this.$module.boundKeyDown = this.handleKeyDown.bind(this);
-//
-//   const $triggerElement = document.querySelector(
-//     '[data-toggle="modal"][data-target="' + this.$module.id + '"]'
-//   );
-//
-//   if ($triggerElement) {
-//     $triggerElement.addEventListener('click', this.$module.open);
-//   }
-//
-//   if (this.$allCloseButtons) {
-//     this.$allCloseButtons.forEach((el) => {
-//       el.addEventListener('click', this.$module.close);
-//     });
-//   }
-// };
-//
-// ModalDialogue.prototype.handleOpen = function (event) {
-//   if (event) {
-//     event.preventDefault();
-//   }
-//
-//   // Close any currently-open modal
-//   const currentOpenModal = this.$body.querySelector(
-//     '[data-modal-open="true"]'
-//   );
-//   if (currentOpenModal) {
-//     currentOpenModal.close();
-//   }
-//
-//   this.$body.classList.add('is-modal');
-//   this.$focusedElementBeforeOpen = document.activeElement;
-//   show(this.$module)
-//   this.$module.setAttribute('data-modal-open', true);
-//   this.$dialogBox.focus();
-//
-//   document.addEventListener('keydown', this.$module.boundKeyDown, true);
-// };
-//
-// ModalDialogue.prototype.handleClose = function (event) {
-//   if (event) {
-//     event.preventDefault();
-//   }
-//
-//   this.$body.classList.remove('is-modal');
-//   hide(this.$module);
-//   this.$module.removeAttribute('data-modal-open');
-//   this.$focusedElementBeforeOpen.focus();
-//
-//   document.removeEventListener('keydown', this.$module.boundKeyDown, true);
-// };
-//
-// ModalDialogue.prototype.handleFocusDialog = function () {
-//   this.$dialogBox.focus();
-// };
-//
-// // while open, prevent tabbing to outside the dialogue
-// // and listen for ESC key to close the dialogue
-// ModalDialogue.prototype.handleKeyDown = function (event) {
-//   const KEY_TAB = 9;
-//   const KEY_ESC = 27;
-//
-//   const focusableModalElements = this.$module.querySelectorAll(
-//     'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-//   );
-//
-//   // Some modals don't have close buttons so we need to find the last focusable element in the modal
-//   // to keep tab keypresses within the modal and not the (hidden) document underneath
-//   const lastFocusableElement = this.$closeButton
-//     ? this.$closeButton
-//     : focusableModalElements[focusableModalElements.length - 1];
-//
-//   switch (event.keyCode) {
-//     case KEY_TAB:
-//       if (event.shiftKey) {
-//         if (document.activeElement === this.$dialogBox) {
-//           event.preventDefault();
-//           lastFocusableElement.focus();
-//         }
-//       } else {
-//         if (document.activeElement === lastFocusableElement) {
-//           event.preventDefault();
-//           this.$dialogBox.focus();
-//         }
-//       }
-//
-//       break;
-//     case KEY_ESC:
-//       if (this.$closeButton) {
-//         this.$module.close();
-//       }
-//       break;
-//     default:
-//       break;
-//   }
-// };
-//
-// function init () {
-//   const modalStartedAttr = 'data-modal-started';
-//   const modalElements = document.querySelectorAll('[data-modal]');
-//   modalElements.forEach((el) => {
-//     const started = el.getAttribute(modalStartedAttr);
-//     if (!started) {
-//       const modal = new ModalDialogue();
-//       modal.start(el);
-//       el.setAttribute(modalStartedAttr, true);
-//     }
-//   });
-// }
-//
-// // Allow triggering a modal via JavaScript if you know its ID
-// function triggerModal (id) {
-//   const modal = document.getElementById(id);
-//   console.log('modal triggered')
-//   if (modal && modal.open) {
-//     modal.open();
-//   }
-// }
-//
-// document.addEventListener('DOMContentLoaded', event => {
-//   init()
-// })
-//
-// export default {
-//   init,
-//   triggerModal,
-// };
+const MODAL_OPEN_MESSAGE = 'A new window has opened inside the current window'
+
+function announceModal () {
+  const messagesDiv = document.querySelector('#screen-reader-messages')
+  messagesDiv.innerText = MODAL_OPEN_MESSAGE
+}
+
+function openModal (modal) {
+  modal.style.visibility = 'visible'
+}
+
+function closeModal (modal) {
+  modal.style.visibility = 'hidden'
+}
+
+function makeModalTabbable (modal, nonModalElems) {
+  const dialogElements = modal.querySelectorAll('.modal-dialog')
+  dialogElements.forEach((elem) => {
+    elem.setAttribute('tabindex', 2)
+  })
+  nonModalElems.forEach((elem) => {
+    elem.setAttribute('data-previous-tabindex', elem.getAttribute('tabindex'))
+    elem.setAttribute('tabindex', -1)
+  })
+}
+
+function updateTabIndex(modal, nonModalElems) {
+  const dialogElements = modal.querySelectorAll('.modal-dialog')
+  dialogElements.forEach((elem) => {
+    elem.setAttribute('tabindex', -1)
+  })
+  nonModalElems.forEach((elem) => {
+    elem.setAttribute('tabindex', elem.getAttribute('data-previous-tabindex'))
+  })
+}
+
+// while open, prevent tabbing to outside the dialogue
+// and listen for ESC key to close the dialogue
+function handleKeyEvents (modal) {
+  modal.focus()
+  const modalContent = modal.querySelector('.modal-content')
+  modalContent.addEventListener('keydown', (event) => {
+    const KEY_TAB = 9
+    const KEY_ESC = 27
+
+    // to keep tab keypresses within the modal and not the (hidden) document underneath
+    const firstFocusableElement = modalContent.firstElementChild
+    const lastFocusableElement = modalContent.lastElementChild
+    console.log(lastFocusableElement)
+    console.log(firstFocusableElement)
+    console.log(document.activeElement)
+
+    switch (event.keyCode) {
+      case KEY_TAB:
+        if (event.shiftKey) {
+          if (document.activeElement === firstFocusableElement) {
+            event.preventDefault()
+            lastFocusableElement.focus()
+            console.log(lastFocusableElement)
+          }
+        } else {
+          if (document.activeElement === lastFocusableElement) {
+            event.preventDefault()
+            firstFocusableElement.focus()
+            console.log(firstFocusableElement)
+          }
+        }
+
+        break
+      case KEY_ESC:
+        closeModal(modal)
+        break
+      default:
+        break
+    }
+  })
+}
+
+function startModal (modal, lastFocusableElement, nonModalElems) {
+  // display the modal
+  openModal(modal)
+  handleKeyEvents(modal)
+  announceModal()
+
+  // Get the button and span elements that close the modal
+  const closeButtons = modal.querySelectorAll('.close-modal');
+  closeButtons.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault()
+      closeModal(modal)
+      updateTabIndex(modal, nonModalElems)
+      lastFocusableElement.focus()
+    })
+  })
+
+  // close modal and refresh page when an application is deleted
+  const confirmDeleteBtn = modal.querySelector('.confirm-delete-btn')
+  confirmDeleteBtn.addEventListener('click', (event) => {
+    closeModal(modal)
+    window.location.reload()
+    lastFocusableElement.focus()
+  })
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      closeModal(modal)
+      updateTabIndex(modal, nonModalElems)
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', event => {
   const deleteButtons = document.querySelectorAll('[data-toggle="modal"]')
+  const nonModalElements = document.querySelectorAll('body *:not(.modal-dialog):not([tabindex="-1"])')
 
   if (deleteButtons) {
     // When the user clicks on the button, open the modal
@@ -155,40 +120,10 @@ document.addEventListener('DOMContentLoaded', event => {
       deleteBtn.addEventListener('click', () => {
         // find the corresponding modal for that button
         const modalId = deleteBtn.id.replace('-btn', '')
-        const modal = document.getElementById(modalId);
-        show(modal)
-      })
-    })
-
-    // close modal and refresh page when an application is deleted
-    const confirmDeleteBtns = document.querySelectorAll('.confirm-delete-btn')
-    confirmDeleteBtns.forEach((btn) => {
-      btn.addEventListener('click', (event) => {
-        const modalId = btn.parentElement.parentElement.id
         const modal = document.getElementById(modalId)
-        hide(modal)
-        window.location.reload()
+        startModal(modal, deleteBtn, nonModalElements)
+        makeModalTabbable(modal, nonModalElements)
       })
     })
-
-    // Get the <span> element that closes the modal
-    const closeButtons = document.querySelectorAll('.close-modal');
-    // When the user clicks on <span> (x), close the modal
-    closeButtons.forEach((btn) => {
-      btn.addEventListener('click', (event) => {
-        event.preventDefault()
-        const modalId = btn.parentElement.parentElement.id
-        const modal = document.getElementById(modalId)
-        hide(modal)
-      })
-    })
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-      const modal = Array.from(document.querySelectorAll('.modal')).filter(mod => { return mod.style.display === 'block' })[0]
-      if (event.target === modal) {
-        hide(modal)
-      }
-    }
   }
 })
