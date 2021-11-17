0e124ede9339b5461913beee685c67caf8c03693
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-11-17 12:11:12 +0000

    AP-2610 Rubocop issues

--- app/services/destroy_purgeable_service.rb
+++ app/services/destroy_purgeable_service.rb
@@ -6,9 +6,7 @@ class DestroyPurgeableService
   end
 
   def call
-    ids = LegalAidApplication.where(purgeable_on: START_OF_EPOCH..Date.today.in_time_zone).pluck(:id)
+    ids = LegalAidApplication.where(purgeable_on: START_OF_EPOCH..Time.zone.today).pluck(:id)
     ids.each { |id| LegalAidApplication.find(id).destroy! }
   end
 end
-
-
--- spec/services/destroy_purgeable_service_spec.rb
+++ spec/services/destroy_purgeable_service_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe DestroyPurgeableService do
-
   let!(:nil_rec) { create :legal_aid_application, purgeable_on: nil }
-  let!(:tomorrow_rec) { create :legal_aid_application, purgeable_on: Date.tomorrow.in_time_zone }
-  let!(:yesterday_rec) { create :legal_aid_application, purgeable_on: Date.yesterday.in_time_zone }
-  let!(:today_rec) { create :legal_aid_application, purgeable_on: Date.today.in_time_zone }
+  let!(:tomorrow_rec) { create :legal_aid_application, purgeable_on: Time.zone.tomorrow }
+  let!(:yesterday_rec) { create :legal_aid_application, purgeable_on: Time.zone.yesterday }
+  let!(:today_rec) { create :legal_aid_application, purgeable_on: Time.zone.today }
 
   before { described_class.call }
 
   describe '.call' do
     it 'does not delete records with null purgeable_on' do
-      expect { LegalAidApplication.find(nil_rec.id) }.not_to raise_error ActiveRecord::RecordNotFound
+      expect(LegalAidApplication.find(nil_rec.id)).to be_present
     end
 
     it 'does not delete records with purgeable_on in the future' do
-      expect { LegalAidApplication.find(tomorrow_rec.id) }.not_to raise_error ActiveRecord::RecordNotFound
+      expect(LegalAidApplication.find(tomorrow_rec.id)).to be_present
     end
 
     it 'deletes records with purgeable_on in the past' do
@@ -25,6 +24,5 @@ RSpec.describe DestroyPurgeableService do
     it 'deletes records with purgeable_on today' do
       expect { LegalAidApplication.find(today_rec.id) }.to raise_error ActiveRecord::RecordNotFound
     end
-
   end
 end
