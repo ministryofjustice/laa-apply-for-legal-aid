361c3ba0eaefed32a44f209309db98b1baeb8306
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-12-07 18:08:37 +0000

    Resolve conflict in data_schema.rb

--- db/data_schema.rb
+++ db/data_schema.rb
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 DataMigrate::Data.define(version: 20211125142125)
-=======
-DataMigrate::Data.define(version: 20211126105433)
->>>>>>> c0b7ea9dd (AP-2564 new employment statuses and form and view)
