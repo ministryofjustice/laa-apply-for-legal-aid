30f6dc1814636fcb2ec21ebeff9b33e56d9b5ff2
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-12-14 15:25:17 +0000

    AP-2778 Fix Digest Exporter cron job issues The digest exporter cron job was failing every night to to running out of memory, and taking 45 minutes to run before failing.  Several steps have been taken to solve this issue: - increase memory on kubernetes cronjob definition - Read the ids of the digest records and process, rather than read all 9,000 into memory at the same time - format the output into an array and write to the spreadsheet once, rather than make an API call to the spreadsheet for each line (this has reduced the running time from 45 minutes to < 3 minutes - Add logging to that progress can be monitored - Add Extracted at time to the header line of the worksheet

--- app/services/digest_exporter.rb
+++ app/services/digest_exporter.rb
@@ -4,38 +4,73 @@ class DigestExporter
   end
 
   def initialize
+    log_message 'DigestExporter starting'
     secret_file = StringIO.new(google_secret.to_json)
     @session = GoogleDrive::Session.from_service_account_key(secret_file)
     @worksheet = @session.spreadsheet_by_key(spreadsheet_key).worksheets[0]
+    @rows = []
     delete_existing_data
     write_header_row
   end
 
   def call
-    ApplicationDigest.all.each_with_index { |digest, index| write_to_sheet(digest.to_google_sheet_row, index) }
-    @worksheet.save
+    save_digest_to_rows(digest_ids)
+    update_and_save_worksheet
   end
 
   private
+
+  def log_message(message)
+    message = Time.zone.now.strftime('%F %T.%L ') + message if Rails.env.development?
+    Rails.logger.info message
+  end
+
+  def update_and_save_worksheet
+    log_message 'updating cells'
+    @worksheet.update_cells(2, 1, @rows)
+    log_message 'saving worksheet'
+    @worksheet.save
+    log_message 'All records written and worksheet saved'
+  end
+
+  def digest_ids
+    @digest_ids ||= initialize_digest_ids
+  end
+
+  def initialize_digest_ids
+    log_message 'Getting digest ids'
+    ApplicationDigest.order(:created_at).pluck(:id)
+  end
+
+  def save_digest_to_rows(digest_ids)
+    log_message "#{digest_ids.size} records to be saved"
+    digest_ids.each_with_index do |digest_id, _index|
+      digest = ApplicationDigest.find(digest_id)
+      @rows << digest.to_google_sheet_row
+    end
+  end
 
   def delete_existing_data
     # You can't delete all the rows in a worksheet, so we delete all but one, and we'll overwrite that
     # with the column headers
     @worksheet.delete_rows(1, @worksheet.max_rows - 1)
     @worksheet.save
-  end
-
-  def write_to_sheet(row, index)
-    @worksheet.insert_rows(index + 2, [row])
+    log_message 'Existing data removed from spreadsheet'
   end
 
   def write_header_row
     # Overwrite the 1 remaining row with column headers
-    @worksheet.update_cells(1, 1, [ApplicationDigest.column_headers])
+    @worksheet.update_cells(1, 1, [ApplicationDigest.column_headers + extraction_date])
     @worksheet.save
 
     # It turns out that you have to re-initialise the worksheet, otherwise the deleted rows haven't really gone.
     @worksheet = @session.spreadsheet_by_key(spreadsheet_key).worksheets[0]
+  end
+
+  def extraction_date
+    [
+      "Extracted at: #{Time.zone.now}"
+    ]
   end
 
   def spreadsheet_key
