79c1220f2949d8d6bd698d88019b072a4bec0775
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2021-12-14 16:40:52 +0000

    put dropzone behind evidence upload feature flag

--- app/webpack/src/dropzone.js
+++ app/webpack/src/dropzone.js
@@ -1,11 +1,13 @@
-import Dropzone from "dropzone";
+import Dropzone from 'dropzone'
 
 // Make sure Dropzone doesn't try to attach itself to the
 // element automatically.
 // This behaviour will change in future versions.
-Dropzone.autoDiscover = false;
+Dropzone.autoDiscover = false
 
-let dropzone = new Dropzone("#my-form");
-dropzone.on("addedfile", file => {
-  console.log(`File added: ${file.name}`);
-});
+document.addEventListener('DOMContentLoaded', event => {
+  const dropzone = new Dropzone('#dropzone-form')
+  dropzone.on('addedfile', file => {
+    console.log(`File added: ${file.name}`)
+  })
+})
