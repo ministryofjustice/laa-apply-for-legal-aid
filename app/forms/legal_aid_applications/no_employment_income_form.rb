5eba62c5d2b2a9fb2aa68e590acd46aa117d8a86
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2021-12-14 15:27:33 +0000

    AP-2570 Add No HMRC data returned page
    Creates a new page to be displayed when no HMRC data is returned for an
    applicant.
    
    This commit adds:
     * route
     * controller
     * form
     * view
     * translations
     * request spec

---
+++ app/forms/legal_aid_applications/no_employment_income_form.rb
@@ -0,0 +1,17 @@
+module LegalAidApplications
+  class NoEmploymentIncomeForm < BaseForm
+    form_for LegalAidApplication
+
+    attr_accessor :full_employment_details
+
+    validate :full_employment_details_presence
+
+    private
+
+    def full_employment_details_presence
+      return if draft?
+
+      errors.add(:full_employment_details, I18n.t('activemodel.errors.models.legal_aid_application.attributes.full_employment_details.blank')) if full_employment_details.blank?
+    end
+  end
+end
