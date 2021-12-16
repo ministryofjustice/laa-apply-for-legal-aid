00b5412fd8b7acd7e66ceccddf2f328697c89f5c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-12-16 12:22:56 +0000

    AP-2777: Add HMRC call to the use_ccms_employed controller
    this should ensure that data is requested before the feature flag is enabled
    and allow Business Assurance Testing to continue

--- app/controllers/providers/use_ccms_employed_controller.rb
+++ app/controllers/providers/use_ccms_employed_controller.rb
@@ -1,6 +1,7 @@
 module Providers
   class UseCCMSEmployedController < ProviderBaseController
     def index
+      HMRC::CreateResponsesService.call(legal_aid_application)
       @legal_aid_application.use_ccms!(:employed)
     end
   end
