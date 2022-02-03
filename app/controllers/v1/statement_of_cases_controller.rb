05c7403c229f678719ab57e912dae9c7b128229a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-02-03 12:23:31 +0000

    Update v1/StatementOfCasesController
    Simplify the returns to remove the if blocks
    Implement malware scanning and down detection
    
    Test new return handling

--- app/controllers/v1/statement_of_cases_controller.rb
+++ app/controllers/v1/statement_of_cases_controller.rb
@@ -3,19 +3,50 @@ module V1
     ATTACHMENT_TYPE = 'statement_of_case'.freeze
 
     def create
-      if legal_aid_application
-        file = form_params[:file]
-        legal_aid_application.attachments.create document: file,
-                                                 attachment_type: ATTACHMENT_TYPE,
-                                                 original_filename: file.original_filename,
-                                                 attachment_name: sequenced_attachment_name
-        render '', status: :ok
-      else
-        render '', status: :not_found
-      end
+      return head :not_found unless legal_aid_application
+
+      file = form_params[:file]
+
+      malware_scan = malware_scan_result(file)
+      return render json: { error: original_file_error_for(:file_virus) }, status: :bad_request if malware_scan.virus_found?
+      return render json: { error: original_file_error_for(:system_down) }, status: :bad_request unless malware_scan.scanner_working
+
+      legal_aid_application.attachments.create document: file,
+                                               attachment_type: ATTACHMENT_TYPE,
+                                               original_filename: file.original_filename,
+                                               attachment_name: sequenced_attachment_name
+      head :ok
     end
 
     private
+
+    def provider_uploader
+      legal_aid_application.provider
+    end
+
+    def malware_scan_result(original_file)
+      MalwareScanner.call(
+        file_path: original_file.tempfile.path,
+        uploader: provider_uploader,
+        file_details: {
+          size: original_file_size(original_file),
+          name: original_file.original_filename,
+          content_type: original_file.content_type
+        }
+      )
+    end
+
+    def original_file_size(original_file)
+      File.size(original_file.tempfile)
+    end
+
+    def original_file_error_for(error_type, options = {})
+      I18n.t("activemodel.errors.models.#{error_path}.attributes.original_file.#{error_type}", **options)
+    end
+
+    def error_path
+      'application_merits_task/statement_of_case'
+    end
 
     def legal_aid_application
       @legal_aid_application ||= LegalAidApplication.find_by(id: form_params[:legal_aid_application_id])
