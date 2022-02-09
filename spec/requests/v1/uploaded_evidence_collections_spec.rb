e66de1391a38c784b42467e1704802c7f311a43e
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2022-02-09 17:58:36 +0000

    AP-2739 Enable malware scanning on Evidence Upload
    Amends app/controllers/v1/uploaded_evidence_collections_controller.rb
    to add malware scanning.

--- spec/requests/v1/uploaded_evidence_collections_spec.rb
+++ spec/requests/v1/uploaded_evidence_collections_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'POST /v1/uploaded_evidence_collections', type: :request do
   let(:legal_aid_application) { create :legal_aid_application }
+  let(:uploaded_evidence_collection) { legal_aid_application.uploaded_evidence_collection }
   let(:id) { legal_aid_application.id }
   let(:file) { uploaded_file('spec/fixtures/files/documents/hello_world.pdf', 'application/pdf') }
   let(:params) { { legal_aid_application_id: id, file: file } }
+  let(:i18n_error_path) { 'activemodel.errors.models.uploaded_evidence_collection.attributes.original_file' }
 
   describe 'POST /v1/uploaded_evidence_collections' do
     subject { post v1_uploaded_evidence_collections_path, params: params }
@@ -40,7 +42,7 @@ RSpec.describe 'POST /v1/uploaded_evidence_collections', type: :request do
         end
       end
 
-      context 'when the application has multiple attachments for statement of case already' do
+      context 'when the application has multiple evidence attachments already' do
         let(:uec1) { create :attachment, :uploaded_evidence_collection }
         let(:uec2) { create :attachment, :uploaded_evidence_collection, attachment_name: 'uploaded_evidence_collection_1' }
         let(:legal_aid_application) { create :legal_aid_application, attachments: [uec1, uec2] }
@@ -49,6 +51,29 @@ RSpec.describe 'POST /v1/uploaded_evidence_collections', type: :request do
           subject
           expect(legal_aid_application.reload.attachments.length).to match(3)
           expect(legal_aid_application.reload.attachments.order(:attachment_name).last.attachment_name).to match('uploaded_evidence_collection_2')
+        end
+      end
+
+      context 'file contains a malware' do
+        let(:file) { uploaded_file('spec/fixtures/files/malware.doc') }
+
+        it 'does not save the object and raises an error' do
+          subject
+          error = I18n.t("#{i18n_error_path}.file_virus", file_name: file.original_filename)
+          expect(response.body).to include(error)
+          expect(uploaded_evidence_collection).to be_nil
+        end
+      end
+
+      context 'virus scanner is down' do
+        before { allow_any_instance_of(MalwareScanResult).to receive(:scanner_working).with(any_args).and_return(false) }
+        let(:i18n_error_path) { 'activemodel.errors.models.application_merits_task/statement_of_case.attributes.original_file' }
+
+        it 'does not save the object and raises a 500 error with text' do
+          subject
+          expect(legal_aid_application.reload.attachments.length).to match(0)
+          expect(response.status).to eq 400
+          expect(response.body).to include(I18n.t("#{i18n_error_path}.system_down"))
         end
       end
     end
