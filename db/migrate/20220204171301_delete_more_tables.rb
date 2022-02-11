77042907642caa10bf0b25b8156b3b04cd2508cd
willc-work <william.clarke@digital.justice.gov.uk>
2022-02-11 14:15:43 +0000

    fix UAT deploys

--- db/migrate/20220204171301_delete_more_tables.rb
+++ db/migrate/20220204171301_delete_more_tables.rb
@@ -1,6 +1,5 @@
 class DeleteMoreTables < ActiveRecord::Migration[6.1]
   def up
-    drop_table :application_proceeding_types_linked_children, force: :cascade
     drop_table :default_cost_limitations, force: :cascade
     drop_table :service_levels, force: :cascade
   end
