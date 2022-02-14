f220990f9ca3205fe5850e6225a2f815b39a4365
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2022-02-14 13:23:54 +0000

    Ap-2739 Move evidence upload validation to model
    Validation of business rules for uploaded evidence is currently done in
    the submission_form, however this results in error messages being
    displayed in the middle of the screen (above the submission form)
    instead of at the top of the screen. This moves validation from the form
    to the model, and use the base page template to render errors for the
    model.

--- app/forms/providers/uploaded_evidence_submission_form.rb
+++ app/forms/providers/uploaded_evidence_submission_form.rb
@@ -3,35 +3,5 @@ module Providers
     form_for UploadedEvidenceCollection
 
     attr_accessor :provider_uploader
-
-    validate :all_evidence_categorised
-    validate :mandatory_evidence_uploaded
-
-    def all_evidence_categorised
-      return true unless uncategorised_evidence_exists
-
-      errors.add(:uploaded_evidence_collection_select,
-                 I18n.t("#{error_path}.uncategorised_evidence"))
-    end
-
-    def uncategorised_evidence_exists
-      model.original_attachments.any? { |attachment| attachment.attachment_type == 'uncategorised' }
-    end
-
-    def mandatory_evidence_uploaded
-      model.mandatory_evidence_types.each do |type|
-        next if model.categorised_evidence_types.include?(type)
-
-        errors.add("uploaded_evidence_collection_#{type}",
-                   I18n.t("#{error_path}.#{type}_missing",
-                          benefit: model.legal_aid_application.dwp_override.passporting_benefit.titleize))
-      end
-    end
-
-    private
-
-    def error_path
-      '.activemodel.errors.models.uploaded_evidence_collection.attributes.original_file'
-    end
   end
 end
--- app/models/uploaded_evidence_collection.rb
+++ app/models/uploaded_evidence_collection.rb
@@ -2,19 +2,52 @@ class UploadedEvidenceCollection < ApplicationRecord
   belongs_to :legal_aid_application
   belongs_to :provider_uploader, class_name: 'Provider', optional: true
 
+  validate :all_evidence_categorised
+  validate :all_mandatory_evidence_uploaded
+
   def original_attachments
     legal_aid_application.attachments.displayable_evidence_types
   end
 
-  def application_evidence_types
-    legal_aid_application.required_document_categories
+  private
+
+  def all_evidence_categorised
+    return true unless uncategorised_evidence_exists?
+
+    errors.add(:'uploaded-files-table-container',
+               I18n.t("#{error_path}.uncategorised_evidence"))
+    false
+  end
+
+  def uncategorised_evidence_exists?
+    original_attachments.any? { |attachment| attachment.attachment_type == 'uncategorised' }
+  end
+
+  def all_mandatory_evidence_uploaded
+    mandatory_evidence_types.each do |type|
+      next if categorised_evidence_types.include?(type)
+
+      errors.add("uploaded_files_table_container#{type}",
+                 I18n.t("#{error_path}.#{type}_missing",
+                        benefit: legal_aid_application.dwp_override.passporting_benefit.titleize))
+    end
+
+    mandatory_evidence_types.all? { |mandatory_type| categorised_evidence_types.include?(mandatory_type) }
   end
 
   def mandatory_evidence_types
     DocumentCategory.where(name: application_evidence_types, mandatory: true).pluck(:name)
   end
 
+  def application_evidence_types
+    legal_aid_application.required_document_categories
+  end
+
   def categorised_evidence_types
     original_attachments.pluck(:attachment_type).uniq
+  end
+
+  def error_path
+    '.activemodel.errors.models.uploaded_evidence_collection.attributes.original_file'
   end
 end
