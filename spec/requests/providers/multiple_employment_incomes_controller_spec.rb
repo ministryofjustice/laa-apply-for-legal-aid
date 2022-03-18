6bbbbacf4b9e287761a16f26d1d3b0edba67850a
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2022-03-18 16:36:33 +0000

    add rspec tests for multiple employment income page

---
+++ spec/requests/providers/multiple_employment_incomes_controller_spec.rb
@@ -0,0 +1,97 @@
+require "rails_helper"
+
+RSpec.describe Providers::MultipleEmploymentIncomesController, type: :request do
+  let(:application) { create :legal_aid_application, :with_applicant, :with_non_passported_state_machine }
+  let(:provider) { application.provider }
+
+  before { create :hmrc_response, :use_case_one, legal_aid_application_id: application.id }
+
+  describe "GET /providers/applications/:id/multiple_employment_income" do
+    subject(:request) { get providers_legal_aid_application_multiple_employment_income_path(application) }
+
+    context "when the provider is not authenticated" do
+      before { request }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        request
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "PATCH /providers/applications/:id/multiple_employment_income" do
+    subject(:request) { patch providers_legal_aid_application_multiple_employment_income_path(application), params: params.merge(submit_button) }
+
+    let(:full_employment_details) { Faker::Lorem.paragraph }
+    let(:params) do
+      {
+        legal_aid_application: {
+          full_employment_details:,
+        },
+      }
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        request
+      end
+
+      context "when form is submitted with continue button" do
+        let(:submit_button) do
+          {
+            continue_button: "Continue",
+          }
+        end
+
+        it "updates legal aid application employment details" do
+          expect(application.reload.full_employment_details).to eq full_employment_details
+        end
+
+        it "redirects to income summary page" do
+          expect(response).to redirect_to(providers_legal_aid_application_no_income_summary_path(application))
+        end
+
+        context "when params are invalid" do
+          let(:full_employment_details) { "" }
+
+          it "displays error" do
+            expect(unescaped_response_body).to include(I18n.t("activemodel.errors.models.legal_aid_application.attributes.full_employment_details.blank"))
+          end
+        end
+      end
+
+      context "when form is submitted with Save as draft button" do
+        let(:submit_button) do
+          {
+            draft_button: "Save as draft",
+          }
+        end
+
+        context "when form is submitted successfully" do
+          before do
+            login_as provider
+            request
+            application.reload
+          end
+
+          it "updates the legal_aid_application.extra_employment_information" do
+            expect(application.full_employment_details).to eq full_employment_details
+          end
+
+          it "redirects to the list of applications" do
+            expect(response).to redirect_to providers_legal_aid_applications_path
+          end
+        end
+      end
+    end
+  end
+end
