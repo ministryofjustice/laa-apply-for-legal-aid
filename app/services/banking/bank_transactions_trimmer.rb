4dc7f9613c62b63b27b6a1b7c6aacc813e19b47a
Chris Groves <chris.groves03@gmail.com>
2022-03-23 17:55:02 +0000

    Move assignment of deletion_period to a separate method to ensure the call method has only one responsibility.

--- app/services/banking/bank_transactions_trimmer.rb
+++ app/services/banking/bank_transactions_trimmer.rb
@@ -9,10 +9,15 @@ module Banking
     end
 
     def call
-      deletion_period = (@legal_aid_application.transaction_period_finish_on..1.year.from_now)
       @legal_aid_application.bank_transactions
                             .where(happened_at: deletion_period)
                             .destroy_all
+    end
+
+  private
+
+    def deletion_period
+      @legal_aid_application.transaction_period_finish_on..1.year.from_now
     end
   end
 end
