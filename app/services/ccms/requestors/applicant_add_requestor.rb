be2230b1e3c7cdb7273a7bf16792020dd8cfbb82
Chris Groves <chris.groves03@gmail.com>
2022-03-28 18:41:10 +0100

    Fix GOVUK Rubocop Style/QuotedSymbols offences in app/services directory

--- app/services/ccms/requestors/applicant_add_requestor.rb
+++ app/services/ccms/requestors/applicant_add_requestor.rb
@@ -24,48 +24,48 @@ module CCMS
       end
 
       def soap_body(xml)
-        xml.__send__(:'clientbim:ClientAddRQ') do
-          xml.__send__(:'hdr:HeaderRQ') { ns3_header_rq(xml, @provider_username) }
-          xml.__send__(:'clientbim:Client') { client(xml) }
+        xml.__send__(:"clientbim:ClientAddRQ") do
+          xml.__send__(:"hdr:HeaderRQ") { ns3_header_rq(xml, @provider_username) }
+          xml.__send__(:"clientbim:Client") { client(xml) }
         end
       end
 
       def client(xml)
-        xml.__send__(:'clientbio:Name') { name(xml) }
-        xml.__send__(:'clientbio:PersonalInformation') { personal_information(xml) }
-        xml.__send__(:'clientbio:Contacts') { contacts(xml) }
-        xml.__send__(:'clientbio:NoFixedAbode', false)
-        xml.__send__(:'clientbio:Address') { applicant_address(xml) }
-        xml.__send__(:'clientbio:EthnicMonitoring', 0)
+        xml.__send__(:"clientbio:Name") { name(xml) }
+        xml.__send__(:"clientbio:PersonalInformation") { personal_information(xml) }
+        xml.__send__(:"clientbio:Contacts") { contacts(xml) }
+        xml.__send__(:"clientbio:NoFixedAbode", false)
+        xml.__send__(:"clientbio:Address") { applicant_address(xml) }
+        xml.__send__(:"clientbio:EthnicMonitoring", 0)
       end
 
       def name(xml)
-        xml.__send__(:'common:Surname', applicant.last_name)
-        xml.__send__(:'common:FirstName', applicant.first_name)
+        xml.__send__(:"common:Surname", applicant.last_name)
+        xml.__send__(:"common:FirstName", applicant.first_name)
       end
 
       # this is all mandatory: we don't hold any of this data except date of birth
       def personal_information(xml)
-        xml.__send__(:'clientbio:DateOfBirth', applicant.date_of_birth.to_s(:ccms_date))
-        xml.__send__(:'clientbio:Gender', "UNSPECIFIED")
-        xml.__send__(:'clientbio:MaritalStatus', "U")
-        xml.__send__(:'clientbio:VulnerableClient', false)
-        xml.__send__(:'clientbio:HighProfileClient', false)
-        xml.__send__(:'clientbio:VexatiousLitigant', false)
-        xml.__send__(:'clientbio:CountryOfOrigin', "GBR")
-        xml.__send__(:'clientbio:MentalCapacityInd', false)
+        xml.__send__(:"clientbio:DateOfBirth", applicant.date_of_birth.to_s(:ccms_date))
+        xml.__send__(:"clientbio:Gender", "UNSPECIFIED")
+        xml.__send__(:"clientbio:MaritalStatus", "U")
+        xml.__send__(:"clientbio:VulnerableClient", false)
+        xml.__send__(:"clientbio:HighProfileClient", false)
+        xml.__send__(:"clientbio:VexatiousLitigant", false)
+        xml.__send__(:"clientbio:CountryOfOrigin", "GBR")
+        xml.__send__(:"clientbio:MentalCapacityInd", false)
       end
 
       # this is the only mandatory item of contact data. i'm not sure what it's for...
       def contacts(xml)
-        xml.__send__(:'clientbio:Password', "Testing")
+        xml.__send__(:"clientbio:Password", "Testing")
       end
 
       def applicant_address(xml)
-        xml.__send__(:'common:AddressLine1', address.first_lines)
-        xml.__send__(:'common:City', address.city)
-        xml.__send__(:'common:Country', "GBR")
-        xml.__send__(:'common:PostalCode', address.pretty_postcode)
+        xml.__send__(:"common:AddressLine1", address.first_lines)
+        xml.__send__(:"common:City", address.city)
+        xml.__send__(:"common:Country", "GBR")
+        xml.__send__(:"common:PostalCode", address.pretty_postcode)
       end
     end
   end
