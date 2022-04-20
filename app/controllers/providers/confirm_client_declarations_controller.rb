47f10674da5902218397c1229c658fb29b55a267
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-04-20 10:33:31 +0100

    ap-2880: add confirm_client_declaration_form

--- app/controllers/providers/confirm_client_declarations_controller.rb
+++ app/controllers/providers/confirm_client_declarations_controller.rb
@@ -1,7 +1,22 @@
 module Providers
   class ConfirmClientDeclarationsController < ProviderBaseController
-    def show; end
+    def show
+      @form = LegalAidApplications::ConfirmClientDeclarationForm.new(model: legal_aid_application)
+    end
 
-    def update; end
+    def update
+      @form = LegalAidApplications::ConfirmClientDeclarationForm.new(form_params)
+      render :show unless save_continue_or_draft(@form)
+    end
+
+    private
+
+    def form_params
+      merge_with_model(legal_aid_application) do
+        next {} unless params[:legal_aid_application]
+
+        params.require(:legal_aid_application).permit(:client_declaration_confirmed)
+      end
+    end
   end
 end
