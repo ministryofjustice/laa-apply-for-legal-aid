cabcf12bc619d29288c45e83d01a44117e503032
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-05-04 14:38:54 +0100

    Add redis_url for rack_attack

--- config/application.rb
+++ config/application.rb
@@ -144,6 +144,7 @@ module LaaApplyForLegalAid
     config.x.redis.base_url = ENV["REDIS_HOST"].present? && ENV["REDIS_PASSWORD"].present? ? "rediss://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_HOST']}:6379" : "redis://localhost:6379" # rubocop:disable Lint/RequireParentheses
     config.x.redis.page_history_url = "#{config.x.redis.base_url}/1"
     config.x.redis.oauth_session_url = "#{config.x.redis.base_url}/2"
+    config.x.redis.rack_attack_url = "#{config.x.redis.base_url}/3"
 
     # automatically include locale in the query string when generating urls with url_helpers
     Rails.application.routes.default_url_options[:locale] = I18n.locale
