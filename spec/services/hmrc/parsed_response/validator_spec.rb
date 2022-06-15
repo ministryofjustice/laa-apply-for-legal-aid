d71bf16db5312816b5554d28196a8c85fd11b852
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-06-15 16:49:51 +0100

    Remove unneeded response hash elements
    Only add minimum response hash key values
    need to pass the test. The others just make
    the test more confusing.

--- spec/services/hmrc/parsed_response/validator_spec.rb
+++ spec/services/hmrc/parsed_response/validator_spec.rb
@@ -743,21 +743,7 @@ RSpec.describe HMRC::ParsedResponse::Validator do
           "data" => [
             { "individuals/matching/individual" => valid_individual_response },
             { "income/paye/paye" => { "income" => [] } },
-            { "income/sa/selfAssessment" => { "registrations" => [], "taxReturns" => [] } },
-            { "income/sa/pensions_and_state_benefits/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/source/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/employments/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/additional_information/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/partnerships/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/uk_properties/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/foreign/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/further_details/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/interests_and_dividends/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/other/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/summary/selfAssessment" => { "taxReturns" => [] } },
-            { "income/sa/trusts/selfAssessment" => { "taxReturns" => [] } },
             { "employments/paye/employments" => [] },
-            { "benefits_and_credits/working_tax_credit/applications" => [{ "awards" => [] }] },
           ] }
       end
 
