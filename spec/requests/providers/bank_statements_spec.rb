36b51d1c535d2432ef12bcd71a01c56ffb0d8853
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-06-24 09:57:04 +0100

    Add model, views, controllers, form objects and flows
      - Add conditional No radio hint
       For providers with the bank statement upload permissions we
       want to show then the hint, otherwise do not!
    
     - Seed additional document category for bank statements
     - Javascript and non-javascript upload
     - Add rails controller testing helpers for testing redner template
     - Add feature test of javascript enabled bank statement upload
     - Add request specs of javascript enabled bank statement upload
     - Add request specs for non-javascript enabled tests of bank statement upload
    
     - Move uploaded files form out of upload form
        This was (and for other upload pages, is) cauing a bug whereby
        you can only upload one file when JS is disabled as a second upload
        will delete the previous file.
    
     - Convert bank statement to a PDF
        Each bank statement is converted on upload.
        Note, this does not follow the same pattern as other
        uploaders which seem to enqueue a conversion job
        only when the save and continue button is pressed. This
        would seem to be uneccesary and could result in multiple
        converted pdfs?!
    
     - Amend ccms doc type for bank statement evidence
        Following discussion with business analyst we are
        to specify this as `BSTMT` "doc type". Although
        this has not impact on how a caseworker on CCMS
        will see/access the files it is a more applicable
        type for it since the bank transactions report
        generated from truelayer data is similarly classified.

---
+++ spec/requests/providers/bank_statements_spec.rb
@@ -0,0 +1,415 @@
+require "rails_helper"
+
+# This excerises the non-javascript enabled path.
+# 1. see v1/bank_statements_controller for requests that would come from dropzone/ajax
+# 2. see cucumber features for full JS enabled integration tests.
+#
+RSpec.describe "Providers::BankStatementsController", type: :request do
+  include Capybara::RSpecMatchers
+
+  let(:legal_aid_application) { create(:legal_aid_application, attachments:) }
+  let(:id) { legal_aid_application.id }
+  let(:attachments) { [] }
+  let(:provider) { legal_aid_application.provider }
+
+  describe "GET /providers/applications/:legal_aid_application_id/bank_statements" do
+    subject(:request) { get providers_legal_aid_application_bank_statements_path(legal_aid_application) }
+
+    context "when the provider is not authenticated" do
+      before { request }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        request
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      context "when no bank statements exists for the application" do
+        let(:attachments) { [] }
+
+        it { expect(response).to render_template("providers/bank_statements/_uploaded_files") }
+
+        it "displays fallback text" do
+          expect(response.body).to include("Files uploaded will appear here")
+        end
+      end
+
+      context "when a bank statement already exists for the application" do
+        let(:attachments) { [attachment] }
+        let(:attachment) { create(:attachment, :bank_statement) }
+
+        # NOTE: factory implicitily attachs the hello_world.pdf
+        it "displays the name of the uploaded file on the page" do
+          expect(response.body).to include("hello_world.pdf")
+        end
+      end
+    end
+  end
+
+  describe "GET /providers/applications/:legal_aid_application_id/bank_statements/list" do
+    subject(:request) { get list_providers_legal_aid_application_bank_statements_path(legal_aid_application) }
+
+    context "when the provider is not authenticated" do
+      before { request }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        request
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it { expect(response).to render_template("providers/bank_statements/_uploaded_files") }
+    end
+  end
+
+  describe "PATCH /providers/applications/:legal_aid_application_id/bank_statements" do
+    subject(:request) { patch providers_legal_aid_application_bank_statements_path(legal_aid_application), params: }
+
+    let(:draft_button) { { draft_button: "" } }
+    let(:continue_button) { { continue_button: "" } }
+    let(:upload_button) { { upload_button: "Upload" } }
+    let(:button_clicked) { {} }
+
+    let(:params) do
+      {
+        legal_aid_application_id: legal_aid_application.id,
+        original_file: file,
+      }.merge(button_clicked)
+    end
+
+    before { login_as provider }
+
+    context "when upload button clicked" do
+      let(:button_clicked) { upload_button }
+
+      context "with acceptable bank statement" do
+        let(:file) { uploaded_file("spec/fixtures/files/acceptable.pdf", "application/pdf") }
+
+        it "adds an attachment object" do
+          expect { request }.to change(legal_aid_application.attachments, :count).by(1)
+        end
+
+        it "enqueues job to convert uploaded attachment document to pdf" do
+          expect { request }.to change(PdfConverterWorker.jobs, :size).by(1)
+          expect(PdfConverterWorker.jobs[0]["args"]).to include(legal_aid_application.reload.attachments.last.id)
+        end
+
+        it "stores the original filename" do
+          request
+          expect(legal_aid_application.reload.attachments.last.original_filename).to eq "acceptable.pdf"
+        end
+
+        it "returns http success" do
+          request
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "sets attachment_name to model name" do
+          request
+          expect(legal_aid_application.reload.attachments.last.attachment_name).to eq("bank_statement_evidence")
+        end
+
+        context "with background job processing" do
+          around do |example|
+            Sidekiq::Testing.inline!
+            example.run
+            Sidekiq::Testing.fake!
+          end
+
+          it "adds 2 attachments" do
+            expect { request }.to change(legal_aid_application.attachments, :count).by(2)
+          end
+
+          it "adds original and converted attachments types" do
+            request
+            expect(legal_aid_application.reload.attachments.pluck(:attachment_type)).to match_array(%w[bank_statement_evidence bank_statement_evidence_pdf])
+          end
+
+          it "associates pdf converted attachment to original attachment" do
+            request
+            original_attachment = legal_aid_application.attachments.find_by(attachment_type: "bank_statement_evidence")
+            expect(original_attachment.pdf_attachment_id).not_to be_nil
+          end
+        end
+
+        context "when the application has one bank statment attachment already" do
+          let(:bank_statement_evidence) { create(:attachment, :bank_statement, attachment_name: "bank_statement_evidence") }
+          let!(:legal_aid_application) { create(:legal_aid_application, attachments: [bank_statement_evidence]) }
+
+          it "increments the attachment name" do
+            request
+            expect(legal_aid_application.reload.attachments.pluck(:attachment_name)).to match_array(%w[bank_statement_evidence bank_statement_evidence_1])
+          end
+        end
+
+        context "when the application has multiple attachments for statement of case already" do
+          let(:bs1) { create(:attachment, :bank_statement, attachment_name: "bank_statement_evidence") }
+          let(:bs2) { create(:attachment, :bank_statement, attachment_name: "bank_statement_evidence_1") }
+          let!(:legal_aid_application) { create(:legal_aid_application, attachments: [bs1, bs2]) }
+
+          it "increments the attachment name" do
+            request
+            expect(legal_aid_application.reload.attachments.pluck(:attachment_name)).to match_array(%w[bank_statement_evidence bank_statement_evidence_1 bank_statement_evidence_2])
+          end
+        end
+      end
+
+      context "when file to big" do
+        let(:file) { uploaded_file("spec/fixtures/files/too_large.pdf", "application/pdf") }
+
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "does not enqueue job to convert upload to pdf" do
+          expect { request }.not_to change(PdfConverterWorker.jobs, :size)
+        end
+
+        it "returns http success" do
+          request
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "displays error indicating file too large" do
+          request
+          expect(response.body).to have_selector("h2", text: "There is a problem").and have_link("too_large.pdf must be smaller than 7MB")
+        end
+      end
+
+      context "when file has no content" do
+        let(:file) { uploaded_file("spec/fixtures/files/empty_file.pdf", "application/pdf") }
+
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "does not enqueue job to convert upload to pdf" do
+          expect { request }.not_to change(PdfConverterWorker.jobs, :size)
+        end
+
+        it "returns http success" do
+          request
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "displays error indicating file is empty" do
+          request
+          expect(response.body).to have_selector("h2", text: "There is a problem").and have_link("empty_file.pdf has no content")
+        end
+      end
+
+      context "when file is of wrong content type" do
+        let(:file) { uploaded_file("spec/fixtures/files/zip.zip", "application/zip") }
+
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "does not enqueue job to convert upload to pdf" do
+          expect { request }.not_to change(PdfConverterWorker.jobs, :size)
+        end
+
+        it "returns http success" do
+          request
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "displays error indicating file is of the wrong content type" do
+          request
+          expect(response.body).to have_selector("h2", text: "There is a problem").and have_link("zip.zip must be a DOC, DOCX, RTF, ODT, JPG, BMP, PNG, TIF or PDF")
+        end
+      end
+
+      context "when file contains a virus" do
+        let(:file) { uploaded_file("spec/fixtures/files/malware.doc") }
+
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "does not enqueue job to convert upload to pdf" do
+          expect { request }.not_to change(PdfConverterWorker.jobs, :size)
+        end
+
+        it "returns http success" do
+          request
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "displays error indicating file contains a virus" do
+          request
+          expect(response.body).to have_selector("h2", text: "There is a problem").and have_link("malware.doc contains a virus")
+        end
+      end
+
+      context "when virus scanner is down" do
+        let(:file) { uploaded_file("spec/fixtures/files/acceptable.pdf", "application/pdf") }
+
+        before do
+          malware_scan_result = instance_double(MalwareScanResult, virus_found?: false, scanner_working: false)
+          allow(MalwareScanner).to receive(:call).and_return(malware_scan_result)
+        end
+
+        it "returns error message" do
+          request
+          expect(response.body).to include("There was a problem uploading your file - try again")
+        end
+      end
+    end
+
+    context "when save and continue button clicked" do
+      subject(:request) do
+        patch(providers_legal_aid_application_bank_statements_path(legal_aid_application),
+              params: { continue_button: "Save and continue" })
+      end
+
+      context "with files already attached" do
+        before do
+          patch(providers_legal_aid_application_bank_statements_path(legal_aid_application),
+                params: { upload_button: "Upload",
+                          original_file: uploaded_file("spec/fixtures/files/acceptable.pdf", "application/pdf") })
+        end
+
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        # TODO: this will need to change to the new provider means flow
+        it "redirects to check_your_answers" do
+          request
+          expect(response).to redirect_to providers_legal_aid_application_check_provider_answers_path
+        end
+      end
+
+      context "with no files attached" do
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "renders :show, again" do
+          request
+          expect(response).to render_template(:show)
+        end
+
+        it "displays error indicating a file is needed" do
+          request
+          expect(response.body).to have_selector("h2", text: "There is a problem").and have_link("Upload your client's bank statements")
+        end
+      end
+    end
+
+    context "when save and come back later is clicked" do
+      subject(:request) do
+        patch(providers_legal_aid_application_bank_statements_path(legal_aid_application),
+              params: { draft_button: "" })
+      end
+
+      context "with files already attached" do
+        before do
+          patch(providers_legal_aid_application_bank_statements_path(legal_aid_application),
+                params: { upload_button: "Upload",
+                          original_file: uploaded_file("spec/fixtures/files/acceptable.pdf", "application/pdf") })
+        end
+
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "redirects to provider\'s application list" do
+          request
+          expect(response).to redirect_to providers_legal_aid_applications_path
+        end
+      end
+
+      context "with no files attached" do
+        it "does not add attachment object" do
+          expect { request }.not_to change(legal_aid_application.attachments, :count)
+        end
+
+        it "redirects to provider\'s application list" do
+          request
+          expect(response).to redirect_to providers_legal_aid_applications_path
+        end
+      end
+    end
+  end
+
+  describe "DELETE /providers/applications/:legal_aid_application_id/bank_statements" do
+    subject(:request) { delete providers_legal_aid_application_bank_statements_path(legal_aid_application), params: }
+
+    before { login_as provider }
+
+    context "with existing file" do
+      let(:params) { { attachment_id: legal_aid_application.attachments.bank_statement_evidence.first.id } }
+
+      before do
+        patch(providers_legal_aid_application_bank_statements_path(legal_aid_application),
+              params: { upload_button: "Upload",
+                        original_file: uploaded_file("spec/fixtures/files/acceptable.pdf", "application/pdf") })
+      end
+
+      it "deletes the attachment object" do
+        expect { request }.to change(legal_aid_application.attachments, :count).by(-1)
+      end
+
+      it "returns http success" do
+        request
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders :show" do
+        request
+        expect(response).to render_template(:show)
+      end
+
+      it "renders govuk notification banner indicating successful deletion" do
+        request
+        expect(response.body).to have_selector("div.govuk-notification-banner", text: "acceptable.pdf has been successfully deleted")
+      end
+
+      context "with background job processing" do
+        around do |example|
+          Sidekiq::Testing.inline!
+          example.run
+          Sidekiq::Testing.fake!
+        end
+
+        it "deletes original attachment and converted pdf attachment" do
+          expect { request }.to change(legal_aid_application.attachments, :count).by(-2)
+        end
+      end
+    end
+
+    context "with non-existent file" do
+      let(:params) { { attachment_id: :unknown } }
+
+      it "does not delete any attachment objects" do
+        expect { request }.not_to change(legal_aid_application.attachments, :count)
+      end
+
+      it "returns http success" do
+        request
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders :show" do
+        request
+        expect(response).to render_template(:show)
+      end
+    end
+  end
+end
