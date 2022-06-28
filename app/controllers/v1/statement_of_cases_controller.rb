13cb4bb365d8f700d7c2ee3bb32c8e75a49fe283
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-06-28 12:22:22 +0100

    Update tesseract file handling
    Allow PDF uploads, convert to image, before OCRing with tesseract

--- app/controllers/v1/statement_of_cases_controller.rb
+++ app/controllers/v1/statement_of_cases_controller.rb
@@ -12,7 +12,11 @@ module V1
       return render json: { error: original_file_error_for(:file_virus, file_name: file.original_filename) }, status: :bad_request if malware_scan.virus_found?
       return render json: { error: original_file_error_for(:system_down) }, status: :bad_request unless malware_scan.scanner_working
 
-      legal_aid_application.statement_of_case.update(statement: extract_text(file))
+      if legal_aid_application.statement_of_case.nil?
+        legal_aid_application.create_statement_of_case(statement: extract_text(file))
+      else
+        legal_aid_application.statement_of_case.update(statement: extract_text(file))
+      end
       legal_aid_application.attachments.create document: file,
                                                attachment_type: ATTACHMENT_TYPE,
                                                original_filename: file.original_filename,
@@ -27,10 +31,36 @@ module V1
     end
 
     def extract_text(file)
-      image = RTesseract.new(file.tempfile.path.to_s)
+      path = get_file_path(file)
+
+      image = RTesseract.new(path)
       image.to_s
-    rescue StandardError
-      String.empty
+    rescue StandardError => e
+      Rails.logger.info e.message
+      ""
+    end
+
+    def get_file_path(file)
+      case file.content_type
+      when "application/pdf"
+        convert_pdf_return_path(file)
+      when "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        raise "Currently unsupported"
+      else
+        # just use temp upload path
+        file.tempfile.path.to_s
+      end
+    end
+
+    def convert_pdf_return_path(uploaded_file)
+      in_path = uploaded_file.path
+      out_path = "#{in_path}--all-pages.png"
+      im = Magick::ImageList.new(in_path)
+      result = im.append(true).write(out_path)
+      Rails.logger.info "-------------"
+      Rails.logger.info out_path
+      Rails.logger.info "-------------"
+      result.filename
     end
 
     def error_path
