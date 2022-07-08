465b3f037c08a48502c24f42145c7bbf7940a01a
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-07-08 15:13:27 +0100

    fixup - refactor spec

--- spec/requests/providers/identify_types_of_outgoings_spec.rb
+++ spec/requests/providers/identify_types_of_outgoings_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
   before { login }
 
   describe "GET /providers/identify_types_of_outgoing" do
-    before { get providers_legal_aid_application_identify_types_of_outgoing_path(legal_aid_application) }
+    subject(:request) { get providers_legal_aid_application_identify_types_of_outgoing_path(legal_aid_application) }
+
+    before { request }
 
     it "returns http success" do
       expect(response).to have_http_status(:ok)
@@ -34,7 +36,7 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
   end
 
   describe "PATCH /providers/identify_types_of_outgoing" do
-    subject do
+    subject(:request) do
       patch(
         providers_legal_aid_application_identify_types_of_outgoing_path(legal_aid_application),
         params: params.merge(submit_button),
@@ -52,18 +54,18 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
     end
 
     it "does not add transaction types to the application" do
-      expect { subject }.not_to change(LegalAidApplicationTransactionType, :count)
+      expect { request }.not_to change(LegalAidApplicationTransactionType, :count)
     end
 
     it "displays an error" do
-      subject
+      request
       expect(response.body).to match("govuk-error-summary")
       expect(unescaped_response_body).to match(I18n.t("generic.none_selected"))
       expect(unescaped_response_body).not_to include("translation missing")
     end
 
     it "returns http success" do
-      subject
+      request
       expect(response).to have_http_status(:ok)
     end
 
@@ -71,16 +73,17 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
       let(:transaction_type_ids) { outgoing_types.map(&:id) }
 
       it "adds transaction types to the application" do
-        expect { subject }.to change(LegalAidApplicationTransactionType, :count).by(outgoing_types.length)
+        expect { request }.to change(LegalAidApplicationTransactionType, :count).by(outgoing_types.length)
         expect(legal_aid_application.reload.transaction_types).to match_array(outgoing_types)
       end
 
-      it "redirects to the next step" do
-        expect(subject).to redirect_to(flow_forward_path)
+      it "redirects to the outgoings summary page" do
+        request
+        expect(response).to redirect_to(providers_legal_aid_application_outgoings_summary_index_path(legal_aid_application))
       end
 
       it "sets no_debit_transaction_types_selected to false" do
-        expect { subject }.to change { legal_aid_application.reload.no_debit_transaction_types_selected }.to(false)
+        expect { request }.to change { legal_aid_application.reload.no_debit_transaction_types_selected }.to(false)
       end
 
       context "Form submitted with Save as draft button" do
@@ -88,7 +91,7 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
         let(:submit_button) { { draft_button: "Save as draft" } }
 
         it "redirects to the list of applications" do
-          subject
+          request
           expect(response).to redirect_to providers_legal_aid_applications_path
         end
       end
@@ -99,11 +102,11 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
       let(:legal_aid_application) { create :legal_aid_application, :with_applicant, :with_non_passported_state_machine, transaction_types: [other_transaction_type] }
 
       it "does not remove existing transation of other type" do
-        expect { subject }.not_to change { legal_aid_application.transaction_types.count }
+        expect { request }.not_to change { legal_aid_application.transaction_types.count }
       end
 
       it "does not delete transaction types" do
-        expect { subject }.not_to change(TransactionType, :count)
+        expect { request }.not_to change(TransactionType, :count)
       end
     end
 
@@ -111,15 +114,16 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
       let(:params) { { legal_aid_application: { none_selected: "true" } } }
 
       it "does not add transaction types to the application" do
-        expect { subject }.not_to change(LegalAidApplicationTransactionType, :count)
+        expect { request }.not_to change(LegalAidApplicationTransactionType, :count)
       end
 
-      it "redirects to the next step" do
-        expect(subject).to redirect_to(flow_forward_path)
+      it "redirects to the outgoings summary page" do
+        request
+        expect(response).to redirect_to(providers_legal_aid_application_outgoings_summary_index_path(legal_aid_application))
       end
 
       it "sets no_debit_transaction_types_selected to true" do
-        expect { subject }.to change { legal_aid_application.reload.no_debit_transaction_types_selected }.to(true)
+        expect { request }.to change { legal_aid_application.reload.no_debit_transaction_types_selected }.to(true)
       end
 
       context "and application has transactions" do
@@ -128,12 +132,13 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
         end
 
         it "removes transaction types from the application" do
-          expect { subject }.to change(LegalAidApplicationTransactionType, :count)
+          expect { request }.to change(LegalAidApplicationTransactionType, :count)
             .by(-outgoing_types.length)
         end
 
-        it "redirects to the next step" do
-          expect(subject).to redirect_to(flow_forward_path)
+        it "redirects to the outgoings summary page" do
+          request
+          expect(response).to redirect_to(providers_legal_aid_application_outgoings_summary_index_path(legal_aid_application))
         end
       end
     end
@@ -143,12 +148,12 @@ RSpec.describe Providers::IdentifyTypesOfOutgoingsController do
       let(:transaction_type_ids) { income_types.map(&:id) }
 
       it "does not add the transaction types" do
-        expect { subject }.not_to change { legal_aid_application.transaction_types.count }
+        expect { request }.not_to change { legal_aid_application.transaction_types.count }
       end
     end
 
     context "when the provider is not authenticated" do
-      before { subject }
+      before { request }
 
       let(:login) { nil }
 
