e7ad94a9af4946b54e10a6ffa5eeec8c2e3cecda
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-07-11 12:25:33 +0100

    fixup - rename event `open_banking_not_selected` --> `provider_assess_means`
    More generic and therefore applicable to other
    pathways where this state may nee to be set.

--- spec/concerns/non_passported_state_machine_spec.rb
+++ spec/concerns/non_passported_state_machine_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe NonPassportedStateMachine do
     end
   end
 
-  describe "#open_banking_not_selected!" do
-    subject(:event) { legal_aid_application.open_banking_not_selected! }
+  describe "#provider_assess_means!" do
+    subject(:event) { legal_aid_application.provider_assess_means! }
 
     context "when from state provider_confirming_applicant_eligibility" do
       let(:legal_aid_application) { create(:legal_aid_application, :provider_confirming_applicant_eligibility) }
