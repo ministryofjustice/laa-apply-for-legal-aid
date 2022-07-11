83108ae6fa144d1e85fa4c2b17cb749b65bbacb0
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-07-11 11:32:47 +0100

    Allow state change `checking_non_passported_means` --> `provider_assessing_means`
    To enable users to change their responses

--- spec/concerns/non_passported_state_machine_spec.rb
+++ spec/concerns/non_passported_state_machine_spec.rb
@@ -18,12 +18,24 @@ RSpec.describe NonPassportedStateMachine do
   describe "#open_banking_not_selected!" do
     subject(:event) { legal_aid_application.open_banking_not_selected! }
 
-    let(:legal_aid_application) { create(:legal_aid_application, :provider_confirming_applicant_eligibility) }
+    context "when from state provider_confirming_applicant_eligibility" do
+      let(:legal_aid_application) { create(:legal_aid_application, :provider_confirming_applicant_eligibility) }
 
-    it "sets state to provider_assessing_means" do
-      expect { event }.to change(legal_aid_application, :state)
-                            .from("provider_confirming_applicant_eligibility")
-                            .to("provider_assessing_means")
+      it "sets state to provider_assessing_means" do
+        expect { event }.to change(legal_aid_application, :state)
+                              .from("provider_confirming_applicant_eligibility")
+                              .to("provider_assessing_means")
+      end
+    end
+
+    context "when from state checking_non_passported_means" do
+      let(:legal_aid_application) { create(:legal_aid_application, :checking_non_passported_means) }
+
+      it "sets state to provider_assessing_means" do
+        expect { event }.to change(legal_aid_application, :state)
+                              .from("checking_non_passported_means")
+                              .to("provider_assessing_means")
+      end
     end
   end
 end
