bf58f14853a68a3db0c45b3ae3f075e3d6cb0266
willc-work <william.clarke@digital.justice.gov.uk>
2022-07-12 15:00:42 +0100

    add second radio button and exclude result from model

--- app/forms/legal_aid_applications/open_banking_consent_form.rb
+++ app/forms/legal_aid_applications/open_banking_consent_form.rb
@@ -2,11 +2,11 @@ module LegalAidApplications
   class OpenBankingConsentForm < BaseForm
     form_for LegalAidApplication
 
-    attr_accessor :provider_received_citizen_consent
+    attr_accessor :provider_received_citizen_consent, :uses_online_banking
 
     validate :online_banking_presence
 
-    # validate :email_presence
+    validate :uses_online_banking_presence
 
   private
 
@@ -16,10 +16,14 @@ module LegalAidApplications
       errors.add(:provider_received_citizen_consent, I18n.t("activemodel.errors.models.legal_aid_application.attributes.open_banking_consents.providers.blank"))
     end
 
-    # def email_presence
-    #   return if draft? || uses_online_banking.present?
-    #
-    #   errors.add(:uses_online_banking, I18n.t("activemodel.errors.models.legal_aid_application.attributes.open_banking_consents.providers.blank"))
-    # end
+    def uses_online_banking_presence
+      return if draft? || uses_online_banking.present?
+
+      errors.add(:uses_online_banking, I18n.t("activemodel.errors.models.legal_aid_application.attributes.open_banking_consents.providers.blank"))
+    end
+
+    def exclude_from_model
+      [:uses_online_banking]
+    end
   end
 end
