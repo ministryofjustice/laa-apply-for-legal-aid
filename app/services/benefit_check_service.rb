1169ffc211d5104ca851b87bea7caf39c14a9dc9
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2022-07-20 14:03:25 +0100

    AP-3298 logging to debig cricle ci

--- app/services/benefit_check_service.rb
+++ app/services/benefit_check_service.rb
@@ -8,11 +8,13 @@ class BenefitCheckService
   end
 
   def self.call(application)
+    # rubocop:disable Rails/Output
     puts ">>>>>>>>>>>> BENEFIT CHECK SERVICE #{__FILE__}:#{__LINE__} <<<<<<<<<<<<".yellow
     puts USE_MOCK
     puts ">>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<".yellow
-    return MockBenefitCheckService.call(application) if USE_MOCK && !Rails.env.production?
+    return MockBenefitCheckService.call(application) if USE_MOCK && !Rails.env.production
 
+    # rubocop:enable Rails/Output
     new(application).call
   end
 
