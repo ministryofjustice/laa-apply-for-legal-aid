f7f4e901ab1101bac5f04274f15353efe74699dd
willc-work <william.clarke@digital.justice.gov.uk>
2022-07-21 17:32:47 +0100

    test making bank transaction report creator conditional

--- app/services/reports/reports_creator.rb
+++ app/services/reports/reports_creator.rb
@@ -3,7 +3,8 @@ module Reports
     def self.call(legal_aid_application)
       MeritsReportCreator.call(legal_aid_application)
       MeansReportCreator.call(legal_aid_application)
-      BankTransactions::BankTransactionReportCreator.call(legal_aid_application) if legal_aid_application.non_passported?
+      # TODO: think about this conditional, should we also check that bank transactions exist? otherwise this fails silently here I think.
+      BankTransactions::BankTransactionReportCreator.call(legal_aid_application) if legal_aid_application.non_passported? && !legal_aid_application.uploading_bank_statements?
       legal_aid_application.reload.generated_reports!
     end
   end
