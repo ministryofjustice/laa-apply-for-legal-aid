b967c8ccc2a5616f71879aba03c450f92e7463d0
willc-work <william.clarke@digital.justice.gov.uk>
2022-07-26 08:29:49 +0100

    find submission.id within the write method

--- app/controllers/providers/no_eligibility_assessments_controller.rb
+++ app/controllers/providers/no_eligibility_assessments_controller.rb
@@ -1,7 +1,6 @@
 module Providers
   class NoEligibilityAssessmentsController < ProviderBaseController
     def show
-      submission
       write_cfe_result
       legal_aid_application.provider_enter_merits! unless legal_aid_application.provider_entering_merits?
       @result_partial = ResultsPanelSelector.call(legal_aid_application)
@@ -12,13 +11,13 @@ module Providers
     end
 
     def submission
-      @submission ||= CFE::Submission.create!(legal_aid_application_id: legal_aid_application.id, aasm_state: "cfe_not_called")
+      CFE::Submission.create!(legal_aid_application_id: legal_aid_application.id, aasm_state: "cfe_not_called")
     end
 
     def write_cfe_result
       CFE::Empty::Result.create!(
         legal_aid_application_id: legal_aid_application.id,
-        submission_id: @submission.id,
+        submission_id: submission.id,
         result: CFE::Empty::EmptyResult.blank_cfe_result.to_json,
         type: "CFE::Empty::Result",
       )
