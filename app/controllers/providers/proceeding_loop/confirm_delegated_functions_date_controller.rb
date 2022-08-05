6bfebd1a12f01dd531162ea3aa9b74f57ed820e5
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-08-05 13:34:16 +0100

    AP-3374: Add controller for confirming delegated function dates

---
+++ app/controllers/providers/proceeding_loop/confirm_delegated_functions_date_controller.rb
@@ -0,0 +1,39 @@
+module Providers
+  module ProceedingLoop
+    class ConfirmDelegatedFunctionsDateController < ProviderBaseController
+      before_action :proceeding, :form
+      def show; end
+
+      def update
+        return continue_or_draft if draft_selected?
+        return go_forward(form.confirm_delegated_functions_date?) if form.valid?
+
+        render :show
+      end
+
+    private
+
+      def form
+        @form ||= BinaryChoiceForm.call(
+          journey: :provider,
+          radio_buttons_input_name: :confirm_delegated_functions_date,
+          form_params:,
+        )
+      end
+
+      def proceeding
+        @proceeding = Proceeding.find(proceeding_id_param)
+      end
+
+      def proceeding_id_param
+        params.require(:id)
+      end
+
+      def form_params
+        return {} unless params[:binary_choice_form]
+
+        params.require(:binary_choice_form).permit(:confirm_delegated_functions_date)
+      end
+    end
+  end
+end
---
+++ spec/requests/providers/proceeding_loop/confirm_delegated_functions_date_controller_spec.rb
@@ -0,0 +1,93 @@
+require "rails_helper"
+
+RSpec.describe "ConfirmDelegatedFunctionsDateController", :vcr, type: :request do
+  let(:application) do
+    create(:legal_aid_application,
+           :with_proceedings,
+           :with_delegated_functions_on_proceedings,
+           df_options: { DA001: 10.days.ago })
+  end
+  let(:application_id) { application.id }
+  let(:proceeding_id) { application.proceedings.first.id }
+  let(:provider) { application.provider }
+
+  before { allow(Setting).to receive(:enable_mini_loop?).and_return(true) } # TODO: Remove when the mini-loop feature flag is removed
+
+  describe "GET /providers/applications/:legal_aid_application_id/confirm_delegated_functions_date/:proceeding_id" do
+    subject(:get_cdf) { get "/providers/applications/#{application_id}/confirm_delegated_functions_date/#{proceeding_id}" }
+
+    context "when the provider is not authenticated" do
+      before { get_cdf }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        get_cdf
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "displays the proceeding header" do
+        expect(response.body).to include("Check delegated functions date")
+      end
+    end
+  end
+
+  describe "POST /providers/applications/:legal_aid_application_id/confirm_delegated_functions_date/:proceeding_id" do
+    subject(:post_df) { patch "/providers/applications/#{application_id}/confirm_delegated_functions_date/#{proceeding_id}", params: }
+
+    let(:params) { { binary_choice_form: { confirm_delegated_functions_date: confirm } } }
+    let(:confirm) { "true" }
+
+    context "when the provider is not authenticated" do
+      before { post_df }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+      end
+
+      context "when the Continue button is pressed" do
+        let(:submit_button) { { continue_button: "Continue" } }
+
+        context "and the user selected yes" do
+          it "redirects to next page" do
+            post_df
+            expect(response.body).to redirect_to(providers_legal_aid_application_limitations_path(application_id))
+          end
+        end
+
+        context "and the user selected no" do
+          let(:confirm) { "false" }
+
+          it "redirects back to the df page" do
+            post_df
+            expect(response.body).to redirect_to(providers_legal_aid_application_delegated_function_path(application_id))
+          end
+        end
+
+        context "and the user selected nothing" do
+          let(:confirm) { nil }
+
+          before { post_df }
+
+          it "returns http_success" do
+            expect(response).to have_http_status(:ok)
+          end
+
+          it "the response includes the error message" do
+            expect(response.body).to include(I18n.t("providers.confirm_delegated_functions_dates.show.error"))
+          end
+        end
+      end
+    end
+  end
+end
