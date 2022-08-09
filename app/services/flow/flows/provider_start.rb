1d603704cea059682acddc21ecaa99a2a4a41428
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-08-09 15:41:17 +0100

    AP-3374: Update provider start flow
    Route correctly when th euser is returning to an application
    from the check your answers page and changing a delegated
    function date to more than a month old

--- app/services/flow/flows/provider_start.rb
+++ app/services/flow/flows/provider_start.rb
@@ -78,13 +78,19 @@ module Flow
           check_answers: :check_provider_answers,
         },
         delegated_functions: {
-          path: lambda do |application|
-            proceeding = Flow::ProceedingLoop.next_proceeding(application)
+          path: lambda do |application, first_visit|
+            proceeding = if first_visit.nil?
+                           Flow::ProceedingLoop.next_proceeding(application)
+                         else
+                           Proceeding.find(application.provider_step_params["id"])
+                         end
             urls.providers_legal_aid_application_delegated_function_path(application, proceeding)
           end,
           forward: ->(application) { Flow::ProceedingLoop.next_step(application) },
           carry_on_sub_flow: false, # TODO: This may need changing when the full loop is implemented as a change of DF affects the LOS and scopes, defaults and otherwise
-          check_answers: :check_provider_answers,
+          check_answers: lambda do |application|
+            Flow::ProceedingLoop.next_step(application) == :confirm_delegated_functions_date ? :confirm_delegated_functions_date : :check_provider_answers
+          end,
         },
         confirm_delegated_functions_date: {
           path: lambda do |application|
