9d58c5718bfa939884c4c66ea1eb94fa9a48fe4b
willc-work <william.clarke@digital.justice.gov.uk>
2022-08-10 13:30:58 +0100

    make a cfe call when conditions are correct

--- app/controllers/providers/no_eligibility_assessments_controller.rb
+++ app/controllers/providers/no_eligibility_assessments_controller.rb
@@ -1,7 +1,7 @@
 module Providers
   class NoEligibilityAssessmentsController < ProviderBaseController
     def show
-      write_cfe_result
+      cfe_assessment_or_mock
       legal_aid_application.provider_enter_merits! unless legal_aid_application.provider_entering_merits?
       @result_partial = "shared/assessment_results/no_cfe_result"
     end
@@ -14,6 +14,18 @@ module Providers
       CFE::Submission.create!(legal_aid_application_id: legal_aid_application.id, aasm_state: "cfe_not_called")
     end
 
+    def cfe_assessment_or_mock
+      employed_and_hmrc_response? ? check_financial_eligibility : write_cfe_result
+    end
+
+    def employed_and_hmrc_response?
+      @legal_aid_application.applicant.employed? && !@legal_aid_application.hmrc_responses.empty?
+      # Is one of these to be preferred? Both positive below seems like it would be better?
+      # @legal_aid_application.applicant.employed? && @legal_aid_application.hmrc_responses.exists?
+    end
+
+  private
+
     def write_cfe_result
       CFE::Empty::Result.create!(
         legal_aid_application_id: legal_aid_application.id,
@@ -21,6 +33,10 @@ module Providers
         result: CFE::Empty::EmptyResult.blank_cfe_result.to_json,
         type: "CFE::Empty::Result",
       )
+    end
+
+    def check_financial_eligibility
+      CFE::SubmissionManager.call(legal_aid_application.id)
     end
   end
 end
