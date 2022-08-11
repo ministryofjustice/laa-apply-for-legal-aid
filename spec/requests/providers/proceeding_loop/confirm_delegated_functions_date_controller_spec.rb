8aab8d67efe054111b1ae9aacf204c9f45cce25c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-08-11 13:25:18 +0100

    AP-3374: Update tests for confirm df controller
    Check that post CYA page submissions are routed correctly

--- spec/requests/providers/proceeding_loop/confirm_delegated_functions_date_controller_spec.rb
+++ spec/requests/providers/proceeding_loop/confirm_delegated_functions_date_controller_spec.rb
@@ -60,9 +60,10 @@ RSpec.describe "ConfirmDelegatedFunctionsDateController", :vcr, type: :request d
       context "when the Continue button is pressed" do
         let(:submit_button) { { continue_button: "Continue" } }
 
+        before { post_df }
+
         context "and the user selected yes" do
           it "redirects to next page" do
-            post_df
             expect(response.body).to redirect_to(providers_legal_aid_application_limitations_path(application_id))
           end
         end
@@ -71,7 +72,6 @@ RSpec.describe "ConfirmDelegatedFunctionsDateController", :vcr, type: :request d
           let(:confirm) { "false" }
 
           it "redirects back to the df page" do
-            post_df
             expect(response.body).to redirect_to(providers_legal_aid_application_delegated_function_path(application_id))
           end
         end
@@ -79,14 +79,43 @@ RSpec.describe "ConfirmDelegatedFunctionsDateController", :vcr, type: :request d
         context "and the user selected nothing" do
           let(:confirm) { nil }
 
-          before { post_df }
-
           it "returns http_success" do
             expect(response).to have_http_status(:ok)
           end
 
           it "the response includes the error message" do
             expect(response.body).to include(I18n.t("providers.confirm_delegated_functions_dates.show.error"))
+          end
+        end
+
+        context "when checking answers" do
+          let(:application) do
+            create(
+              :legal_aid_application,
+              :with_applicant_and_address_lookup,
+              :checking_applicant_details,
+              :with_proceedings,
+              :with_delegated_functions_on_proceedings,
+              explicit_proceedings: %i[da001 se013],
+              df_options: { DA001: [10.days.ago, 10.days.ago], SE013: nil },
+              substantive_application_deadline_on: 10.days.from_now,
+            ).reload
+          end
+
+          context "when users confirms the old date is correct" do
+            let(:confirm) { "true" }
+
+            it "redirects to check provider answers page" do
+              expect(response).to redirect_to(providers_legal_aid_application_check_provider_answers_path(application_id))
+            end
+          end
+
+          context "when user requests to change the date" do
+            let(:confirm) { "false" }
+
+            it "redirects to delegated functions page" do
+              expect(response).to redirect_to(providers_legal_aid_application_delegated_function_path(application_id, proceeding_id))
+            end
           end
         end
       end
