b211c3acc224920be054e45f00242118145e828b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-08-24 09:43:54 +0100

    AP-3388: Add the Emergency defaults controller
    Include routes, locales and tests - with cassettes

---
+++ spec/requests/providers/proceeding_loop/emergency_defaults_controller_spec.rb
@@ -0,0 +1,102 @@
+require "rails_helper"
+
+RSpec.describe "EmergencyDefaultsController", :vcr, type: :request do
+  let(:application) do
+    create(:legal_aid_application,
+           :with_proceedings,
+           :with_delegated_functions_on_proceedings,
+           explicit_proceedings: %i[da001],
+           df_options: { DA001: [10.days.ago, 10.days.ago] },
+           substantive_application_deadline_on: 10.days.from_now)
+  end
+  let(:application_id) { application.id }
+  let(:proceeding_id) { application.proceedings.first.id }
+  let(:provider) { application.provider }
+
+  before do
+    allow(Setting).to receive(:enable_mini_loop?).and_return(true) # TODO: Remove when the mini-loop feature flag is removed
+    allow(Setting).to receive(:enable_loop?).and_return(true) # TODO: Remove when the loop feature flag is removed
+  end
+
+  describe "GET /providers/applications/:legal_aid_application_id/emergency_defaults/:proceeding_id" do
+    subject(:get_sd) { get "/providers/applications/#{application_id}/emergency_defaults/#{proceeding_id}" }
+
+    context "when the provider is not authenticated" do
+      before { get_sd }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        get_sd
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "displays the proceeding header" do
+        expect(response.body).to include("Proceeding 1")
+        expect(response.body).to include("Inherent jurisdiction high court injunction")
+        expect(response.body).to include("For the emergency application, do you want to use the default level of service and scope?")
+      end
+    end
+  end
+
+  describe "POST /providers/applications/:legal_aid_application_id/emergency_defaults/:proceeding_id" do
+    subject(:post_sd) { patch "/providers/applications/#{application_id}/emergency_defaults/#{proceeding_id}", params: }
+
+    before { allow(DelegatedFunctionsDateService).to receive(:call).and_return(true) }
+
+    let(:params) do
+      {
+        proceeding: {
+          accepted_emergency_defaults: false,
+        },
+      }
+    end
+
+    context "when the provider is not authenticated" do
+      before { post_sd }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        post_sd
+      end
+
+      context "when the Continue button is pressed" do
+        let(:submit_button) { { continue_button: "Continue" } }
+
+        it "redirects to next page" do
+          expect(response.body).to redirect_to(providers_legal_aid_application_substantive_default_path(application_id, proceeding_id))
+        end
+
+        context "when checking answers" do
+          let(:application) do
+            create(:legal_aid_application,
+                   :with_proceedings,
+                   :checking_applicant_details,
+                   :with_delegated_functions_on_proceedings,
+                   explicit_proceedings: %i[da001],
+                   df_options: { DA001: [10.days.ago, 10.days.ago] },
+                   substantive_application_deadline_on: 10.days.from_now)
+          end
+
+          before { application.reload }
+
+          context "when the date is within the last month" do
+            it "redirects to check provider answers page" do
+              expect(response).to redirect_to(providers_legal_aid_application_check_provider_answers_path(application_id))
+            end
+          end
+        end
+      end
+    end
+  end
+end
