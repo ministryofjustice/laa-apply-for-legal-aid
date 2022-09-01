9177cdf3085e12f86a3d082c5667c7fd92a214c5
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-09-01 12:15:04 +0100

    Remove spec testing implementation

--- spec/models/legal_aid_application_transaction_type_spec.rb
+++ spec/models/legal_aid_application_transaction_type_spec.rb
@@ -67,33 +67,25 @@ RSpec.describe LegalAidApplicationTransactionType, type: :model do
 
       legal_aid_application.cash_transactions.create!(transaction_type_id: credit_transaction_type.id, amount: 101, month_number: 1, transaction_date: Time.zone.now.to_date)
       legal_aid_application.cash_transactions.create!(transaction_type_id: debit_transaction_type.id, amount: 103, month_number: 3, transaction_date: 2.months.ago)
-
-      allow(instance).to receive(:cascade_delete_cash_transactions).and_call_original
     end
 
     context "when destroying object instance" do
       let(:action) { instance.destroy! }
 
-      it "calls cascade_delete_cash_transactions" do
-        action
-        expect(instance).to have_received(:cascade_delete_cash_transactions)
-      end
-
-      it "cascades delete to cash transactions of that type" do
+      it "deletes related cash transaction" do
         expect { action }.to change(legal_aid_application.cash_transactions, :count).by(-1)
+        transaction_types = legal_aid_application.cash_transactions.map(&:transaction_type)
+        expect(transaction_types).not_to include(credit_transaction_type)
       end
     end
 
     context "when updating object instance" do
       let(:action) { instance.update!(updated_at: Time.zone.now) }
 
-      it "does not call cascade_delete_cash_transactions" do
-        action
-        expect(instance).not_to have_received(:cascade_delete_cash_transactions)
-      end
-
       it "does not delete cash transactions" do
         expect { action }.not_to change(legal_aid_application.cash_transactions, :count)
+        transaction_types = legal_aid_application.cash_transactions.map(&:transaction_type)
+        expect(transaction_types).to include(credit_transaction_type, debit_transaction_type)
       end
     end
   end
