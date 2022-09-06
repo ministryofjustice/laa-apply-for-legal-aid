cfa900d7eb2c1b03fd0315a7b856f0ffaddd865e
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-09-06 11:12:23 +0100

    ap-3408: add level_of_service pages to flow

--- spec/requests/providers/proceeding_loop/emergency_level_of_service_spec.rb
+++ spec/requests/providers/proceeding_loop/emergency_level_of_service_spec.rb
@@ -41,6 +41,60 @@ RSpec.describe "EmergencyLevelOfServiceController", :vcr, type: :request do
         expect(response.body).to include("Proceeding 1")
         expect(response.body).to include("Inherent jurisdiction high court injunction")
       end
+
+      context "when the proceeding has only one possible level of service" do
+        it "displays a message that the level of service cannot be changed" do
+          expect(response.body).to include("You cannot change the default level of service for the emergency application for this proceeding")
+        end
+      end
+
+      context "when the proceeding has more than one possible level of service" do
+        let(:application) do
+          create(:legal_aid_application,
+                 :with_proceedings,
+                 :with_delegated_functions_on_proceedings,
+                 explicit_proceedings: %i[se013],
+                 df_options: { SE013: [10.days.ago, 10.days.ago] },
+                 substantive_application_deadline_on: 10.days.from_now)
+        end
+
+        it "displays a message that the level of service cannot be changed" do
+          expect(response.body).to include("For the emergency application, select the level of service")
+        end
+      end
+    end
+  end
+
+  describe "POST /providers/applications/:legal_aid_application_id/emergency_level_of_service/:proceeding_id" do
+    subject(:post_sd) { patch "/providers/applications/#{application_id}/emergency_level_of_service/#{proceeding_id}", params: }
+
+    let(:params) do
+      {
+        proceeding: {
+          emergency_level_of_service: 3,
+        },
+      }
+    end
+
+    context "when the provider is not authenticated" do
+      before { post_sd }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+        post_sd
+      end
+
+      context "when the Continue button is pressed" do
+        let(:submit_button) { { continue_button: "Continue" } }
+
+        it "redirects to next page" do
+          expect(response.body).to redirect_to(providers_legal_aid_application_substantive_default_path(application_id, proceeding_id))
+        end
+      end
     end
   end
 end
