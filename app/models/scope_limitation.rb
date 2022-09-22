45bf00f1afffcd99e803b8d09fe3a3d678143be2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-09-22 09:07:38 +0100

    AP-3495: Update ScopeLimitation model
    Add scopes for emergency and substantive grouping

--- app/models/scope_limitation.rb
+++ app/models/scope_limitation.rb
@@ -2,4 +2,6 @@ class ScopeLimitation < ApplicationRecord
   belongs_to :proceeding
 
   enum scope_type: { substantive: 0, emergency: 1 }
+  scope :emergency, -> { where(scope_type: :emergency) }
+  scope :substantive, -> { where(scope_type: :substantive) }
 end
