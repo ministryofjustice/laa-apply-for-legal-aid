1169d3f76c90cfaab3be3c7aed53d86a3f59ee31
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-09-27 09:45:46 +0100

    AP-3495: remove scope_limitation fields from Proceeding
    After the previous migration has been executed and the rake task
    has been run that moves data from the proceedings table to the
    new ScopeLimitation table, this can be done to remove the original
    columns

---
+++ db/migrate/20220915131126_remove_proceedings_scope_limitations.rb
@@ -0,0 +1,34 @@
+class RemoveProceedingsScopeLimitations < ActiveRecord::Migration[7.0]
+  def up
+    raise StandardError, "Proceeding table has scope limitation data" if any_proceeding_data_present?
+
+    change_table :proceedings, bulk: true do |t|
+      t.remove :substantive_scope_limitation_code
+      t.remove :substantive_scope_limitation_meaning
+      t.remove :substantive_scope_limitation_description
+      t.remove :delegated_functions_scope_limitation_code
+      t.remove :delegated_functions_scope_limitation_meaning
+      t.remove :delegated_functions_scope_limitation_description
+    end
+  end
+
+  def down
+    change_table :proceedings, bulk: true do |t|
+      t.string :substantive_scope_limitation_code
+      t.string :substantive_scope_limitation_meaning
+      t.string :substantive_scope_limitation_description
+      t.string :delegated_functions_scope_limitation_code
+      t.string :delegated_functions_scope_limitation_meaning
+      t.string :delegated_functions_scope_limitation_description
+    end
+  end
+
+  def any_proceeding_data_present?
+    Proceeding.where.not(substantive_scope_limitation_code: nil,
+                         substantive_scope_limitation_meaning: nil,
+                         substantive_scope_limitation_description: nil,
+                         delegated_functions_scope_limitation_code: nil,
+                         delegated_functions_scope_limitation_meaning: nil,
+                         delegated_functions_scope_limitation_description: nil).count.positive?
+  end
+end
