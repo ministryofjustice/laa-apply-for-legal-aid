f3bcaa59b83a09241eb76e2202aeda7ae8b70816
Peter Hunt <naseberry@users.noreply.github.com>
2022-10-03 15:13:00 +0100

    Resolve StandardJS rule `no-unused-vars`

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -11,8 +11,7 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+require.context('../images', true)
 
 require('@rails/ujs').start()
 require('@rails/activestorage').start()
