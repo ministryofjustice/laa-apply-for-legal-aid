c4937cb83ece783119583581eea5c616352cf968
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-10-04 14:57:44 +0100

    AP-3534: Update default firm permissions
    Ensure that a newly created firm has the employed permissions

--- spec/services/provider_details_creator_spec.rb
+++ spec/services/provider_details_creator_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe ProviderDetailsCreator do
 
     it "adds non passported permission to the firm" do
       expect { subject }.to change(Firm, :count).by(1)
-      expect(firm.permissions.map(&:role)).to match_array(["application.passported.*", "application.non_passported.*"])
+      expect(firm.permissions.map(&:role)).to match_array(["application.passported.*", "application.non_passported.*", "application.non_passported.employment.*"])
     end
 
     it "creates the right offices" do
