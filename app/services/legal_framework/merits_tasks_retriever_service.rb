52adb3b22949277a3495845df1f3cb3675b4a641
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-10-13 10:24:52 +0100

    AP-3491: Update MeritsTaskRetriverService
    Allow different URLs to be generated and
    send a new request body if the mini-loop
    feature flag is on

--- app/services/legal_framework/merits_tasks_retriever_service.rb
+++ app/services/legal_framework/merits_tasks_retriever_service.rb
@@ -1,22 +1,42 @@
 module LegalFramework
   class MeritsTasksRetrieverService < BaseService
     ENDPOINT = "/merits_tasks".freeze
+    NEW_ENDPOINT = "/civil_merits_questions".freeze
 
     def url_path
+      return NEW_ENDPOINT if Setting.enable_mini_loop?
+
       ENDPOINT
     end
 
     def request_body
-      {
-        request_id:,
-        proceeding_types: proceeding_types_codes,
-      }.to_json
+      if Setting.enable_mini_loop?
+        {
+          request_id:,
+          proceedings: proceedings_data,
+        }.to_json
+      else
+        {
+          request_id:,
+          proceeding_types: proceeding_types_codes,
+        }.to_json
+      end
     end
 
   private
 
     def proceeding_types_codes
       legal_aid_application.proceedings.map(&:ccms_code)
+    end
+
+    def proceedings_data
+      legal_aid_application.proceedings.map do |proceeding|
+        {
+          ccms_code: proceeding.ccms_code,
+          delegated_functions_used: proceeding.used_delegated_functions,
+          client_involvement_type: proceeding.client_involvement_type_ccms_code,
+        }
+      end
     end
   end
 end
