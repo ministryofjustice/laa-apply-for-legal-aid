e555155843fb2e3b022d4d931a938e7b11014aac
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-10-27 08:52:38 +0100

    AP-3486: Implement page for ProhibitedSteps
    Add route, controller, view and labels

---
+++ app/controllers/providers/proceeding_merits_task/prohibited_steps_controller.rb
@@ -0,0 +1,34 @@
+module Providers
+  module ProceedingMeritsTask
+    class ProhibitedStepsController < ProviderBaseController
+      def show
+        @form = ProhibitedStepsForm.new(model: prohibited_steps)
+      end
+
+      def update
+        @form = ProhibitedStepsForm.new(form_params.merge(proceeding_id: proceeding.id))
+        render :show unless update_task_save_continue_or_draft(proceeding.ccms_code.to_sym, :prohibited_steps)
+      end
+
+    private
+
+      def prohibited_steps
+        @prohibited_steps ||= proceeding.prohibited_steps || proceeding.build_prohibited_steps
+      end
+
+      def proceeding
+        @proceeding ||= Proceeding.find(params[:merits_task_list_id])
+      end
+
+      def legal_aid_application
+        @legal_aid_application ||= proceeding.legal_aid_application
+      end
+
+      def form_params
+        merge_with_model(prohibited_steps) do
+          params.require(:proceeding_merits_task_prohibited_steps).permit(:uk_removal, :details)
+        end
+      end
+    end
+  end
+end
