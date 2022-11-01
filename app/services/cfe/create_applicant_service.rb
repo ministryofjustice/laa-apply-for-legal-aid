8f2715e9af012b88e10c7b268adaf1ff7c4c46e0
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-11-01 12:57:00 +0000

    Ensure only booleans sent to CFE for employed?

--- app/services/cfe/create_applicant_service.rb
+++ app/services/cfe/create_applicant_service.rb
@@ -8,7 +8,7 @@ module CFE
       {
         applicant: {
           date_of_birth: applicant.date_of_birth.strftime("%Y-%m-%d"),
-          employed: applicant.employed,
+          employed: applicant.employed?,
           involvement_type: "applicant",
           has_partner_opponent: false,
           receives_qualifying_benefit: legal_aid_application.applicant_receives_benefit?,
