3dd6ba4df3f899e741d6d36f0e7356ec5fa038a2
Connor McQuillan <me@cpjmcquillan.com>
2022-11-21 12:13:46 +0000

    Stop ignoring columns
    These columns were ignored so they could be removed safely.
    
    They have since been removed in #4595, so we can stop ignoring the columns.

--- app/models/proceeding_merits_task/attempts_to_settle.rb
+++ app/models/proceeding_merits_task/attempts_to_settle.rb
@@ -1,7 +1,5 @@
 module ProceedingMeritsTask
   class AttemptsToSettle < ApplicationRecord
-    self.ignored_columns += %w[application_proceeding_type_id]
-
     belongs_to :proceeding
   end
 end
--- app/models/proceeding_merits_task/chances_of_success.rb
+++ app/models/proceeding_merits_task/chances_of_success.rb
@@ -1,7 +1,5 @@
 module ProceedingMeritsTask
   class ChancesOfSuccess < ApplicationRecord
-    self.ignored_columns += %w[application_proceeding_type_id]
-
     belongs_to :proceeding
 
     PRETTY_SUCCESS_PROSPECTS = {
