49753318bd27448e45c53fd74d9e53593fcb4d67
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-11-22 08:48:26 +0000

    AP-3593: Remove full_name column from Opponents DB table

---
+++ db/migrate/20221122083958_remove_full_name_from_opponent.rb
@@ -0,0 +1,13 @@
+class RemoveFullNameFromOpponent < ActiveRecord::Migration[7.0]
+  def change
+    raise StandardError, "Opponent table still has full_name values" if full_name_values_present?
+
+    safety_assured do
+      remove_column :opponents, :full_name, :string
+    end
+  end
+
+  def full_name_values_present?
+    ApplicationMeritsTask::Opponent.where.not(full_name: nil).count.positive?
+  end
+end
