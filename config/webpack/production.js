dcc40360e5794760e42bfd909a9737c1bcba92ed
Peter Hunt <naseberry@users.noreply.github.com>
2022-11-25 14:11:24 +0000

    Omit asset sourcemaps from prod env

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.config.merge({
+  devtool: 'none'
+})
+
 module.exports = environment.toWebpackConfig()
