aade0b1d0af74ac3597027729e606dd413c59524
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2022-11-30 16:45:17 +0000

    alert provider when a client start email has failed
    add new method to undeliverable email alerts mailer, add tests

--- app/services/govuk_emails/monitor.rb
+++ app/services/govuk_emails/monitor.rb
@@ -22,9 +22,44 @@ module GovukEmails
       AlertManager.capture_message("Unable to deliver mail to #{@scheduled_mail.addressee} - ScheduledMailing record #{@scheduled_mail.id}")
       ScheduledMailing.send_now!(mailer_klass: UndeliverableEmailAlertMailer,
                                  mailer_method: :notify_apply_team,
-                                 legal_aid_application_id: @scheduled_mail.legal_aid_application_id,
+                                 legal_aid_application_id: legal_aid_application.id,
                                  addressee: Rails.configuration.x.support_email_address,
                                  arguments: [@scheduled_mail.id])
+
+      notify_provider_email_failed if @scheduled_mail.mailer_method == "citizen_start_email"
+    end
+
+    def notify_provider_email_failed
+      ScheduledMailing.send_now!(mailer_klass: UndeliverableEmailAlertMailer,
+                                 mailer_method: :notify_provider,
+                                 legal_aid_application_id: legal_aid_application.id,
+                                 addressee: provider_email_or_support,
+                                 arguments: mailer_args)
+    end
+
+    def mailer_args
+      [
+        provider.email,
+        legal_aid_application.application_ref,
+        applicant.full_name,
+        applicant.email,
+      ]
+    end
+
+    def provider_email_or_support
+      HostEnv.staging? ? Rails.configuration.x.support_email_address : provider.email
+    end
+
+    def legal_aid_application
+      @legal_aid_application = @scheduled_mail.legal_aid_application
+    end
+
+    def applicant
+      @applicant ||= legal_aid_application.applicant
+    end
+
+    def provider
+      @provider ||= legal_aid_application.provider
     end
   end
 end
--- spec/services/govuk_emails/monitor_spec.rb
+++ spec/services/govuk_emails/monitor_spec.rb
@@ -42,26 +42,65 @@ RSpec.describe GovukEmails::Monitor do
 
         it "schedules an undeliverable alert email" do
           expect { subject }.to change(ScheduledMailing, :count).by(1)
-          undeliverable_mail = ScheduledMailing.order(:created_at).last
+          undeliverable_alert_mail = ScheduledMailing.order(:created_at).last
 
-          expect(undeliverable_mail.legal_aid_application_id).to eq scheduled_mailing.legal_aid_application_id
-          expect(undeliverable_mail.mailer_klass).to eq "UndeliverableEmailAlertMailer"
-          expect(undeliverable_mail.mailer_method).to eq "notify_apply_team"
-          expect(undeliverable_mail.arguments).to eq [scheduled_mailing.id]
-          expect(undeliverable_mail.scheduled_at).to have_been_in_the_past
-          expect(undeliverable_mail.addressee).to eq Rails.configuration.x.support_email_address
+          expect(undeliverable_alert_mail.legal_aid_application_id).to eq scheduled_mailing.legal_aid_application_id
+          expect(undeliverable_alert_mail.mailer_klass).to eq "UndeliverableEmailAlertMailer"
+          expect(undeliverable_alert_mail.mailer_method).to eq "notify_apply_team"
+          expect(undeliverable_alert_mail.arguments).to eq [scheduled_mailing.id]
+          expect(undeliverable_alert_mail.scheduled_at).to have_been_in_the_past
+          expect(undeliverable_alert_mail.addressee).to eq Rails.configuration.x.support_email_address
+        end
+      end
+
+      context "when the failed email is a citizen start email" do
+        let(:applicant) { create(:applicant, email: "johndoe@example.net") }
+        let(:provider) { create(:provider) }
+        let(:legal_aid_application) { create(:legal_aid_application, applicant:, provider:) }
+        let(:scheduled_mailing) do
+          create(:scheduled_mailing,
+                 :citizen_start_email,
+                 :processing,
+                 addressee: applicant.email,
+                 legal_aid_application_id: legal_aid_application.id)
+        end
+        let(:mailer_args) do
+          [
+            provider.email,
+            legal_aid_application.application_ref,
+            applicant.full_name,
+            applicant.email,
+          ]
+        end
+
+        before { allow(HostEnv).to receive(:production?).and_return(true) }
+
+        it "schedules an email to notify the provider of the failure" do
+          expect { subject }.to change(ScheduledMailing, :count).by(2)
+        end
+
+        it "uses the undeliverable email alert mailer and the notify_provider method with the right args" do
+          subject
+          provider_alert_mail = ScheduledMailing.order(:created_at).last
+
+          expect(provider_alert_mail.legal_aid_application_id).to eq scheduled_mailing.legal_aid_application_id
+          expect(provider_alert_mail.mailer_klass).to eq "UndeliverableEmailAlertMailer"
+          expect(provider_alert_mail.mailer_method).to eq "notify_provider"
+          expect(provider_alert_mail.addressee).to eq provider.email
+          expect(provider_alert_mail.arguments).to eq mailer_args
+          expect(provider_alert_mail.scheduled_at).to have_been_in_the_past
         end
       end
 
       context "not on production host" do
         before { allow(HostEnv).to receive(:production?).and_return(false) }
 
-        it "does not captures raven message" do
+        it "does not capture raven message" do
           expect(AlertManager).not_to receive(:capture_message)
           subject
         end
 
-        it "does not schedules an undeliverable alert email" do
+        it "does not schedule an undeliverable alert email" do
           expect { subject }.not_to change(ScheduledMailing, :count)
         end
       end
