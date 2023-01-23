925a6319f1e1f9e020b23fcfa38abeec7fdb08dc
Peter Hunt <naseberry@users.noreply.github.com>
2023-01-23 12:41:31 +0000

    Correct spelling of Ordnance Survey
    Not all references were updated in commit #6852f31
    This new commit captures the remaining references

--- spec/services/address_lookup_service_spec.rb
+++ spec/services/address_lookup_service_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AddressLookupService do
 
   let(:query_params) do
     {
-      key: ENV.fetch("ORDNANACE_SURVEY_API_KEY", nil),
+      key: ENV.fetch("ORDNANCE_SURVEY_API_KEY", nil),
       postcode:,
       lr: "EN",
     }
