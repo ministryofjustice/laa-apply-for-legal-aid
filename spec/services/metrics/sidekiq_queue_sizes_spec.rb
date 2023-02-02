8360cb6d4a3accfb08897075267be71e2d770667
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-02-02 11:41:29 +0000

    Fix: Remove sidekiq-alive queue
    We removed the sidekiq-alive gem in Nov 2021 #2823
    This should have been removed at the same time!

--- spec/services/metrics/sidekiq_queue_sizes_spec.rb
+++ spec/services/metrics/sidekiq_queue_sizes_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Metrics::SidekiqQueueSizes do
   describe "#call" do
     subject { described_class.call(prometheus_client) }
 
-    let(:queues) { %w[default mailers sidekiq_alive active_storage_analysis active_storage_purge] }
+    let(:queues) { %w[default mailers active_storage_analysis active_storage_purge] }
     let(:prometheus_client) { spy(PrometheusExporter::Client) }
     let(:collector_type) { PrometheusCollectors::SidekiqQueueCollector::COLLECTOR_TYPE }
 
