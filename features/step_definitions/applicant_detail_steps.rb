e56a7e2aa956c08b1e018c23f545f98916847d45
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-02-07 11:50:00 +0000

    Feature test for blocking under 16 client applications
    Under 16 year old clients cannot apply for legal aid via this
    service. They must use CCMS.
    
    This rule ceases to have meaning after MTR phase one is enabled
    as any under 18s can apply via this service without a means test.
    
    NOTE: This feature can be deleted entirely once MTR phase one is enabled

--- features/step_definitions/applicant_detail_steps.rb
+++ features/step_definitions/applicant_detail_steps.rb
@@ -1,3 +1,10 @@
+When("I enter a date of birth that will make me 16 today") do
+  dob = 16.years.ago
+  fill_in("applicant_date_of_birth_3i", with: dob.day)
+  fill_in("applicant_date_of_birth_2i", with: dob.month)
+  fill_in("applicant_date_of_birth_1i", with: dob.year)
+end
+
 When("I enter a date of birth that will make me 18 tomorrow") do
   dob = (18.years - 1.day).ago
   fill_in("applicant_date_of_birth_3i", with: dob.day)
