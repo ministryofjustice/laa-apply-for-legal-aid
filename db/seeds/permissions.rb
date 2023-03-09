268a5cc77d94fbb30c618552f647b9c374553fed
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-03-09 07:49:55 +0000

    AP-3896: Update PermissionsPopulator tidy method
    Also ensure that all remaining ActorPermissions are for an exitsing
    Permission record.  Previously we deleted records from the Permissions
    table but neglected to remove the linked ActorPermissions, this script
    groups all ActorPermissions by permission_id and iterates the loop if
    the matching Permission can be found, otherwise it deletes all
    ActorPermissions and outputs into the seed logs so we can monitor
    output

--- db/seeds/permissions.rb
+++ db/seeds/permissions.rb
@@ -16,6 +16,13 @@ class PermissionsPopulator
       Rails.logger.info "Removing #{permission.role} permission from database"
       permission.destroy!
     end
+    ActorPermission.group(:permission_id).count.each do |permission_id, count|
+      next if Permission.find(permission_id)
+
+    rescue ActiveRecord::RecordNotFound
+      Rails.logger.info "Delete #{count} ActorPermissions for #{permission_id}"
+      ActorPermission.where(permission_id:).delete_all
+    end
   end
 end
 
