c009172625b3a149db83109bac57a41964da929b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-03-22 08:02:22 +0000

    AP-3800: Update client_has_partner_form_spec
    Split save and validate tests, remove some older style
    now superfluous tests

--- spec/forms/providers/partners/client_has_partner_form_spec.rb
+++ spec/forms/providers/partners/client_has_partner_form_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Providers::Partners::ClientHasPartnerForm, type: :form do
-  subject(:instance) { described_class.new(params) }
+  subject(:client_partner_form) { described_class.new(params) }
 
-  let(:applicant) { create(:applicant, has_partner: nil) }
-  let(:legal_aid_application) { create(:legal_aid_application, applicant:) }
+  let(:applicant) { create(:applicant, has_partner: applicant_has_partner) }
+  let(:applicant_has_partner) { nil }
 
   let(:params) do
     {
@@ -13,14 +13,43 @@ RSpec.describe Providers::Partners::ClientHasPartnerForm, type: :form do
     }
   end
 
-  describe ".model_name" do
-    it 'is "Applicant"' do
-      expect(described_class.model_name).to eq("Applicant")
+  describe "#validate" do
+    subject(:validate_form) { client_partner_form.valid? }
+
+    before { validate_form }
+
+    context "with yes chosen" do
+      let(:has_partner) { "true" }
+
+      it "is valid" do
+        expect(client_partner_form).to be_valid
+      end
+    end
+
+    context "with no chosen" do
+      let(:has_partner) { "false" }
+
+      it "is valid" do
+        expect(client_partner_form).to be_valid
+      end
+    end
+
+    context "when no answer chosen" do
+      let(:params) { { has_partner: "", model: applicant } }
+
+      it "is invalid" do
+        expect(client_partner_form).to be_invalid
+      end
+
+      it "adds custom blank error message" do
+        error_messages = client_partner_form.errors.messages.values.flatten
+        expect(error_messages).to include("Select yes if the client has a partner")
+      end
     end
   end
 
   describe "#save" do
-    subject(:call_save) { instance.save }
+    subject(:call_save) { client_partner_form.save }
 
     context "with yes chosen and valid national insurance number provided" do
       let(:params) { { model: applicant, has_partner: "true" } }
@@ -53,62 +82,29 @@ RSpec.describe Providers::Partners::ClientHasPartnerForm, type: :form do
         end
       end
     end
-
-    context "with no answer chosen" do
-      let(:params) { { has_partner: "", model: applicant } }
-
-      it "is invalid" do
-        call_save
-        expect(instance).to be_invalid
-      end
-
-      it "adds custom blank error message" do
-        call_save
-        error_messages = instance.errors.messages.values.flatten
-        expect(error_messages).to include("Select yes if the client has a partner")
-      end
-    end
   end
 
   describe "#save_as_draft" do
-    subject(:save_as_draft) { instance.save_as_draft }
+    subject(:save_as_draft) { client_partner_form.save_as_draft }
 
     context "with yes chosen" do
-      let(:params) { { model: applicant, has_partner: "true" } }
+      let(:has_partner) { "true" }
 
       it "updates has_partner" do
         expect { save_as_draft }.to change { applicant.attributes.symbolize_keys }
                                       .from(hash_including(has_partner: nil))
                                       .to(hash_including(has_partner: true))
       end
-
-      it "is valid" do
-        save_as_draft
-        expect(instance).to be_valid
-      end
     end
 
     context "with no chosen" do
-      let(:params) { { model: applicant, has_partner: "false" } }
+      let(:applicant_has_partner) { nil }
+      let(:has_partner) { "false" }
 
       it "updates has_partner" do
         expect { save_as_draft }.to change { applicant.attributes.symbolize_keys }
                                       .from(hash_including(has_partner: nil))
                                       .to(hash_including(has_partner: false))
-      end
-
-      it "is valid" do
-        save_as_draft
-        expect(instance).to be_valid
-      end
-    end
-
-    context "with no answer chosen" do
-      let(:params) { { model: applicant, has_partner: "" } }
-
-      it "is valid" do
-        save_as_draft
-        expect(instance).to be_valid
       end
     end
   end
