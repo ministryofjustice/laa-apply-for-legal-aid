8ca09f35e0d3c1eb3a628c335e93a20af3ce54ce
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-04-21 17:14:20 +0100

    AP-4024: Add method of storing result in spreadsheet

---
+++ app/services/cfe/store_compare_result.rb
@@ -0,0 +1,66 @@
+module CFE
+  class StoreCompareResult
+    SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+
+    def self.call(value_array)
+      new.call(value_array)
+    end
+
+    def initialize
+      log_message "Initializing"
+      secret_file = StringIO.new(google_secret.to_json)
+      authorizer = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: secret_file, scope: SCOPE)
+      authorizer.fetch_access_token!
+
+      @sheet_service = Google::Apis::SheetsV4::SheetsService.new
+      @sheet_service.authorization = authorizer
+      worksheet_reload
+      @rows = []
+    end
+
+    def call(array)
+      @rows = [array]
+      range = "Sheet1!A1:A"
+      value_range = Google::Apis::SheetsV4::ValueRange.new(major_dimension: "ROWS",
+                                                           range:,
+                                                           values: @rows)
+      output = @sheet_service.append_spreadsheet_value(spreadsheet_key,
+                                                       range,
+                                                       value_range,
+                                                       value_input_option: "USER_ENTERED")
+      log_message = output.updates.updated_range
+      log_message "Updated #{log_message}"
+    end
+
+  private
+
+    def worksheet_reload
+      @spreadsheet = @sheet_service.get_spreadsheet(spreadsheet_key)
+    end
+
+    def spreadsheet_key
+      @spreadsheet_key ||= ENV.fetch("CFE_COMPARISON_SHEET_ID", nil)
+    end
+
+    def log_message(message)
+      message = "#{self.class} :: #{message}"
+      message = Time.zone.now.strftime("%F %T.%L ") + message if Rails.env.development?
+      Rails.logger.info message
+    end
+
+    def google_secret
+      {
+        type: "service_account",
+        project_id: "laa-apply-for-legal-aid",
+        private_key_id: ENV.fetch("GOOGLE_SHEETS_PRIVATE_KEY_ID", nil),
+        private_key: ENV.fetch("GOOGLE_SHEETS_PRIVATE_KEY_VALUE", nil),
+        client_email: ENV.fetch("GOOGLE_SHEETS_CLIENT_EMAIL", nil),
+        client_id: ENV.fetch("GOOGLE_SHEETS_CLIENT_ID", nil),
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/laa-apply-service%40laa-apply-for-legal-aid.iam.gserviceaccount.com",
+      }
+    end
+  end
+end
