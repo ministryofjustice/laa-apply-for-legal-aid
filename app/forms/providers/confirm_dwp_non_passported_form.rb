1ee7c8f8e49349de97e70274633b7f72a69c931d
Peter Hunt <naseberry@users.noreply.github.com>
2023-05-23 12:12:59 +0100

    Remove class `ConfirmDwpNonPassportedForm`
    Class added to previous commit seem irrelevant to the task

--- app/forms/providers/confirm_dwp_non_passported_form.rb
+++
@@ -1,9 +0,0 @@
-module Providers
-  class ConfirmDwpNonPassportedForm < BaseForm
-    form_for Provider
-
-    attr_accessor :correct_dwp_result
-
-    validates :selected_office_id, presence: true
-  end
-end
