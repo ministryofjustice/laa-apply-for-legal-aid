de80efa9f34c12188b8a9cd68d873cc2662f3d4b
Adam Goldstone <adam.goldstone@digital.justice.gov.uk>
2023-05-24 13:13:01 +0100

    update wording on spec, remove unused translation file text

--- spec/forms/providers/confirm_dwp_non_passported_applications_form_spec.rb
+++ spec/forms/providers/confirm_dwp_non_passported_applications_form_spec.rb
@@ -5,41 +5,39 @@ RSpec.describe Providers::ConfirmDWPNonPassportedApplicationsForm, type: :form d
 
   let(:params) { { confirm_dwp_result: nil } }
 
-  describe "#save" do
-    context "with validation" do
-      before { form.valid? }
+  describe "validation" do
+    before { form.valid? }
 
-      context "when the dwp result is correct" do
-        let(:params) { { confirm_dwp_result: "dwp_correct" } }
+    context "when the dwp result is correct" do
+      let(:params) { { confirm_dwp_result: "dwp_correct" } }
 
-        it { is_expected.to be_valid }
+      it { is_expected.to be_valid }
+    end
+
+    context "when the applicant receives a passporting benefit that is not joint" do
+      let(:params) { { confirm_dwp_result: "joint_with_partner_false" } }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when the applicant receives a joint passporting benefit with their partner" do
+      let(:params) { { confirm_dwp_result: "joint_with_partner_true" } }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when the parameters are missing" do
+      it { is_expected.to be_invalid }
+
+      it "records the error message" do
+        expect(form.errors[:confirm_dwp_result]).to eq [I18n.t("providers.confirm_dwp_non_passported_applications.show.error")]
       end
+    end
 
-      context "when the applicant receives a passporting benefit that is not joint" do
-        let(:params) { { confirm_dwp_result: "joint_with_partner_false" } }
+    context "when saving as draft" do
+      before { form.save_as_draft }
 
-        it { is_expected.to be_valid }
-      end
-
-      context "when the applicant receives a joint passporting benefit with their partner" do
-        let(:params) { { confirm_dwp_result: "joint_with_partner_true" } }
-
-        it { is_expected.to be_valid }
-      end
-
-      context "when the parameters are missing" do
-        it { is_expected.to be_invalid }
-
-        it "records the error message" do
-          expect(form.errors[:confirm_dwp_result]).to eq [I18n.t("providers.confirm_dwp_non_passported_applications.show.error")]
-        end
-      end
-
-      context "when saving as draft" do
-        before { form.save_as_draft }
-
-        it { is_expected.to be_valid }
-      end
+      it { is_expected.to be_valid }
     end
   end
 end
