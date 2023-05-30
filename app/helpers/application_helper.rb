02e0e0a495fc9705712811638b6f6290eaeac452
Peter Hunt <naseberry@users.noreply.github.com>
2023-05-30 14:18:57 +0100

    Remove redundant start button code
    CSS - remove IE11 specific style
    Ruby - remove redundant application helper `start_button_label`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -73,17 +73,6 @@ module ApplicationHelper
     content_tag :button, text, class: "govuk-button govuk-button--secondary no-print print-button", type: "button"
   end
 
-  def start_button_label(button_text)
-    "#{t("generic.#{button_text}")} ".html_safe << content_tag(:svg,
-                                                               content_tag(:path, "", fill: "currentColor", d: "M0 0h13l20 20-20 20H0l20-20z"),
-                                                               class: "govuk-button__start-icon",
-                                                               xmlns: "http://www.w3.org/2000/svg",
-                                                               height: "19",
-                                                               viewBox: "0 0 33 40",
-                                                               role: "presentation",
-                                                               focusable: "false")
-  end
-
   def linked_children_names(proceeding)
     proceeding.involved_children.map(&:full_name).join("</br>").html_safe
   end
