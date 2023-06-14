4a1d57c039bab3401311b26205c8a7a48d0cd2e8
Adam Goldstone <adam.goldstone@digital.justice.gov.uk>
2023-06-14 16:09:23 +0100

    update tests to correctly create partner

--- spec/requests/providers/partners/about_financial_means_controller_spec.rb
+++ spec/requests/providers/partners/about_financial_means_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Providers::Partners::AboutFinancialMeansController do
-  let(:legal_aid_application) { create(:legal_aid_application, :applicant_details_checked, :with_applicant) }
+  let(:legal_aid_application) { create(:legal_aid_application, :with_applicant_and_partner) }
   let(:provider) { legal_aid_application.provider }
 
   describe "GET /providers/applications/:id/partners/about_financial_means" do
