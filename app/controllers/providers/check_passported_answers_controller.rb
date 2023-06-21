8e0be3c516df694eae3222e81339ccdc8908555a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-06-21 11:59:21 +0100

    AP-4263: Update CheckPassportedAnswers controller CFE handling
    Stop calling the CFE::SubmissionRouter that relies on HostEnv, only
    call the CFECivil::SubmissionBuilder direct

--- app/controllers/providers/check_passported_answers_controller.rb
+++ app/controllers/providers/check_passported_answers_controller.rb
@@ -31,7 +31,7 @@ module Providers
     def check_financial_eligibility
       result = false
       log_duration("CFE Submission :: Total call time for #{legal_aid_application.id}") do
-        result = CFE::SubmissionRouter.call(legal_aid_application)
+        result = CFECivil::SubmissionBuilder.call(@legal_aid_application)
       end
       result
     end
