f2a9103f1383bcefb204026102c5a6c3d8ba4308
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-06-26 14:15:32 +0100

    AP-4222: Implement new query object for incomplete proceedings

---
+++ app/services/query/incomplete_proceedings.rb
@@ -0,0 +1,44 @@
+module Query
+  class IncompleteProceedings
+    def initialize(legal_aid_application)
+      @legal_aid_application = legal_aid_application
+    end
+
+    def self.call(legal_aid_application)
+      new(legal_aid_application).call
+    end
+
+    def call
+      results = ActiveRecord::Base.connection.execute(query)
+      Proceeding.where(id: results.values.flatten)
+    end
+
+  private
+
+    def query
+      <<~SQL.squish
+        SELECT
+            proceedings.id
+        FROM proceedings
+        WHERE (legal_aid_application_id='#{@legal_aid_application.id}')
+        AND
+        (
+            NOT EXISTS (select 'has_scope_limitations' FROM scope_limitations where (proceeding_id = proceedings.id))
+            OR (used_delegated_functions IS NULL)
+            OR (accepted_substantive_defaults IS NULL)
+            OR (accepted_substantive_defaults IS NULL)
+            OR (client_involvement_type_ccms_code IS NULL)
+            OR (used_delegated_functions = true AND accepted_emergency_defaults IS NULL)
+            OR (accepted_substantive_defaults = false
+                AND (substantive_level_of_service IS NULL
+                    OR substantive_level_of_service_name IS NULL
+                    OR substantive_level_of_service_stage IS NULL))
+            OR (accepted_emergency_defaults = false
+                AND (emergency_level_of_service IS NULL
+                    OR emergency_level_of_service_name IS NULL
+                    OR emergency_level_of_service_stage IS NULL))
+        );
+      SQL
+    end
+  end
+end
---
+++ spec/services/query/incomplete_proceedings_spec.rb
@@ -0,0 +1,105 @@
+require "rails_helper"
+
+module Query
+  RSpec.describe IncompleteProceedings do
+    subject(:query) { described_class.call(proceeding.legal_aid_application) }
+
+    context "when a proceeding has all values present" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               accepted_emergency_defaults: true,
+               accepted_substantive_defaults: true)
+      end
+
+      it { expect(query).not_to include(proceeding) }
+    end
+
+    context "when a proceeding has not used df and accepted_emergency_defaults is missing" do
+      let(:proceeding) do
+        create(:proceeding, :da001,
+               used_delegated_functions: false,
+               accepted_emergency_defaults: nil,
+               accepted_substantive_defaults: true)
+      end
+
+      it { expect(query).not_to include(proceeding) }
+    end
+
+    context "when a proceeding has not had the client involvement type question answered" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               client_involvement_type_description: nil,
+               client_involvement_type_ccms_code: nil,
+               used_delegated_functions: true,
+               accepted_emergency_defaults: true,
+               accepted_substantive_defaults: true)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+
+    context "when a proceeding has not had the used_delegated_functions question answered" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               used_delegated_functions: nil,
+               accepted_emergency_defaults: true,
+               accepted_substantive_defaults: true)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+
+    context "when a proceeding has not had the accepted_emergency_defaults question answered" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               accepted_emergency_defaults: nil)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+
+    context "when a proceeding has not had the accepted_substantive_defaults question answered" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               accepted_substantive_defaults: nil)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+
+    context "when a proceeding has not accepted_substantive_defaults and not set a new default" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               accepted_substantive_defaults: false,
+               substantive_level_of_service: nil,
+               substantive_level_of_service_name: nil,
+               substantive_level_of_service_stage: nil)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+
+    context "when a proceeding has not accepted_emergency_defaults and not set a new default" do
+      let(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               accepted_emergency_defaults: false,
+               emergency_level_of_service: nil,
+               emergency_level_of_service_name: nil,
+               emergency_level_of_service_stage: nil)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+
+    context "when a proceeding has rejected the substantive defaults and not added scope_limitations" do
+      let!(:proceeding) do
+        create(:proceeding, :da001, :with_df_date,
+               no_scope_limitations: true,
+               accepted_emergency_defaults: true,
+               accepted_substantive_defaults: false)
+      end
+
+      it { expect(query).to include(proceeding) }
+    end
+  end
+end
