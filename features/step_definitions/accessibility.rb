c48f99005a8c78bbfc6b69ae01e3fb7ec8b3793c
Peter Hunt <naseberry@users.noreply.github.com>
2023-06-27 15:09:33 +0100

    [AP-4209] remove accessibility step definition
    This step definition is now redundant as all explicit calls have been removed

--- features/step_definitions/accessibility.rb
+++
@@ -1,4 +0,0 @@
-Given("the page is accessible") do
-  # skip aria-allowed-attr to allow for gov_uk conditional radio buttons
-  steps %(Then the page should be axe clean skipping: region, aria-allowed-attr)
-end
