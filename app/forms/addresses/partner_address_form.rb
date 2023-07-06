d6d315fdda0bde2513528e767f4003f66ccf69ad
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-07-06 11:39:52 +0100

    AP-4266: remove partner address forms

--- app/forms/addresses/partner_address_form.rb
+++
@@ -1,7 +0,0 @@
-module Addresses
-  class PartnerAddressForm < BaseAddressForm
-    form_for Partner
-
-    attr_accessor :address_line_one, :address_line_two, :city, :county, :postcode, :lookup_postcode, :lookup_error
-  end
-end
--- app/forms/addresses/partner_address_lookup_form.rb
+++
@@ -1,7 +0,0 @@
-module Addresses
-  class PartnerAddressLookupForm < BaseAddressLookupForm
-    form_for Partner
-
-    attr_accessor :postcode
-  end
-end
