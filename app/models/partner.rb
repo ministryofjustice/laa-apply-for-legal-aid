e2c7c7559f9287d8aa7b9bd73f2f0eef2e89e702
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-07-06 13:29:40 +0100

    AP-4266: Update Partner model to remove address methods
    Ensure validation tests are run

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,40 +2,4 @@ class Partner < ApplicationRecord
   belongs_to :legal_aid_application, dependent: :destroy
   has_many :hmrc_responses, class_name: "HMRC::Response", as: :owner
   has_many :employments, as: :owner
-
-  def pretty_postcode
-    pretty_postcode? ? postcode : postcode.insert(-4, " ")
-  end
-
-  def pretty_postcode?
-    postcode[-4] == " "
-  end
-
-  def duplicate_applicants_address
-    update!(
-      address_line_one: applicants_address.address_line_one,
-      address_line_two: applicants_address.address_line_two,
-      city: applicants_address.city,
-      county: applicants_address.county,
-      postcode: applicants_address.postcode,
-      organisation: applicants_address.organisation,
-    )
-  end
-
-  def clear_stored_address
-    update!(
-      address_line_one: nil,
-      address_line_two: nil,
-      city: nil,
-      county: nil,
-      postcode: nil,
-      organisation: nil,
-    )
-  end
-
-private
-
-  def applicants_address
-    @applicants_address ||= legal_aid_application.applicant.address
-  end
 end
--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -1,47 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Partner do
-  subject(:partner) { described_class.create(legal_aid_application:) }
+  subject(:new_partner) { described_class.create(legal_aid_application:) }
 
   let(:legal_aid_application) { create(:legal_aid_application) }
 
-  describe ".duplicate_applicants_address" do
-    subject(:duplicate_applicants_address) { partner.duplicate_applicants_address }
-
-    context "when an applicant with an address exists" do
-      let!(:applicant) { create(:applicant, :with_address, legal_aid_application:) }
-
-      before { duplicate_applicants_address }
-
-      it "duplicates the address data into the partner record" do
-        expect(partner.reload).to have_attributes(
-          address_line_one: applicant.address.address_line_one,
-          address_line_two: applicant.address.address_line_two,
-          city: applicant.address.city,
-          county: applicant.address.county,
-          postcode: applicant.address.postcode,
-          organisation: applicant.address.organisation,
-        )
-      end
+  describe "on validation" do
+    before do
+      new_partner.first_name = "John"
+      new_partner.last_name = "Doe"
+      new_partner.date_of_birth = Date.new(1988, 0o2, 0o1)
+      new_partner.national_insurance_number = "AB123456D"
     end
-  end
 
-  describe ".clear_stored_address" do
-    subject(:cleared_stored_address) { partner.clear_stored_address }
-
-    let(:partner) { create(:partner, :with_address) }
-
-    before { cleared_stored_address }
-
-    it "clears the address fields" do
-      expect(partner.reload).to have_attributes(
-        address_line_one: nil,
-        address_line_two: nil,
-        city: nil,
-        county: nil,
-        postcode: nil,
-        organisation: nil,
-      )
+    it "is valid with all valid attributes" do
+      expect(new_partner).to be_valid
     end
   end
 end
