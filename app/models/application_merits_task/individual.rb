01b431b38104ee08598632c399f4b3d05c939525
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-07-18 10:16:20 +0100

    AP-4324: Extract Individual model from Opponent
    Extract from opponent with polymorphic relationship to
    opponennt.

---
+++ app/models/application_merits_task/individual.rb
@@ -0,0 +1,21 @@
+module ApplicationMeritsTask
+  class Individual < ApplicationRecord
+    has_one :opponent, as: :opposable
+
+    def ccms_relationship_to_case
+      "OPP"
+    end
+
+    def ccms_child?
+      false
+    end
+
+    def ccms_opponent_relationship_to_case
+      "Opponent"
+    end
+
+    def full_name
+      "#{first_name} #{last_name}".strip
+    end
+  end
+end
--- app/models/application_merits_task/opponent.rb
+++ app/models/application_merits_task/opponent.rb
@@ -2,22 +2,7 @@ module ApplicationMeritsTask
   class Opponent < ApplicationRecord
     include CCMSOpponentIdGenerator
 
+    belongs_to :opposable, polymorphic: true
     belongs_to :legal_aid_application
-
-    def ccms_relationship_to_case
-      "OPP"
-    end
-
-    def ccms_child?
-      false
-    end
-
-    def ccms_opponent_relationship_to_case
-      "Opponent"
-    end
-
-    def full_name
-      "#{first_name} #{last_name}".strip
-    end
   end
 end
