1d6c944692f8a3e5f7d99e710e85f2b23c73a177
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-07-19 08:28:11 +0100

    AP-4324: Add dependent destroy for individuals
    Opponent destruction should destroy the individual (and vice versa)

--- app/models/application_merits_task/opponent.rb
+++ app/models/application_merits_task/opponent.rb
@@ -2,7 +2,7 @@ module ApplicationMeritsTask
   class Opponent < ApplicationRecord
     include CCMSOpponentIdGenerator
 
-    belongs_to :opposable, polymorphic: true
+    belongs_to :opposable, polymorphic: true, dependent: :destroy
     belongs_to :legal_aid_application
 
     delegate :first_name,
