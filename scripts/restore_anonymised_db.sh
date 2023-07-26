347be35f2dcbac47cf8b56344676afa1ba7ebd06
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-07-26 07:43:43 +0100

    AP-4284: Tweak wording on restore_anonymised_db script

--- scripts/restore_anonymised_db.sh
+++ scripts/restore_anonymised_db.sh
@@ -8,15 +8,15 @@ echo "Finding pod for branch $1 in UAT"
 POD=$(kubectl -n laa-apply-for-legalaid-uat get pods | grep -o -m4 "apply-$1-.*2/2" | head -n1 | cut -d' ' -f 1)
 echo "$POD"
 
-echo "Copying anonymised db to kubernetes pod"
+echo "Copying anonymised db to kubernetes pod..."
 kubectl -n laa-apply-for-legalaid-uat cp --retries=10 ./tmp/production.anon.sql -c web laa-apply-for-legalaid-uat/"$POD":./tmp/anonymised_db.sql
 
-echo "Connect to the pod and run the rake task"
+echo "Importing data..."
 kubectl -n laa-apply-for-legalaid-uat -c web exec "$POD" -- rake db:import_to_uat
 
 if [ $? -eq 0 ]; then
-   echo 'Restore complete'
-   echo "Delete the restore file from the pod"
+   echo 'Restore complete!'
+   echo "Deleting the data file from the pod"
    kubectl -n laa-apply-for-legalaid-uat -c web exec "$POD" -- sh -c 'rm tmp/anonymised_db.sql'
 else
    echo "Restore may have failed, check logs before re-running the script"
