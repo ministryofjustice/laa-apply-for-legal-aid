e8544984bb82f1b620b1dd03d7705e83b0b0aa92
RoseSAK <rose.azadkan@digital.justice.gov.uk>
2023-07-26 12:51:24 +0100

    AP-4132 combine property pages
    remove outstanding mortgage amount page

--- config/routes.rb
+++ config/routes.rb
@@ -143,7 +143,6 @@ Rails.application.routes.draw do
         resources :dependants, only: %i[new show update]
         resources :remove_dependants, only: %i[show update]
         resource :has_other_dependants, only: %i[show update]
-        resource :outstanding_mortgage, only: %i[show update]
         resource :own_home, only: %i[show update]
         resource :property_details, only: %i[show update]
         resource :shared_ownership, only: %i[show update]
