90a075d8c5274482e6980dec2b8664f9ddacdd76
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-07-28 09:55:20 +0100

    AP-4336: Save the opposable object
    activerecord polymorphic relations do not save
    automatically when the child is saved. This was
    breaking tests that rely on explicit individual
    opponent names.

--- spec/factories/opponents.rb
+++ spec/factories/opponents.rb
@@ -17,6 +17,7 @@ FactoryBot.define do
       after(:build) do |opponent, evaluator|
         opponent.opposable.first_name = evaluator.first_name if evaluator.first_name
         opponent.opposable.last_name = evaluator.last_name if evaluator.last_name
+        opponent.opposable.save! if evaluator.first_name || evaluator.last_name
       end
     end
 
@@ -27,6 +28,7 @@ FactoryBot.define do
         opponent.opposable.name = evaluator.organisation_name if evaluator.organisation_name
         opponent.opposable.ccms_code = evaluator.organisation_ccms_code if evaluator.organisation_ccms_code
         opponent.opposable.description = evaluator.organisation_description if evaluator.organisation_description
+        opponent.opposable.save! if evaluator.organisation_name || evaluator.organisation_ccms_code || evaluator.organisation_description
       end
     end
   end
