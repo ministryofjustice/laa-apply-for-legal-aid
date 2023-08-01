3a4163967e7a9897e4f26f0c8f0358b186254070
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2023-08-01 09:40:53 +0100

    remove redundant method, remove base finance form

--- app/forms/applicants/student_finance_form.rb
+++ app/forms/applicants/student_finance_form.rb
@@ -1,7 +1,16 @@
 module Applicants
-  class StudentFinanceForm < StudentFinances::BaseStudentFinanceForm
+  class StudentFinanceForm < BaseForm
     form_for Applicant
 
     attr_accessor :student_finance, :student_finance_amount
+
+    validates :student_finance, inclusion: { in: %w[true false] }
+    validates :student_finance_amount, currency: { greater_than_or_equal_to: 0 }, if: :student_finance?
+
+  private
+
+    def student_finance?
+      student_finance.eql?("true")
+    end
   end
 end
