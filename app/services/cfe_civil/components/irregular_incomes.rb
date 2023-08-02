c915edcb0fbe3bcd1f193e6b31e025168cfe8770
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2023-08-02 15:09:46 +0100

    update cfe submission to reflect changes

--- app/services/cfe_civil/components/irregular_incomes.rb
+++ app/services/cfe_civil/components/irregular_incomes.rb
@@ -1,14 +1,36 @@
 module CFECivil
   module Components
     class IrregularIncomes < BaseDataBlock
-      delegate :irregular_incomes, to: :legal_aid_application
-
       def call
         {
           irregular_incomes: {
-            payments: irregular_incomes.map(&:as_json),
+            payments: payment_data,
           },
         }.to_json
+      end
+
+    private
+
+      def payment_data
+        if student_finance_data?
+          [
+            {
+              income_type: "student_loan",
+              frequency: "annual",
+              amount: student_finance_amount.to_f,
+            },
+          ]
+        else
+          []
+        end
+      end
+
+      def student_finance_data?
+        legal_aid_application.applicant.student_finance? && student_finance_amount.positive?
+      end
+
+      def student_finance_amount
+        legal_aid_application.applicant.student_finance_amount
       end
     end
   end
