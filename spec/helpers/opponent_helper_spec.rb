056ace1993310f443888e48e04294b2d9e387371
Katharine Ahern <katharine.ahern@justice.gov.uk>
2023-08-09 10:31:31 +0100

    ap-4304: add spec for ..opponent_type_description

--- spec/helpers/opponent_helper_spec.rb
+++ spec/helpers/opponent_helper_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe OpponentHelper do
   let(:legal_aid_application) { create(:legal_aid_application) }
   let(:individual_opponent) { create(:opponent, :for_individual, legal_aid_application:) }
-  let(:organisation_opponent) { create(:opponent, :for_organisation, legal_aid_application:) }
+  let(:organisation_opponent) { create(:opponent, :for_organisation, legal_aid_application:, organisation_name: "Mid Beds Council", organisation_ccms_code: "LA", organisation_description: "Local Authority") }
 
   describe ".opponent_url" do
     context "when opponent has type Individual" do
@@ -23,6 +23,22 @@ RSpec.describe OpponentHelper do
     context "when called with unexpected values" do
       it "raises an error" do
         expect { opponent_url("Fake") }.to raise_error "type Fake not supported"
+      end
+    end
+  end
+
+  describe ".opponent_type_description" do
+    context "when opponent has type Individual" do
+      it "returns the opponent type" do
+        type_description = opponent_type_description(individual_opponent)
+        expect(type_description).to eq "Individual"
+      end
+    end
+
+    context "when opponent has type Organisation" do
+      it "returns the organisation description" do
+        type_description = opponent_type_description(organisation_opponent)
+        expect(type_description).to eq "Local Authority"
       end
     end
   end
