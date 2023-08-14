bde14bf70733dfd18df0077906837870db553b25
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-08-14 15:06:48 +0100

    AP-4140: Update provider flow for vehicles
    Remove the single pages and only route via the new, single, page

--- app/services/flow/flows/provider_vehicle.rb
+++ app/services/flow/flows/provider_vehicle.rb
@@ -6,7 +6,7 @@ module Flow
           path: ->(application) { urls.providers_legal_aid_application_means_vehicle_path(application) },
           forward: lambda do |application|
             if application.own_vehicle?
-              :vehicles_estimated_values
+              :vehicle_details
             elsif application.non_passported? && !application.uploading_bank_statements?
               :applicant_bank_accounts
             else
@@ -18,29 +18,6 @@ module Flow
         },
         vehicle_details: {
           path: ->(application) { urls.providers_legal_aid_application_means_vehicle_details_path(application) },
-          forward: lambda do |application|
-            if application.non_passported? && !application.uploading_bank_statements?
-              :applicant_bank_accounts
-            else
-              :offline_accounts
-            end
-          end,
-          check_answers: ->(app) { app.checking_non_passported_means? ? :check_capital_answers : :check_passported_answers },
-        },
-        vehicles_estimated_values: {
-          path: ->(application) { urls.providers_legal_aid_application_vehicles_estimated_value_path(application) },
-          forward: :vehicles_remaining_payments,
-        },
-        vehicles_remaining_payments: {
-          path: ->(application) { urls.providers_legal_aid_application_vehicles_remaining_payment_path(application) },
-          forward: :vehicles_ages,
-        },
-        vehicles_ages: {
-          path: ->(application) { urls.providers_legal_aid_application_vehicles_age_path(application) },
-          forward: :vehicles_regular_uses,
-        },
-        vehicles_regular_uses: {
-          path: ->(application) { urls.providers_legal_aid_application_vehicles_regular_use_path(application) },
           forward: lambda do |application|
             if application.non_passported? && !application.uploading_bank_statements?
               :applicant_bank_accounts
