b1a5df8cc3a377323070ae11c7505db762c8acb6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-08-14 13:17:12 +0100

    AP-4140: Merge vehicle questions into single page
    Create a form that will handle all vehicle questions

---
+++ app/forms/vehicles/details_form.rb
@@ -0,0 +1,43 @@
+module Vehicles
+  class DetailsForm < BaseForm
+    form_for Vehicle
+
+    attr_accessor :estimated_value,
+                  :more_than_three_years_old,
+                  :payment_remaining,
+                  :payments_remain,
+                  :used_regularly
+
+    validates :estimated_value,
+              currency: { greater_than_or_equal_to: 0, allow_blank: true },
+              presence: { unless: :draft? }
+    validates :more_than_three_years_old, presence: { unless: :draft? }
+    validates :payments_remain, presence: true, unless: :draft?
+    validates(
+      :payment_remaining,
+      currency: { greater_than_or_equal_to: 0, allow_blank: true },
+      presence: { unless: :draft? },
+      if: :payments_remain?,
+    )
+    validates :used_regularly, presence: { unless: :draft? }
+
+    def payments_remain?
+      payments_remain.to_s == "true"
+    end
+
+    def save
+      attributes[:payment_remaining] = 0 if valid? && !payments_remain?
+      super
+    end
+
+  private
+
+    def exclude_from_model
+      [:payments_remain]
+    end
+
+    def attributes_to_clean
+      %i[payment_remaining estimated_value]
+    end
+  end
+end
