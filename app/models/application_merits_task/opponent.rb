20ebfded5d9d54cf2d91af51027ea4091b8213d8
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-08-14 16:56:52 +0100

    AP-4350: Remove hook that retrospectively updates opponent unneeded attributes
    This hook was only need to update the old attributes in case we
    needed to back out the change that switched to using individuals
    table to store first_name and last_name.
    
    The `ignored_coluns` call now breaks any reference to the old
    attributes, as if they had been dropped.

--- app/models/application_merits_task/opponent.rb
+++ app/models/application_merits_task/opponent.rb
@@ -7,15 +7,6 @@ module ApplicationMeritsTask
     belongs_to :opposable, polymorphic: true, dependent: :destroy
     belongs_to :legal_aid_application
 
-    # TODO: remove this when we remove the first and last name attributes from opponent - review from 31/07/2023
-    # NOTE: This is purely to continue to fill data on the opponent in case we need to rollback
-    before_save do
-      if opposable.is_a?(Individual)
-        self.first_name = opposable.first_name
-        self.last_name = opposable.last_name
-      end
-    end
-
     delegate :first_name,
              :last_name,
              :full_name,
