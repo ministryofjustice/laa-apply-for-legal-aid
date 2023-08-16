5c0200684b5d3c603841738394eb21249ed4f895
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-08-16 12:57:27 +0100

    Revert audit config to application root

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module LaaApplyForLegalAid
 
     config.active_record.encryption.support_unencrypted_data = true
     config.console1984.ask_for_username_if_empty = true
+    config.audits1984.auditor_class = "AdminUser"
+    config.audits1984.auditor_name_attribute = :username
+    config.audits1984.base_controller_class = "Admin::AdminBaseController"
 
     # Changes in rails 7.0.3.1 prevented Synbols being used in serialised fields
     # this overrides the setting and allows the code(and tests) to run as normal
--- config/initializers/audits1984.rb
+++
@@ -1,3 +0,0 @@
-Audits1984.auditor_class = "AdminUser"
-Audits1984.auditor_name_attribute = :username
-Audits1984.base_controller_class = "Admin::AdminBaseController"
