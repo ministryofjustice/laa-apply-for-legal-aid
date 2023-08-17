ef01ef181c9eaa98cca91f216d12ce605df4a595
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-08-17 14:58:43 +0100

    AP-4104: Create the RegularIncomeForm
    This refactors almost the entire contents to the BaseRegularIncomeForm,
    and shares it with the existing Means::RegularIncomeForm it also moves
    the RegularTransactionForm as it is nolonger specific to the means
    namespace.

---
+++ app/forms/providers/base_regular_income_form.rb
@@ -0,0 +1,31 @@
+module Providers
+  class BaseRegularIncomeForm < RegularTransactionForm
+    INCOME_TYPES = %w[
+      benefits
+      friends_or_family
+      maintenance_in
+      property_or_lodger
+      student_loan
+      pension
+    ].freeze
+
+    INCOME_TYPES.each do |income_type|
+      attr_accessor "#{income_type}_amount".to_sym,
+                    "#{income_type}_frequency".to_sym
+    end
+
+  private
+
+    def transaction_type_conditions
+      { operation: "credit", parent_id: nil }
+    end
+
+    def transaction_type_exclusions
+      { name: "benefits" }
+    end
+
+    def legal_aid_application_attributes
+      { no_credit_transaction_types_selected: none_selected? }
+    end
+  end
+end
