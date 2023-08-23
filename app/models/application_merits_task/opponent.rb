82f8e6a27e1f465c2a93a858983e82163cd67e26
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-08-23 17:52:36 +0100

    AP-4303: add scopes to opponent
    Simplify retrieval of individuals and organisations
    for an application so we can iterate over to contruct SOAP
    XML payload elements.

--- app/models/application_merits_task/opponent.rb
+++ app/models/application_merits_task/opponent.rb
@@ -5,6 +5,9 @@ module ApplicationMeritsTask
     belongs_to :opposable, polymorphic: true, dependent: :destroy
     belongs_to :legal_aid_application
 
+    scope :individuals, -> { where(opposable_type: "ApplicationMeritsTask::Individual") }
+    scope :organisations, -> { where(opposable_type: "ApplicationMeritsTask::Organisation") }
+
     delegate :first_name,
              :last_name,
              :full_name,
