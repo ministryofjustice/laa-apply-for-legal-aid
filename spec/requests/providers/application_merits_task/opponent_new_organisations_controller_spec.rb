ee73f7991d0c482c91edf868abc7121dc46d21cf
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-09-12 15:10:42 +0100

    AP-4399: Change subject names for new organisation controller spec
    For clarity

--- spec/requests/providers/application_merits_task/opponent_new_organisations_controller_spec.rb
+++ spec/requests/providers/application_merits_task/opponent_new_organisations_controller_spec.rb
@@ -9,12 +9,12 @@ module Providers
       let(:proceeding) { laa.proceedings.detect { |p| p.ccms_code == "SE014" } }
 
       describe "new: GET /providers/applications/:legal_aid_application_id/opponent_new_organisations/new" do
-        subject(:new) { get new_providers_legal_aid_application_opponent_new_organisation_path(legal_aid_application) }
+        subject(:request_new) { get new_providers_legal_aid_application_opponent_new_organisation_path(legal_aid_application) }
 
         context "when authenticated" do
           before do
             login_provider
-            new
+            request_new
           end
 
           it "returns ok" do
@@ -28,21 +28,21 @@ module Providers
         end
 
         context "when unauthenticated" do
-          before { new }
+          before { request_new }
 
           it_behaves_like "a provider not authenticated"
         end
       end
 
       describe "show: GET /providers/applications/:legal_aid_application_id/opponent_new_organisations/:opponent_id" do
-        subject(:show) { get providers_legal_aid_application_opponent_new_organisation_path(legal_aid_application, opponent) }
+        subject(:request_show) { get providers_legal_aid_application_opponent_new_organisation_path(legal_aid_application, opponent) }
 
         let(:opponent) { create(:opponent, :for_organisation, legal_aid_application:) }
 
         context "when authenticated" do
           before do
             login_provider
-            show
+            request_show
           end
 
           it "returns ok" do
@@ -56,15 +56,14 @@ module Providers
         end
 
         context "when unauthenticated" do
-          before { show }
+          before { request_show }
 
           it_behaves_like "a provider not authenticated"
         end
       end
 
-      # rubocop:disable Rails/SaveBang
       describe "update: PATCH /providers/applications/:legal_aid_application_id/opponent_new_organisation/:opponent_id" do
-        subject(:update) do
+        subject(:request_update) do
           patch(
             providers_legal_aid_application_opponent_new_organisation_path(legal_aid_application, opponent),
             params: params.merge(button_clicked),
@@ -91,26 +90,26 @@ module Providers
         end
 
         it "amends the opponent opposable organisation with the values entered" do
-          expect { update }.not_to change(::ApplicationMeritsTask::Organisation, :count)
+          expect { request_update }.not_to change(::ApplicationMeritsTask::Organisation, :count)
           opponent.opposable.reload
           expect(opponent.opposable.name).to eql "Central Bedfordshire Council"
           expect(opponent.opposable.ccms_type_code).to eql "LA"
         end
 
         it "sets the task to complete" do
-          update
+          request_update
           expect(legal_aid_application.legal_framework_merits_task_list).to have_completed_task(:application, :opponent_name)
         end
 
         it "redirects to the has another opponent question" do
-          update
+          request_update
           expect(response).to redirect_to(providers_legal_aid_application_has_other_opponent_path(legal_aid_application))
         end
 
         context "when not authenticated" do
           let(:login_provider) { nil }
 
-          before { update }
+          before { request_update }
 
           it_behaves_like "a provider not authenticated"
         end
@@ -126,19 +125,19 @@ module Providers
           end
 
           it "returns ok" do
-            update
+            request_update
             expect(response).to have_http_status(:ok)
           end
 
           it "contains error message" do
-            update
+            request_update
             expect(response.body).to include("govuk-error-summary")
               .and include(html_compare("Enter the organisation's name"))
               .and include(html_compare("Select the organisation's type"))
           end
 
           it "does not set the task to complete" do
-            update
+            request_update
             expect(legal_aid_application.legal_framework_merits_task_list).to have_task_in_state(:application, :opponent_name, :not_started)
           end
         end
@@ -147,17 +146,16 @@ module Providers
           let(:button_clicked) { draft_button }
 
           it "redirects to provider draft endpoint" do
-            update
+            request_update
             expect(response).to redirect_to providers_legal_aid_applications_path
           end
 
           it "does not set the task to complete" do
-            update
+            request_update
             expect(legal_aid_application.legal_framework_merits_task_list.serialized_data).to match(/name: :opponent_name\n\s+dependencies: \*\d+\n\s+state: :not_started/)
           end
         end
       end
-      # rubocop:enable Rails/SaveBang
     end
   end
 end
