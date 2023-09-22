c4f43b1fb2903bc69ed4763e23004647f4ac1b6e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-09-22 10:06:55 +0100

    AP-4139: Update vehicles_controller handling
    Do not create an empty object, let the proper page
    handle it. Amend the tests to match the new state

--- app/controllers/providers/means/vehicles_controller.rb
+++ app/controllers/providers/means/vehicles_controller.rb
@@ -8,12 +8,7 @@ module Providers
       def update
         @form = LegalAidApplications::OwnVehicleForm.new(form_params)
 
-        if @form.save
-          legal_aid_application.own_vehicle ? vehicle.save! : vehicle.destroy!
-          continue_or_draft
-        else
-          render :show
-        end
+        render :show unless save_continue_or_draft(@form)
       end
 
     private
@@ -24,10 +19,6 @@ module Providers
 
           params.require(:legal_aid_application).permit(:own_vehicle)
         end
-      end
-
-      def vehicle
-        @vehicle = legal_aid_application.vehicle || legal_aid_application.build_vehicle
       end
     end
   end
