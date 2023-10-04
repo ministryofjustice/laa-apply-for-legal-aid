c16f0ef44bf5ad8c3bdafcec2659f3d310fffcae
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-10-04 17:38:57 +0100

    AP-4452: Remove flag from task list helper and refactor
    opponent_name merits task list option now goes to
    the opponent_type controller. There is no new action for
    the opponent_type so this logic has been removed.
    
    However further refactorinig is possible to rename
    the merits task list "name" itself and make this more
    transparent. Currently we are "remapping" the opponent_name
    task to opponent_type to work around this but ideally we would
    just use the name opponent_type to start with.

--- app/helpers/task_list_helper.rb
+++ app/helpers/task_list_helper.rb
@@ -31,10 +31,6 @@ private
     legal_aid_application.involved_children.empty?
   end
 
-  def application_has_no_opponents?(legal_aid_application)
-    legal_aid_application.opponents.empty?
-  end
-
   def proceeding_task_url(name, application, ccms_code)
     url = "providers_merits_task_list_#{url_fragment(name)}_path".to_sym
 
@@ -48,7 +44,7 @@ private
   def new_url_fragment(name, status, application)
     name = "has_other_involved_children" if name.eql?(:children_application) && (status.eql?(:complete) || application.involved_children.any?)
     name = "has_other_opponent" if name.eql?(:opponent_name) && (status.eql?(:complete) || application.opponents.any?)
-    name = "opponent_type" if name.eql?(:opponent_name) && Setting.opponent_organisations?
+    name = "opponent_type" if name.eql?(:opponent_name)
     I18n.t("providers.merits_task_lists.task_list_item.urls.#{name}")
   end
 
@@ -60,20 +56,6 @@ private
   end
 
   def display_new_page?(legal_aid_application, task_name)
-    return false if Setting.opponent_organisations? && task_name.eql?(:opponent_name)
-    return false unless %i[children_application opponent_name].include?(task_name)
-
-    [
-      starting_children_application_for_first_time?(legal_aid_application, task_name),
-      starting_opponents_name_for_first_time?(legal_aid_application, task_name),
-    ].any?(true)
-  end
-
-  def starting_children_application_for_first_time?(legal_aid_application, task_name)
     task_name.eql?(:children_application) && application_has_no_involved_children?(legal_aid_application)
-  end
-
-  def starting_opponents_name_for_first_time?(legal_aid_application, task_name)
-    task_name.eql?(:opponent_name) && application_has_no_opponents?(legal_aid_application)
   end
 end
