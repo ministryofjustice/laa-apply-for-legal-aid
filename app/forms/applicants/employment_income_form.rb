9d82ba5068ee712309de4b3d58f9d1d56cd9fd2a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-10-19 09:39:10 +0100

    AP-4326: Refactor forms to reduce duplication

--- app/forms/applicants/employment_income_form.rb
+++ app/forms/applicants/employment_income_form.rb
@@ -1,34 +1,13 @@
 module Applicants
-  class EmploymentIncomeForm < BaseForm
+  class EmploymentIncomeForm < BaseEmploymentIncomeForm
     form_for Applicant
 
     attr_accessor :extra_employment_information, :extra_employment_information_details
 
-    before_validation :clear_employment_info_details
-
-    validate :extra_employment_information_presence
-    validate :extra_employment_information_details_presence
-
   private
-
-    def extra_employment_information_presence
-      return if draft? || extra_employment_information.present?
-
-      add_blank_error_for :extra_employment_information
-    end
-
-    def extra_employment_information_details_presence
-      return if draft? || extra_employment_information.to_s != "true"
-
-      add_blank_error_for :extra_employment_information_details if extra_employment_information_details.blank?
-    end
 
     def add_blank_error_for(attribute)
       errors.add(attribute, I18n.t("activemodel.errors.models.applicant.attributes.#{attribute}.blank"))
-    end
-
-    def clear_employment_info_details
-      extra_employment_information_details&.clear if extra_employment_information.to_s == "false"
     end
   end
 end
---
+++ app/forms/base_employment_income_form.rb
@@ -0,0 +1,26 @@
+class BaseEmploymentIncomeForm < BaseForm
+  attr_accessor :extra_employment_information, :extra_employment_information_details
+
+  before_validation :clear_employment_info_details
+
+  validate :extra_employment_information_presence
+  validate :extra_employment_information_details_presence
+
+private
+
+  def extra_employment_information_presence
+    return if draft? || extra_employment_information.present?
+
+    add_blank_error_for :extra_employment_information
+  end
+
+  def extra_employment_information_details_presence
+    return if draft? || extra_employment_information.to_s != "true"
+
+    add_blank_error_for :extra_employment_information_details if extra_employment_information_details.blank?
+  end
+
+  def clear_employment_info_details
+    extra_employment_information_details&.clear if extra_employment_information.to_s == "false"
+  end
+end
