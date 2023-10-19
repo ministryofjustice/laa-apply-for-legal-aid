1ce25e83c9bf87ab577289284c3ac25f8028badf
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2023-10-19 16:15:26 +0100

    AP-4489: add different error messages for whether client has partner Add proof of concept for specific messages depending on whether client has a partner

--- app/forms/legal_aid_applications/property_details_form.rb
+++ app/forms/legal_aid_applications/property_details_form.rb
@@ -11,8 +11,8 @@ module LegalAidApplications
 
     attr_accessor(*ATTRIBUTES)
 
-    validates :property_value, presence: { unless: :draft? }
-    validates :property_value, allow_blank: true, currency: { greater_than_or_equal_to: 0.0 }
+    validate :property_value_presence
+    validate :property_value_amount
 
     validates :outstanding_mortgage_amount, presence: { unless: :outstanding_mortgage_amount_presence }
     validates :outstanding_mortgage_amount, allow_blank: true, currency: { greater_than_or_equal_to: 0.0 }
@@ -27,6 +27,22 @@ module LegalAidApplications
     end
 
   private
+
+    def property_value_presence
+      return if draft? || property_value.present?
+
+      errors.add(:property_value, I18n.t("activemodel.errors.models.legal_aid_application.attributes.property_value.blank#{individual}"))
+    end
+
+    def property_value_amount
+      return if property_value.blank? || property_value.to_f >= 0.0
+
+      errors.add(:property_value, I18n.t("activemodel.errors.models.legal_aid_application.attributes.property_value.greater_than_or_equal_to#{individual}"))
+    end
+
+    def individual
+      return "_with_partner" if model.applicant.has_partner_with_no_contrary_interest?
+    end
 
     def outstanding_mortgage_amount_presence
       draft? || model.own_home == "owned_outright"
