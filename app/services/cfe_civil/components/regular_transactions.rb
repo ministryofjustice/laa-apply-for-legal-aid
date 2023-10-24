cf9393fb39edd4c4f7fdb3946bb09a77fe26e9f0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-10-24 15:04:15 +0100

    AP-4359: Update regular_transaction cfe component
    Handle applicant/partner specific values

--- app/services/cfe_civil/components/regular_transactions.rb
+++ app/services/cfe_civil/components/regular_transactions.rb
@@ -1,8 +1,6 @@
 module CFECivil
   module Components
     class RegularTransactions < BaseDataBlock
-      delegate :regular_transactions, to: :legal_aid_application
-
       def call
         {
           regular_transactions: build_regular_transactions,
@@ -11,8 +9,12 @@ module CFECivil
 
     private
 
+      def party
+        @party ||= legal_aid_application.send(owner_type.downcase)
+      end
+
       def build_regular_transactions
-        regular_transactions.each_with_object([]) do |regular_transaction, payload|
+        party.regular_transactions.each_with_object([]) do |regular_transaction, payload|
           payload << {
             category: regular_transaction.transaction_type.name,
             operation: regular_transaction.transaction_type.operation,
--- spec/services/cfe_civil/components/regular_transactions_spec.rb
+++ spec/services/cfe_civil/components/regular_transactions_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe CFECivil::Components::RegularTransactions do
   subject(:call) { described_class.call(legal_aid_application) }
 
-  let(:legal_aid_application) { create(:legal_aid_application, :with_applicant) }
+  let(:legal_aid_application) { create(:legal_aid_application, :with_applicant_and_partner) }
 
   context "when there are no regular transactions" do
     it "returns the expected, empty, JSON block" do
@@ -13,37 +13,69 @@ RSpec.describe CFECivil::Components::RegularTransactions do
     end
   end
 
-  context "when there are regular transactions" do
+  context "when there are regular transactions for both Applicant and Partner" do
     before do
+      applicant = legal_aid_application.applicant
+      partner = legal_aid_application.partner
       create(:regular_transaction,
              :maintenance_out,
              legal_aid_application:,
              amount: 222.22,
-             frequency: "monthly")
+             frequency: "monthly",
+             owner_type: applicant.class,
+             owner_id: applicant.id)
       create(:regular_transaction,
              :maintenance_in,
              legal_aid_application:,
              amount: 111.11,
-             frequency: "monthly")
+             frequency: "monthly",
+             owner_type: applicant.class,
+             owner_id: applicant.id)
+      create(:regular_transaction,
+             :friends_or_family,
+             legal_aid_application:,
+             amount: 501.00,
+             frequency: "monthly",
+             owner_type: partner.class,
+             owner_id: partner.id)
     end
 
-    it "returns the expected JSON block" do
-      expect(JSON.parse(call)).to match_json_expression({
-        regular_transactions: [
-          {
-            category: "maintenance_in",
-            operation: "credit",
-            amount: 111.11,
-            frequency: "monthly",
-          },
-          {
-            category: "maintenance_out",
-            operation: "debit",
-            amount: 222.22,
-            frequency: "monthly",
-          },
-        ],
-      })
+    context "and no owner is specified" do
+      it "returns the expected JSON block" do
+        expect(JSON.parse(call)).to match_json_expression({
+          regular_transactions: [
+            {
+              category: "maintenance_in",
+              operation: "credit",
+              amount: 111.11,
+              frequency: "monthly",
+            },
+            {
+              category: "maintenance_out",
+              operation: "debit",
+              amount: 222.22,
+              frequency: "monthly",
+            },
+          ],
+        })
+      end
+    end
+
+    context "and partner is specified as owner type" do
+      subject(:call) { described_class.call(legal_aid_application, "Partner") }
+
+      it "returns the expected JSON block just for the partner" do
+        expect(JSON.parse(call)).to match_json_expression({
+          regular_transactions: [
+            {
+              category: "friends_or_family",
+              operation: "credit",
+              amount: 501.00,
+              frequency: "monthly",
+            },
+          ],
+        })
+      end
     end
   end
 end
