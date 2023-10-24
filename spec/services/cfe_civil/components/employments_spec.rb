36ee9e3468d2dcfe2f11d53ac90958820fc4e0e9
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-10-24 11:18:21 +0100

    AP-4359: Update CFE Employment component
    Handle both applicant and partner employment data

--- spec/services/cfe_civil/components/employments_spec.rb
+++ spec/services/cfe_civil/components/employments_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 RSpec.describe CFECivil::Components::Employments do
   subject(:call) { described_class.call(legal_aid_application) }
 
-  let(:legal_aid_application) { create(:legal_aid_application, :with_applicant) }
+  let(:legal_aid_application) { create(:legal_aid_application) }
 
   context "when the applicant is not employed" do
-    before { create(:applicant, :not_employed) }
+    before { create(:applicant, :not_employed, legal_aid_application:) }
 
     it "renders the expected, empty hash" do
       expect(call).to eq({}.to_json)
@@ -15,7 +15,7 @@ RSpec.describe CFECivil::Components::Employments do
 
   context "when the applicant is employed but has no PAYE data" do
     before do
-      applicant = create(:applicant, :employed)
+      applicant = create(:applicant, :employed, legal_aid_application:)
       create(:employment, legal_aid_application:, owner_id: applicant.id, owner_type: applicant.class)
     end
 
@@ -24,61 +24,116 @@ RSpec.describe CFECivil::Components::Employments do
     end
   end
 
-  context "when the applicant is employed" do
-    let(:applicant) { create(:applicant, :employed) }
-
+  context "when the applicant and partner are employed" do
     before do
-      create(:applicant, :employed)
+      applicant = create(:applicant, :employed, legal_aid_application:)
+      partner = create(:partner, :employed, legal_aid_application:)
       create(:employment, :example1_usecase1, legal_aid_application:, owner_id: applicant.id, owner_type: applicant.class)
+      create(:employment, :example2_usecase1, legal_aid_application:, owner_id: partner.id, owner_type: partner.class)
     end
 
-    it "renders the expected JSON" do
-      expect(call).to eq({
-        employment_income: [
-          {
-            name: "Job 788",
-            client_id: "12345678-1234-1234-1234-123456789abc",
-            payments: [
-              {
-                client_id: "20211128-0000-0000-0000-123456789abc",
-                date: "2021-11-28",
-                gross: 1868.98,
-                benefits_in_kind: 0.0,
-                tax: -161.8,
-                national_insurance: -128.64,
-                net_employment_income: 1578.54,
-              },
-              {
-                client_id: "20211028-0000-0000-0000-123456789abc",
-                date: "2021-10-28",
-                gross: 1868.98,
-                benefits_in_kind: 0.0,
-                tax: -111.0,
-                national_insurance: -128.64,
-                net_employment_income: 1629.34,
-              },
-              {
-                client_id: "20210928-0000-0000-0000-123456789abc",
-                date: "2021-09-28",
-                gross: 2492.61,
-                benefits_in_kind: 0.0,
-                tax: -286.6,
-                national_insurance: -203.47,
-                net_employment_income: 2002.54,
-              },
-              {
-                client_id: "20210828-0000-0000-0000-123456789abc",
-                date: "2021-08-28",
-                gross: 2345.29,
-                benefits_in_kind: 0.0,
-                tax: -257.2,
-                national_insurance: -185.79,
-                net_employment_income: 1902.3,
-              },
-            ],
-          },
-        ],
-      }.to_json)
+    context "and no owner type is specified" do
+      it "renders the expected JSON for just the client" do
+        expect(call).to eq({
+          employment_income: [
+            {
+              name: "Job 788",
+              client_id: "12345678-1234-1234-1234-123456789abc",
+              payments: [
+                {
+                  client_id: "20211128-0000-0000-0000-123456789abc",
+                  date: "2021-11-28",
+                  gross: 1868.98,
+                  benefits_in_kind: 0.0,
+                  tax: -161.8,
+                  national_insurance: -128.64,
+                  net_employment_income: 1578.54,
+                },
+                {
+                  client_id: "20211028-0000-0000-0000-123456789abc",
+                  date: "2021-10-28",
+                  gross: 1868.98,
+                  benefits_in_kind: 0.0,
+                  tax: -111.0,
+                  national_insurance: -128.64,
+                  net_employment_income: 1629.34,
+                },
+                {
+                  client_id: "20210928-0000-0000-0000-123456789abc",
+                  date: "2021-09-28",
+                  gross: 2492.61,
+                  benefits_in_kind: 0.0,
+                  tax: -286.6,
+                  national_insurance: -203.47,
+                  net_employment_income: 2002.54,
+                },
+                {
+                  client_id: "20210828-0000-0000-0000-123456789abc",
+                  date: "2021-08-28",
+                  gross: 2345.29,
+                  benefits_in_kind: 0.0,
+                  tax: -257.2,
+                  national_insurance: -185.79,
+                  net_employment_income: 1902.3,
+                },
+              ],
+            },
+          ],
+        }.to_json)
+      end
+    end
+
+    context "and partner is specified as owner type" do
+      subject(:call) { described_class.call(legal_aid_application, "Partner") }
+
+      it "renders the expected JSON for just the partner" do
+        expect(call).to eq({
+          employments: [
+            {
+              name: "Job 877",
+              client_id: "87654321-1234-1234-1234-123456789abc",
+              payments: [
+                {
+                  client_id: "20231024-0000-0000-0000-123456789abc",
+                  date: "2021-11-28",
+                  gross: 1868.98,
+                  benefits_in_kind: 0.0,
+                  tax: -161.8,
+                  national_insurance: -128.64,
+                  net_employment_income: 1578.54,
+                },
+                {
+                  client_id: "20230924-0000-0000-0000-123456789abc",
+                  date: "2021-10-28",
+                  gross: 1868.98,
+                  benefits_in_kind: 0.0,
+                  tax: -111.0,
+                  national_insurance: -128.64,
+                  net_employment_income: 1629.34,
+                },
+                {
+                  client_id: "20230824-0000-0000-0000-123456789abc",
+                  date: "2021-09-28",
+                  gross: 2492.61,
+                  benefits_in_kind: 0.0,
+                  tax: -286.6,
+                  national_insurance: -203.47,
+                  net_employment_income: 2002.54,
+                },
+                {
+                  client_id: "20230724-0000-0000-0000-123456789abc",
+                  date: "2021-08-28",
+                  gross: 2345.29,
+                  benefits_in_kind: 0.0,
+                  tax: -257.2,
+                  national_insurance: -185.79,
+                  net_employment_income: 1902.3,
+                },
+              ],
+            },
+          ],
+        }.to_json)
+      end
     end
   end
 end
