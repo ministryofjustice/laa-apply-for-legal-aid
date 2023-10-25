d9cd1a363e332f0b833aaf671cdafbf37df7805c
RoseSAK <rose.azadkan@digital.justice.gov.uk>
2023-10-25 17:33:33 +0100

    AP-4328 CYA contrary interest question
    add contrary interest section to the check your answers page

--- app/services/flow/flows/provider_partner.rb
+++ app/services/flow/flows/provider_partner.rb
@@ -15,11 +15,10 @@ module Flow
         contrary_interests: {
           path: ->(application) { urls.providers_legal_aid_application_contrary_interest_path(application) },
           forward: lambda do |_application, options|
-            if options[:partner_has_contrary_interest]
-              :check_provider_answers
-            else
-              :partner_details
-            end
+            options[:partner_has_contrary_interest] ? :check_provider_answers : :partner_details
+          end,
+          check_answers: lambda do |_application, options|
+            options[:partner_has_contrary_interest] ? :check_provider_answers : :partner_details
           end,
         },
         partner_details: {
