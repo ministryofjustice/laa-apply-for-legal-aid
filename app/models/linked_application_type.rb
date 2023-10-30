a111cb82828a346486f5fbc54668f8745bfee7c7
Katharine Ahern <katharine.ahern@justice.gov.uk>
2023-10-30 08:49:10 +0000

    ap-4508: simplify model
    and add tests for type

--- app/models/linked_application_type.rb
+++ app/models/linked_application_type.rb
@@ -2,11 +2,11 @@ class LinkedApplicationType
   LinkTypeStruct = Struct.new(:code, :description)
 
   LINK_TYPES = [
-    { code: "FAMILY", description: "Family" },
-    { code: "LEGAL", description: "Legal" },
+    LinkTypeStruct.new(code: "FAMILY", description: "Family"),
+    LinkTypeStruct.new(code: "LEGAL", description: "Legal"),
   ].freeze
 
   def self.all
-    LINK_TYPES.map { |link_type_hash| LinkTypeStruct.new(code: link_type_hash[:code], description: link_type_hash[:description]) }
+    LINK_TYPES
   end
 end
