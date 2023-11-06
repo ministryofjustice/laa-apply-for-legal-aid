b8cc2567005e164abe2fd85acecc820ddaa8a319
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-11-06 11:37:42 +0000

    AP-4575: correct copy_case_confirmations form_params

--- app/controllers/providers/copy_case_confirmations_controller.rb
+++ app/controllers/providers/copy_case_confirmations_controller.rb
@@ -16,6 +16,8 @@ module Providers
 
     def form_params
       merge_with_model(legal_aid_application) do
+        next {} unless params[:legal_aid_application]
+
         params.require(:legal_aid_application).permit(:copy_case_id, :copy_case_confirmation)
       end
     end
