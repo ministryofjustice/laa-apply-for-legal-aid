22bc5f193cbd3be3b17de7388df13cb2fef7e824
Katharine Ahern <katharine.ahern@justice.gov.uk>
2023-11-13 10:30:08 +0000

    ap-4503: amend ineligible partial to display ineligible reasons

--- app/controllers/providers/capital_income_assessment_results_controller.rb
+++ app/controllers/providers/capital_income_assessment_results_controller.rb
@@ -7,6 +7,7 @@ module Providers
       handle_unknown
       @details = ManualReviewDetailer.call(legal_aid_application)
       @result_partial = ResultsPanelSelector.call(legal_aid_application)
+      @ineligible_reasons = ineligible_reasons if @cfe_result.assessment_result == "ineligible"
     end
 
     def update
@@ -19,6 +20,46 @@ module Providers
       return if KNOWN_RESULTS.include?(@cfe_result.assessment_result)
 
       raise "Unknown capital_income_assessment_result: '#{@cfe_result.assessment_result}'"
+    end
+
+    def ineligible_reasons
+      return "" unless @cfe_result.assessment_result == "ineligible"
+      return ineligible_reasons_array.first if ineligible_reasons_array.length == 1
+
+      ineligible_reasons = ":<br><br>"
+      ineligible_reasons_array.each { |reason| ineligible_reasons += "-#{reason}<br>" }
+      ineligible_reasons
+    end
+
+    def ineligible_reasons_array
+      return [] unless @cfe_result.assessment_result == "ineligible"
+
+      ineligible_reasons_array = []
+      ineligible_reasons_array << "gross income" if ineligible_gross_income?
+      ineligible_reasons_array << "disposable income" if ineligible_disposable_income?
+      ineligible_reasons_array << "disposable capital" if ineligible_disposable_capital?
+      ineligible_reasons_array
+    end
+
+    def ineligible_gross_income?
+      return false unless @cfe_result.assessment_result == "ineligible"
+      return false unless (@cfe_result.gross_income_results - %w[ineligible]).empty?
+
+      true
+    end
+
+    def ineligible_disposable_income?
+      return false unless @cfe_result.assessment_result == "ineligible"
+      return false unless (@cfe_result.disposable_income_results - %w[ineligible]).empty?
+
+      true
+    end
+
+    def ineligible_disposable_capital?
+      return false unless @cfe_result.assessment_result == "ineligible"
+      return false unless (@cfe_result.capital_results - %w[ineligible]).empty?
+
+      true
     end
   end
 end
