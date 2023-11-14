86c7f861757924eca81c943eebb12b3e67d5f7f9
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-11-14 15:35:05 +0000

    AP-4511: Refactor and extend linked_application model spec

--- spec/factories/linked_applications.rb
+++ spec/factories/linked_applications.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :linked_application, class: "LinkedApplication" do
     lead_application { lead_application }
     associated_application { associated_application }
-    link_type_code { "FAMILY" }
+    link_type_code { "FC_LEAD" }
 
     trait :family do
-      link_type_code { "FAMILY" }
+      link_type_code { "FC_LEAD" }
     end
   end
 end
