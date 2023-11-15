46f4486f4d72da85f506e43df0a94ccb75cd2014
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-11-15 12:42:59 +0000

    AP-4574: Update own_vehicle handling
    Remove the journey option from rthe controller and
    update the form to set a partner version of the
    error message

--- app/forms/legal_aid_applications/own_vehicle_form.rb
+++ app/forms/legal_aid_applications/own_vehicle_form.rb
@@ -2,18 +2,14 @@ module LegalAidApplications
   class OwnVehicleForm < BaseForm
     form_for LegalAidApplication
 
-    attr_accessor :own_vehicle, :journey
+    attr_accessor :own_vehicle
 
     validate :own_vehicle_presence
 
     def own_vehicle_presence
       return if draft? || own_vehicle.present?
 
-      errors.add(:own_vehicle, I18n.t("activemodel.errors.models.legal_aid_application.attributes.own_vehicle.#{journey}.blank"))
-    end
-
-    def exclude_from_model
-      [:journey]
+      errors.add(:own_vehicle, I18n.t("activemodel.errors.models.legal_aid_application.attributes.own_vehicle.#{error_key('blank')}"))
     end
   end
 end
