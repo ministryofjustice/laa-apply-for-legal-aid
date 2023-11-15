492a08c7910b9da76db84bea1c0c817646ade592
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-11-15 11:19:38 +0000

    AP-4574: Update currency_validator to overwrite errors _with_partner

--- app/validators/currency_validator.rb
+++ app/validators/currency_validator.rb
@@ -3,13 +3,39 @@ class CurrencyValidator < ActiveModel::Validations::NumericalityValidator
 
   def validate_each(record, attr_name, value)
     clean_value = clean_numeric_value(value)
-    super(record, attr_name, clean_value)
-    return if record.errors[attr_name].any?
+    super(record, attr_name, clean_value) # this requires the actual attribute symbol, e.g. :cash
 
-    record.errors.add(attr_name, :too_many_decimals) unless ONLY_2_DECIMALS_PATTERN.match?(clean_value)
+    replace_error_with_partner(record, attr_name) if any_errors_for?(record, attr_name) && use_partner_labels?(record)
+    return if any_errors_for?(record, attr_name)
+
+    record.errors.add(attr_name, decimal_error(record)) unless ONLY_2_DECIMALS_PATTERN.match?(clean_value)
   end
 
   def clean_numeric_value(value)
     CurrencyCleaner.new(value).call
+  end
+
+private
+
+  def any_errors_for?(record, attr_name)
+    record.errors[attr_name].any?
+  end
+
+  def use_partner_labels?(record)
+    return false if options[:partner_labels].blank?
+
+    record.send(options[:partner_labels])
+  end
+
+  def replace_error_with_partner(record, attr_name)
+    original_error_type = record.errors.where(attr_name).first.type
+    partner_error_type = "#{original_error_type}_with_partner".to_sym
+
+    record.errors.delete(attr_name) # delete original
+    record.errors.add(attr_name, partner_error_type) # replace with partner version
+  end
+
+  def decimal_error(record)
+    use_partner_labels?(record) ? :too_many_decimals_with_partner : :too_many_decimals
   end
 end
--- spec/validators/currency_validator_spec.rb
+++ spec/validators/currency_validator_spec.rb
@@ -1,18 +1,33 @@
 require "rails_helper"
 
-class DummyClass
+class DummyCurrencyClass
   include ActiveModel::Model
   include ActiveModel::Validations
 
-  attr_accessor :val1, :val2, :val3
+  attr_accessor :val1, :val2, :val3, :val4, :val5
 
   validates :val1, :val2, currency: { greater_than_or_equal_to: 0 }, allow_blank: true
   validates :val3, currency: { greater_than_or_equal_to: 0 }
+  validates :val4, currency: { greater_than_or_equal_to: 0, partner_labels: :always_include_partner? }
+  validates :val5, currency: { greater_than_or_equal_to: 0, partner_labels: :always_exclude_partner? }
+
+  def always_include_partner?
+    true
+  end
+
+  def always_exclude_partner?
+    false
+  end
 end
 
 RSpec.describe CurrencyValidator do
   describe "numericality validation" do
-    let(:dummy) { DummyClass.new }
+    before do
+      en = { activemodel: { errors: { models: { dummy_currency_class: { attributes: { val4: { not_a_number_with_partner: "fake error" }, val5: { not_a_number_with_partner: "fake not a number partner error" } } } } } } }
+      I18n.backend.store_translations(:en, en)
+    end
+
+    let(:dummy) { DummyCurrencyClass.new }
 
     it "raises not numeric errors on each invalid field" do
       dummy.val1 = "abc"
@@ -22,6 +37,8 @@ RSpec.describe CurrencyValidator do
       expect(dummy.errors[:val1]).to eq ["is not a number"]
       expect(dummy.errors[:val2]).to be_empty
       expect(dummy.errors[:val3]).to eq ["is not a number"]
+      expect(dummy.errors.details[:val4].first[:error]).to eq :not_a_number_with_partner
+      expect(dummy.errors[:val4]).to eq ["fake error"]
     end
 
     it "errors if given negative number" do
@@ -66,6 +83,17 @@ RSpec.describe CurrencyValidator do
       expect(dummy.errors.details[:val2].first[:error]).to eq :not_a_number
       expect(dummy.val1).to eq "1,23.00"
       expect(dummy.val2).to eq "-£1,23"
+    end
+
+    it "returns _with_partner suffixed labels when prompted" do
+      # this requires keys to be present in the activemodel errors locale
+      # as they use non standard key names
+      dummy.val4 = "1,234.123"
+      dummy.val5 = ""
+      expect(dummy).to be_invalid
+      expect(dummy.errors.details[:val4].first[:error]).to eq :too_many_decimals_with_partner
+      expect(dummy.errors.details[:val5].first[:error]).to eq :not_a_number
+      expect(dummy.val4).to eq "1,234.123"
     end
   end
 end
