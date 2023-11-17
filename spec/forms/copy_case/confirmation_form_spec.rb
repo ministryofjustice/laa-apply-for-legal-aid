e73fb5f68699b4914d9390df8d8a3b3f793996da
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-11-17 08:26:48 +0000

    AP-4575-ext: Add cleanup to copy case confirmation form
    When No is selected on the confirmation page we should
    nullify the copy_case boolean flag to indicate this as
    false/unset and remove the id that was search for. this
    leaves the application in a state equivalent to not
    having copied anything. Flows can then allow the user
    to restart the process.

--- spec/forms/copy_case/confirmation_form_spec.rb
+++ spec/forms/copy_case/confirmation_form_spec.rb
@@ -34,10 +34,32 @@ RSpec.describe CopyCase::ConfirmationForm, type: :form do
     context "with yes chosen" do
       let(:copy_case_confirmation) { "true" }
 
+      it "does not change copy_case" do
+        expect { call_save }.not_to change { legal_aid_application.reload.copy_case }
+      end
+
+      it "does not change copy_case_id" do
+        expect { call_save }.not_to change { legal_aid_application.reload.copy_case_id }
+      end
+
       it "calls the cloner service" do
         call_save
         expect(cloner_klass).to have_received(:new).with(legal_aid_application, source_application)
         expect(cloner).to have_received(:call)
+      end
+
+      context "when copy_case and copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case: true, copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case" do
+          expect { call_save }.not_to change { legal_aid_application.reload.copy_case }.from(true)
+        end
+
+        it "does not update copy_case_id" do
+          expect { call_save }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
       end
     end
 
@@ -47,6 +69,20 @@ RSpec.describe CopyCase::ConfirmationForm, type: :form do
       it "does not call the cloner service" do
         call_save
         expect(cloner).not_to have_received(:call)
+      end
+
+      context "when copy_case and copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case: true, copy_case_id: source_application.id)
+        end
+
+        it "nullifies copy_case" do
+          expect { call_save }.to change { legal_aid_application.reload.copy_case }.from(true).to(nil)
+        end
+
+        it "nullifies copy_case_id" do
+          expect { call_save }.to change { legal_aid_application.reload.copy_case_id }.from(source_application.id).to(nil)
+        end
       end
     end
 
@@ -63,6 +99,20 @@ RSpec.describe CopyCase::ConfirmationForm, type: :form do
         expect(instance.errors.messages.values.flatten)
           .to include("Select yes if you want to copy the application")
       end
+
+      context "when copy_case and copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case: true, copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case" do
+          expect { call_save }.not_to change { legal_aid_application.reload.copy_case }.from(true)
+        end
+
+        it "does not update copy_case_id" do
+          expect { call_save }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
+      end
     end
   end
 
@@ -76,6 +126,20 @@ RSpec.describe CopyCase::ConfirmationForm, type: :form do
         save_as_draft
         expect(cloner).not_to have_received(:call)
       end
+
+      context "when copy_case and copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case: true, copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case }.from(true)
+        end
+
+        it "does not update copy_case_id" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
+      end
     end
 
     context "with no chosen" do
@@ -85,6 +149,20 @@ RSpec.describe CopyCase::ConfirmationForm, type: :form do
         save_as_draft
         expect(cloner).not_to have_received(:call)
       end
+
+      context "when copy_case and copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case: true, copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case }.from(true)
+        end
+
+        it "does not update copy_case_id" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
+      end
     end
 
     context "with no answer chosen" do
@@ -93,6 +171,20 @@ RSpec.describe CopyCase::ConfirmationForm, type: :form do
       it "is valid" do
         save_as_draft
         expect(instance).to be_valid
+      end
+
+      context "when copy_case and copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case: true, copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case }.from(true)
+        end
+
+        it "does not update copy_case_id" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
       end
     end
   end
