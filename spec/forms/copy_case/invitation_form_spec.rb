53d92b584dce9ee03f26396d037d51fa65d3ba42
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-11-17 10:07:13 +0000

    AP-4575-ext: Add cleanup to copy case invitation form
    When traverse backward though the flow the copy_case_id
    could already have been added and even its details copied.
    
    If this page is reached we need to remove the copy case id
    to start the process again.

--- spec/forms/copy_case/invitation_form_spec.rb
+++ spec/forms/copy_case/invitation_form_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
 
   let(:legal_aid_application) { create(:legal_aid_application) }
 
+  let(:source_application) do
+    create(:legal_aid_application,
+           application_ref: "L-TVH-U0T",
+           provider: legal_aid_application.provider)
+  end
+
   let(:params) do
     {
       model: legal_aid_application,
@@ -24,11 +30,25 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
           .to(true)
       end
 
+      it "maintains copy_case_id as nil" do
+        expect { call_save }.not_to change { legal_aid_application.reload.copy_case_id }.from(nil)
+      end
+
       context "when no previously chosen" do
         before { legal_aid_application.update!(copy_case: false) }
 
         it "updates copy_case to true" do
           expect { call_save }.to change(legal_aid_application, :copy_case).from(false).to(true)
+        end
+      end
+
+      context "when copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case_id: source_application.id)
+        end
+
+        it "nullifies copy_case_id" do
+          expect { call_save }.to change { legal_aid_application.reload.copy_case_id }.from(source_application.id).to(nil)
         end
       end
     end
@@ -49,6 +69,16 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
           expect { call_save }.to change(legal_aid_application, :copy_case).from(true).to(false)
         end
       end
+
+      context "when copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case_id: source_application.id)
+        end
+
+        it "nullifies copy_case_id" do
+          expect { call_save }.to change { legal_aid_application.reload.copy_case_id }.from(source_application.id).to(nil)
+        end
+      end
     end
 
     context "with no answer chosen" do
@@ -64,6 +94,16 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
         error_messages = instance.errors.messages.values.flatten
         expect(error_messages).to include("Select yes if you want to copy an application")
       end
+
+      context "when copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case_id" do
+          expect { call_save }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
+      end
     end
   end
 
@@ -78,6 +118,16 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
           .from(nil)
           .to(true)
       end
+
+      context "when copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case_id: source_application.id)
+        end
+
+        it "nullifies copy_case_id" do
+          expect { save_as_draft }.to change { legal_aid_application.reload.copy_case_id }.from(source_application.id).to(nil)
+        end
+      end
     end
 
     context "with no chosen" do
@@ -87,6 +137,16 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
         expect { save_as_draft }.to change(legal_aid_application, :copy_case)
           .from(nil)
           .to(false)
+      end
+
+      context "when copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case_id: source_application.id)
+        end
+
+        it "nullifies copy_case_id" do
+          expect { save_as_draft }.to change { legal_aid_application.reload.copy_case_id }.from(source_application.id).to(nil)
+        end
       end
     end
 
@@ -100,6 +160,16 @@ RSpec.describe CopyCase::InvitationForm, type: :form do
 
       it "does not update copy_case" do
         expect { save_as_draft }.not_to change(legal_aid_application, :copy_case).from(nil)
+      end
+
+      context "when copy_case_id already set" do
+        before do
+          legal_aid_application.update!(copy_case_id: source_application.id)
+        end
+
+        it "does not update copy_case_id" do
+          expect { save_as_draft }.not_to change { legal_aid_application.reload.copy_case_id }.from(source_application.id)
+        end
       end
     end
   end
