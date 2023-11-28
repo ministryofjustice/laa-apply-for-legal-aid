57d3951e010c9d6b247d560d1a581eba26fcaf5d
RoseSAK <rose.azadkan@digital.justice.gov.uk>
2023-11-28 16:49:07 +0000

    AP-3811 partner means assessment CFE V6 rspecs
    add rspecs for monthly income and outgoings and deductions

--- spec/models/cfe/v6/with_partner_result_spec.rb
+++ spec/models/cfe/v6/with_partner_result_spec.rb
@@ -3,24 +3,25 @@ require "rails_helper"
 module CFE
   module V6
     RSpec.describe Result do
-      let(:cfe_result_with_partner) { create(:cfe_v6_result, :with_employments, :with_partner) }
-      let(:with_maintenance) { create(:cfe_v6_result, :with_partner, :with_maintenance_received) }
-      let(:with_student_finance) { create(:cfe_v6_result, :with_partner, :with_student_finance_received) }
-      let(:with_total_deductions) { create(:cfe_v6_result, :with_partner, :with_total_deductions) }
-      let(:with_monthly_income_equivalents) { create(:cfe_v6_result, :with_partner, :with_monthly_income_equivalents) }
-      let(:with_monthly_outgoing_equivalents) { create(:cfe_v6_result, :with_partner, :with_monthly_outgoing_equivalents) }
-      let(:with_total_gross_income) { create(:cfe_v6_result, :with_partner, :with_total_gross_income) }
-      let(:with_employments) { create(:cfe_v6_result, :with_partner, :with_employments) }
-      let(:with_no_employments) { create(:cfe_v6_result, :with_partner, :with_no_employments) }
-      let(:legal_aid_application) { create(:legal_aid_application, :with_applicant_and_partner, :with_restrictions, :with_cfe_v6_result) }
-      let(:cfe_submission) { create(:cfe_submission, legal_aid_application:) }
+      let(:cfe_result) { create(:cfe_v6_result, :with_employments, :with_partner) }
+      # let(:with_maintenance) { create(:cfe_v6_result, :with_partner, :with_maintenance_received) }
+      # let(:with_student_finance) { create(:cfe_v6_result, :with_partner, :with_student_finance_received) }
+      # let(:with_total_deductions) { create(:cfe_v6_result, :with_partner, :with_total_deductions) }
+      # let(:with_monthly_income_equivalents) { create(:cfe_v6_result, :with_partner, :with_monthly_income_equivalents) }
+      # let(:with_monthly_outgoing_equivalents) { create(:cfe_v6_result, :with_partner, :with_monthly_outgoing_equivalents) }
+      # let(:with_total_gross_income) { create(:cfe_v6_result, :with_partner, :with_total_gross_income) }
+      # let(:with_no_employments) { create(:cfe_v6_result, :with_partner, :with_no_employments) }
+      # let(:legal_aid_application) { create(:legal_aid_application, :with_applicant_and_partner, :with_restrictions, :with_cfe_v6_result) }
+      # let(:cfe_submission) { create(:cfe_submission, legal_aid_application:) }
+
+      # INCOME SUMMARIES
 
       describe "gross_income_breakdown" do
         context "with partner set to false" do
           let(:partner) { false }
 
           it "returns the gross income breakdown for the client" do
-            expect(cfe_result_with_partner.gross_income_breakdown(partner:)[:state_benefits][:monthly_equivalents][:all_sources]).to eq(75.0)
+            expect(cfe_result.gross_income_breakdown(partner:)[:state_benefits][:monthly_equivalents][:all_sources]).to eq 75.0
           end
         end
 
@@ -28,7 +29,7 @@ module CFE
           let(:partner) { true }
 
           it "returns the gross income breakdown for the partner" do
-            expect(cfe_result_with_partner.gross_income_breakdown(partner:)[:state_benefits][:monthly_equivalents][:all_sources]).to eq(86.67)
+            expect(cfe_result.gross_income_breakdown(partner:)[:state_benefits][:monthly_equivalents][:all_sources]).to eq 86.67
           end
         end
       end
@@ -38,7 +39,7 @@ module CFE
           let(:partner) { false }
 
           it "returns the gross income summary for the client" do
-            expect(cfe_result_with_partner.gross_income_summary(partner:)[:total_gross_income]).to eq(0.0)
+            expect(cfe_result.gross_income_summary(partner:)[:total_gross_income]).to be_zero
           end
         end
 
@@ -46,7 +47,7 @@ module CFE
           let(:partner) { true }
 
           it "returns the gross income summary for the partner" do
-            expect(cfe_result_with_partner.gross_income_summary(partner:)[:total_gross_income]).to eq(150.0)
+            expect(cfe_result.gross_income_summary(partner:)[:total_gross_income]).to eq 150.0
           end
         end
       end
@@ -56,7 +57,7 @@ module CFE
           let(:partner) { false }
 
           it "returns the total gross income for the client" do
-            expect(cfe_result_with_partner.total_gross_income_assessed(partner:)).to eq(0.0)
+            expect(cfe_result.total_gross_income_assessed(partner:)).to be_zero
           end
         end
 
@@ -64,7 +65,7 @@ module CFE
           let(:partner) { true }
 
           it "returns the total gross income for the partner" do
-            expect(cfe_result_with_partner.total_gross_income_assessed(partner:)).to eq(150.0)
+            expect(cfe_result.total_gross_income_assessed(partner:)).to eq 150.0
           end
         end
       end
@@ -74,7 +75,7 @@ module CFE
           let(:partner) { false }
 
           it "returns the total disposable income for the client only" do
-            expect(cfe_result_with_partner.total_disposable_income_assessed(partner:)).to eq(0.0)
+            expect(cfe_result.total_disposable_income_assessed(partner:)).to be_zero
           end
         end
 
@@ -82,7 +83,7 @@ module CFE
           let(:partner) { true }
 
           it "returns the total disposable income for the client and the partner" do
-            expect(cfe_result_with_partner.total_disposable_income_assessed(partner:)).to eq(2577.11)
+            expect(cfe_result.total_disposable_income_assessed(partner:)).to eq 2577.11
           end
         end
       end
@@ -92,7 +93,7 @@ module CFE
           let(:partner) { false }
 
           it "returns the disposable income summary for the client" do
-            expect(cfe_result_with_partner.disposable_income_summary(partner:)[:total_disposable_income]).to eq(0.0)
+            expect(cfe_result.disposable_income_summary(partner:)[:total_disposable_income]).to be_zero
           end
         end
 
@@ -100,7 +101,7 @@ module CFE
           let(:partner) { true }
 
           it "returns the disposable income summary for the partner" do
-            expect(cfe_result_with_partner.disposable_income_summary(partner:)[:total_disposable_income]).to eq(2577.11)
+            expect(cfe_result.disposable_income_summary(partner:)[:total_disposable_income]).to eq 2577.11
           end
         end
       end
@@ -110,7 +111,7 @@ module CFE
           let(:partner) { false }
 
           it "returns the disposable income breakdown for the client" do
-            expect(cfe_result_with_partner.disposable_income_breakdown(partner:)[:monthly_equivalents][:all_sources][:rent_or_mortgage]).to eq(125.0)
+            expect(cfe_result.disposable_income_breakdown(partner:)[:monthly_equivalents][:all_sources][:rent_or_mortgage]).to eq 125.0
           end
         end
 
@@ -118,17 +119,19 @@ module CFE
           let(:partner) { true }
 
           it "returns the disposable income breakdown for the partner" do
-            expect(cfe_result_with_partner.disposable_income_breakdown(partner:)[:monthly_equivalents][:all_sources][:rent_or_mortgage]).to eq(0.0)
+            expect(cfe_result.disposable_income_breakdown(partner:)[:monthly_equivalents][:all_sources][:rent_or_mortgage]).to be_zero
           end
         end
       end
+
+      # EMPLOYMENT
 
       describe "employment_income" do
         context "with partner set to false" do
           let(:partner) { false }
 
           it "returns the employment income for the client" do
-            expect(cfe_result_with_partner.employment_income(partner:)[:gross_income]).to eq(2143.97)
+            expect(cfe_result.employment_income(partner:)[:gross_income]).to eq 2143.97
           end
         end
 
@@ -136,7 +139,7 @@ module CFE
           let(:partner) { true }
 
           it "returns the employment income for the partner" do
-            expect(cfe_result_with_partner.employment_income(partner:)[:gross_income]).to eq(2229.17)
+            expect(cfe_result.employment_income(partner:)[:gross_income]).to eq 2229.17
           end
         end
       end
@@ -146,7 +149,7 @@ module CFE
           let(:partner) { false }
 
           it "returns the employment income for the client" do
-            expect(cfe_result_with_partner.employment_income_gross_income(partner:)).to eq(2143.97)
+            expect(cfe_result.employment_income_gross_income(partner:)).to eq 2143.97
           end
         end
 
@@ -154,10 +157,276 @@ module CFE
           let(:partner) { true }
 
           it "returns the employment income for the partner" do
-            expect(cfe_result_with_partner.employment_income_gross_income(partner:)).to eq(2229.17)
+            expect(cfe_result.employment_income_gross_income(partner:)).to eq 2229.17
           end
         end
       end
+
+      describe "employment_income_benefits_in_kind" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the benefits in kind for the client" do
+            expect(cfe_result.employment_income_benefits_in_kind(partner:)).to eq 16.60
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the benefits in kind for the partner" do
+            expect(cfe_result.employment_income_benefits_in_kind(partner:)).to be_zero
+          end
+        end
+      end
+
+      describe "employment_income_tax" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the employemnt income tax for the client" do
+            expect(cfe_result.employment_income_tax(partner:)).to eq(-204.15)
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the employment income tax for the partner" do
+            expect(cfe_result.employment_income_tax(partner:)).to eq(-235.2)
+          end
+        end
+      end
+
+      describe "employment_income_national_insurance" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the employemnt income national_insurance for the client" do
+            expect(cfe_result.employment_income_national_insurance(partner:)).to eq(-161.64)
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the employment income national_insurance for the partner" do
+            expect(cfe_result.employment_income_national_insurance(partner:)).to eq(-171.86)
+          end
+        end
+      end
+
+      describe "employment_income_fixed_employment_deduction" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the employemnt income fixed_employment_deduction for the client" do
+            expect(cfe_result.employment_income_fixed_employment_deduction(partner:)).to eq(-45.0)
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the employment income fixed_employment_deduction for the partner" do
+            expect(cfe_result.employment_income_fixed_employment_deduction(partner:)).to eq(-45.0)
+          end
+        end
+      end
+
+      describe "employment_income_net_employment_income" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the employemnt income net_employment_income for the client" do
+            expect(cfe_result.employment_income_net_employment_income(partner:)).to eq 1778.18
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the employment income net_employment_income for the partner" do
+            expect(cfe_result.employment_income_net_employment_income(partner:)).to eq 1777.11
+          end
+        end
+      end
+
+      describe "partner_jobs" do
+        it "returns the jobs for the partner" do
+          expect(cfe_result.partner_jobs[0][:name]).to eq "Job 1"
+          expect(cfe_result.partner_jobs[0][:payments].length).to eq 2
+        end
+      end
+
+      describe "partner_jobs?" do
+        it "returns true" do
+          expect(cfe_result.partner_jobs?).to be true
+        end
+      end
+
+      # MONTHLY INCOME EQUIVALENTS
+
+      describe "monthly_income_equivalents" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the expected values" do
+            expect(cfe_result.monthly_state_benefits(partner:)).to eq 75.0
+            expect(cfe_result.mei_friends_or_family(partner:)).to be_zero
+            expect(cfe_result.mei_maintenance_in(partner:)).to be_zero
+            expect(cfe_result.mei_property_or_lodger(partner:)).to be_zero
+            expect(cfe_result.mei_student_loan(partner:)).to be_zero
+            expect(cfe_result.mei_pension(partner:)).to be_zero
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the expected values" do
+            expect(cfe_result.monthly_state_benefits(partner:)).to eq 86.67
+            expect(cfe_result.mei_friends_or_family(partner:)).to eq 166.67
+            expect(cfe_result.mei_maintenance_in(partner:)).to eq 21.0
+            expect(cfe_result.mei_property_or_lodger(partner:)).to eq 200.0
+            expect(cfe_result.mei_student_loan(partner:)).to eq 100.0
+            expect(cfe_result.mei_pension(partner:)).to eq 30.0
+            expect(cfe_result.total_monthly_income_including_employment_income(partner:)).to eq 5052.48
+          end
+        end
+      end
+
+      describe "total_monthly_income" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the total monthly income for the client" do
+            expect(cfe_result.total_monthly_income(partner:)).to eq 75.0
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the total monthly income for the partner" do
+            expect(cfe_result.total_monthly_income(partner:).round(2)).to eq 604.34
+          end
+        end
+      end
+
+      describe "total_monthly_income_including_employment_income" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the total monthly income including employment income for the client only" do
+            expect(cfe_result.total_monthly_income_including_employment_income(partner:)).to eq 2218.97
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the total monthly income including employment income for both the client and partner combined" do
+            client_monthly_income = cfe_result.total_monthly_income(partner: false)
+            client_employment_income = cfe_result.employment_income_gross_income(partner: false)
+            partner_monthly_income = cfe_result.total_monthly_income(partner: true)
+            partner_employment_income = cfe_result.employment_income_gross_income(partner: true)
+            expected_total = client_monthly_income + client_employment_income + partner_monthly_income + partner_employment_income
+            expect(cfe_result.total_monthly_income_including_employment_income(partner:).round(2)).to eq expected_total
+          end
+        end
+      end
+
+      # MONTHLY OUTGOING EQUIVALENTS
+
+      describe "monthly_outgoing_equivalents" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the expected values" do
+            expect(cfe_result.moe_childcare(partner:)).to be_zero
+            expect(cfe_result.moe_housing(partner:)).to eq 125.0
+            expect(cfe_result.moe_maintenance_out(partner:)).to be_zero
+            expect(cfe_result.moe_legal_aid(partner:)).to eq 100.0
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the expected values" do
+            expect(cfe_result.moe_childcare(partner:)).to eq 30.0
+            expect(cfe_result.moe_housing(partner:)).to eq 400.0
+            expect(cfe_result.moe_maintenance_out(partner:)).to eq 50.0
+            expect(cfe_result.moe_legal_aid(partner:)).to be_zero
+          end
+        end
+      end
+
+      describe "total_monthly_outgoings" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the total monthly outgoings for the client" do
+            expect(cfe_result.total_monthly_outgoings(partner:)).to eq 225.0
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the total monthly outgoings for the partner" do
+            expect(cfe_result.total_monthly_outgoings(partner:)).to eq 480.0
+          end
+        end
+      end
+
+      describe "total_monthly_outgoings_including_employment_outgoings" do
+        context "with partner set to false" do
+          let(:partner) { false }
+
+          it "returns the total monthly outgoings including employment outgoings for the client only" do
+            expect(cfe_result.total_monthly_outgoings_including_tax_and_ni(partner:)).to eq 590.79
+          end
+        end
+
+        context "with partner set to true" do
+          let(:partner) { true }
+
+          it "returns the total monthly outgoings including employment outgoings for both the client and partner combined" do
+            client_tax_and_ni = cfe_result.employment_income_tax(partner: false) + cfe_result.employment_income_national_insurance(partner: false)
+            client_total_outgoings = cfe_result.total_monthly_outgoings(partner: false) - client_tax_and_ni
+            partner_tax_and_ni = cfe_result.employment_income_tax(partner: true) + cfe_result.employment_income_national_insurance(partner: true)
+            partner_total_outgoings = cfe_result.total_monthly_outgoings(partner: true) - partner_tax_and_ni
+            expected_total = client_total_outgoings + partner_total_outgoings
+            expect(cfe_result.total_monthly_outgoings_including_tax_and_ni(partner:)).to eq expected_total
+          end
+        end
+      end
+
+      # DEDUCTIONS
+
+      describe "partner_allowance" do
+        it "returns the partner allowance" do
+          expect(cfe_result.partner_allowance).to eq(211.32)
+        end
+      end
+
+      describe "total_deductions" do
+        it "returns the total deductions amount" do
+          expected_amount = cfe_result.dependants_allowance + cfe_result.disregarded_state_benefits + cfe_result.partner_allowance
+          expect(cfe_result.total_deductions).to eq expected_amount
+        end
+      end
+
+      describe "total_deductions_including_fixed_employment_allowance" do
+        it "returns total deductions including fixed employment allowance" do
+          expected_amount = cfe_result.total_deductions + cfe_result.employment_income_fixed_employment_deduction
+          expect(cfe_result.total_deductions_including_fixed_employment_allowance).to eq expected_amount
+        end
+      end
+
+      # CAPITAL
     end
   end
 end
