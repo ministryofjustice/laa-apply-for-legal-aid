7b9e1833c49a877b58a8aac4aa8a87417a62e22e
skdejong <StephanieKdeJong@gmail.com>
2023-12-05 18:33:35 +0000

    AP-4630:Add partner_no_cash_outgoings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_092447) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_180352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -765,6 +765,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_092447) do
     t.boolean "extra_employment_information"
     t.string "extra_employment_information_details"
     t.boolean "no_cash_income"
+    t.boolean "no_cash_outgoings"
     t.index ["legal_aid_application_id"], name: "index_partners_on_legal_aid_application_id"
   end
 
