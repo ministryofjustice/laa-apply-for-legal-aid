aed490196f1fc9977507549ed6f0314a4458e1f5
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-12-08 09:43:17 +0000

    Add route controller and view for servicedown

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def servicedown
+    respond_to do |format|
+      format.html do
+        render :servicedown
+      end
+      format.json do
+        render  json:
+                [{ error: "Service temporarily unavailable" }],
+                status: :service_unavailable
+      end
+      format.js do
+        render  json:
+                [{ error: "Service temporarily unavailable" }],
+                status: :service_unavailable
+      end
+      format.all do
+        render  plain: "error: Service temporarily unavailable",
+                status: :service_unavailable
+      end
+    end
+  end
+end
