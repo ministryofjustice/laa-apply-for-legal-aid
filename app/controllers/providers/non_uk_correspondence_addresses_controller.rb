c16fb33746b0d444bd9529d7938e40bd3bbb8923
skdejong <StephanieKdeJong@gmail.com>
2024-02-22 12:39:01 +0000

    AP-4801: Update controller to add update function

--- app/controllers/providers/non_uk_correspondence_addresses_controller.rb
+++ app/controllers/providers/non_uk_correspondence_addresses_controller.rb
@@ -4,10 +4,23 @@ module Providers
       @form = Addresses::NonUkCorrespondenceAddressForm.new(model: non_uk_correspondence_address)
     end
 
+    def update
+      @form = Addresses::NonUkCorrespondenceAddressForm.new(form_params)
+      render :show unless save_continue_or_draft(@form)
+    end
+
   private
 
     def non_uk_correspondence_address
       applicant.address || applicant.build_address(location: "correspondence")
+    end
+
+    def form_params
+      merge_with_model(non_uk_correspondence_address) do
+        params.require(:non_uk_correspondence_address).permit(
+          :country, :address_line_one, :address_line_two, :city, :county
+        )
+      end
     end
   end
 end
