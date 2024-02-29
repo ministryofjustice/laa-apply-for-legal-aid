716336338f6e35d47318dcad5634ef1539491b4b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-02-29 07:49:45 +0000

    Update "18 years old today" logic for leap day

--- features/step_definitions/applicant_detail_steps.rb
+++ features/step_definitions/applicant_detail_steps.rb
@@ -13,7 +13,11 @@ When("I enter a date of birth that will make me 18 tomorrow") do
 end
 
 When("I enter a date of birth that will make me 18 today") do
-  dob = 18.years.ago
+  dob = if Time.zone.today.month == 2 && Time.zone.today.day == 29
+          18.years.ago + 1.day
+        else
+          18.years.ago
+        end
   fill_in("applicant_date_of_birth_3i", with: dob.day)
   fill_in("applicant_date_of_birth_2i", with: dob.month)
   fill_in("applicant_date_of_birth_1i", with: dob.year)
