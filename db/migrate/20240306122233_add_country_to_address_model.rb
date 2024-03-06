db2bdfe18ca14f944b59de44d8728d2659573cfb
skdejong <StephanieKdeJong@gmail.com>
2024-03-06 12:28:36 +0000

    AP-4801: Reorder migration Moved country migration to the end

--- db/migrate/20240306122233_add_country_to_address_model.rb
+++ db/migrate/20240306122233_add_country_to_address_model.rb
@@ -1,4 +1,4 @@
-class AddCountryToAddress < ActiveRecord::Migration[7.1]
+class AddCountryToAddressModel < ActiveRecord::Migration[7.1]
   def change
     add_column :addresses, :country, :string
     Address.update_all(country: "GBR")
