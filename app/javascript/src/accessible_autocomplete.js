1249876ad50848d11697f6d022421c6373ade1c1
skdejong <StephanieKdeJong@gmail.com>
2024-03-07 15:20:20 +0000

    AP-4801:Remove accessible-autocomplete

--- app/javascript/src/accessible_autocomplete.js
+++
@@ -1,4 +0,0 @@
-import accessibleAutocomplete from 'accessible-autocomplete'
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('[data-module="accessible-autocomplete"]')
-})
