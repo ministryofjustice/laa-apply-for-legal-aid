b86da9cfa359b45414cc12d80343d17035cdcbb3
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-04-15 10:17:59 +0100

    ap-4825: add make_links to flow
    and add update action to make_links controller

--- app/forms/providers/link_applications/make_link_form.rb
+++
@@ -1,7 +0,0 @@
-module Providers
-  module LinkApplications
-    class MakeLinkForm < BaseForm
-      form_for LegalAidApplication
-    end
-  end
-end
--- spec/requests/providers/link_application/make_links_controller_spec.rb
+++ spec/requests/providers/link_application/make_links_controller_spec.rb
@@ -25,4 +25,87 @@ RSpec.describe Providers::LinkApplication::MakeLinksController do
       end
     end
   end
+
+  describe "PATCH /providers/applications/:legal_aid_application_id/link_application/make_link" do
+    subject(:patch_request) { patch providers_legal_aid_application_link_application_make_link_path(legal_aid_application), params: }
+
+    let(:params) do
+      {
+        linked_application: {
+          link_type_code:,
+        },
+      }
+    end
+    let(:link_type_code) { "FC_LEAD" }
+
+    context "when the provider is not authenticated" do
+      before { patch_request }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when the provider is authenticated" do
+      before do
+        login_as provider
+      end
+
+      context "when the Continue button is pressed" do
+        context "when a link type is chosen" do
+          it "redirects to next page" do
+            patch_request
+            # TODO: This will change when ap-4826 is complete
+            expect(response).to redirect_to(providers_legal_aid_application_proceedings_types_path)
+          end
+
+          it "creates a new lead linked application" do
+            expect { patch_request }.to change(LinkedApplication, :count).by(1)
+          end
+
+          it "sets the correct link type" do
+            patch_request
+            expect(legal_aid_application.reload.lead_linked_application.link_type_code).to eq "FC_LEAD"
+          end
+        end
+
+        context "when no is chosen" do
+          let(:link_type_code) { "false" }
+
+          it "redirects to next page" do
+            patch_request
+            expect(response).to redirect_to(providers_legal_aid_application_proceedings_types_path)
+          end
+
+          it "does not create a new lead linked application" do
+            expect { patch_request }.not_to change(LinkedApplication, :count)
+          end
+        end
+
+        context "with no invalid postcode" do
+          let(:link_type_code) { nil }
+
+          it "re-renders the form with the validation errors" do
+            patch_request
+            expect(unescaped_response_body).to include("There is a problem")
+          end
+        end
+
+        context "when form submitted with Save as draft button" do
+          let(:params) do
+            {
+              draft_button: "Save and come back later",
+            }
+          end
+
+          it "redirects to the list of applications" do
+            patch_request
+            expect(response.body).to redirect_to providers_legal_aid_applications_path
+          end
+
+          it "sets the application as draft" do
+            expect { patch_request }.to change { legal_aid_application.reload.draft? }.from(false).to(true)
+          end
+        end
+      end
+    end
+  end
 end
