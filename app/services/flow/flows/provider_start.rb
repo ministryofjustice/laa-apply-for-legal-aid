67d004793abc888cbd80c502b83b8fff08526579
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2024-04-22 16:55:00 +0100

    AP-4679: Add correspondence address selections

--- app/services/flow/flows/provider_start.rb
+++ app/services/flow/flows/provider_start.rb
@@ -7,19 +7,7 @@ module Flow
         applicants: Steps::ProviderStart::ApplicantsStep,
         applicant_details: Steps::ProviderStart::ApplicantDetailsStep,
         correspondence_address_lookups: Steps::Addresses::CorrespondenceAddressLookupsStep,
-        correspondence_address_selections: {
-          path: ->(application) { urls.providers_legal_aid_application_correspondence_address_selection_path(application) },
-          forward: lambda do |application|
-            if Setting.home_address?
-              :different_addresses
-            elsif Setting.linked_applications?
-              :link_application_make_links
-            else
-              application.proceedings.any? ? :has_other_proceedings : :proceedings_types
-            end
-          end,
-          check_answers: :check_provider_answers,
-        },
+        correspondence_address_selections: Steps::Addresses::CorrespondenceAddressSelectionsStep,
         home_address_lookups: {
           path: ->(application) { urls.providers_legal_aid_application_home_address_lookup_path(application) },
           forward: :home_address_selections,
---
+++ app/services/flow/steps/addresses/correspondence_address_selections_step.rb
@@ -0,0 +1,19 @@
+module Flow
+  module Steps
+    module Addresses
+      CorrespondenceAddressSelectionsStep = Step.new(
+        path: ->(application) { Steps.urls.providers_legal_aid_application_correspondence_address_selection_path(application) },
+        forward: lambda do |application|
+          if Setting.home_address?
+            :different_addresses
+          elsif Setting.linked_applications?
+            :link_application_make_links
+          else
+            application.proceedings.any? ? :has_other_proceedings : :proceedings_types
+          end
+        end,
+        check_answers: :check_provider_answers,
+      )
+    end
+  end
+end
