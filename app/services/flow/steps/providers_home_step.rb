7495cd1759a333f0fde06113f26b0b29f868b1cd
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2024-04-24 09:07:17 +0100

    AP-4679: Update unused block variable for consistency

--- app/services/flow/steps/providers_home_step.rb
+++ app/services/flow/steps/providers_home_step.rb
@@ -1,7 +1,7 @@
 module Flow
   module Steps
     ProvidersHomeStep = Step.new(
-      path: ->(_application) { urls.providers_legal_aid_applications_path },
+      path: ->(_) { urls.providers_legal_aid_applications_path },
     )
   end
 end
