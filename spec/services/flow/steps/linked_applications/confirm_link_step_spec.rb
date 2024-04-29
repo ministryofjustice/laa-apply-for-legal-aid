5097506bccea4e5aa77de7648961c5aeb857935a
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-04-29 09:03:09 +0100

    ap-4830: refactor routing after linking

--- spec/services/flow/steps/linked_applications/confirm_link_step_spec.rb
+++ spec/services/flow/steps/linked_applications/confirm_link_step_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe Flow::Steps::LinkedApplications::ConfirmLinkStep, type: :request do
   let(:legal_aid_application) { create(:legal_aid_application, link_case:) }
+  let(:lead_application) { create(:legal_aid_application) }
   let(:link_case) { true }
+  let(:link_type_code) { "FC_LEAD" }
 
   describe "#path" do
     subject { described_class.path.call(legal_aid_application) }
@@ -13,8 +15,18 @@ RSpec.describe Flow::Steps::LinkedApplications::ConfirmLinkStep, type: :request 
   describe "#forward" do
     subject { described_class.forward.call(legal_aid_application) }
 
+    before { LinkedApplication.create!(lead_application_id: lead_application.id, associated_application_id: legal_aid_application.id, link_type_code:) }
+
     context "when the provider confirms they wish to link the application" do
-      it { is_expected.to be :link_application_copies }
+      context "when the link type is family" do
+        it { is_expected.to be :link_application_copies }
+      end
+
+      context "when the link_type is legal" do
+        let(:link_type_code) { "LEGAL" }
+
+        it { is_expected.to be :proceedings_types }
+      end
     end
 
     context "when the provider confirms they do not want to link to an application" do
