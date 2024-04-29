e08557048bb9db3e14f41a39a079737c9426a323
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-04-29 15:31:14 +0100

    ap-4678: refactor delegated_functions_step

--- app/services/flow/flows/provider_proceeding_loop.rb
+++ app/services/flow/flows/provider_proceeding_loop.rb
@@ -3,17 +3,7 @@ module Flow
     class ProviderProceedingLoop < FlowSteps
       STEPS = {
         client_involvement_type: Steps::ProviderProceedingLoop::ClientInvolvementTypeStep,
-        delegated_functions: {
-          path: lambda do |application|
-            proceeding = application.provider_step_params["id"]
-            urls.providers_legal_aid_application_delegated_function_path(application, proceeding)
-          end,
-          forward: ->(application) { Flow::ProceedingLoop.next_step(application) },
-          carry_on_sub_flow: true,
-          check_answers: lambda do |application|
-            Flow::ProceedingLoop.next_step(application) == :confirm_delegated_functions_date ? :confirm_delegated_functions_date : :check_provider_answers
-          end,
-        },
+        delegated_functions: Steps::ProviderProceedingLoop::DelegatedFunctionsStep,
         confirm_delegated_functions_date: {
           path: lambda do |application|
             # get the proceeding that has just been submitted, not the next `incomplete` one
