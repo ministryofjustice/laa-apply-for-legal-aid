60e3b3fb73bef1f667dc9877f733d33722744307
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-04-30 09:32:23 +0100

    ap-4678: refactor emergency_defaults step

--- app/services/flow/flows/provider_proceeding_loop.rb
+++ app/services/flow/flows/provider_proceeding_loop.rb
@@ -5,18 +5,7 @@ module Flow
         client_involvement_type: Steps::ProviderProceedingLoop::ClientInvolvementTypeStep,
         delegated_functions: Steps::ProviderProceedingLoop::DelegatedFunctionsStep,
         confirm_delegated_functions_date: Steps::ProviderProceedingLoop::ConfirmDelegatedFunctionsDateStep,
-        emergency_defaults: {
-          path: lambda do |application|
-            proceeding = Proceeding.find(application.provider_step_params["id"])
-            urls.providers_legal_aid_application_emergency_default_path(application, proceeding)
-          end,
-          forward: lambda do |application|
-            proceeding = Proceeding.find(application.provider_step_params["id"])
-            proceeding.accepted_emergency_defaults ? :substantive_defaults : :emergency_level_of_service
-          end,
-          carry_on_sub_flow: true,
-          check_answers: :check_provider_answers,
-        },
+        emergency_defaults: Steps::ProviderProceedingLoop::EmergencyDefaultsStep,
         substantive_defaults: {
           path: lambda do |application|
             proceeding = Proceeding.find(application.provider_step_params["id"])
