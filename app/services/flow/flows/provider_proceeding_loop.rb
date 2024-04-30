519dd6a02f0d7e994cd07c47b2e3efc9497dd200
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-04-30 10:05:20 +0100

    ap-4678: refactor substantive_defaults step

--- app/services/flow/flows/provider_proceeding_loop.rb
+++ app/services/flow/flows/provider_proceeding_loop.rb
@@ -6,18 +6,7 @@ module Flow
         delegated_functions: Steps::ProviderProceedingLoop::DelegatedFunctionsStep,
         confirm_delegated_functions_date: Steps::ProviderProceedingLoop::ConfirmDelegatedFunctionsDateStep,
         emergency_defaults: Steps::ProviderProceedingLoop::EmergencyDefaultsStep,
-        substantive_defaults: {
-          path: lambda do |application|
-            proceeding = Proceeding.find(application.provider_step_params["id"])
-            urls.providers_legal_aid_application_substantive_default_path(application, proceeding)
-          end,
-          forward: lambda do |application|
-            proceeding = Proceeding.find(application.provider_step_params["id"])
-            proceeding.accepted_substantive_defaults ? Flow::ProceedingLoop.next_step(application) : :substantive_level_of_service
-          end,
-          carry_on_sub_flow: true,
-          check_answers: :check_provider_answers,
-        },
+        substantive_defaults: Steps::ProviderProceedingLoop::SubstantiveDefaultsStep,
         emergency_level_of_service: {
           path: lambda do |application|
             proceeding = Proceeding.find(application.provider_step_params["id"])
