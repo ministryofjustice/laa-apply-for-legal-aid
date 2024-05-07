7ccc15413be7095f137c1955fa1f586d13c5df7c
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-05-07 13:26:21 +0100

    ap-4991-update-linked-applications-helper-for-discarded-applications

--- app/helpers/linked_applications_helper.rb
+++ app/helpers/linked_applications_helper.rb
@@ -12,10 +12,10 @@ private
   end
 
   def all_associated_linked_applications(legal_aid_application)
-    LinkedApplication.where(lead_application_id: legal_aid_application.lead_application.id, link_type_code: legal_aid_application.lead_linked_application.link_type_code).map(&:associated_application)
+    LinkedApplication.where(lead_application_id: legal_aid_application.lead_application.id, link_type_code: legal_aid_application.lead_linked_application.link_type_code).map(&:associated_application).reject(&:discarded?)
   end
 
   def all_lead_linked_applications(legal_aid_application)
-    LinkedApplication.where(associated_application_id: legal_aid_application.lead_application.id, link_type_code: legal_aid_application.lead_linked_application.link_type_code).map(&:lead_application)
+    LinkedApplication.where(associated_application_id: legal_aid_application.lead_application.id, link_type_code: legal_aid_application.lead_linked_application.link_type_code).map(&:lead_application).reject(&:discarded?)
   end
 end
--- spec/helpers/linked_applications_helper_spec.rb
+++ spec/helpers/linked_applications_helper_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe LinkedApplicationsHelper do
     let(:linked_application_one) { create(:legal_aid_application, application_ref: "L-INK-001", applicant: applicant_one) }
     let(:linked_application_two) { create(:legal_aid_application, application_ref: "L-INK-002", applicant: applicant_two) }
     let(:linked_application_three) { create(:legal_aid_application, application_ref: "L-INK-003", applicant: applicant_three) }
+    let(:linked_application_four) { create(:legal_aid_application, application_ref: "L-INK-004", applicant: applicant_four, discarded_at: Date.current) }
     let(:applicant_one) { create(:applicant, last_name: "applicant-one", first_name: "linked") }
     let(:applicant_two) { create(:applicant, last_name: "applicant-two", first_name: "linked") }
     let(:applicant_three) { create(:applicant, last_name: "applicant-three", first_name: "linked") }
+    let(:applicant_four) { create(:applicant, last_name: "applicant-four", first_name: "linked") }
 
     context "with a lead application" do
       before do
@@ -19,9 +21,10 @@ RSpec.describe LinkedApplicationsHelper do
         LinkedApplication.create!(lead_application_id: lead_application.id, associated_application_id: linked_application_one.id, link_type_code: "FC_LEAD")
         LinkedApplication.create!(lead_application_id: lead_application.id, associated_application_id: linked_application_two.id, link_type_code: "LEGAL")
         LinkedApplication.create!(lead_application_id: linked_application_three.id, associated_application_id: lead_application.id, link_type_code: "FC_LEAD")
+        LinkedApplication.create!(lead_application_id: lead_application.id, associated_application_id: linked_application_four.id, link_type_code: "FC_LEAD")
       end
 
-      it "returns details of all other applications linked to the lead application with the same link type" do
+      it "returns details of all other applications linked to the lead application with the same link type that have not been discarded" do
         expect(details).to eq "L-INK-001, linked applicant-one<br>L-INK-003, linked applicant-three"
       end
     end
