e8467742eeaf671302dab46d7f3b5236c4505df2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-05-10 13:30:49 +0100

    AP-4992: Update confirm_link flow step
    Read the new confirm_link value and simplify the return values

--- app/services/flow/steps/linked_applications/confirm_link_step.rb
+++ app/services/flow/steps/linked_applications/confirm_link_step.rb
@@ -4,18 +4,11 @@ module Flow
       ConfirmLinkStep = Step.new(
         path: ->(application) { Steps.urls.providers_legal_aid_application_link_application_confirm_link_path(application) },
         forward: lambda do |application|
-          case application.link_case
-          when true
-            if application.lead_linked_application&.link_type_code == "FC_LEAD"
-              :link_application_copies
-            else
-              application.proceedings.any? ? :has_other_proceedings : :proceedings_types
-            end
-          when false
-            application.proceedings.any? ? :has_other_proceedings : :proceedings_types
-          else
-            :link_application_make_links
-          end
+          link = application&.lead_linked_application
+          return :link_application_copies if link.confirm_link && link.link_type_code == "FC_LEAD"
+          return :link_application_make_links if link.confirm_link.nil?
+
+          application.proceedings.any? ? :has_other_proceedings : :proceedings_types
         end,
         check_answers: :check_provider_answers,
         carry_on_sub_flow: ->(application) { application.lead_linked_application&.link_type_code == "FC_LEAD" },
--- spec/services/flow/steps/linked_applications/confirm_link_step_spec.rb
+++ spec/services/flow/steps/linked_applications/confirm_link_step_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Flow::Steps::LinkedApplications::ConfirmLinkStep, type: :request do
-  let(:legal_aid_application) { create(:legal_aid_application, link_case:) }
+  let(:legal_aid_application) { create(:legal_aid_application) }
   let(:lead_application) { create(:legal_aid_application) }
   let(:link_case) { true }
   let(:link_type_code) { "FC_LEAD" }
@@ -15,9 +15,16 @@ RSpec.describe Flow::Steps::LinkedApplications::ConfirmLinkStep, type: :request 
   describe "#forward" do
     subject { described_class.forward.call(legal_aid_application) }
 
-    before { LinkedApplication.create!(lead_application_id: lead_application.id, associated_application_id: legal_aid_application.id, link_type_code:) }
+    before do
+      LinkedApplication.create!(lead_application_id: lead_application.id,
+                                associated_application_id: legal_aid_application.id,
+                                link_type_code:,
+                                confirm_link:)
+    end
 
     context "when the provider confirms they wish to link the application" do
+      let(:confirm_link) { true }
+
       context "when the link type is family" do
         it { is_expected.to be :link_application_copies }
       end
@@ -30,13 +37,13 @@ RSpec.describe Flow::Steps::LinkedApplications::ConfirmLinkStep, type: :request 
     end
 
     context "when the provider confirms they do not want to link to an application" do
-      let(:link_case) { false }
+      let(:confirm_link) { false }
 
       it { is_expected.to be :proceedings_types }
     end
 
     context "when the provider confirms they want to link to a different application" do
-      let(:link_case) { nil }
+      let(:confirm_link) { nil }
 
       it { is_expected.to be :link_application_make_links }
     end
