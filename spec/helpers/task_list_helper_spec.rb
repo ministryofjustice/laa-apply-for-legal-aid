92caee0889ed1795b7061eb22e9e8ec93d076d44
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-05-14 15:43:14 +0100

    ap-4942: obtain merits_task_list from copied case

--- spec/helpers/task_list_helper_spec.rb
+++ spec/helpers/task_list_helper_spec.rb
@@ -7,20 +7,43 @@ RSpec.describe TaskListHelper do
     let(:legal_aid_application) { create(:legal_aid_application, :with_multiple_proceedings_inc_section8) }
     let(:task_name) { :opponent_name }
 
-    before do
-      create(:legal_framework_merits_task_list, :da001, legal_aid_application:)
+    context "when the application has not been copied" do
+      before do
+        create(:legal_framework_merits_task_list, :da001, legal_aid_application:)
+      end
+
+      context "when task name is included in list of required tasks from LFA" do
+        let(:task_name) { :opponent_name }
+
+        it { is_expected.to be_truthy }
+      end
+
+      context "when task name is NOT included in list of required tasks from LFA" do
+        let(:task_name) { :why_matter_opposed }
+
+        it { is_expected.to be_falsey }
+      end
     end
 
-    context "when task name is included in list of required tasks from LFA" do
-      let(:task_name) { :opponent_name }
+    context "when the application has been copied" do
+      before do
+        create(:legal_framework_merits_task_list, :da001, legal_aid_application: source_application)
+      end
 
-      it { is_expected.to be_truthy }
-    end
+      let(:source_application) { create(:legal_aid_application, :with_multiple_proceedings_inc_section8) }
+      let(:legal_aid_application) { create(:legal_aid_application, copy_case: true, copy_case_id: source_application.id) }
 
-    context "when task name is NOT included in list of required tasks from LFA" do
-      let(:task_name) { :why_matter_opposed }
+      context "when task name is included in list of required tasks from LFA" do
+        let(:task_name) { :opponent_name }
 
-      it { is_expected.to be_falsey }
+        it { is_expected.to be_truthy }
+      end
+
+      context "when task name is NOT included in list of required tasks from LFA" do
+        let(:task_name) { :why_matter_opposed }
+
+        it { is_expected.to be_falsey }
+      end
     end
   end
 end
