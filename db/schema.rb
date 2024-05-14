47b3788fb800ca125956d89fa4540dd8dc661d09
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-05-14 10:50:56 +0100

    AP-5005: Update legal_aid_application database table
    Change the no-longer used link_case filed to case_cloned
    
    This should allow us to track the cloner being run and
    prevent it being run multiple times

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_114941) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_091845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -612,7 +612,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_114941) do
     t.boolean "applicant_in_receipt_of_housing_benefit"
     t.boolean "copy_case"
     t.uuid "copy_case_id"
-    t.boolean "link_case"
+    t.boolean "case_cloned"
     t.index ["applicant_id"], name: "index_legal_aid_applications_on_applicant_id"
     t.index ["application_ref"], name: "index_legal_aid_applications_on_application_ref", unique: true
     t.index ["discarded_at"], name: "index_legal_aid_applications_on_discarded_at"
