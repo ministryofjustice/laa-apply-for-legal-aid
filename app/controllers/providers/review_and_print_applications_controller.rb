e7cc6a19e378e947150f97ecf3bc811cfb20b936
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2024-05-20 15:10:06 +0100

    AP-5021: Display correct copying info on review_and_print and submitted_applications

--- app/controllers/providers/review_and_print_applications_controller.rb
+++ app/controllers/providers/review_and_print_applications_controller.rb
@@ -4,8 +4,7 @@ module Providers
     helper_method :display_employment_income?
 
     def show
-      @show_linked_proceedings = @legal_aid_application.copy_case? && @legal_aid_application.proceedings.count.zero?
-      @source_application = @show_linked_proceedings ? LegalAidApplication.find(legal_aid_application.copy_case_id) : legal_aid_application
+      @source_application = LegalAidApplication.find(legal_aid_application.copy_case_id) if @legal_aid_application.copy_case?
     end
 
     def continue
