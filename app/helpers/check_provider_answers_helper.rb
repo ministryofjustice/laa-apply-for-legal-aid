bb663bf5a9aa693de6fc9903fba3af80c208701c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-05-31 15:57:53 +0100

    AP-5041: Update CYA logic for home/correspondence address handling

--- app/helpers/check_provider_answers_helper.rb
+++ app/helpers/check_provider_answers_helper.rb
@@ -1,9 +1,10 @@
 module CheckProviderAnswersHelper
   def change_address_link(legal_aid_application, location)
     if location == "correspondence"
+      return providers_legal_aid_application_correspondence_address_choice_path(legal_aid_application.id) if Setting.home_address?
       return providers_legal_aid_application_correspondence_address_lookup_path(legal_aid_application.id, anchor: :postcode) if legal_aid_application.applicant&.address&.lookup_used?
 
-      providers_legal_aid_application_correspondence_address_manual_path(legal_aid_application.id, anchor: :postcode)
+      providers_legal_aid_application_correspondence_address_choice_path(legal_aid_application.id, anchor: :postcode)
 
     elsif location == "home"
       providers_legal_aid_application_home_address_status_path(legal_aid_application.id)
@@ -11,12 +12,24 @@ module CheckProviderAnswersHelper
   end
 
   def home_address_text(applicant)
-    if applicant.same_correspondence_and_home_address?
+    if Setting.home_address? && applicant.same_correspondence_and_home_address?
+      # TODO: Delete this if block when home_address? flag is removed
+      # and leave `if applicant.no_fixed_residence?` as the first option
       "Same as correspondence address"
     elsif applicant.no_fixed_residence?
       "No fixed residence"
     else
       address_with_line_breaks(applicant.home_address)
+    end
+  end
+
+  def correspondence_address_text(applicant)
+    if Setting.home_address? && applicant.same_correspondence_and_home_address?
+      # TODO: Just delete the `Setting.home_address? &&` from this if block
+      # when the home_address? flag is removed
+      address_with_line_breaks(applicant.home_address)
+    else
+      address_with_line_breaks(applicant.address)
     end
   end
 end
--- spec/helpers/check_provider_answers_helper_spec.rb
+++ spec/helpers/check_provider_answers_helper_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 RSpec.describe CheckProviderAnswersHelper do
   let(:legal_aid_application) { create(:legal_aid_application, applicant:) }
   let(:applicant) { create(:applicant, address:, same_correspondence_and_home_address:, no_fixed_residence:) }
-  let(:address) { create(:address, lookup_used:) }
-  let(:lookup_used) { false }
   let(:same_correspondence_and_home_address) { false }
-  let(:no_fixed_abode) { false }
   let(:no_fixed_residence) { false }
+  let(:lookup_used) { false }
+  let(:address) { create(:address, lookup_used:) }
 
   describe ".change_address_link" do
     subject(:link) { helper.change_address_link(legal_aid_application, location) }
@@ -24,8 +23,8 @@ RSpec.describe CheckProviderAnswersHelper do
       let(:location) { "correspondence" }
 
       context "when lookup_used is false" do
-        it "returns the manual address entry path" do
-          expect(link).to eq "/providers/applications/#{legal_aid_application.id}/correspondence_address/enter_correspondence_address?locale=en#postcode"
+        it "returns the address choice path" do
+          expect(link).to eq "/providers/applications/#{legal_aid_application.id}/correspondence_address/where_to_send_correspondence?locale=en#postcode"
         end
       end
 
@@ -44,12 +43,6 @@ RSpec.describe CheckProviderAnswersHelper do
 
     let(:address) { create(:address, location: "home") }
 
-    context "when applicant has same home and correspondence address" do
-      let(:same_correspondence_and_home_address) { true }
-
-      it { expect(text).to eq "Same as correspondence address" }
-    end
-
     context "when applicant has no fixed residence" do
       let(:no_fixed_residence) { true }
 
@@ -57,6 +50,24 @@ RSpec.describe CheckProviderAnswersHelper do
     end
 
     context "when applicant has a home address" do
+      it { expect(text).to eq "#{address.address_line_one}<br>#{address.address_line_two}<br>#{address.city}<br>#{address.county}<br>#{address.pretty_postcode}" }
+    end
+  end
+
+  describe ".correspondence_address_text" do
+    subject(:text) { helper.correspondence_address_text(applicant) }
+
+    context "when applicant has same home and correspondence address" do
+      let(:address) { create(:address, location: "home") }
+      let(:same_correspondence_and_home_address) { true }
+
+      it { expect(text).to eq "#{address.address_line_one}<br>#{address.address_line_two}<br>#{address.city}<br>#{address.county}<br>#{address.pretty_postcode}" }
+    end
+
+    context "when applicant has a separate correspondence address" do
+      let(:address) { create(:address, location: "correspondence") }
+      let(:same_correspondence_and_home_address) { false }
+
       it { expect(text).to eq "#{address.address_line_one}<br>#{address.address_line_two}<br>#{address.city}<br>#{address.county}<br>#{address.pretty_postcode}" }
     end
   end
