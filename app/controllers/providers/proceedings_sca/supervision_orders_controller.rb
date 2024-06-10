e13bb231cdfe50364ffdae7d39756ee89419796e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-06-10 12:03:34 +0100

    AP-4532: Add route, controller, view, etc for SCASupervisionOrder question

---
+++ app/controllers/providers/proceedings_sca/supervision_orders_controller.rb
@@ -0,0 +1,43 @@
+module Providers
+  module ProceedingsSCA
+    class SupervisionOrdersController < ProviderBaseController
+      prefix_step_with :proceedings_sca
+
+      def show
+        form
+      end
+
+      def update
+        return continue_or_draft if draft_selected?
+
+        if form.valid?
+          return redirect_to providers_legal_aid_application_sca_interrupt_path(legal_aid_application, "supervision") if form.change_supervision_order?
+
+          return go_forward
+        end
+        render :show, status: :unprocessable_entity
+      end
+
+    private
+
+      def form_params
+        return {} unless params[:binary_choice_form]
+
+        params.require(:binary_choice_form).permit(:change_supervision_order)
+      end
+
+      def form
+        @form ||= BinaryChoiceForm.call(
+          journey: :provider,
+          radio_buttons_input_name: :change_supervision_order,
+          form_params:,
+          error: error_message,
+        )
+      end
+
+      def error_message
+        I18n.t("providers.proceedings_sca.supervision_orders.error")
+      end
+    end
+  end
+end
---
+++ spec/requests/providers/proceedings_sca/supervision_orders_controller_spec.rb
@@ -0,0 +1,91 @@
+require "rails_helper"
+
+RSpec.describe Providers::ProceedingsSCA::SupervisionOrdersController do
+  let(:legal_aid_application) { create(:legal_aid_application, :with_proceedings, explicit_proceedings: %i[da002]) }
+  let(:login_provider) { login_as legal_aid_application.provider }
+
+  describe "GET /providers/applications/:id/supervision_orders" do
+    subject(:get_request) { get providers_legal_aid_application_supervision_orders_path(legal_aid_application) }
+
+    before do
+      login_provider
+      get_request
+    end
+
+    it "renders show with ok status" do
+      expect(response).to have_http_status(:ok).and render_template("providers/proceedings_sca/supervision_orders/show")
+    end
+
+    context "when not authenticated" do
+      let(:login_provider) { nil }
+
+      it_behaves_like "a provider not authenticated"
+    end
+
+    context "when authenticated as a different provider" do
+      let(:login_provider) { login_as create(:provider) }
+
+      it_behaves_like "an authenticated provider from a different firm"
+    end
+  end
+
+  describe "PATCH /providers/applications/:id/supervision_orders" do
+    subject(:patch_request) do
+      patch(providers_legal_aid_application_supervision_orders_path(legal_aid_application, params:))
+    end
+
+    let(:params) do
+      {
+        binary_choice_form: {
+          change_supervision_order:,
+        },
+      }
+    end
+    let(:skip?) { false }
+
+    before do
+      login_provider
+      patch_request unless skip?
+    end
+
+    context "when the provider responds yes" do
+      let(:change_supervision_order) { "true" }
+
+      it "redirects to the next page" do
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(providers_legal_aid_application_sca_interrupt_path(legal_aid_application, "supervision"))
+      end
+    end
+
+    context "when the provider responds no" do
+      let(:change_supervision_order) { "false" }
+
+      it "redirects to the next page" do
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "when the provider does not provide a response" do
+      let(:change_supervision_order) { "" }
+
+      it "renders the same page with an error message" do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include("Select yes if the application is to vary, discharge or extend a supervision order").twice
+      end
+    end
+
+    context "with form submitted using Save as draft button" do
+      let(:skip?) { true }
+      let(:params) { { draft_button: "Save and come back later" } }
+
+      it "redirects provider to provider's applications page" do
+        patch_request
+        expect(response).to redirect_to(providers_legal_aid_applications_path)
+      end
+
+      it "sets the application as draft" do
+        expect { patch_request }.to change { legal_aid_application.reload.draft? }.from(false).to(true)
+      end
+    end
+  end
+end
