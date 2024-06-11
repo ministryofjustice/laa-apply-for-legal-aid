326d6e6927665dc22a9692ea830d95432b6073c4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-06-11 15:55:40 +0100

    AP-4840: Broken CYA with Home Address
    When we changed the home address flow, we can now go straight
    to the home address controller.  This had a no_state_change_required?
    override that always by-passed the state change.  This meant
    that when we reached the CYA page the applictaion state was
    in the initialised state and could not transition to the
    next state.
    
    Reverting the home controller to match the correspondence
    controller allows either one to change the state and enable
    access to the CYA page.
    
    As it is the same I have moved it into the AddressSelectionsBaseController

--- app/controllers/providers/correspondence_address/selections_controller.rb
+++ app/controllers/providers/correspondence_address/selections_controller.rb
@@ -5,10 +5,6 @@ module Providers
 
     private
 
-      def no_state_change_required?
-        legal_aid_application.entering_applicant_details? || legal_aid_application.checking_applicant_details?
-      end
-
       def address
         applicant.address || applicant.build_address(country_code: "GBR", country_name: "United Kingdom")
       end
