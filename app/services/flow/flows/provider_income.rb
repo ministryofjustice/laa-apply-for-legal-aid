3e43e9f7b35dea9a97be60d9a9fcdececd89455a
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-06-13 15:58:49 +0100

    ap-4671: refactor client_completed_means_step

--- app/services/flow/flows/provider_income.rb
+++ app/services/flow/flows/provider_income.rb
@@ -2,28 +2,7 @@ module Flow
   module Flows
     class ProviderIncome < FlowSteps
       STEPS = {
-        client_completed_means: {
-          path: ->(application) { urls.providers_legal_aid_application_client_completed_means_path(application) },
-          forward: lambda do |application|
-            status = HMRC::StatusAnalyzer.call(application)
-            case status
-            when :applicant_multiple_employments, :applicant_no_hmrc_data
-              :full_employment_details
-            when :applicant_single_employment
-              :employment_incomes
-            when :applicant_unexpected_employment_data
-              :unexpected_employment_incomes
-            when :applicant_not_employed
-              if application.uploading_bank_statements?
-                :receives_state_benefits
-              else
-                :identify_types_of_incomes
-              end
-            else
-              raise "Unexpected hmrc status #{status.inspect}"
-            end
-          end,
-        },
+        client_completed_means: Steps::ProviderIncome::ClientCompletedMeansStep,
         employment_incomes: {
           path: ->(application) { urls.providers_legal_aid_application_means_employment_income_path(application) },
           forward: lambda do |application|
