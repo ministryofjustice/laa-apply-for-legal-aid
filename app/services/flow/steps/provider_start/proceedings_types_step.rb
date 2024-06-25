b0fcc9a540db2c74a022d8d80031dbd55121104f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-06-25 13:24:45 +0100

    AP-5062: Update proceeding_types step

--- app/services/flow/steps/provider_start/proceedings_types_step.rb
+++ app/services/flow/steps/provider_start/proceedings_types_step.rb
@@ -3,7 +3,16 @@ module Flow
     module ProviderStart
       ProceedingsTypesStep = Step.new(
         path: ->(application) { Steps.urls.providers_legal_aid_application_proceedings_types_path(application) },
-        forward: :has_other_proceedings,
+        forward: lambda do |_application, proceeding|
+          case proceeding.sca_type
+          when "core"
+            :proceedings_sca_proceeding_issue_statuses
+          when "related"
+            :proceedings_sca_heard_togethers
+          else
+            :has_other_proceedings
+          end
+        end,
       )
     end
   end
--- spec/services/flow/steps/provider_start/proceedings_types_step_spec.rb
+++ spec/services/flow/steps/provider_start/proceedings_types_step_spec.rb
@@ -10,8 +10,24 @@ RSpec.describe Flow::Steps::ProviderStart::ProceedingsTypesStep, type: :request 
   end
 
   describe "#forward" do
-    subject { described_class.forward }
+    subject { described_class.forward.call(legal_aid_application, proceeding) }
 
-    it { is_expected.to eq :has_other_proceedings }
+    context "when the chosen proceeding does not have an sca_type" do
+      let(:proceeding) { build_stubbed(:proceeding, :da004) }
+
+      it { is_expected.to eq :has_other_proceedings }
+    end
+
+    context "when the chosen proceeding is an sca core proceeding" do
+      let(:proceeding) { build_stubbed(:proceeding, :pb003) }
+
+      it { is_expected.to eq :proceedings_sca_proceeding_issue_statuses }
+    end
+
+    context "when the chosen proceeding is an sca related proceeding" do
+      let(:proceeding) { build_stubbed(:proceeding, :pb007) }
+
+      it { is_expected.to eq :proceedings_sca_heard_togethers }
+    end
   end
 end
