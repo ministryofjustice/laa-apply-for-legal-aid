7ed69c55558a49f9e90309b54551dfa5a87f54cb
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-06-26 09:30:37 +0100

    ap-4681: refactor check_provider_answers_step

--- app/services/flow/flows/provider_start.rb
+++ app/services/flow/flows/provider_start.rb
@@ -54,19 +54,7 @@ module Flow
           carry_on_sub_flow: false,
         },
         # partner_flow called here
-        check_provider_answers: {
-          path: ->(application) { urls.providers_legal_aid_application_check_provider_answers_path(application) },
-          forward: lambda do |application|
-            if application.under_16_blocked?
-              :use_ccms_under16s
-            elsif application.non_means_tested?
-              application.change_state_machine_type("NonMeansTestedStateMachine")
-              :confirm_non_means_tested_applications
-            else
-              application.applicant.national_insurance_number? ? :check_benefits : :no_national_insurance_numbers
-            end
-          end,
-        },
+        check_provider_answers: Steps::ProviderStart::CheckProviderAnswersStep,
         confirm_non_means_tested_applications: Steps::ProviderStart::ConfirmNonMeansTestedApplicationStep,
         no_national_insurance_numbers: {
           path: ->(application) { urls.providers_legal_aid_application_no_national_insurance_number_path(application) },
