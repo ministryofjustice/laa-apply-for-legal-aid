4a476f00bc6ff55d913bdc9e4d3d0a5b39f489dd
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-06-27 12:00:00 +0100

    AP-5107: Add target_application to linked_application
    The aim is to populate target application with the search result
    and, if the target is already the lead for something else or is
    neither a lead or an associated application, then copy it into
    the lead_application field as well.
    
    If the target application itself has a lead application, then we
    copy the target.lead_application into the current.lead_application
    field.  This should allow us to show the provider linking to
    `target` but under the hood, setting `target.lead` as the lead
    application
    
    fixup to db udate?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_094124) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_104142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -664,9 +664,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_094124) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "confirm_link"
+    t.uuid "target_application_id"
     t.index ["associated_application_id"], name: "index_linked_applications_on_associated_application_id"
     t.index ["lead_application_id", "associated_application_id"], name: "index_linked_applications", unique: true
     t.index ["lead_application_id"], name: "index_linked_applications_on_lead_application_id"
+    t.index ["target_application_id"], name: "index_linked_applications_on_target_application_id"
   end
 
   create_table "malware_scan_results", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
