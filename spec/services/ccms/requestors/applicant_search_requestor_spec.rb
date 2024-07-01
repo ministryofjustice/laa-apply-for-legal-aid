3643adf0942e18e9bda411d9b20def6eed136c21
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-07-01 15:38:37 +0100

    ap-5097: test special characters in applicant_search_requestor

--- spec/services/ccms/requestors/applicant_search_requestor_spec.rb
+++ spec/services/ccms/requestors/applicant_search_requestor_spec.rb
@@ -50,6 +50,28 @@ module CCMS
             )
           end
         end
+
+        context "when the applicant has a surname containing special characters" do
+          let(:applicant) do
+            create(:applicant,
+                   first_name: "lenovo",
+                   last_name: "O’‘Hare",
+                   date_of_birth: Date.new(1969, 1, 1),
+                   national_insurance_number: "YS327299B")
+          end
+
+          it "generates the expected XML" do
+            allow(requestor).to receive(:transaction_request_id).and_return(expected_tx_id)
+            expect(requestor.formatted_xml).to be_soap_envelope_with(
+              command: "clientbim:ClientInqRQ",
+              transaction_id: expected_tx_id,
+              matching: %w[
+                <clientbio:Surname>O''Hare</clientbio:Surname>
+                <clientbio:FirstName>lenovo</clientbio:FirstName>
+              ],
+            )
+          end
+        end
       end
 
       describe "#transaction_request_id" do
