94626f239ec57c8c1e8df97f3147fc704ac2f54d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-07-08 11:24:28 +0100

    AP-4643: Add flow for biological parent step

---
+++ spec/services/flow/steps/provider_merits/biological_parent_step_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe Flow::Steps::ProviderMerits::BiologicalParentStep, type: :request do
+  let(:legal_aid_application) { create(:legal_aid_application) }
+  let(:proceeding) { create(:proceeding, :pb003, legal_aid_application:) }
+
+  before do
+    allow(legal_aid_application).to receive(:provider_step_params).and_return({ "merits_task_list_id" => proceeding.id })
+    allow(Flow::MeritsLoop).to receive(:forward_flow).and_return(:merits_task_lists)
+  end
+
+  describe "#path" do
+    subject { described_class.path.call(legal_aid_application) }
+
+    it { is_expected.to eq providers_merits_task_list_is_client_biological_parent_path(proceeding) }
+  end
+
+  describe "#forward" do
+    subject { described_class.forward.call(legal_aid_application) }
+
+    it { is_expected.to eq :merits_task_lists }
+  end
+end
