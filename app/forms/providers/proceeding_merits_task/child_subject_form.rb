5656e3a1f5bb4691cf40261fede09c6174de80ba
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-07-10 15:12:02 +0100

    ap-4645: Add child_subject form and form spec

--- app/forms/providers/proceeding_merits_task/child_subject_form.rb
+++ app/forms/providers/proceeding_merits_task/child_subject_form.rb
@@ -3,7 +3,14 @@ module Providers
     class ChildSubjectForm < BaseForm
       form_for Proceeding
 
-      attr_accessor :child_subject
+      attr_accessor :relationship_to_child
+
+      validates :relationship_to_child,
+                inclusion: {
+                  in: ["child_subject", ""],
+                  message: I18n.t("providers.proceeding_merits_task.relationship_to_child.child_subject.error"),
+                },
+                unless: :draft?
     end
   end
 end
---
+++ spec/forms/providers/proceeding_merits_task/child_subject_form_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe Providers::ProceedingMeritsTask::ChildSubjectForm, type: :form do
+  subject(:form) { described_class.new(params) }
+
+  let(:params) { { relationship_to_child: } }
+
+  describe "#save" do
+    context "with validation" do
+      before { form.valid? }
+
+      context "when sent text in the allow list" do
+        let(:relationship_to_child) { "child_subject" }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "when sent an empty string" do
+        let(:relationship_to_child) { "" }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "when sent unknown text" do
+        let(:relationship_to_child) { "unknown" }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context "when the parameters are missing" do
+        let(:relationship_to_child) { nil }
+
+        it { is_expected.not_to be_valid }
+
+        it "records the error message" do
+          expect(form.errors[:relationship_to_child]).to eq ["Select yes if your client is a child subject of the proceeding"]
+        end
+      end
+    end
+  end
+end
