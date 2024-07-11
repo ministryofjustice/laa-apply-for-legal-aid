472a1d1547ec0736aa88d3df5686ed044f1cc1b4
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-07-11 15:04:50 +0100

    ap-4623: add separate_representation_required boolean database field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_04_134837) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_11_135846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -624,6 +624,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_04_134837) do
     t.boolean "copy_case"
     t.uuid "copy_case_id"
     t.boolean "case_cloned"
+    t.boolean "separate_representation_required"
     t.index ["applicant_id"], name: "index_legal_aid_applications_on_applicant_id"
     t.index ["application_ref"], name: "index_legal_aid_applications_on_application_ref", unique: true
     t.index ["discarded_at"], name: "index_legal_aid_applications_on_discarded_at"
