31b19537dc2ead6903d72c02a8e48b7d8fe5c431
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-07-17 13:43:27 +0100

    ap-4623: add update action to controller

--- app/controllers/providers/check_merits_answers_controller.rb
+++ app/controllers/providers/check_merits_answers_controller.rb
@@ -2,11 +2,18 @@ module Providers
   class CheckMeritsAnswersController < ProviderBaseController
     def show
       @form = LegalAidApplications::ConfirmSeparateRepresentationForm.new(model: legal_aid_application)
+
       legal_aid_application.check_merits_answers! unless legal_aid_application.checking_merits_answers?
     end
 
     def continue
-      continue_or_draft
+      return continue_or_draft unless params[:legal_aid_application]
+
+      @form = LegalAidApplications::ConfirmSeparateRepresentationForm.new(form_params)
+
+      unless save_continue_or_draft(@form)
+        render :show, status: :unprocessable_content
+      end
     end
 
     def reset
@@ -14,10 +21,20 @@ module Providers
       redirect_to back_path
     end
 
+  private
+
     def back_path
       return providers_legal_aid_application_uploaded_evidence_collection_path(legal_aid_application) if legal_aid_application.evidence_is_required?
 
       providers_legal_aid_application_merits_task_list_path(legal_aid_application)
+    end
+
+    def form_params
+      merge_with_model(legal_aid_application) do
+        next {} unless params[:legal_aid_application]
+
+        params.require(:legal_aid_application).permit(:separate_representation_required)
+      end
     end
   end
 end
--- app/forms/legal_aid_applications/confirm_separate_representation_form.rb
+++ app/forms/legal_aid_applications/confirm_separate_representation_form.rb
@@ -1,5 +1,9 @@
 module LegalAidApplications
   class ConfirmSeparateRepresentationForm < BaseForm
     form_for LegalAidApplication
+
+    attr_accessor :separate_representation_required
+
+    validates :separate_representation_required, presence: true, unless: :draft?
   end
 end
