a14de9703fa632d4d9ccfedf573726bdf79c1627
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-08-12 15:34:48 +0100

    AP-5235: Add new ProceedingTypes Filter service

---
+++ app/services/legal_framework/proceeding_types/all.rb
@@ -0,0 +1,70 @@
+module LegalFramework
+  module ProceedingTypes
+    class All < BaseApiCall
+      class ProceedingTypeStruct
+        attr_reader :ccms_code, :meaning, :description, :ccms_category_law, :ccms_matter_code, :ccms_matter, :sca_core, :sca_related
+
+        def initialize(pt_hash)
+          @ccms_code = pt_hash["ccms_code"]
+          @meaning = pt_hash["meaning"]
+          @description = pt_hash["description"]
+          @ccms_category_law = pt_hash["ccms_category_law"]
+          @ccms_matter_code = pt_hash["ccms_matter_code"]
+          @ccms_matter = pt_hash["ccms_matter"]
+          @sca_core = pt_hash["sca_core"]
+          @sca_related = pt_hash["sca_related"]
+        end
+
+        # TODO: remove the below when the SCA feature flag is removed
+        def not_sca?
+          [@sca_core, @sca_related].all?(false)
+        end
+      end
+
+      def self.call(legal_aid_application)
+        new(legal_aid_application).call
+      end
+
+      def initialize(legal_aid_application)
+        super()
+        @legal_aid_application = legal_aid_application
+      end
+
+      def call
+        result = JSON.parse(request.body).map { |pt_hash| ProceedingTypeStruct.new(pt_hash) }
+        # TODO: remove the below when the SCA feature flag is removed
+        # filter out SCA applications
+        result.select!(&:not_sca?) unless Setting.special_childrens_act?
+        result
+      end
+
+    private
+
+      def request_body
+        {
+          current_proceedings: @legal_aid_application.proceedings&.map(&:ccms_code)&.join(","),
+          allowed_categories: %w[MAT], # TODO: replace with details from new PDA, for now hardcoded to only current category type
+          search_term: "", # TODO: add optional parameter to the initializer, but for now leave always empty
+        }.to_json
+      end
+
+      def request
+        conn.post do |request|
+          request.url url
+          request.headers["Content-Type"] = "application/json"
+          request.body = request_body
+        end
+      end
+
+      def path
+        "/proceeding_types/filter"
+      end
+
+      def headers
+        {
+          "Content-Type" => "application/json",
+        }
+      end
+    end
+  end
+end
---
+++ spec/services/legal_framework/proceeding_types/all_spec.rb
@@ -0,0 +1,112 @@
+require "rails_helper"
+
+RSpec.describe LegalFramework::ProceedingTypes::All do
+  subject(:all) { described_class.new(legal_aid_application) }
+
+  before do
+    allow(Setting).to receive(:special_childrens_act?).and_return(sca_enabled)
+    stub_request(:post, uri).to_return(body: all_proceeding_types_payload)
+  end
+
+  let(:legal_aid_application) { create :legal_aid_application }
+  let(:uri) { "#{Rails.configuration.x.legal_framework_api_host}/proceeding_types/filter" }
+
+  describe ".call" do
+    subject(:call) { all.call }
+
+    before { call }
+
+    context "when the special children act setting is on" do
+      let(:sca_enabled) { true }
+
+      it "returns the expected proceedings" do
+        expect(call.map(&:ccms_code)).to match_array %w[DA001 SE097 DA003 SE016E DA006 PB003]
+      end
+    end
+
+    context "when the special children act setting is off" do
+      let(:sca_enabled) { false }
+
+      it "returns the expected proceedings" do
+        expect(call.map(&:ccms_code)).to match_array %w[DA001 SE097 DA003 SE016E DA006]
+      end
+    end
+  end
+
+  def all_proceeding_types_payload
+    [
+      {
+        ccms_code: "DA001",
+        meaning: "Inherent jurisdiction high court injunction",
+        description: "to be represented on an application for an injunction, order or declaration under the inherent jurisdiction of the court.",
+        full_s8_only: false,
+        sca_core: false,
+        sca_related: false,
+        ccms_category_law: "Family",
+        ccms_category_law_code: "MAT",
+        ccms_matter_code: "MINJN",
+        ccms_matter: "Domestic abuse",
+      },
+      {
+        ccms_code: "SE097",
+        meaning: "Reloacation enforcement",
+        description: "to be represented on an application for the revocation of an enforcement order under section 11J and Schedule A1 Children Act 1989.",
+        full_s8_only: true,
+        sca_core: false,
+        sca_related: false,
+        ccms_category_law: "Family",
+        ccms_category_law_code: "MAT",
+        ccms_matter_code: "KSEC8",
+        ccms_matter: "Children - section 8",
+      },
+      {
+        ccms_code: "DA003",
+        meaning: "Harassment - injunction",
+        description: "to be represented in an action for an injunction under section 3 Protection from Harassment Act 1997.",
+        full_s8_only: false,
+        sca_core: false,
+        sca_related: false,
+        ccms_category_law: "Family",
+        ccms_category_law_code: "MAT",
+        ccms_matter_code: "MINJN",
+        ccms_matter: "Domestic abuse",
+      },
+      {
+        ccms_code: "SE016E",
+        meaning: "Vary CAO residence-Enforcement",
+        description: "to be represented on an application to vary or discharge a child arrangements order –where the child(ren) will live. Enforcement only.",
+        full_s8_only: true,
+        sca_core: false,
+        sca_related: false,
+        ccms_category_law: "Family",
+        ccms_category_law_code: "MAT",
+        ccms_matter_code: "KSEC8",
+        ccms_matter: "Children - section 8",
+      },
+      {
+        ccms_code: "DA006",
+        meaning: "Extend, variation or discharge - Part IV",
+        description: "to be represented on an application to extend, vary or discharge an order under Part IV Family Law Act 1996",
+        full_s8_only: false,
+        sca_core: false,
+        sca_related: false,
+        ccms_category_law: "Family",
+        ccms_category_law_code: "MAT",
+        ccms_matter_code: "MINJN",
+        ccms_matter: "Domestic abuse",
+      },
+      {
+        ccms_code: "PB003",
+        meaning: "Child assessment order",
+        description: "to be represented on an application for a child assessment order.",
+        full_s8_only: false,
+        sca_core: true,
+        sca_related: false,
+        ccms_category_law: "Family",
+        ccms_category_law_code: "MAT",
+        ccms_matter_code: "KPBLW",
+        ccms_matter: "Special Children Act",
+      },
+    ].to_json
+  end
+end
