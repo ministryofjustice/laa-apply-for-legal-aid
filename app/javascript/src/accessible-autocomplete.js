a0b16fa6f69d0049122de1063f83f37246ae16c4
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2024-09-10 16:33:55 +0100

    Add back clear search link

--- app/javascript/src/accessible-autocomplete.js
+++ app/javascript/src/accessible-autocomplete.js
@@ -4,3 +4,13 @@ accessibleAutocomplete.enhanceSelectElement({
   defaultValue: '',
   selectElement: document.querySelector('#non-uk-home-address-country-code-field')
 })
+
+const screenReaderMessageDelay = 1000 // wait before updating the screen reader message, to avoid interrupting queue
+const searchInputBox = document.querySelector('#non-uk-home-address-country-code-field')
+
+document
+  .querySelector('#clear-country-search')
+  .addEventListener('click', () => {
+    searchInputBox.value = ''
+    setTimeout(() => { document.querySelector('#screen-reader-messages').innerHTML = 'Search box has been cleared.' }, screenReaderMessageDelay)
+  })
