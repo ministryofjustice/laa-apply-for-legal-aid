2ea1401e8a534dbc06dbadbb9e1982636094d614
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-09-16 13:19:38 +0100

    AP-5176: Add PDA::CurrentContracts
    This calls the PDA endpoint and stores the outcome in a database table so we can extract it and
    pass it to the Business Analysts so we can assess what data we require moving forward as well
    as tracking how often the calls will fail

---
+++ app/services/pda/current_contracts.rb
@@ -0,0 +1,67 @@
+module PDA
+  class CurrentContracts
+    def initialize(office_code)
+      @office_code = office_code
+    end
+
+    def self.call(office_code)
+      new(office_code).call
+    end
+
+    def call
+      body = response.body
+
+      # TODO: 16 September 2024 Colin Bruce - Replace once data has been analysed and responses can be stored correctly
+      # TEMP: Create TempContractData model and store it
+      case response.status
+      when 204
+        log_record_not_found_error
+      when 200
+        create_temp_contract_data(JSON.parse(body))
+        JSON.parse(body)
+      else
+        log_error
+      end
+    end
+
+  private
+
+    def url
+      @url ||= "#{Rails.configuration.x.pda.url}/provider-office/#{@office_code}/office-contract-details"
+    end
+
+    def headers
+      {
+        "accept" => "application/json",
+        "X-Authorization" => Rails.configuration.x.pda.auth_key,
+      }
+    end
+
+    def conn
+      @conn ||= Faraday.new(url:, headers:)
+    end
+
+    def response
+      @response ||= query_api
+    end
+
+    def query_api
+      conn.get url
+    end
+
+    def log_error
+      message = "API Call Failed: (#{response.status}) #{response.body}"
+      create_temp_contract_data({ error: message })
+    end
+
+    def log_record_not_found_error
+      message = "Retrieval Failed: (#{response.status}) #{response.body}"
+      create_temp_contract_data({ error: message })
+    end
+
+    def create_temp_contract_data(json)
+      success = !json.stringify_keys.keys[0].eql?("error")
+      TempContractData.create!(success:, office_code: @office_code, response: json)
+    end
+  end
+end
