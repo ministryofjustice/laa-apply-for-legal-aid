a7c2e72c04c5267fa0446799d4a94fc756de8f9c
skdejong <StephanieKdeJong@gmail.com>
2024-09-19 15:45:30 +0100

    AP-5262: Fix schema.rb date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_05_104607) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_13_145848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
