796345a26b9a11e5966b03e9189e95da65a48725
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-09-20 07:33:22 +0100

    Retrieve firm offices for provider and compare
    The existing CCMS PDA returns all offices in the providers
    firm as part of the "provider offices". The new PDA API
    only returns the offices of the specific individual provider (a person/user).
    
    To understand the impact and how to handle it we want to compare all the
    offices in the firm of the specific provider from the new PDA with those
    coming from the old CCSM PDA.
    
    The new PDA requires a second call to achieve this, so the relevant part
    of the response body of the second call have been merged with the response
    body of the first call and returned as a result object.

--- spec/services/pda/compare_provider_details_spec.rb
+++ spec/services/pda/compare_provider_details_spec.rb
@@ -1,72 +1,75 @@
 require "rails_helper"
 
 RSpec.describe PDA::CompareProviderDetails do
-  let(:firm_struct) { Struct.new(:id, :name) }
-  let(:office_struct) { Struct.new(:id, :code) }
-  let(:provider_details_struct) { Struct.new(:firm_id, :contact_id, :firm_name, :offices) }
-
   describe ".call" do
     subject(:call) { described_class.call(provider.id) }
 
     before do
       allow(Rails.logger).to receive(:info).at_least(:once)
       firm.providers << provider
-      provider.offices << office
+      provider.offices += offices
     end
 
-    let(:provider) { create(:provider, username:, contact_id:) }
     let(:firm) { create(:firm, ccms_id: ccms_firm_id, name: firm_name) }
-    let(:office) { create(:office, ccms_id: ccms_office_id, code: office_code) }
-    let(:firm_name) { "Test Firm" }
-    let(:office_code) { "6D456C" }
-    let(:username) { "test-user" }
-    let(:ccms_firm_id) { "1" }
-    let(:ccms_office_id) { "2" }
-    let(:contact_id) { 104 }
-    let(:ccms_firm) { firm_struct.new(ccms_firm_id, firm_name) }
-    let(:ccms_office) { office_struct.new(ccms_office_id, office_code) }
+    let(:ccms_firm_id) { 99_999 }
+    let(:firm_name) { "Test firm" }
 
-    context "when PDA returns details that match the provider's details" do
+    let(:provider) { create(:provider, contact_id: 494_000) }
+    let(:contact_id) { 494_000 }
+
+    let(:offices) do
+      [
+        create(:office, ccms_id: 111_111, code: "0A000B"),
+        create(:office, ccms_id: 222_222, code: "1A111B"),
+      ]
+    end
+
+    context "when old PDA returns details that match the new PDA provider's details" do
       before do
-        stub_provider_details_retriever(
-          provider:,
-          contact_id:,
-          firm: ccms_firm,
-          offices: [ccms_office],
-        )
+        stub_provider_offices
+        stub_provider_firm_offices
+      end
+
+      it "logs start and end time" do
+        call
+        expect(Rails.logger)
+          .to have_received(:info)
+          .with(%r{PDA::CompareProviderDetails:: start: .* end: .* duration: .* seconds})
       end
 
       it "logs that the details match" do
-        expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} results match.")
         call
+        expect(Rails.logger)
+          .to have_received(:info)
+          .with("PDA::CompareProviderDetails:: Provider #{provider.id} results match.")
       end
     end
 
-    context "when PDA returns details that do not match the provider's details" do
+    context "when old PDA returns details that do NOT match the new PDA provider's details" do
       before do
-        stub_provider_details_retriever(
-          provider:,
-          contact_id:,
-          firm: ccms_firm,
-          offices: [ccms_office],
-        )
+        stub_provider_offices
+        stub_provider_firm_offices
       end
 
-      context "when the firm.ccms_id does not match" do
-        let(:ccms_firm) { firm_struct.new(3, firm_name) }
+      context "when users firm.ccms_id does not match" do
+        let(:ccms_firm_id) { 99_888 }
 
         it "logs that the details do not match" do
-          expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} #{ccms_firm.id} does not match firm.ccms_id #{firm.ccms_id}")
           call
+          expect(Rails.logger)
+            .to have_received(:info)
+            .with("PDA::CompareProviderDetails:: Provider #{provider.id} 99999 does not match firm.ccms_id #{ccms_firm_id}")
         end
       end
 
       context "when the firm.name does not match" do
-        let(:ccms_firm) { firm_struct.new(ccms_firm_id, "Wrong firm name") }
+        let(:firm_name) { "Wrong firm name" }
 
         it "logs that the details do not match" do
-          expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} #{ccms_firm.name} does not match firm.name #{firm.name}")
           call
+          expect(Rails.logger)
+            .to have_received(:info)
+            .with("PDA::CompareProviderDetails:: Provider #{provider.id} \"Test firm\" does not match firm.name \"#{firm.name}\"")
         end
       end
 
@@ -74,76 +77,147 @@ RSpec.describe PDA::CompareProviderDetails do
         before { provider.update!(contact_id: 105) }
 
         it "logs that the details do not match" do
-          expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} #{contact_id} does not match provider.contact_id #{provider.contact_id}")
           call
+          expect(Rails.logger)
+            .to have_received(:info)
+            .with("PDA::CompareProviderDetails:: Provider #{provider.id} #{contact_id} does not match provider.contact_id #{provider.contact_id}")
         end
       end
 
-      context "when the office details do not match" do
-        context "when the office codes do not match" do
-          let(:ccms_office) { office_struct.new(ccms_office_id, "9R678Y") }
-
-          it "logs that the details do not match" do
-            expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} [\"#{ccms_office.id} #{ccms_office.code}\"] does not match [\"#{office.ccms_id} #{office.code}\"]")
-            call
-          end
+      context "when no office codes match" do
+        let(:offices) do
+          [create(:office, ccms_id: 333_333, code: "2A222B")]
         end
 
-        context "when the office returned by PDA does not exist in apply" do
-          let(:ccms_office) { office_struct.new(3, office_code) }
+        it "logs that the details do not match" do
+          call
+          expect(Rails.logger)
+            .to have_received(:info)
+            .with("PDA::CompareProviderDetails:: Provider #{provider.id} [\"111111 0A000B\", \"222222 1A111B\"] does not match [\"333333 2A222B\"]")
+        end
+      end
 
-          it "logs that the details do not match" do
-            expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} [\"#{ccms_office.id} #{ccms_office.code}\"] does not match [\"#{office.ccms_id} #{office.code}\"]")
-            call
-          end
+      context "when an office returned by PDA does not exist in apply" do
+        before do
+          provider.offices.find_by(ccms_id: 222_222).destroy!
         end
 
-        context "when an office is missing from the PDA response" do
-          let(:additional_office) { create(:office, ccms_id: "3", code: "4C880Q") }
+        it "logs that the details do not match" do
+          call
+          expect(Rails.logger)
+            .to have_received(:info)
+            .with("PDA::CompareProviderDetails:: Provider #{provider.id} [\"111111 0A000B\", \"222222 1A111B\"] does not match [\"111111 0A000B\"]")
+        end
+      end
 
-          it "logs that the details do not match" do
-            provider.offices << additional_office
-            expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: Provider #{provider.id} [\"#{ccms_office.id} #{ccms_office.code}\"] does not match [\"#{office.ccms_id} #{office.code}\", \"#{additional_office.ccms_id} #{additional_office.code}\"]")
-            call
-          end
+      context "when an office is missing from the PDA response" do
+        let(:additional_office) { create(:office, ccms_id: 333_333, code: "2A222B") }
+
+        before do
+          provider.offices << additional_office
+        end
+
+        it "logs that the details do not match" do
+          call
+          expect(Rails.logger)
+            .to have_received(:info)
+            .with("PDA::CompareProviderDetails:: Provider #{provider.id} [\"111111 0A000B\", \"222222 1A111B\"] does not match [\"111111 0A000B\", \"222222 1A111B\", \"333333 2A222B\"]")
         end
       end
     end
 
-    context "when provider_details_retriever raises an error" do
-      context "when PDA returns an empty response" do
-        before do
-          stub_provider_details_retriever_record_not_found(
-            provider:,
-          )
-        end
-
-        it "logs that the details do not match" do
-          expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: User not found for #{provider.id}.")
-          call
-        end
+    context "when PDA returns an empty response" do
+      before do
+        stub_provider_details_retriever_record_not_found(
+          provider:,
+        )
       end
 
-      context "when PDA returns an error" do
-        before do
-          stub_provider_details_retriever_api_error(
-            provider:,
-          )
-        end
+      it "logs that the details do not match" do
+        call
+        expect(Rails.logger)
+          .to have_received(:info)
+          .with("PDA::CompareProviderDetails:: User not found for #{provider.id}.")
+      end
+    end
 
-        it "logs that the details do not match" do
-          expect(Rails.logger).to receive(:info).with("PDA::CompareProviderDetails:: User #{provider.id} PDA::ProviderDetailsRetriever::ApiError")
-          call
-        end
+    context "when PDA returns an error" do
+      before do
+        stub_provider_details_retriever_api_error(
+          provider:,
+        )
+      end
+
+      it "logs that the details do not match" do
+        call
+        expect(Rails.logger)
+          .to have_received(:info)
+          .with("PDA::CompareProviderDetails:: User #{provider.id} PDA::ProviderDetailsRetriever::ApiError")
       end
     end
   end
 
-  def stub_provider_details_retriever(provider:, firm:, offices:, contact_id:)
-    allow(PDA::ProviderDetailsRetriever)
-      .to receive(:call)
-      .with(provider.username)
-      .and_return(api_response(firm:, offices:, contact_id:))
+  def stub_provider_offices
+    stub_request(:get, %r{#{Rails.configuration.x.pda.url}/provider-user/.*/provider-offices})
+      .to_return(
+        status: 200,
+        body: provider_offices_json,
+        headers: { "Content-Type" => "application/json; charset=utf-8" },
+      )
+  end
+
+  def provider_offices_json
+    {
+      firm: {
+        ccmsFirmId: 99_999,
+        firmId: 1639,
+        firmName: "Test firm",
+        firmNumber: "1639",
+      },
+      officeCodes: [
+        {
+          ccmsFirmOfficeId: 111_111,
+          firmOfficeCode: "0A000B",
+        },
+        {
+          ccmsFirmOfficeId: 222_222,
+          firmOfficeCode: "1A111B",
+        },
+      ],
+      user: {
+        ccmsContactId: 494_000,
+      },
+    }.to_json
+  end
+
+  def stub_provider_firm_offices
+    stub_request(:get, %r{#{Rails.configuration.x.pda.url}/provider-firms/.*/provider-offices})
+    .to_return(
+      status: 200,
+      body: provider_firm_offices_json,
+      headers: { "Content-Type" => "application/json; charset=utf-8" },
+    )
+  end
+
+  def provider_firm_offices_json
+    {
+      firm: {
+        ccmsFirmId: 99_999,
+        firmId: 1639,
+        firmName: "Test firm",
+        firmNumber: "1639",
+      },
+      offices: [
+        {
+          ccmsFirmOfficeId: 111_111,
+          firmOfficeCode: "0A000B",
+        },
+        {
+          ccmsFirmOfficeId: 222_222,
+          firmOfficeCode: "1A111B",
+        },
+      ],
+    }.to_json
   end
 
   def stub_provider_details_retriever_record_not_found(provider:)
@@ -158,9 +232,5 @@ RSpec.describe PDA::CompareProviderDetails do
       .to receive(:call)
       .with(provider.username)
       .and_raise(PDA::ProviderDetailsRetriever::ApiError)
-  end
-
-  def api_response(firm:, offices:, contact_id:)
-    provider_details_struct.new(firm_id: firm.id, contact_id:, firm_name: firm.name, offices:)
   end
 end
--- spec/services/pda/provider_details_retriever_spec.rb
+++ spec/services/pda/provider_details_retriever_spec.rb
@@ -16,11 +16,12 @@ RSpec.describe PDA::ProviderDetailsRetriever, :vcr do
       let(:username) { "DT_SCRIPT_USER1" }
 
       it "returns provider details" do
-        response = call
-        expect(response.contact_id).to eq 0
-        expect(response.firm_id).to eq 0
-        expect(response.firm_name).to eq "DT SCRIPT PROVIDER 1"
-        expect(response.offices.first.code).to eq "2Q242F"
+        result = call
+        expect(result.contact_id).to eq 0
+        expect(result.firm_id).to eq 10_835_831
+        expect(result.firm_name).to eq "DT SCRIPT PROVIDER 1"
+        expect(result.provider_offices.first.code).to eq "2Q242F"
+        expect(result.firm_offices.first.code).to eq "2Q242F"
       end
     end
 
@@ -34,6 +35,19 @@ RSpec.describe PDA::ProviderDetailsRetriever, :vcr do
         expect {
           call
         }.to raise_error(PDA::ProviderDetailsRetriever::ApiError, "API Call Failed: (500) An error has occurred")
+      end
+    end
+
+    context "when api returns no content" do
+      before do
+        stub_request(:get, "#{Rails.configuration.x.pda.url}/provider-user/#{username}/provider-offices")
+          .to_return(body: nil, status: 204)
+      end
+
+      it "raises ApiError" do
+        expect {
+          call
+        }.to raise_error(PDA::ProviderDetailsRetriever::ApiRecordNotFoundError, %r{Retrieval Failed: \(204\)})
       end
     end
   end
