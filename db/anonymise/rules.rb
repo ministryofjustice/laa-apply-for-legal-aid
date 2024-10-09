ef237f61dd0779d61bf49c431059d944cb0bf0d1
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-10-09 16:44:06 +0100

    ap-5348: add discretionary_disregards db table

--- db/anonymise/rules.rb
+++ db/anonymise/rules.rb
@@ -103,6 +103,7 @@ NINO_REGEXP = /^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]{1}$/
   dependants: {
     name: -> { Faker::Name.name },
   },
+  discretionary_disregards: {},
   document_categories: {},
   domestic_abuse_summaries: {
     warning_letter_sent_details: Faker::Lorem.paragraph(sentence_count: 2),
