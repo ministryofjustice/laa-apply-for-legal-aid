3b46e106d0030d8609cbb889a798ea2b7a7f8c07
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2024-10-09 13:40:39 +0100

    AP-5267: Design changes
    Separate voided and in progress applications into two
    pages

--- app/controllers/providers/legal_aid_applications_controller.rb
+++ app/controllers/providers/legal_aid_applications_controller.rb
@@ -3,10 +3,22 @@ module Providers
     include Pagy::Backend
     legal_aid_application_not_required!
 
+    before_action :set_scope, only: %i[submitted in_progress]
+
     DEFAULT_PAGE_SIZE = 20
 
     def index
       applications
+    end
+
+    def submitted
+      submitted_applications
+      render "index"
+    end
+
+    def in_progress
+      in_progress_applications
+      render "index"
     end
 
     def search
@@ -27,6 +39,10 @@ module Providers
 
   private
 
+    def set_scope
+      @scope = params[:action].to_sym
+    end
+
     def applications
       @pagy, @legal_aid_applications = pagy(
         applications_query,
@@ -36,8 +52,40 @@ module Providers
       @initial_sort = { created_at: :desc }
     end
 
+    def submitted_applications
+      @pagy, @legal_aid_applications = pagy(
+        submitted_query,
+        limit: params.fetch(:page_size, DEFAULT_PAGE_SIZE),
+        size: 5, # control of how many elements shown in page info
+      )
+      @initial_sort = { created_at: :desc }
+    end
+
+    def in_progress_applications
+      @pagy, @legal_aid_applications = pagy(
+        in_progress_query,
+        limit: params.fetch(:page_size, DEFAULT_PAGE_SIZE),
+        size: 5, # control of how many elements shown in page info
+      )
+      @initial_sort = { created_at: :desc }
+    end
+
     def applications_query
       query = current_provider.firm.legal_aid_applications.kept.includes(:applicant, :chances_of_success).latest
+      return query if search_term.blank?
+
+      query.search(search_term)
+    end
+
+    def submitted_query
+      query = current_provider.firm.legal_aid_applications.kept.includes(:applicant, :chances_of_success).where.not(merits_submitted_at: nil).latest
+      return query if search_term.blank?
+
+      query.search(search_term)
+    end
+
+    def in_progress_query
+      query = current_provider.firm.legal_aid_applications.kept.includes(:applicant, :chances_of_success).where(merits_submitted_at: nil).latest
       return query if search_term.blank?
 
       query.search(search_term)
