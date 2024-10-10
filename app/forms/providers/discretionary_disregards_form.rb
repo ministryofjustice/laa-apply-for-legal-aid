1132719be0bfaf90f18ea9d30870dc9342dc1b97
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-10-10 10:20:14 +0100

    ap-5348: update view

--- app/forms/providers/discretionary_disregards_form.rb
+++ app/forms/providers/discretionary_disregards_form.rb
@@ -1,5 +1,17 @@
 module Providers
   class DiscretionaryDisregardsForm < BaseForm
     form_for DiscretionaryDisregards
+
+    SINGLE_VALUE_ATTRIBUTES = %i[
+      backdated_benefits
+      compensation_for_personal_harm
+      criminal_injuries_compensation
+      grenfell_tower_fire_victims
+      london_emergencies_trust
+      national_emergencies_trust
+      loss_or_harm_relating_to_this_application
+      victims_of_overseas_terrorism_compensation
+      we_love_manchester_emergency_fund
+    ].freeze
   end
 end
