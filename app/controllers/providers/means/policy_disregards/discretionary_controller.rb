38af05f09d88f971de26b6f97a06867044f34995
Katharine Ahern <katharine.ahern@justice.gov.uk>
2024-10-23 09:51:45 +0100

    ap-5348: add validations

--- app/controllers/providers/means/policy_disregards/discretionary_controller.rb
+++ app/controllers/providers/means/policy_disregards/discretionary_controller.rb
@@ -5,6 +5,17 @@ module Providers
         def show
           @form = Providers::PolicyDisregards::DiscretionaryForm.new(model: legal_aid_application)
         end
+
+        def update
+          @form = Providers::PolicyDisregards::DiscretionaryForm.new(form_params)
+          render :show unless save_continue_or_draft(@form)
+        end
+
+      private
+
+        def form_params
+          params.require(:legal_aid_application).permit(:none_selected, discretionary_disregards: [])
+        end
       end
     end
   end
