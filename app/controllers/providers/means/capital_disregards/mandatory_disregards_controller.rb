fc1b44eb7e19a5df80665b7102cafef90ddd2a07
RoseSAK <rose.azadkan@digital.justice.gov.uk>
2024-10-24 16:42:57 +0100

    update mandatory disregards controlle and form

--- app/controllers/providers/means/capital_disregards/mandatory_disregards_controller.rb
+++ app/controllers/providers/means/capital_disregards/mandatory_disregards_controller.rb
@@ -10,6 +10,16 @@ module Providers
           @form = Providers::Means::CapitalDisregards::MandatoryDisregardsForm.new(form_params)
           render :show unless save_continue_or_draft(@form)
         end
+
+      private
+
+        def form_params
+          merge_with_model(legal_aid_application) do
+            params.require(:legal_aid_application)
+                  .permit(mandatory_capital_disregards: [])
+                  .merge(none_selected: params[:legal_aid_application][:none_selected])
+          end
+        end
       end
     end
   end
--- app/forms/providers/means/capital_disregards/mandatory_disregards_form.rb
+++ app/forms/providers/means/capital_disregards/mandatory_disregards_form.rb
@@ -4,58 +4,48 @@ module Providers
       class MandatoryDisregardsForm < BaseForm
         form_for LegalAidApplication
 
-        MANDATORY_DISREGARDS_ATTRIBUTES = %i[
-          backdated_benefits
-          backdated_community_care
-          budgeting_advances
-          compensation_miscarriage_of_justice
-          government_cost_of_living
-          independent_living_fund
-          infected_blood_support
-          modern_slavery
-          account_of_benefit
-          historical_child_abuse
-          social_fund
-          vaccine_damage
-          variant_creutzfeldt_jakob_disease
-          welsh_independent_living_grant
-          windrush_compensation_scheme
-        ].freeze
+        attr_reader :mandatory_capital_disregards, :none_selected
 
-        attr_accessor(*MANDATORY_DISREGARDS_ATTRIBUTES)
+        validate :validate_types
 
-        validate :validate_any_checkbox_checked, unless: :draft?
-        validate :validate_no_account_and_another_checkbox_not_both_checked, unless: :draft?
-
-        def has_partner_with_no_contrary_interest?
-          model.legal_aid_application.applicant&.has_partner_with_no_contrary_interest?
+        def validate_types
+          errors.add(:mandatory_capital_disregards, :invalid) if mandatory_capital_disregards.nil? && !none_selected
         end
+
+        def save
+          return false unless valid?
+
+          # TODO: don't destroy all, compare with existing disregards
+          model.capital_disregards.where(mandatory: true).destroy_all
+          mandatory_capital_disregards&.each { |disregard| model.capital_disregards.create!(name: disregard, mandatory: true) }
+        end
+        alias_method :save!, :save
 
       private
 
-        def any_checkbox_checked?
-          checkbox_hash.values.any?(&:present?)
-        end
-
-        def checkbox_hash
-          check_box_attributes.index_with { |attribute| __send__(attribute) }
-        end
-
-        def check_box_attributes
-          self.class.check_box_attributes
-        end
-
-        def none_and_another_checkbox_checked?
-          checkbox_hash[:none_selected].present? && checkbox_hash.except(:none_selected).values.any?(&:present?)
-        end
-
-        def validate_any_checkbox_checked
-          errors.add check_box_attributes.first.to_sym, error_message_for_none_selected unless any_checkbox_checked?
-        end
-
-        def validate_no_account_and_another_checkbox_not_both_checked
-          errors.add check_box_attributes.first.to_sym, error_message_for_none_and_another_option_selected if none_and_another_checkbox_checked?
-        end
+        # def any_checkbox_checked?
+        #   checkbox_hash.values.any?(&:present?)
+        # end
+        #
+        # def checkbox_hash
+        #   check_box_attributes.index_with { |attribute| __send__(attribute) }
+        # end
+        #
+        # def check_box_attributes
+        #   self.class.check_box_attributes
+        # end
+        #
+        # def none_and_another_checkbox_checked?
+        #   checkbox_hash[:none_selected].present? && checkbox_hash.except(:none_selected).values.any?(&:present?)
+        # end
+        #
+        # def validate_any_checkbox_checked
+        #   errors.add check_box_attributes.first.to_sym, error_message_for_none_selected unless any_checkbox_checked?
+        # end
+        #
+        # def validate_no_account_and_another_checkbox_not_both_checked
+        #   errors.add check_box_attributes.first.to_sym, error_message_for_none_and_another_option_selected if none_and_another_checkbox_checked?
+        # end
 
         def error_message_for_none_selected
           I18n.t("activemodel.errors.models.capital_disregards.attributes.base.#{error_key('none_selected')}")
