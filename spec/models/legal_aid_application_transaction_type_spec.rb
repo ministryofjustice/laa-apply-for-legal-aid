95e89d66a53f5e6fa56fa81388750d362bed14cf
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-10-29 09:30:45 +0000

    AP-5416: Fix bug that keeps bank transactions when type has been removed
    It was possible on the truelayer journey to remove an income or outgoing
    type after bank transactions had been categorised as that type and the
    categorisation would remain.
    
    This does not make logic sense and, to follow the cash transaction example,
    should instead removed the categorisation of any any bank transactions
    categorised as that type.

--- spec/models/legal_aid_application_transaction_type_spec.rb
+++ spec/models/legal_aid_application_transaction_type_spec.rb
@@ -54,38 +54,83 @@ RSpec.describe LegalAidApplicationTransactionType do
   end
 
   describe ".after_commit" do
+    let(:credit_transaction_type) { create(:transaction_type, :maintenance_in) }
+    let(:debit_transaction_type) { create(:transaction_type, :maintenance_out) }
+
     let(:instance) do
       create(:legal_aid_application_transaction_type,
              legal_aid_application:,
              transaction_type: credit_transaction_type)
     end
 
-    before do
-      create(:legal_aid_application_transaction_type,
-             legal_aid_application:,
-             transaction_type: debit_transaction_type)
+    context "when application has cash transactions of that type" do
+      before do
+        create(:legal_aid_application_transaction_type,
+               legal_aid_application:,
+               transaction_type: debit_transaction_type)
 
-      legal_aid_application.cash_transactions.create!(owner_type: "Applicant", owner_id: legal_aid_application.applicant.id, transaction_type_id: credit_transaction_type.id, amount: 101, month_number: 1, transaction_date: Time.zone.now.to_date)
-      legal_aid_application.cash_transactions.create!(owner_type: "Applicant", owner_id: legal_aid_application.applicant.id, transaction_type_id: debit_transaction_type.id, amount: 103, month_number: 3, transaction_date: 2.months.ago)
-    end
+        legal_aid_application.cash_transactions.create!(owner_type: "Applicant", owner_id: legal_aid_application.applicant.id, transaction_type_id: credit_transaction_type.id, amount: 101, month_number: 1, transaction_date: Time.zone.now.to_date)
+        legal_aid_application.cash_transactions.create!(owner_type: "Applicant", owner_id: legal_aid_application.applicant.id, transaction_type_id: debit_transaction_type.id, amount: 103, month_number: 3, transaction_date: 2.months.ago)
+      end
 
-    context "when destroying object instance" do
-      let(:action) { instance.destroy! }
+      context "when destroying object instance" do
+        let(:action) { instance.destroy! }
 
-      it "deletes related cash transaction" do
-        expect { action }.to change(legal_aid_application.cash_transactions, :count).by(-1)
-        transaction_types = legal_aid_application.cash_transactions.map(&:transaction_type)
-        expect(transaction_types).not_to include(credit_transaction_type)
+        it "deletes related cash transaction" do
+          expect { action }.to change(legal_aid_application.cash_transactions, :count).by(-1)
+          transaction_types = legal_aid_application.cash_transactions.map(&:transaction_type)
+          expect(transaction_types).not_to include(credit_transaction_type)
+        end
+      end
+
+      context "when updating object instance" do
+        let(:action) { instance.update!(updated_at: Time.zone.now) }
+
+        it "does not delete cash transactions" do
+          expect { action }.not_to change(legal_aid_application.cash_transactions, :count)
+          transaction_types = legal_aid_application.cash_transactions.map(&:transaction_type)
+          expect(transaction_types).to include(credit_transaction_type, debit_transaction_type)
+        end
       end
     end
 
-    context "when updating object instance" do
-      let(:action) { instance.update!(updated_at: Time.zone.now) }
+    context "when application has bank transactions of that type" do
+      before do
+        create(:legal_aid_application_transaction_type,
+               legal_aid_application:,
+               transaction_type: debit_transaction_type)
 
-      it "does not delete cash transactions" do
-        expect { action }.not_to change(legal_aid_application.cash_transactions, :count)
-        transaction_types = legal_aid_application.cash_transactions.map(&:transaction_type)
-        expect(transaction_types).to include(credit_transaction_type, debit_transaction_type)
+        bank_provider = create(:bank_provider, applicant: legal_aid_application.applicant)
+        bank_account = create(:bank_account, bank_provider:)
+
+        create_list(:bank_transaction, 6, bank_account:, transaction_type: credit_transaction_type)
+        create_list(:bank_transaction, 3, bank_account:, transaction_type: debit_transaction_type)
+      end
+
+      context "when destroying object instance" do
+        let(:action) { instance.destroy! }
+
+        it "removes associated bank transaction categorisation" do
+          expect { action }.to change(legal_aid_application.bank_transactions.where(transaction_type: credit_transaction_type), :count).by(-6)
+          transaction_types = legal_aid_application.bank_transactions.map(&:transaction_type)
+          expect(transaction_types).not_to include(credit_transaction_type)
+        end
+
+        it "does not delete bank transactions" do
+          expect { action }.not_to change(legal_aid_application.bank_transactions, :count)
+          transaction_types = legal_aid_application.bank_transactions.map(&:transaction_type)
+          expect(transaction_types).to include(debit_transaction_type)
+        end
+      end
+
+      context "when updating object instance" do
+        let(:action) { instance.update!(updated_at: Time.zone.now) }
+
+        it "does not remove bank transactions categorisation" do
+          expect { action }.not_to change(legal_aid_application.bank_transactions.where(transaction_type: nil), :count)
+          transaction_types = legal_aid_application.bank_transactions.map(&:transaction_type)
+          expect(transaction_types).to include(credit_transaction_type, debit_transaction_type)
+        end
       end
     end
   end
