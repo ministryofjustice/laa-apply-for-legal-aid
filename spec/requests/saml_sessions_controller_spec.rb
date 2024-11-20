532c4d0ad11109c5be2bd2c2b1725658040b541a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-11-20 09:22:07 +0000

    AP-5177: Update SAML session tests
    These needed to be restructured to expect new PDA responses
    to be called and returned

--- spec/requests/saml_sessions_controller_spec.rb
+++ spec/requests/saml_sessions_controller_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe SamlSessionsController do
   let(:office) { create(:office) }
   let(:provider) { create(:provider, firm:, selected_office: office, offices: [office], username:) }
   let(:username) { "bob the builder" }
-  let(:provider_details_api_url) { "#{Rails.configuration.x.provider_details.url}#{username.gsub(' ', '%20')}" }
+  let(:firm_id) { "3959183" }
+  let(:provider_details_api_url) { "#{Rails.configuration.x.pda.url}/provider-users/#{username.gsub(' ', '%20')}/provider-offices" }
+  let(:provider_firms_api_url) { "#{Rails.configuration.x.pda.url}/provider-firms/#{firm_id}/provider-offices" }
   let(:provider_details_api_response) { api_response.to_json }
   let(:enable_mock_saml) { false }
 
@@ -49,6 +51,7 @@ RSpec.describe SamlSessionsController do
     before do
       allow_any_instance_of(Warden::Proxy).to receive(:authenticate!).and_return(provider)
       stub_request(:get, provider_details_api_url).to_return(body: provider_details_api_response, status:)
+      stub_request(:get, provider_firms_api_url).to_return(body: firm_response.to_json, status:)
     end
 
     context "when on staging or production" do
@@ -61,16 +64,16 @@ RSpec.describe SamlSessionsController do
           let(:provider) { create(:provider, :created_by_devise, :with_ccms_apply_role, username:) }
 
           it "calls the Provider details api" do
-            expect(ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
+            expect(PDA::ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
             post_request
           end
 
           it "updates the record with firm and offices" do
             post_request
-            firm = Firm.find_by(ccms_id: raw_details_response[:providerFirmId])
+            firm = Firm.find_by(ccms_id: raw_details_response.dig(:firm, :ccmsFirmId))
             expect(provider.firm_id).to eq firm.id
             expect(firm.offices.size).to eq 1
-            expect(firm.offices.first.ccms_id).to eq raw_details_response[:providerOffices].first[:id].to_s
+            expect(firm.offices.first.ccms_id).to eq raw_details_response[:officeCodes].first[:ccmsFirmOfficeId].to_s
           end
 
           context "when mock_saml is activated" do
@@ -96,7 +99,7 @@ RSpec.describe SamlSessionsController do
           before { allow(Rails.configuration.x.laa_portal).to receive(:mock_saml).and_return(false) }
 
           it "does not call the ProviderDetailsCreator" do
-            expect(ProviderDetailsCreator).not_to receive(:call)
+            expect(PDA::ProviderDetailsCreator).not_to receive(:call)
             post_request
           end
 
@@ -112,12 +115,12 @@ RSpec.describe SamlSessionsController do
         end
 
         context "and the provider does not exist on Provider details api" do
-          let(:api_response) { raw_404_response }
-          let(:status) { 404 }
+          let(:api_response) { blank_response }
+          let(:status) { 204 }
           let(:provider) { create(:provider, :created_by_devise, :with_ccms_apply_role, username:) }
 
           it "calls the Provider details creator" do
-            expect(ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
+            expect(PDA::ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
             post_request
           end
 
@@ -154,7 +157,7 @@ RSpec.describe SamlSessionsController do
         it "uses a worker to update details" do
           ProviderDetailsCreatorWorker.clear
           expect(ProviderDetailsCreatorWorker).to receive(:perform_async).with(provider.id).and_call_original
-          expect(ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
+          expect(PDA::ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
           post_request
           ProviderDetailsCreatorWorker.drain
         end
@@ -172,16 +175,16 @@ RSpec.describe SamlSessionsController do
           let(:provider) { create(:provider, :created_by_devise, :with_ccms_apply_role, username:) }
 
           it "calls the Provider details api" do
-            expect(ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
+            expect(PDA::ProviderDetailsCreator).to receive(:call).with(provider).and_call_original
             post_request
           end
 
           it "updates the record with firm and offices" do
             post_request
-            firm = Firm.find_by(ccms_id: raw_details_response[:providerFirmId])
+            firm = Firm.find_by(ccms_id: raw_details_response.dig(:firm, :ccmsFirmId))
             expect(provider.firm_id).to eq firm.id
             expect(firm.offices.size).to eq 1
-            expect(firm.offices.first.ccms_id).to eq raw_details_response[:providerOffices].first[:id].to_s
+            expect(firm.offices.first.ccms_id).to eq raw_details_response[:officeCodes].first[:ccmsFirmOfficeId].to_s
           end
 
           it "displays the start page" do
@@ -200,7 +203,7 @@ RSpec.describe SamlSessionsController do
           it "does not schedule a job to update the provider details" do
             expect(provider).to receive(:update_details).and_call_original
             expect(ProviderDetailsCreatorWorker).not_to receive(:perform_async).with(provider.id)
-            expect(ProviderDetailsCreator).not_to receive(:call).with(provider)
+            expect(PDA::ProviderDetailsCreator).not_to receive(:call).with(provider)
             post_request
           end
         end
@@ -221,34 +224,74 @@ RSpec.describe SamlSessionsController do
 
   def raw_details_response
     {
-      providerFirmId: 22_381,
-      contactUserId: 29_562,
-      contacts: [
+      firm: {
+        firmId: 3_959_183,
+        firmNumber: "50736",
+        ccmsFirmId: 10_835_831,
+        firmName: "DT SCRIPT PROVIDER 1",
+      },
+      user: {
+        userId: 454_031,
+        ccmsContactId: 0,
+        userLogin: "DT_SCRIPT_USER1",
+        name: "Dts User1",
+        emailAddress: "deepak.tanna@digital.justice.gov.uk",
+        portalStatus: "Active",
+      },
+      officeCodes: [
         {
-          id: 568_352,
-          name: "SALLYCORNHILL",
-        },
-        {
-          id: 2_017_809,
-          name: username,
-        },
-      ],
-      providerOffices: [
-        {
-          id: 81_693,
-          name: "Test1 and Co",
+          firmOfficeId: 145_434,
+          ccmsFirmOfficeId: 34_406_595,
+          firmOfficeCode: "2Q242F",
+          officeName: "2Q242F,1 Skyscraper",
+          officeCodeAlt: "DT Script Office 1",
         },
       ],
     }
   end
 
-  def raw_404_response
+  def firm_response
     {
-      timestamp: "2020-10-27T12:15:13.639+0000",
-      status: 404,
-      error: "Not Found",
-      message: "No records found for [bob%20the%20builder]",
-      path: "/api/providerDetails/bob%20the%20builder",
+      firm: {
+        firmId: 3_959_183,
+        firmNumber: "50736",
+        ccmsFirmId: 10_835_831,
+        firmName: "DT SCRIPT PROVIDER 1",
+      },
+      offices: [
+        {
+          firmOfficeId: 145_434,
+          ccmsFirmOfficeId: 34_406_595,
+          firmOfficeCode: "2Q242F",
+          officeName: "2Q242F,1 Skyscraper",
+          officeCodeAlt: "DT Script Office 1",
+          addressLine1: "1 Skyscraper",
+          addressLine2: "1 Some Road",
+          addressLine3: "",
+          addressLine4: "",
+          city: "Metropolis",
+          county: "",
+          postCode: "LE1 1AA",
+          dxCentre: "",
+          dxNumber: "",
+          telephoneAreaCode: "01162",
+          telephoneNumber: "555124",
+          faxAreaCode: "",
+          faxNumber: "",
+          emailAddress: "me@email.uk",
+          vatRegistrationNumber: "",
+          type: "Legal Services Provider",
+          headOffice: "N/A",
+          creationDate: "2023-11-10",
+          lscRegion: "Midlands",
+          lscBidZone: "Leicester",
+          lscAreaOffice: "Nottingham",
+          cjsForceName: "Leicestershire",
+          localAuthority: "City of Leicester",
+          policeStationAreaName: "Leicester",
+          dutySolicitorAreaName: "Leicester",
+        },
+      ],
     }
   end
 
