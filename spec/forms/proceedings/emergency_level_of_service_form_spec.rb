7f95f5c4ed56685ba75a2577c5217e4e46421047
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-12-02 14:34:46 +0000

    AP-5566: Amend unit tests for substantive and emergency LOS forms
    There were some false/fake positives due to the factories creating
    the data that was under test, plus this locks down the functionality
    that updates the `stage` attributes.

--- spec/forms/proceedings/emergency_level_of_service_form_spec.rb
+++ spec/forms/proceedings/emergency_level_of_service_form_spec.rb
@@ -3,35 +3,69 @@ require "rails_helper"
 RSpec.describe Proceedings::EmergencyLevelOfServiceForm, :vcr, type: :form do
   subject(:form) { described_class.new(form_params) }
 
-  let(:proceeding) { create(:proceeding, :se014, :without_df_date, :with_cit_z) }
+  let(:proceeding) do
+    create(:proceeding,
+           :se014,
+           :without_df_date,
+           :with_cit_z,
+           emergency_level_of_service: nil,
+           emergency_level_of_service_name: nil,
+           emergency_level_of_service_stage: nil)
+  end
+
   let(:params) do
     {
       emergency_level_of_service:,
     }
   end
+
   let(:form_params) { params.merge(model: proceeding) }
 
   describe "#save" do
     subject(:save_form) { form.save }
 
-    before { save_form }
-
     context "when the submission is valid" do
-      context "and the user selects Family Help (Higher)" do
+      context "with level of service for Family Help (Higher)" do
         let(:emergency_level_of_service) { 1 }
 
-        it "updates the emergency_level_of_service value" do
-          expect(proceeding.reload.emergency_level_of_service).to eq 1
-          expect(proceeding.reload.emergency_level_of_service_name).to eq "Family Help (Higher)"
+        it "updates to the expected values" do
+          expect { save_form }.to change { proceeding.reload.attributes.symbolize_keys }
+            .from(
+              hash_including(
+                emergency_level_of_service: nil,
+                emergency_level_of_service_name: nil,
+                emergency_level_of_service_stage: nil,
+              ),
+            )
+            .to(
+              hash_including(
+                emergency_level_of_service: 1,
+                emergency_level_of_service_name: "Family Help (Higher)",
+                emergency_level_of_service_stage: 1,
+              ),
+            )
         end
       end
 
-      context "and the user selects Full Representation" do
+      context "with level of service for Full Representation" do
         let(:emergency_level_of_service) { 3 }
 
-        it "updates the accepted_emergency_defaults value" do
-          expect(proceeding.reload.emergency_level_of_service).to eq 3
-          expect(proceeding.reload.emergency_level_of_service_name).to eq "Full Representation"
+        it "updates to the expected values" do
+          expect { save_form }.to change { proceeding.reload.attributes.symbolize_keys }
+            .from(
+              hash_including(
+                emergency_level_of_service: nil,
+                emergency_level_of_service_name: nil,
+                emergency_level_of_service_stage: nil,
+              ),
+            )
+            .to(
+              hash_including(
+                emergency_level_of_service: 3,
+                emergency_level_of_service_name: "Full Representation",
+                emergency_level_of_service_stage: 8,
+              ),
+            )
         end
       end
     end
@@ -44,6 +78,7 @@ RSpec.describe Proceedings::EmergencyLevelOfServiceForm, :vcr, type: :form do
       end
 
       it "generates the expected error message" do
+        save_form
         expect(form.errors.map(&:attribute)).to eq [:emergency_level_of_service]
       end
     end
--- spec/forms/proceedings/substantive_level_of_service_form_spec.rb
+++ spec/forms/proceedings/substantive_level_of_service_form_spec.rb
@@ -3,35 +3,69 @@ require "rails_helper"
 RSpec.describe Proceedings::SubstantiveLevelOfServiceForm, :vcr, type: :form do
   subject(:form) { described_class.new(form_params) }
 
-  let(:proceeding) { create(:proceeding, :se014, :without_df_date, :with_cit_z) }
+  let(:proceeding) do
+    create(:proceeding,
+           :se014,
+           :without_df_date,
+           :with_cit_z,
+           substantive_level_of_service: nil,
+           substantive_level_of_service_name: nil,
+           substantive_level_of_service_stage: nil)
+  end
+
   let(:params) do
     {
       substantive_level_of_service:,
     }
   end
+
   let(:form_params) { params.merge(model: proceeding) }
 
   describe "#save" do
     subject(:save_form) { form.save }
 
-    before { save_form }
-
     context "when the submission is valid" do
-      context "and the user selects Family Help (Higher)" do
+      context "with level of service for Family Help (Higher)" do
         let(:substantive_level_of_service) { 1 }
 
-        it "updates the substantive_level_of_service value" do
-          expect(proceeding.reload.substantive_level_of_service).to eq 1
-          expect(proceeding.reload.substantive_level_of_service_name).to eq "Family Help (Higher)"
+        it "updates to the expected values" do
+          expect { save_form }.to change { proceeding.reload.attributes.symbolize_keys }
+            .from(
+              hash_including(
+                substantive_level_of_service: nil,
+                substantive_level_of_service_name: nil,
+                substantive_level_of_service_stage: nil,
+              ),
+            )
+            .to(
+              hash_including(
+                substantive_level_of_service: 1,
+                substantive_level_of_service_name: "Family Help (Higher)",
+                substantive_level_of_service_stage: 1,
+              ),
+            )
         end
       end
 
-      context "and the user selects Full Representation" do
+      context "with level of service for Full Representation" do
         let(:substantive_level_of_service) { 3 }
 
-        it "updates the accepted_substantive_defaults value" do
-          expect(proceeding.reload.substantive_level_of_service).to eq 3
-          expect(proceeding.reload.substantive_level_of_service_name).to eq "Full Representation"
+        it "updates to the expected values" do
+          expect { save_form }.to change { proceeding.reload.attributes.symbolize_keys }
+            .from(
+              hash_including(
+                substantive_level_of_service: nil,
+                substantive_level_of_service_name: nil,
+                substantive_level_of_service_stage: nil,
+              ),
+            )
+            .to(
+              hash_including(
+                substantive_level_of_service: 3,
+                substantive_level_of_service_name: "Full Representation",
+                substantive_level_of_service_stage: 8,
+              ),
+            )
         end
       end
     end
@@ -44,6 +78,7 @@ RSpec.describe Proceedings::SubstantiveLevelOfServiceForm, :vcr, type: :form do
       end
 
       it "generates the expected error message" do
+        save_form
         expect(form.errors.map(&:attribute)).to eq [:substantive_level_of_service]
       end
     end
