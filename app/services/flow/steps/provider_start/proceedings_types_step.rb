1cbcf66165c719a0788bddfcc3c2300af32681a9
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-12-06 07:57:55 +0000

    AP-4409: Update the proceeding_types flow
    Redirect all PLF proceedings to the :related_orders step

--- app/services/flow/steps/provider_start/proceedings_types_step.rb
+++ app/services/flow/steps/provider_start/proceedings_types_step.rb
@@ -2,6 +2,7 @@ module Flow
   module Steps
     module ProviderStart
       PROHIBITED_STEPS_OR_SPECIFIC_ISSUE_REGEXP = /^SE00[3478](.*)\z/
+      PUBLIC_LAW_FAMILY_PROCEEDING_REGEXP = /^PBM(16|17|18|19|20|21|22)[AE]?$/
 
       ProceedingsTypesStep = Step.new(
         path: ->(application) { Steps.urls.providers_legal_aid_application_proceedings_types_path(application) },
@@ -12,6 +13,8 @@ module Flow
             :proceedings_sca_heard_togethers
           elsif PROHIBITED_STEPS_OR_SPECIFIC_ISSUE_REGEXP.match?(proceeding.ccms_code)
             :change_of_names
+          elsif PUBLIC_LAW_FAMILY_PROCEEDING_REGEXP.match?(proceeding.ccms_code)
+            :related_orders
           else
             :has_other_proceedings
           end
