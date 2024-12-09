ec8cb12fecf39211f58f5a4491f7a8469d305e3d
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-12-09 13:29:55 +0000

    AP-4408: Add second_appeal to legal_aid_application model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_06_121926) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_09_132733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
