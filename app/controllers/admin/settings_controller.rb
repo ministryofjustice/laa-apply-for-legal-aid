a57c02ff058bd2c178999d4a28e0b24953736dbb
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2024-12-09 16:22:17 +0000

    AP-5383: Remove from settings and associated specs

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -25,7 +25,6 @@ module Admin
                                       :linked_applications,
                                       :collect_hmrc_data,
                                       :special_childrens_act,
-                                      :means_test_review_a,
                                       :public_law_family)
     end
 
