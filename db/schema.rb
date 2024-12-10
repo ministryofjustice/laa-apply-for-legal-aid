c3ae97eb52de307726fe32ad79461e05f6feba15
skdejong <StephanieKdeJong@gmail.com>
2024-12-10 17:23:36 +0000

    AP-5378: Remove confirmation checkbox and text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_06_121926) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_10_171604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -1011,7 +1011,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_06_121926) do
 
   create_table "specific_issues", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "proceeding_id", null: false
-    t.boolean "confirmed"
     t.string "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
