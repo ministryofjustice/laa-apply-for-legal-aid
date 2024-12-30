3a7fb5554e35c81d694cc00520bb85c4d55b74b0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-12-30 13:54:28 +0000

    Chore: Update DB restore script
    Allow us to pass in a named file to restore, this allows us to
    backup a UAT instance and restore it, it improves the instructions
    to make the new uses clearer.

--- scripts/restore_anonymised_db.sh
+++ scripts/restore_anonymised_db.sh
@@ -13,9 +13,10 @@ production_warning() {
 }
 
 help_msg() {
-  echo "Usage: db_export.sh [environment]
+  echo "Usage: db_export.sh [environment] [optional: source_file]
   Where:
-  environment  [staging or a branch name, i.e. ap-1234]"
+  environment  [staging or a branch name, i.e. ap-1234]
+  source_file  [the local file to restore, defaults to ./tmp/production.anon.sql"
   exit 1
 }
 
@@ -34,15 +35,27 @@ case "$1" in
   ;;
 *)
   environment='uat'
-  echo "Finding pod for branch $1 in $environment"
+  echo "Finding pod for branch '$1' in $environment"
   POD=$(kubectl -n "laa-apply-for-legalaid-$environment" get pods | grep -o -m4 "apply-$1-.*2/2" | head -n1 | cut -d' ' -f 1)
 esac
 
-echo "compressing local production backup to speed up upload"
-gzip tmp/production.anon.sql -k -3 -f ./tmp/production.anon.sql
+case "$2" in
+"")
+  source_file="tmp/production.anon.sql"
+  ;;
+*)
+  source_file="$2"
+esac
+
+if [ ! -f $source_file ]; then
+   echo "File $source_file does not exist."
+   exit 1;
+fi
+echo "compressing $source_file backup to speed up upload"
+gzip $source_file -k -3 -f -c $source_file > tmp/anonymised_db.sql.gz
 
 echo "Copying anonymised db to kubernetes pod $POD"
-kubectl -n laa-apply-for-legalaid-$environment cp --retries=10 ./tmp/production.anon.sql.gz -c web laa-apply-for-legalaid-"$environment"/"$POD":./tmp/anonymised_db.sql.gz
+kubectl -n laa-apply-for-legalaid-$environment cp --retries=10 ./tmp/anonymised_db.sql.gz -c web laa-apply-for-legalaid-"$environment"/"$POD":./tmp/anonymised_db.sql.gz
 
 echo "Extracting compressed, uploaded file"
 kubectl -n laa-apply-for-legalaid-$environment -c web exec "$POD" -- sh -c 'gunzip ./tmp/anonymised_db.sql.gz'
@@ -59,8 +72,19 @@ if [ $? -eq 0 ]; then
    echo 'Restore complete!'
    echo "Deleting the data file from the pod"
    kubectl -n laa-apply-for-legalaid-$environment -c web exec "$POD" -- sh -c 'rm tmp/anonymised_db.sql'
-   echo "Deleting local zipped copy"
-   rm ./tmp/production.anon.sql.gz
+
+  echo "Delete local zipped copy? This will leave the original, uncompressed file untouched"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes)
+        echo "Deleting local zipped copy"
+        rm ./tmp/anonymised_db.sql.gz
+        break;;
+      No)
+        echo "./tmp/anonymised_db.sql.gz left in place"
+        break;;
+    esac
+  done
 else
    echo "Restore may have failed, check logs before re-running the script"
 fi
