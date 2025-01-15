52567503b63c0d07ce1f6deb6e22c89d10360660
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-01-15 09:26:38 +0000

    AP-4305: Add controller, view and tests

---
+++ app/controllers/providers/application_merits_task/court_order_copies_controller.rb
@@ -0,0 +1,22 @@
+module Providers
+  module ApplicationMeritsTask
+    class CourtOrderCopiesController < ProviderBaseController
+      def show
+        @form = ApplicationMeritsTask::PLFCourtOrderForm.new(model: legal_aid_application)
+      end
+
+      def update
+        @form = ApplicationMeritsTask::PLFCourtOrderForm.new(form_params)
+        render :show unless update_task_save_continue_or_draft(:application, :court_order_copy)
+      end
+
+      def form_params
+        merge_with_model(legal_aid_application) do
+          return {} unless params[:legal_aid_application]
+
+          params.require(:legal_aid_application).permit(:plf_court_order)
+        end
+      end
+    end
+  end
+end
---
+++ spec/requests/providers/application_merits_task/court_order_copies_controller_spec.rb
@@ -0,0 +1,97 @@
+require "rails_helper"
+
+module Providers
+  module ApplicationMeritsTask
+    RSpec.describe DateClientToldIncidentsController do
+      let(:legal_aid_application) { create(:legal_aid_application, :with_public_law_family_prohibited_steps_order) }
+      let(:login_provider) { login_as legal_aid_application.provider }
+      let(:smtl) { create(:legal_framework_merits_task_list, :pbm16_as_defendant, legal_aid_application:) }
+
+      describe "GET /providers/applications/:legal_aid_application_id/court_order" do
+        subject(:get_request) do
+          get providers_legal_aid_application_court_order_copy_path(legal_aid_application)
+        end
+
+        before do
+          login_provider
+          get_request
+        end
+
+        it "renders successfully" do
+          expect(response).to have_http_status(:ok)
+        end
+
+        context "when not authenticated" do
+          let(:login_provider) { nil }
+
+          it_behaves_like "a provider not authenticated"
+        end
+      end
+
+      describe "PATCH /providers/applications/:legal_aid_application_id/court_order" do
+        subject(:patch_request) do
+          patch(
+            providers_legal_aid_application_court_order_copy_path(legal_aid_application),
+            params:,
+          )
+        end
+
+        before do
+          allow(LegalFramework::MeritsTasksService).to receive(:call).with(legal_aid_application).and_return(smtl)
+          login_provider
+          patch_request
+        end
+
+        context "when not authenticated" do
+          let(:login_provider) { nil }
+          let(:params) { { legal_aid_application: { plf_court_order: true } } }
+
+          before { patch_request }
+
+          it_behaves_like "a provider not authenticated"
+        end
+
+        context "when the user chooses yes" do
+          let(:params) { { legal_aid_application: { plf_court_order: true } } }
+
+          it "updates the record" do
+            expect(legal_aid_application.reload.plf_court_order).to be(true)
+          end
+
+          it "redirects to the next page" do
+            expect(response).to redirect_to(flow_forward_path)
+          end
+        end
+
+        context "when the user chooses no" do
+          let(:params) { { legal_aid_application: { plf_court_order: false } } }
+
+          it "updates the record" do
+            expect(legal_aid_application.reload.plf_court_order).to be(false)
+          end
+
+          it "redirects to the next page" do
+            expect(response).to redirect_to(flow_forward_path)
+          end
+        end
+
+        context "when the user chooses nothing" do
+          let(:params) { { legal_aid_application: { plf_court_order: nil } } }
+
+          it "stays on the page if there is a validation error" do
+            expect(response).to have_http_status(:ok)
+            expect(unescaped_response_body).to include(I18n.t("activemodel.errors.models.legal_aid_application.attributes.plf_court_order.inclusion"))
+          end
+        end
+
+        context "when the form submitted with Save as draft button" do
+          let(:params) { { legal_aid_application: { plf_court_order: false }, draft_button: "Save and come back later" } }
+
+          it "redirects to the list of applications" do
+            expect(response).to redirect_to submitted_providers_legal_aid_applications_path
+          end
+        end
+      end
+    end
+  end
+end
