1d1484a9ebab7c9b9459a0fd27c09750288886c3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-01-24 12:12:43 +0000

    AP-5380: Update controller and view to handle model and value correctly

--- app/controllers/providers/application_merits_task/is_matter_opposed_controller.rb
+++ app/controllers/providers/application_merits_task/is_matter_opposed_controller.rb
@@ -2,7 +2,7 @@ module Providers
   module ApplicationMeritsTask
     class IsMatterOpposedController < ProviderBaseController
       def show
-        @form = ApplicationMeritsTask::MatterOpposedForm.new(model: matter_opposed)
+        @form = ApplicationMeritsTask::MatterOpposedForm.new(model: matter_opposition)
       end
 
       def update
@@ -12,13 +12,13 @@ module Providers
 
     private
 
-      def matter_opposed
-        legal_aid_application.matter_opposed || legal_aid_application.build_matter_opposed
+      def matter_opposition
+        legal_aid_application.matter_opposition || legal_aid_application.build_matter_opposition
       end
 
       def form_params
-        merge_with_model(matter_opposed) do
-          params.require(:application_merits_task_matter_opposed).permit(:matter_opposed)
+        merge_with_model(matter_opposition) do
+          params.require(:application_merits_task_matter_opposition).permit(:matter_opposed)
         end
       end
     end
