47b82e292376bf87261c2a274962ce8e2eba212b
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-01-28 09:23:58 +0000

    AP-4415: Add uploaded_evidence_helper
    Move logic for message at top of evidence upload page
    into its own helper as it is getting quite complex

---
+++ app/helpers/uploaded_evidence_helper.rb
@@ -0,0 +1,32 @@
+module UploadedEvidenceHelper
+  def evidence_message(required_documents, legal_aid_application, evidence_type_translation)
+    if required_documents.size == 1
+      single_evidence_message(required_documents.first, legal_aid_application, evidence_type_translation)
+    else
+      multiple_evidence_message(required_documents, legal_aid_application)
+    end
+  end
+
+private
+
+  def single_evidence_message(evidence, legal_aid_application, evidence_type_translation)
+    content_tag(:div, class: "govuk-body") do
+      if legal_aid_application.section_8_proceedings?
+        t(".section_8_evidence")
+      elsif evidence_type_translation.present?
+        t(".single_item", evidence_type: evidence_type_translation)
+      else
+        t(".single_non_specific_item", item: t(".#{evidence}"))
+      end
+    end
+  end
+
+  def multiple_evidence_message(required_documents, legal_aid_application)
+    content_tag(:div, t(".list_text"), class: "govuk-body") +
+      govuk_list(
+        required_documents.map { |evidence| t(".#{evidence}", benefit: legal_aid_application&.dwp_override&.passporting_benefit&.titleize) },
+        type: :bullet,
+        classes: "govuk-!-padding-bottom-2",
+      )
+  end
+end
