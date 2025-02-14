e1db21a3c11181bf91ce61f840b01b0e52045ccd
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-02-14 14:42:32 +0000

    AP-5580: Duplicate controllers, views, routes, etc

---
+++ app/controllers/providers/application_merits_task/client_has_parental_responsibilities_controller.rb
@@ -0,0 +1,33 @@
+module Providers
+  module ApplicationMeritsTask
+    class ClientHasParentalResponsibilitiesController < ProviderBaseController
+      prefix_step_with :application_merits_task
+      def show
+        legal_aid_application.provider_recording_parental_responsibilities!
+        @form = Providers::ApplicationMeritsTask::ParentalResponsibilitiesForm.new(model: applicant)
+      end
+
+      def update
+        @form = Providers::ApplicationMeritsTask::ParentalResponsibilitiesForm.new(form_params)
+        if @form.relationship_to_children.eql?("false") && !draft_selected?
+          @form.save!
+          return redirect_to providers_legal_aid_application_client_is_child_subject_path(legal_aid_application)
+        end
+
+        render :show unless update_task_save_continue_or_draft(:application, :client_relationship_to_children, reshow_check_client: legal_aid_application.merits_parental_responsibilities_all_rejected?)
+      end
+
+    private
+
+      def applicant
+        legal_aid_application.applicant
+      end
+
+      def form_params
+        merge_with_model(applicant) do
+          params.require(:applicant).permit(:relationship_to_children)
+        end
+      end
+    end
+  end
+end
