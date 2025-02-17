07ccd1122450b86c93b54cd3705655089f27ec1e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-02-17 14:30:46 +0000

    AP-5580: Update merits_loop with new application level question

--- app/services/flow/merits_loop.rb
+++ app/services/flow/merits_loop.rb
@@ -46,6 +46,7 @@ module Flow
         second_appeal: :second_appeals,
         vary_order: :vary_order,
         opponents_application: :opponents_application,
+        client_relationship_to_children: :client_is_biological_parent,
         client_relationship_to_proceeding: :is_client_biological_parent,
         client_child_care_assessment: :child_care_assessments,
         court_order_copy: :court_order_copies,
