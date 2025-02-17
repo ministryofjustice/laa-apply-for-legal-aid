bcd0cd1ae89b732250d957cc6a198e90680591b8
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-02-17 16:10:55 +0000

    AP-5669: Update new framework defaults

--- config/initializers/new_framework_defaults_7_2.rb
+++ config/initializers/new_framework_defaults_7_2.rb
@@ -9,38 +9,38 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html
 
-###
+# ##
 # Controls whether Active Job's `#perform_later` and similar methods automatically defer
 # the job queuing to after the current Active Record transaction is committed.
-#
+
 # Example:
 #   Topic.transaction do
 #     topic = Topic.create(...)
 #     NewTopicNotificationJob.perform_later(topic)
 #   end
-#
+
 # In this example, if the configuration is set to `:never`, the job will
 # be enqueued immediately, even though the `Topic` hasn't been committed yet.
 # Because of this, if the job is picked up almost immediately, or if the
 # transaction doesn't succeed for some reason, the job will fail to find this
 # topic in the database.
-#
+
 # If `enqueue_after_transaction_commit` is set to `:default`, the queue adapter
 # will define the behaviour.
-#
-# Note: Active Job backends can disable this feature. This is generally done by
+
+# NOTE: Active Job backends can disable this feature. This is generally done by
 # backends that use the same database as Active Record as a queue, hence they
 # don't need this feature.
-#++
-# Rails.application.config.active_job.enqueue_after_transaction_commit = :default
+# ++
+Rails.application.config.active_job.enqueue_after_transaction_commit = :default
 
-###
+# ##
 # Adds image/webp to the list of content types Active Storage considers as an image
 # Prevents automatic conversion to a fallback PNG, and assumes clients support WebP, as they support gif, jpeg, and png.
 # This is possible due to broad browser support for WebP, but older browsers and email clients may still not support
 # WebP. Requires imagemagick/libvips built with WebP support.
-#++
-# Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
+# ++
+Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
 
 ###
 # Enable validation of migration timestamps. When set, an ActiveRecord::InvalidMigrationTimestampError
@@ -51,7 +51,7 @@
 # Applications with existing timestamped migrations that do not adhere to the
 # expected format can disable validation by setting this config to `false`.
 #++
-# Rails.application.config.active_record.validate_migration_timestamps = true
+Rails.application.config.active_record.validate_migration_timestamps = true
 
 ###
 # Controls whether the PostgresqlAdapter should decode dates automatically with manual queries.
@@ -61,10 +61,10 @@
 #
 # This query used to return a `String`.
 #++
-# Rails.application.config.active_record.postgresql_adapter_decode_dates = true
+Rails.application.config.active_record.postgresql_adapter_decode_dates = true
 
 ###
 # Enables YJIT as of Ruby 3.3, to bring sizeable performance improvements. If you are
 # deploying to a memory constrained environment you may want to set this to `false`.
 #++
-# Rails.application.config.yjit = true
+Rails.application.config.yjit = true
