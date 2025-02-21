f706d0db17b4f26a2b6eb96c1d896e6eb30fe7b9
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-02-21 07:34:07 +0000

    Add require "sidekiq/rails" to rails helper
    The changes in sidekiq 7.3.9 were discussed and I chose this
    method of globally adding rather than adding to each test
    file that _may_ need sidekiq - happy to discuss

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
+require "sidekiq/rails"
 require "json_expressions/rspec"
 require "awesome_print"
 require "pry-rescue/rspec" if Rails.env.development?
