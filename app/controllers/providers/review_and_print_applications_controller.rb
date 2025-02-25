1706c1e61c546580664e455af6c5cd517277635c
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-02-25 08:41:32 +0000

    AP-5429: Swap to using partial from income cya
    Add read only attributes so change links appear correctly. Update
    headings. Remove unnecessary markup.

--- app/controllers/providers/review_and_print_applications_controller.rb
+++ app/controllers/providers/review_and_print_applications_controller.rb
@@ -6,6 +6,7 @@ module Providers
     def show
       @source_application = @legal_aid_application.copy_case? ? LegalAidApplication.find(legal_aid_application.copy_case_id) : @legal_aid_application
       @read_only = true
+      set_transaction_types
     end
 
     def continue
@@ -14,6 +15,18 @@ module Providers
         legal_aid_application.merits_complete!
       end
       continue_or_draft
+    end
+
+  private
+
+    def set_transaction_types
+      @credit_transaction_types = if legal_aid_application.client_uploading_bank_statements?
+                                    TransactionType.credits.without_disregarded_benefits.without_benefits
+                                  else
+                                    TransactionType.credits.without_housing_benefits
+                                  end
+
+      @debit_transaction_types = TransactionType.debits
     end
   end
 end
