3684de0b1bb6d31e5fd3db83063574312ea0bc06
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-02-27 13:34:36 +0000

    Fix: remove webdack references

--- db/migrate/20180829150801_create_legal_aid_application_proceeding_types.rb
+++ db/migrate/20180829150801_create_legal_aid_application_proceeding_types.rb
@@ -1,6 +1,6 @@
 class CreateLegalAidApplicationProceedingTypes < ActiveRecord::Migration[5.2]
   def change
-    create_table :application_proceeding_types do |t|
+    create_table :application_proceeding_types, id: :uuid do |t|
       t.references :legal_aid_application
       t.references :proceeding_type
       t.timestamps
