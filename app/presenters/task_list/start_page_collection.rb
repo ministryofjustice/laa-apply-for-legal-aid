778bdfe61db6d114ec51ff1d2201b0e6bb7d7b23
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2025-03-10 11:54:24 +0000

    AP-6089: Build restrictive tasklist pattern
    Implement a pattern in the applcation code base that can be extended
    to build the restrictive task list.
    
    Based on a spike that used the NSCC task list but modified
    and then customised to handle
    - our own task list design customisations
      - sections with content-only under certain circumstances
      - sub-sections
      - subsections with content-only under certain circumstances
      - conditional task list items
    - an example of determing a task status through validators, forms and or
      models via the notion of a "task validator".
    
    This is non-navigable and is merely intended as a step to
    build the task list.

---
+++ app/presenters/task_list/start_page_collection.rb
@@ -0,0 +1,61 @@
+module TaskList
+  class StartPageCollection < Collection
+    NEW_RECORD = "00000000-0000-0000-0000-000000000000".freeze
+
+    # Template:
+    #
+    # SECTIONS = {
+    #   task_list_section: {
+    #     body_override ->(application) { "my section message content" if application.my_condition? },
+    #     task_list_item_1 (name after step): displayed?,
+    #     task_list_item_2 (name after step): displayed?,
+    #     ....
+    #     subsections: {
+    #       subsection_name: {
+    #         body_override ->(application) { "my subsection message content" if application.my_condition? },
+    #         sub_task_list_item_1 (name after step): displayed?
+    #      }
+    #    }
+    #   }
+    # }
+    #
+    # Rules: name task list items after the [first] step in its flow.
+    #
+    # e.g.
+    #  - Steps::ProviderStart::ApplicantsStep --> applicants
+    #  - Steps::ProviderStart::AboutFinancialMeansStep --> about_financial_means
+    #
+    # Body overide:
+    #
+    # If the section's task list cannot be shown and requires text to be displayed instead then the
+    # body_override task key should be added. It's lambda should return the message to be displayed
+    # only if it needs to be displayed. This will override displaying of the tasks for that section
+    # replacing the tasks with the message body provided.
+    #
+    # e.g. body_override: ->(application) { "Not available yet because..." unless application.client_and_case_details_completed? },
+    #
+
+    SECTIONS = {
+      client_and_case_details: {
+        applicants: true, # Steps::ProviderStart::ApplicantsStep
+      },
+      means_assessment: {
+        body_override: ->(application) { t!("task_list.body_override.means.requirement_unknown") if application.journey_unknown? || application.non_means_tested? },
+        subsections: {
+          financial_information: {
+            body_override: ->(application) { t!("task_list.body_override.means.financial_information_not_needed") if application.passported? },
+          },
+          capital_and_assets: {},
+        },
+      },
+      merits_assessment: {
+        body_override: ->(application) { t!("task_list.body_override.merits.requirement_unknown") if application.proceedings.present? },
+        subsections: {
+          about_this_application: {},
+          about_the_proceedings: {},
+        },
+      },
+      confirm_and_submit: {},
+    }.freeze
+  end
+end
