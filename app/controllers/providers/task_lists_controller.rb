411d428a95783c4cd0379fa1397dd68446ddbbb6
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2025-03-10 11:54:24 +0000

    AP-6089: Build restrictive tasklist pattern
    Implement a pattern in the applcation code base that can be extended
    to build the restrictive task list.
    
    Based on a spike that used the NSCC task list but modified
    and then customised to handle
    - our own task list design customisations
      - sections with content-only under certain circumstances
      - sub-sections
      - subsections with content-only under certain circumstances
      - conditional task list items
    - an example of determing a task status through validators, forms and or
      models via the notion of a "task validator".
    
    This is non-navigable and is merely intended as a step to
    build the task list.

---
+++ app/controllers/providers/task_lists_controller.rb
@@ -0,0 +1,27 @@
+module Providers
+  class TaskListsController < ApplicationController
+    before_action :authenticate_provider!
+    before_action :update_locale
+    include Authorizable
+    include ApplicationDependable
+
+    def show
+      legal_aid_application
+
+      @task_list = ::TaskList::StartPageCollection.new(
+        view_context, application: legal_aid_application
+      )
+    end
+
+  private
+
+    def legal_aid_application
+      @legal_aid_application ||=
+        LegalAidApplication.find_by(id: task_list_params[:legal_aid_application_id])
+    end
+
+    def task_list_params
+      params.permit(%i[legal_aid_application_id locale])
+    end
+  end
+end
