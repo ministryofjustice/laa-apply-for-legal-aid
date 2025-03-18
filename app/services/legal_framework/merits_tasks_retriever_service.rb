2e9ac0d53e471ac203f04d63b7eb4b0be50a83b1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-03-18 13:33:35 +0000

    AP-5698: Update merits task retriever to send level of service

--- app/services/legal_framework/merits_tasks_retriever_service.rb
+++ app/services/legal_framework/merits_tasks_retriever_service.rb
@@ -21,6 +21,7 @@ module LegalFramework
           ccms_code: proceeding.ccms_code,
           delegated_functions_used: proceeding.used_delegated_functions,
           client_involvement_type: proceeding.client_involvement_type_ccms_code,
+          substantive_level_of_service: proceeding.substantive_level_of_service,
         }
       end
     end
