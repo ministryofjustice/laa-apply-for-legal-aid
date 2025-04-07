adccbb52e5018571e78bb0318e7c59e3834146be
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-04-07 12:01:11 +0100

    AP-5880: Extend scheduled_ccms_submissions_toggle_job functionality
    Add functionality so that, rather than just toggling from on to off, the
    behaviour is different based on whether ccms submissions are currently
    on. Extend tests to cover these scenarios

--- app/jobs/scheduled_ccms_submissions_toggle_job.rb
+++ app/jobs/scheduled_ccms_submissions_toggle_job.rb
@@ -1,19 +1,24 @@
 class ScheduledCCMSSubmissionsToggleJob < ApplicationJob
   include Sidekiq::Status::Worker
 
-  def perform
+  def perform(input)
     log "starting at #{Time.zone.now}"
 
-    if Setting.enable_ccms_submission?
-      turn_off_ccms_submissions
-      alert_slack("CCMS submissions have been automatically turned *OFF*. They can be turned back on in the <#{admin_settings_url}|admin settings.>")
-    else
-      turn_on_ccms_submissions
-      alert_slack("CCMS submissions have been automatically turned *ON*. They can be turned back off in the <#{admin_settings_url}|admin settings.>")
+    case input
+    when :turn_on
+      Setting.enable_ccms_submission? ? ccms_submissions_already_on : turn_on_ccms_submissions
+    when :turn_off
+      Setting.enable_ccms_submission? ? turn_off_ccms_submissions : ccms_submissions_already_off
+    when :alert
+      reminder_turn_on_ccms_submissions unless Setting.enable_ccms_submission?
     end
   end
 
 private
+
+  def reminder_turn_on_ccms_submissions
+    alert_slack("REMINDER: Turn CCMS submissions back on in the <#{admin_settings_url}|admin settings>. If no action is taken, they will automatically be turned on at 08:00.")
+  end
 
   def admin_settings_url
     Rails.application.routes.url_helpers.admin_settings_url
@@ -22,10 +27,20 @@ private
   def turn_on_ccms_submissions
     Setting.setting.update!(enable_ccms_submission: true)
     CCMS::RestartSubmissions.call
+    alert_slack("CCMS submissions have been automatically turned *ON*. They can be turned back off in the <#{admin_settings_url}|admin settings.>")
   end
 
   def turn_off_ccms_submissions
     Setting.setting.update!(enable_ccms_submission: false)
+    alert_slack("CCMS submissions have been automatically turned *OFF*. They can be turned back on in the <#{admin_settings_url}|admin settings.>")
+  end
+
+  def ccms_submissions_already_on
+    alert_slack("CCMS submissions are already turned *ON* so no action has been taken. They can be turned off in the <#{admin_settings_url}|admin settings.>")
+  end
+
+  def ccms_submissions_already_off
+    alert_slack("CCMS submissions are already turned *OFF* so no action has been taken. They can be turned on in the <#{admin_settings_url}|admin settings.>")
   end
 
   def alert_slack(message)
--- spec/jobs/scheduled_ccms_submissions_toggle_job_spec.rb
+++ spec/jobs/scheduled_ccms_submissions_toggle_job_spec.rb
@@ -9,37 +9,85 @@ RSpec.describe ScheduledCCMSSubmissionsToggleJob do
     allow(Slack::SendMessage).to receive(:call)
   end
 
-  context "when ccms submissions are enabled" do
-    before do
-      setting.update!(enable_ccms_submission: true)
+  describe "#perform" do
+    context "when ccms submissions are enabled" do
+      before do
+        setting.update!(enable_ccms_submission: true)
+      end
+
+      context "and the job is trying to turn them off" do
+        it "disables ccms submissions" do
+          expect { job.perform(:turn_off) }.to change { setting.reload.enable_ccms_submission }.from(true).to(false)
+        end
+
+        it "sends a Slack alert" do
+          job.perform(:turn_off)
+          expect(Slack::SendMessage).to have_received(:call).with(
+            text: "CCMS submissions have been automatically turned *OFF*. They can be turned back on in the <http://www.example.com/admin/settings?locale=en|admin settings.>",
+          )
+        end
+      end
+
+      context "and the job is trying to turn them on" do
+        it "does not change the ccms submissions setting" do
+          expect { job.perform(:turn_on) }.not_to change { setting.reload.enable_ccms_submission }
+        end
+
+        it "sends a Slack alert" do
+          job.perform(:turn_on)
+          expect(Slack::SendMessage).to have_received(:call).with(
+            text: "CCMS submissions are already turned *ON* so no action has been taken. They can be turned off in the <http://www.example.com/admin/settings?locale=en|admin settings.>",
+          )
+        end
+      end
+
+      context "and an alert is sent to turn on them on" do
+        it "does not send a reminder" do
+          job.perform(:alert)
+          expect(Slack::SendMessage).not_to have_received(:call)
+        end
+      end
     end
 
-    it "disables ccms submissions" do
-      expect { job.perform }.to change { setting.reload.enable_ccms_submission }.from(true).to(false)
-    end
+    context "when ccms submissions are disabled" do
+      before do
+        setting.update!(enable_ccms_submission: false)
+      end
 
-    it "sends a Slack alert" do
-      job.perform
-      expect(Slack::SendMessage).to have_received(:call).with(
-        text: "CCMS submissions have been automatically turned *OFF*. They can be turned back on in the <http://www.example.com/admin/settings?locale=en|admin settings.>",
-      )
-    end
-  end
+      context "and the job is trying to turn them off" do
+        it "does not change the ccms submissions setting" do
+          expect { job.perform(:turn_off) }.not_to change { setting.reload.enable_ccms_submission }
+        end
 
-  context "when ccms submissions are disabled" do
-    before do
-      setting.update(enable_ccms_submission: false)
-    end
+        it "sends a Slack alert" do
+          job.perform(:turn_off)
+          expect(Slack::SendMessage).to have_received(:call).with(
+            text: "CCMS submissions are already turned *OFF* so no action has been taken. They can be turned on in the <http://www.example.com/admin/settings?locale=en|admin settings.>",
+          )
+        end
+      end
 
-    it "enables ccms submissions" do
-      expect { job.perform }.to change { setting.reload.enable_ccms_submission }.from(false).to(true)
-    end
+      context "and the job is trying to turn them on" do
+        it "enables ccms submissions" do
+          expect { job.perform(:turn_on) }.to change { setting.reload.enable_ccms_submission }.from(false).to(true)
+        end
 
-    it "sends a Slack alert" do
-      job.perform
-      expect(Slack::SendMessage).to have_received(:call).with(
-        text: "CCMS submissions have been automatically turned *ON*. They can be turned back off in the <http://www.example.com/admin/settings?locale=en|admin settings.>",
-      )
+        it "sends a Slack alert" do
+          job.perform(:turn_on)
+          expect(Slack::SendMessage).to have_received(:call).with(
+            text: "CCMS submissions have been automatically turned *ON*. They can be turned back off in the <http://www.example.com/admin/settings?locale=en|admin settings.>",
+          )
+        end
+      end
+
+      context "and an alert is sent to turn on them on" do
+        it "sends a reminder" do
+          job.perform(:alert)
+          expect(Slack::SendMessage).to have_received(:call).with(
+            text: "REMINDER: Turn CCMS submissions back on in the <http://www.example.com/admin/settings?locale=en|admin settings>. If no action is taken, they will automatically be turned on at 08:00.",
+          )
+        end
+      end
     end
   end
 
