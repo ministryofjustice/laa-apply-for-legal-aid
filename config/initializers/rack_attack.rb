99cf627d757450964aff9bab965a7f8fca2fcafc
skdejong <StephanieKdeJong@gmail.com>
2025-04-09 09:24:27 +0100

    AP-5927: Autocorrect Style/RedundantParentheses

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,7 +6,7 @@ class Rack::Attack
                              end
 
   Rack::Attack.blocklist("block all crawlergo feedback") do |request|
-    (request.body.is_a?(StringIO) && request.body.string.match?(/[C|c]rawlergo/)) && request.path.include?("feedback") && request.post?
+    request.body.is_a?(StringIO) && request.body.string.match?(/[C|c]rawlergo/) && request.path.include?("feedback") && request.post?
   end
 
   Rack::Attack.throttle("prevent feedback spamming", limit: 2, period: 10) do |request|
