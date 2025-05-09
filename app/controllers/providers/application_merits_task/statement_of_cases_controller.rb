576e9d5c36c1b624b111fab4d80e81366f689377
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-05-09 11:36:12 +0100

    AP-5910: Update StatementOfCasesController to handle deletion when upload changed from true to false

--- app/controllers/providers/application_merits_task/statement_of_cases_controller.rb
+++ app/controllers/providers/application_merits_task/statement_of_cases_controller.rb
@@ -1,12 +1,16 @@
 module Providers
   module ApplicationMeritsTask
     class StatementOfCasesController < ProviderBaseController
+      include DeleteAttachments
+
       def show
         @form = ApplicationMeritsTask::StatementOfCaseChoiceForm.new(model: statement_of_case)
       end
 
       def update
         @form = StatementOfCaseChoiceForm.new(form_params)
+        delete_attachments(statements_of_case) if legal_aid_application.statement_of_case_uploaded? && @form.upload.eql?(false)
+
         render :show unless update_task_save_continue_or_draft(:application, :statement_of_case, uploaded: @form.upload)
       end
 
@@ -20,6 +24,10 @@ module Providers
         merge_with_model(statement_of_case) do
           params.expect(application_merits_task_statement_of_case: [:statement, { upload: [], typed: [] }])
         end
+      end
+
+      def statements_of_case
+        @statements_of_case ||= find_attachments_starting_with("statement_of_case")
       end
     end
   end
