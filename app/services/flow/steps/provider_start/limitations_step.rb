a4d6bfc5b6ab7fe009bb19ba8b5b00b305815df6
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-05-12 16:48:34 +0100

    AP-5970: Remove NI page from old position
    Skip out national insurance page from later in the journey by routing to
    the page after it

--- app/services/flow/steps/provider_start/limitations_step.rb
+++ app/services/flow/steps/provider_start/limitations_step.rb
@@ -3,7 +3,13 @@ module Flow
     module ProviderStart
       LimitationsStep = Step.new(
         path: ->(application) { Steps.urls.providers_legal_aid_application_limitations_path(application) },
-        forward: :has_national_insurance_numbers,
+        forward: lambda do |application|
+          if application.overriding_dwp_result?
+            :check_provider_answers
+          else
+            :client_has_partners
+          end
+        end,
         check_answers: :check_provider_answers,
       )
     end
