abce755573fd6e32b1307ac33a48e78a701a7ced
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-06-02 10:28:50 +0100

    AP-5998: Update helper for efficiency
    Improve efficiency of helper as total number of proceedings on LFA grows

--- app/helpers/has_other_proceedings_helper.rb
+++ app/helpers/has_other_proceedings_helper.rb
@@ -2,9 +2,12 @@ module HasOtherProceedingsHelper
   # Where a proceeding could come under more than one matter type (e.g. child arrangement orders come under both PLF and S8), this method is to
   # add warning component to has_other_proceedings page. This is to warn users to check they have selected the correct matter type.
   def show_check_proceeding_warning?(application)
+    lfa_index = lfa_proceedings.group_by { |p| p.meaning.downcase }
+
     application.proceedings.any? do |application_proceeding|
-      lfa_proceedings.any? do |lfa_proceeding|
-        lfa_proceeding.ccms_matter_code != application_proceeding.ccms_matter_code && lfa_proceeding.meaning.casecmp?(application_proceeding.meaning)
+      matching_lfa = lfa_index[application_proceeding.meaning.downcase] || []
+      matching_lfa.any? do |lfa_proceeding|
+        lfa_proceeding.ccms_matter_code != application_proceeding.ccms_matter_code
       end
     end
   end
