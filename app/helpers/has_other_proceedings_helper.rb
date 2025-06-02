aedea455eb63b4db9e4a001f175f85b1a2ae03a7
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-06-02 08:35:27 +0100

    AP-5998: Add comment to make method clearer

--- app/helpers/has_other_proceedings_helper.rb
+++ app/helpers/has_other_proceedings_helper.rb
@@ -1,4 +1,6 @@
 module HasOtherProceedingsHelper
+  # Where a proceeding could come under more than one matter type (e.g. child arrangement orders come under both PLF and S8), this method is to
+  # add warning component to has_other_proceedings page. This is to warn users to check they have selected the correct matter type.
   def show_check_proceeding_warning?(application)
     application.proceedings.any? do |application_proceeding|
       lfa_proceedings.any? do |lfa_proceeding|
