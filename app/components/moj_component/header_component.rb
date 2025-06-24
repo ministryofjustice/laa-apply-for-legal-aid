279135105f3ddd2a396dbc0b45f1fb59f9898892
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-06-24 16:07:42 +0100

    AP-6069: Add navigation items to header

--- app/components/moj_component/header_component.rb
+++ app/components/moj_component/header_component.rb
@@ -2,15 +2,42 @@
 
 module MojComponent
   class HeaderComponent < ViewComponent::Base
+    renders_many :navigation_items, "NavigationItem"
+
     attr_reader :organisation_name,
                 :url,
                 :nav_items
 
-    def initialize(organisation_name:, url:, nav_items:)
-      super
+    def initialize(organisation_name:, url:)
       @organisation_name = organisation_name
       @url = url
-      @nav_items = nav_items
+      super
+    end
+
+    class NavigationItem < ViewComponent::Base
+      attr_reader :text, :href, :current, :options
+
+      def initialize(text:, href: nil, current: nil, options: {})
+        @text = text
+        @href = href
+        @current = current
+        @options = options
+        super
+      end
+
+      def call
+        tag.li class: "moj-header__navigation-item" do
+          link_to(text, href, class: "moj-header__navigation-link", aria: current_override, **options)
+        end
+      end
+
+    private
+
+      def current_override
+        if current
+          { current: "page" }
+        end
+      end
     end
   end
 end
