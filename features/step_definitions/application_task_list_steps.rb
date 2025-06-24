d5f952fc41bb7a37e740d97586dd5b0b075602a5
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2025-06-24 16:34:41 +0100

    AP-6089: Add task list item feature tests

--- features/step_definitions/application_task_list_steps.rb
+++ features/step_definitions/application_task_list_steps.rb
@@ -37,6 +37,7 @@ Given("I created a non-means-tested application") do
 end
 
 When("I go to the application task list") do
+  @legal_aid_application ||= LegalAidApplication.find(id_from_current_page_url)
   visit providers_legal_aid_application_task_list_path(@legal_aid_application)
 end
 
@@ -52,4 +53,26 @@ Then(/^I should (see|not see) subsection header (.*)$/) do |visibility, text|
   else
     expect(page).to have_no_css("h3.govuk-task-list__section", text:)
   end
+end
+
+Then("the {string} task list section should contain:") do |text, table|
+  heading = page.find("h2.govuk-task-list__section", text:)
+  section_list = heading.ancestor("li")
+
+  within(section_list) do
+    table.hashes.each do |row|
+      expect(page).to have_css(".govuk-task-list__name-and-hint", text: row[:name])
+      expect(page).to have_link(row[:name]) if row[:link_enabled].downcase.eql?("true")
+      expect(page).to have_css(".govuk-task-list__status", text: row[:status])
+    end
+  end
+end
+
+def id_from_current_page_url
+  path = URI.parse(page.current_url).path
+  paths = path.split("/").reject! { |fragment| fragment.to_s.empty? }
+  id_at = paths.find_index("applications")
+  return paths[id_at + 1] if id_at
+
+  raise "Unable to locate LegalAidApplication ID accurately"
 end
