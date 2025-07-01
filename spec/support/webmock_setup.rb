09e625d429387be4f05c3adc86714b9a8a652898
Joel Sugarman <joel.sugarman@justice.gov.uk>
2025-07-01 16:31:07 +0100

    AP-6089: Fix interplay of capybara/webmock for system and other specs

--- spec/support/webmock_setup.rb
+++ spec/support/webmock_setup.rb
@@ -10,4 +10,7 @@ allowed_sites = [
   end,
 ]
 
-WebMock.disable_net_connect!(allow: allowed_sites, net_http_connect_on_start: true)
+# NOTE: net_http_connect_on_start is required to have capybara and webmock play nice in system specs but can
+# prevent stubbing in other specs (such as request specs). see https://github.com/bblimke/webmock/issues/914
+#
+WebMock.disable_net_connect!(allow: allowed_sites, net_http_connect_on_start: %w[localhost 127.0.0.1])
