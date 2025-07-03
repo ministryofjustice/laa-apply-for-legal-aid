3cf878978f38d81da0a7be507adfa7ded1f0a066
arthurashman <arthruashman@gmail.com>
2025-07-03 16:44:30 +0100

    Add cannot start determination

--- app/services/task_status/make_link.rb
+++ app/services/task_status/make_link.rb
@@ -3,15 +3,20 @@ module TaskStatus
     def call
       status = ValueObject.new
 
-      status.cannot_start!
-      status.in_progress! if in_progress?
       status.not_started! if not_started?
+      status.cannot_start! if cannot_start?
+      status.in_progress! if in_progress?
       status.completed! if completed?
 
       status
     end
 
   private
+
+    def cannot_start?
+      # Correct to proceedings complete when available
+      !applicants_validator.valid?
+    end
 
     def in_progress?
       application.lead_linked_application.present? && !completed?
@@ -27,6 +32,10 @@ module TaskStatus
 
     def make_link_validator
       @make_link_validator ||= Validators::MakeLink.new(application)
+    end
+
+    def applicants_validator
+      @applicants_validator ||= Validators::Applicants.new(application)
     end
   end
 end
