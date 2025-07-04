123edd02b70ea73d4a10d1c3cf1f9df6528bde2b
Joel Sugarman <joel.sugarman@justice.gov.uk>
2025-07-04 17:14:52 +0100

    AP-6125: Handle failure from EntraID
    Testing is difficult to find a scenario but under certain
    circumstances like having an invalid_grant or invalid_request, or using a previously
    used token EntraID wil throw an error that fallbacks to calling
    `providers/auth/failure?message=invalid_grant&...other-parmas` for example.
    
    Full example:
    
    ```
    (entra_id) Authentication failure! invalid_request: Rack::OAuth2::Client::Error, invalid_request :: AADSTS1100001: Non-retryable error has occurred. Underlying error code: 1003002. Trace ID: 7b2d23ca-f02e-402f-b3a4-325ee1b57500 Correlation ID: 327940d4-65b0-41a5-a7fb-2cc094e3236d Timestamp: 2025-07-14 07:22:23Z
    ```

---
+++ app/controllers/providers/auth_controller.rb
@@ -0,0 +1,17 @@
+module Providers
+  class AuthController < ApplicationController
+    skip_before_action :authenticate_provider!
+
+    def failure
+      # set_flash_message(:error, :failure, kind: "EntraID", reason:)
+      flash[:error] = I18n.t("devise.omniauth_callbacks.failure", kind: failure_params[:strategy]&.humanize || "Identity manager", reason: failure_params[:message]&.humanize || "Authentication failure!")
+      redirect_to error_path(:access_denied)
+    end
+
+  private
+
+    def failure_params
+      params.permit(:message, :origin, :strategy)
+    end
+  end
+end
