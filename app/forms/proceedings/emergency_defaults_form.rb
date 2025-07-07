55a9645c9aad1dd6d22e07f8c2ad69359101db5b
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-07-07 12:44:10 +0100

    AP-6092: Emergency defaults validator
    Add emergency defaults to validator. Add spec. Update form to use dig
    method as it safely deals with a `nil` value response, which occurs when
    the client involvement type hasn't been set.

--- app/forms/proceedings/emergency_defaults_form.rb
+++ app/forms/proceedings/emergency_defaults_form.rb
@@ -22,13 +22,13 @@ module Proceedings
     def initialize(*args)
       super
       @defaults = JSON.parse(LegalFramework::ProceedingTypes::Defaults.call(args.first[:model], true))
-      self.emergency_level_of_service = @defaults["default_level_of_service"]["level"]
-      self.emergency_level_of_service_name = @defaults["default_level_of_service"]["name"]
-      self.emergency_level_of_service_stage = @defaults["default_level_of_service"]["stage"]
-      self.delegated_functions_scope_limitation_code = @defaults["default_scope"]["code"]
-      self.delegated_functions_scope_limitation_meaning = @defaults["default_scope"]["meaning"]
-      self.delegated_functions_scope_limitation_description = @defaults["default_scope"]["description"]
-      self.additional_params = @defaults["default_scope"]["additional_params"]
+      self.emergency_level_of_service = @defaults.dig("default_level_of_service", "level")
+      self.emergency_level_of_service_name = @defaults.dig("default_level_of_service", "name")
+      self.emergency_level_of_service_stage = @defaults.dig("default_level_of_service", "stage")
+      self.delegated_functions_scope_limitation_code = @defaults.dig("default_scope", "code")
+      self.delegated_functions_scope_limitation_meaning = @defaults.dig("default_scope", "meaning")
+      self.delegated_functions_scope_limitation_description = @defaults.dig("default_scope", "description")
+      self.additional_params = @defaults.dig("default_scope", "additional_params")
     end
 
     def save
