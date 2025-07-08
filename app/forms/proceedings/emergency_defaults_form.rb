cb1e8950e95c4e475ac9a569bc99f08c7ca23c47
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-07-08 11:46:32 +0100

    AP-6092: Emergency level of service validators
    Add emergency level of service to validator. Update spec. Rerecord
    casettes to reflect changes. Add in filter map to remove nil values.
    Update presence checker to use inclusion instead, so validation doesn't
    fail when the boolean value is false

--- app/forms/proceedings/emergency_defaults_form.rb
+++ app/forms/proceedings/emergency_defaults_form.rb
@@ -7,7 +7,7 @@ module Proceedings
                   :hearing_date,
                   :limitation_note
 
-    validates :accepted_emergency_defaults, presence: { unless: :draft? }
+    validates :accepted_emergency_defaults, inclusion: ["true", "false", true, false], unless: :draft?
     validates :hearing_date, presence: true, if: :hearing_date_required?
     validates :hearing_date,
               date: {
