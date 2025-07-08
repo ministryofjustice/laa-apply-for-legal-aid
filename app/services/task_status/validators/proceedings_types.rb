33050f2f306adeb0c387a8e494a6ad5b9e2bc678
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-07-08 11:46:32 +0100

    AP-6092: Emergency level of service validators
    Add emergency level of service to validator. Update spec. Rerecord
    casettes to reflect changes. Add in filter map to remove nil values.
    Update presence checker to use inclusion instead, so validation doesn't
    fail when the boolean value is false

--- app/services/task_status/validators/proceedings_types.rb
+++ app/services/task_status/validators/proceedings_types.rb
@@ -16,26 +16,33 @@ module TaskStatus
           client_involvement_type_forms,
           delegated_functions_forms,
           emergency_defaults_forms,
-        ].flatten
+          emergency_level_of_service_forms,
+        ].flatten.compact
       end
 
       def client_involvement_type_forms
-        proceedings.map do |proceeding|
+        proceedings.filter_map do |proceeding|
           Proceedings::ClientInvolvementTypeForm.new(model: proceeding)
         end
       end
 
       def delegated_functions_forms
-        proceedings.map do |proceeding|
+        proceedings.filter_map do |proceeding|
           Proceedings::DelegatedFunctionsForm.new(model: proceeding)
         end
       end
 
       def emergency_defaults_forms
-        proceedings.map do |proceeding|
+        proceedings.filter_map do |proceeding|
           next unless proceeding.uses_emergency_certificate?
 
           Proceedings::EmergencyDefaultsForm.new(model: proceeding)
+        end
+      end
+
+      def emergency_level_of_service_forms
+        proceedings.filter_map do |proceeding|
+          Proceedings::EmergencyLevelOfServiceForm.new(model: proceeding) unless proceeding.accepted_emergency_defaults?
         end
       end
     end
