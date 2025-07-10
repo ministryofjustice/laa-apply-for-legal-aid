961077a06c255793b48309f98968d0fb2842a095
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2025-07-10 18:02:14 +0100

    AP-6094 add specs

--- app/controllers/providers/no_national_insurance_numbers_controller.rb
+++ app/controllers/providers/no_national_insurance_numbers_controller.rb
@@ -2,6 +2,7 @@ module Providers
   class NoNationalInsuranceNumbersController < ProviderBaseController
     include ApplicantDetailsCheckable
     include BenefitCheckSkippable
+    include DWPOutcomeHelper
 
     def show
       @applicant = legal_aid_application.applicant
@@ -9,6 +10,7 @@ module Providers
       if @applicant.national_insurance_number?
         redirect_to providers_legal_aid_application_dwp_result_path(legal_aid_application)
       else
+        reset_confirm_dwp_status(legal_aid_application)
         details_checked! unless details_checked?
         mark_as_benefit_check_skipped!("no_national_insurance_number")
         render :show
@@ -16,6 +18,7 @@ module Providers
     end
 
     def update
+      update_confirm_dwp_status(legal_aid_application, true)
       continue_or_draft
     end
   end
