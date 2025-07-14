fcead669b4bacefca643849bc9f9c51b60ec6a65
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2025-07-14 10:14:40 +0100

    address pr comments

--- spec/services/task_status/dwp_outcome_spec.rb
+++ spec/services/task_status/dwp_outcome_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe TaskStatus::DWPOutcome do
-  subject(:task_status) { described_class.new(application).call }
-
   describe "#call" do
+    subject(:task_status) { described_class.new(application).call }
+
     # TODO: add this spec in when check your answers validation is available
     # context "when check_your_answers is not completed" do
     # let(:application) { create(:application) }
@@ -16,25 +16,19 @@ RSpec.describe TaskStatus::DWPOutcome do
     context "with `confirm_dwp_result=nil`" do
       let(:application) { create(:application) }
 
-      it "returns the status of :not_started" do
-        expect(task_status.value).to eq(:not_started)
-      end
+      it { is_expected.to be_not_started }
     end
 
     context "with `confirm_dwp_result=false`" do
       let(:application) { create(:application, confirm_dwp_result: false) }
 
-      it "returns the status of :in_progress" do
-        expect(task_status.value).to eq(:in_progress)
-      end
+      it { is_expected.to be_in_progress }
     end
 
     context "with `confirm_dwp_result=true`" do
       let(:application) { create(:application, confirm_dwp_result: true) }
 
-      it "returns the status of :completed" do
-        expect(task_status.value).to eq(:completed)
-      end
+      it { is_expected.to be_completed }
     end
   end
 end
