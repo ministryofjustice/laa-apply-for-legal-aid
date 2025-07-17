2fc32b4f58a4b76b52664edaf9d4137877381425
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-07-17 15:33:33 +0100

    AP-6125: Remove authentication from application controller
    Rely on authentication on provider controller so other pages remain public.
    Remove Omniauth path helper

--- app/services/flow/steps/citizen_start/true_layer_step.rb
+++ app/services/flow/steps/citizen_start/true_layer_step.rb
@@ -2,7 +2,7 @@ module Flow
   module Steps
     module CitizenStart
       TrueLayerStep = Step.new(
-        path: ->(_) { FlowSteps.omniauth_login_start_path(:true_layer) },
+        path: ->(_) { "/auth/true_layer" },
       )
     end
   end
