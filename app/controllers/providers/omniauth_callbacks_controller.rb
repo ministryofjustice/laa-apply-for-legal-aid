efa8b6413300c509692add3207e0f61647f066aa
Colin Bruce <colin.bruce1@justice.gov.uk>
2025-07-24 14:56:35 +0100

    AP-6125: Switch the sign_in handling (#7929)
    Add a new, explicit, providers/sign_in path
    Remove the new, unauthenticated_root_path and replace the test expectations for it
    
    Co-authored-by: ColinBruce <colin.bruce@digital.justice.gov.uk>

--- app/controllers/providers/omniauth_callbacks_controller.rb
+++ app/controllers/providers/omniauth_callbacks_controller.rb
@@ -10,14 +10,14 @@ module Providers
       else
         flash[:notice] = I18n.t "devise.omniauth_callbacks.unauthorised"
         Rails.logger.error "Couldn't login user"
-        redirect_back(fallback_location: unauthenticated_root_path, allow_other_host: false)
+        redirect_back(fallback_location: root_path, allow_other_host: false)
       end
     end
 
     def failure
       flash[:alert] = I18n.t "devise.omniauth_callbacks.failure"
       Rails.logger.error "omniauth error authenticating a user!"
-      redirect_to unauthenticated_root_path
+      redirect_to root_path
     end
 
   protected
