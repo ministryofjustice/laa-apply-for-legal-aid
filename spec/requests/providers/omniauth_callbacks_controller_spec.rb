0c272b495fcfff115255174f1daedefb542ba6e0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-07-29 09:06:05 +0100

    AP-6125: Add tests for the new controller methods

---
+++ spec/requests/providers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Providers::OmniauthCallbacksController do
+  around do |example|
+    OmniAuth.config.test_mode = true
+    example.run
+    OmniAuth.config.mock_auth[:entra_id] = nil
+    OmniAuth.config.test_mode = false
+  end
+
+  describe "GET /auth/entra_id/callback" do
+    subject(:get_request) do
+      get provider_entra_id_omniauth_callback_path
+    end
+
+    it "redirects to provider confirm_office path" do
+      expect(get_request).to redirect_to(providers_confirm_office_path)
+    end
+
+    context "when the user is unknown" do
+      before do
+        OmniAuth.config.mock_auth[:entra_id] = nil
+      end
+
+      it "redirects to error page" do
+        expect(get_request).to redirect_to(root_path)
+      end
+
+      it "displays failure information" do
+        get_request
+        follow_redirect!
+        expect(response.body).to include("Could not authorise you! Ask an admin for access")
+      end
+    end
+  end
+end
