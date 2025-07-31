17f79f6970c84be637087448efffce18eb3dfece
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-07-31 17:00:38 +0100

    ap-6168: create a migration to so add a database table for schedule details

---
+++ db/migrate/20250731075330_create_schedules.rb
@@ -0,0 +1,15 @@
+class CreateSchedules < ActiveRecord::Migration[8.0]
+  def change
+    create_table :schedules, id: :uuid do |t|
+      t.belongs_to :office, foreign_key: true, null: false, type: :uuid
+      t.string :area_of_law
+      t.string :category_of_law
+      t.string :authorisation_status
+      t.string :status
+      t.date :start_date
+      t.date :end_date
+      t.boolean :cancelled
+      t.timestamps
+    end
+  end
+end
