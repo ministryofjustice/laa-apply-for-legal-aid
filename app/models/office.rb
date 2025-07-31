cd7edd77392c7f23980ed3f5def7d2c7cd53133f
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-07-31 17:00:38 +0100

    ap-6168: create a migration to so add a database table for schedule details

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,4 +2,5 @@ class Office < ApplicationRecord
   belongs_to :firm
   has_many :legal_aid_applications
   has_and_belongs_to_many :providers
+  has_many :schedules
 end
---
+++ app/models/schedule.rb
@@ -0,0 +1,3 @@
+class Schedule < ApplicationRecord
+  belongs_to :office
+end
