021c349e5d49e4642af127afa473bc7bb9bb533d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-07-31 09:59:16 +0100

    AP-6125: Update omniauth
    Switch to new settings for Omniauth config and rename strategy so it has
    an explict name to avoid confusion with underlying strategy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,8 +6,8 @@
 # Many of these configuration options can be set straight in your model.
 
 Devise.setup do |config|
-  require Rails.root.join "app/lib/omni_auth/strategies/entra_id_oidc"
-  OmniAuth::Strategies::EntraIdOidc.mock_auth if ENV["OMNIAUTH_ENTRAID_MOCK_AUTH"] == "true"
+  require Rails.root.join "app/lib/omni_auth/strategies/silas"
+  OmniAuth::Strategies::Silas.mock_auth if ENV["OMNIAUTH_ENTRAID_MOCK_AUTH"] == "true"
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
