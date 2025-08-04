8459db23601c099b8892898a2fcc6f4bd4fab4e9
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-08-04 13:09:55 +0100

    AP-6125: Update controllers and tests
    Add new controllers, remove some and rename others

---
+++ app/controllers/providers/sessions_controller.rb
@@ -0,0 +1,15 @@
+module Providers
+  class SessionsController < Devise::SessionsController
+    def destroy
+      session["signed_out"] = true
+      session["feedback_return_path"] = destroy_provider_session_path
+      super
+    end
+
+  protected
+
+    def after_sign_out_path_for(_resource_or_scope)
+      new_feedback_path
+    end
+  end
+end
