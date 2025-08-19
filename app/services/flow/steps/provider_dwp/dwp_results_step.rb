2dd3f96ac55c5a3686d7f9c70bd6afe68ecfb53c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-08-19 09:42:59 +0100

    AP-6144: Add new dwp_results controller

---
+++ app/services/flow/steps/provider_dwp/dwp_results_step.rb
@@ -0,0 +1,13 @@
+module Flow
+  module Steps
+    module ProviderDWP
+      DWPResultsStep = Step.new(
+        path: ->(application) { Steps.urls.providers_legal_aid_application_dwp_result_path(application) },
+        forward: lambda do |application|
+          application.change_state_machine_type("NonPassportedStateMachine")
+          :dwp_overrides
+        end,
+      )
+    end
+  end
+end
