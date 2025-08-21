d8c543fae55f9397c1514287d75d9fd53686f442
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-08-21 09:42:47 +0100

    AP-6203: Fix spacing

--- db/migrate/20250617140126_add_omni_auth_columns_to_providers.rb
+++ db/migrate/20250617140126_add_omni_auth_columns_to_providers.rb
@@ -1,5 +1,5 @@
 class AddOmniAuthColumnsToProviders < ActiveRecord::Migration[8.0]
-    def change
+  def change
     # Omniauthable, custom devise
     add_column :providers, :auth_provider, :string, null: false, default: ""
     add_column :providers, :auth_subject_uid, :string
