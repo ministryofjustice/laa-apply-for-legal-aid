62a2c5b29582d7d0874e3bcbf423a418b258cf40
RoseSAK <rose.azadkhan@digital.justice.gov.uk>
2025-09-04 17:59:21 +0100

    AP-5620 Add contact made question to merits
    Add form spec, request spec, and controller spec

--- spec/factories/incidents.rb
+++ spec/factories/incidents.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :incident, class: "ApplicationMeritsTask::Incident" do
     told_on { Faker::Date.backward(days: 90) }
     occurred_on { Faker::Date.backward(days: 90) - 1.year }
+    first_contact_date { Faker::Date.backward(days: 90) }
     details { Faker::Lorem.paragraph }
     legal_aid_application
   end
