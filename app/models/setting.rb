7f98d0acaeea2a47c11dce8856d47c3307bd60cc
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-09-10 14:22:21 +0100

    AP-6236: Add out_of_hours? to the Setting class
    This ise called by the application_controller and it currently
    implements a rule that returns a boolean value when the time is
    is not 7-7 Mon-Fri
    
    The skip value is set to true in the citizen_controller so that
    citizen requests will be allowed through

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -34,4 +34,11 @@ class Setting < ApplicationRecord
   def self.setting
     Setting.first || Setting.create!
   end
+
+  def self.out_of_hours?
+    # Provider access to the service only required between 7AM and 7PM on weekdays
+    # Citizen access is required 24/7
+
+    (Time.zone.now.hour < 7 || Time.zone.now.hour >= 19) || Time.zone.now.on_weekend?
+  end
 end
