c6a5a23bb3176ac315e670f785bcf2a340ad9cb6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-09-15 14:07:23 +0100

    AP-6236: Refactor skip override list
    Allow for simpler extension in the future if
    more pages are needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ private
 
   def skip_out_of_hours?
     # these are the only pages on the citizen path that are not namespaced as /citizen
-    return true if request.path.include?("auth/true_layer") || request.path.include?("error/assessment_already_completed")
+    return true if non_standard_citizen_path?
 
     false
   end
@@ -34,5 +34,12 @@ private
   def convert_date_params(params)
     # gsub finds ([digit]i) and replaces with _[digit]i
     params.transform_keys! { |key| key.gsub(/\((\di)\)/, '_\\1') }
+  end
+
+  def non_standard_citizen_path?
+    [
+      request.path.include?("auth/true_layer"),
+      request.path.include?("error/assessment_already_completed"),
+    ].any?
   end
 end
