f05b6ace411f7467a53853e47da8c0eb9bb5323c
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-09-16 11:29:07 +0100

    sp-5739: encrypt auth_subject_uid

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,13 @@
 class AdminUser < ApplicationRecord
+  encrypts :auth_subject_uid, deterministic: true
+
   devise(
     :database_authenticatable, :trackable, :lockable,
     authentication_keys: [:username], unlock_strategy: :time
   )
 
   def self.from_omniauth(auth)
+    # Find by auth.uid if admin has previously logged in with entra, else email
     admin = find_by(auth_subject_uid: auth.uid) || find_by(email: auth.info.email.downcase)
 
     if admin
