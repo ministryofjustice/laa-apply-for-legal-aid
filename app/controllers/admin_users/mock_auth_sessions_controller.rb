ee74ac7f647bec8e90b4af0492a04bbaac4b58e2
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-09-18 09:14:37 +0100

    AP-5740: Share logic for mock auth controllers

--- app/controllers/admin_users/mock_auth_sessions_controller.rb
+++ app/controllers/admin_users/mock_auth_sessions_controller.rb
@@ -1,25 +1,23 @@
 module AdminUsers
-  class MockAuthSessionsController < Devise::SessionsController
-    def create
-      if mock_auth_session_params[:email] == Rails.configuration.x.admin_omniauth.mock_username &&
-          mock_auth_session_params[:password] == Rails.configuration.x.admin_omniauth.mock_password
-        @admin_user = AdminUser.from_omniauth(auth_data)
-        flash[:notice] = I18n.t "devise.sessions.signed_in"
-        sign_in_and_redirect @admin_user, event: :authentication
-      else
-        flash[:notice] = I18n.t("devise.failure.invalid", authentication_keys: "email")
-        render :new
-      end
+  class MockAuthSessionsController < MockAuthSessionsBaseController
+  protected
+
+    def mock_username
+      Rails.configuration.x.admin_omniauth.mock_username
     end
 
-  protected
+    def mock_password
+      Rails.configuration.x.admin_omniauth.mock_password
+    end
 
     def mock_auth_session_params
       params.expect(admin_user: %i[email password])
     end
 
-    # TODO: we could potentially use a hash of auth data then call auth_data[mock_auth_session_params[:email]] to
-    # allow for multiple different mock users/providers here.
+    def user
+      @user = AdminUser.from_omniauth(auth_data)
+    end
+
     def auth_data
       OmniAuth::Strategies::AdminLogin.mock_auth
     end
