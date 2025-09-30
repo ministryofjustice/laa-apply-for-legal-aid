18d70c27c78574314a152e74981bf854d0e8494a
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-09-30 13:53:34 +0100

    ap-6094: rename confirm_dwp_result to dwp_result_confirmed

--- db/migrate/20250709140406_add_confirm_dwp_result_to_legal_aid_application.rb
+++ db/migrate/20250709140406_add_confirm_dwp_result_to_legal_aid_application.rb
@@ -1,5 +1,5 @@
 class AddConfirmDWPResultToLegalAidApplication < ActiveRecord::Migration[8.0]
   def change
-    add_column :legal_aid_applications, :confirm_dwp_result, :text
+    add_column :legal_aid_applications, :dwp_result_confirmed, :boolean
   end
 end
--- spec/services/task_status/dwp_outcome_spec.rb
+++ spec/services/task_status/dwp_outcome_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe TaskStatus::DWPOutcome do
   describe "#call" do
     subject(:task_status) { described_class.new(application).call }
 
-    let(:application) { create(:application, :with_complete_applicant, confirm_dwp_result:) }
-    let(:confirm_dwp_result) { nil }
+    let(:application) { create(:application, :with_complete_applicant, dwp_result_confirmed:) }
+    let(:dwp_result_confirmed) { nil }
 
     context "when check_your_answers is not completed" do
       before do
@@ -22,18 +22,18 @@ RSpec.describe TaskStatus::DWPOutcome do
         application.save!
       end
 
-      context "when confirm_dwp_result is nil" do
+      context "when dwp_result_confirmed is nil" do
         it { is_expected.to be_not_started }
       end
 
-      context "when confirm_dwp_result is false" do
-        let(:confirm_dwp_result) { false }
+      context "when dwp_result_confirmed is false" do
+        let(:dwp_result_confirmed) { false }
 
         it { is_expected.to be_in_progress }
       end
 
-      context "when confirm_dwp_result is true" do
-        let(:confirm_dwp_result) { true }
+      context "when dwp_result_confirmed is true" do
+        let(:dwp_result_confirmed) { true }
 
         it { is_expected.to be_completed }
       end
