39e3213f327e07fedb2fb1431b140846e4c302b5
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-10-09 08:31:24 +0100

    ap-6094: update dwp_outcome_helper as per pr review comments

--- app/helpers/dwp_outcome_helper.rb
+++ app/helpers/dwp_outcome_helper.rb
@@ -1,16 +1,19 @@
 module DWPOutcomeHelper
-  def reset_confirm_dwp_status!(legal_aid_application)
-    legal_aid_application.update!(dwp_result_confirmed: nil)
-    legal_aid_application.save!
+  def reset_confirm_dwp_status!(application)
+    dwp_result_confirmation!(application, nil)
   end
 
-  def checking_dwp_status!(legal_aid_application)
-    legal_aid_application.update!(dwp_result_confirmed: false)
-    legal_aid_application.save!
+  def checking_dwp_status!(application)
+    dwp_result_confirmation!(application, false)
   end
 
-  def confirm_dwp_status_correct!(legal_aid_application)
-    legal_aid_application.update!(dwp_result_confirmed: true)
-    legal_aid_application.save!
+  def confirm_dwp_status_correct!(application)
+    dwp_result_confirmation!(application, true)
+  end
+
+private
+
+  def dwp_result_confirmation!(application, val)
+    application.update!(dwp_result_confirmed: val)
   end
 end
