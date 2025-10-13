06499563339058cf2d9c2ad3e2ba280bab66d034
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-10-13 10:02:02 +0100

    ap-6091: update make link presenter

--- app/presenters/task/make_link.rb
+++ app/presenters/task/make_link.rb
@@ -1,7 +1,17 @@
 module Task
   class MakeLink < Base
     def path
-      Flow::Steps::LinkedApplications::MakeLinkStep.path.call(application)
+      if application.lead_linked_application.present?
+        if application.lead_linked_application.lead_application_id.nil?
+          Flow::Steps::LinkedApplications::FindLinkStep.path.call(application)
+        elsif !application.lead_linked_application.confirm_link
+          Flow::Steps::LinkedApplications::ConfirmLinkStep.path.call(application)
+        elsif application.lead_linked_application.link_type_code == "FC_LEAD" && !application.linked_application_completed
+          Flow::Steps::LinkedApplications::CopyStep.path.call(application)
+        end
+      else
+        Flow::Steps::LinkedApplications::MakeLinkStep.path.call(application)
+      end
     end
   end
 end
--- spec/presenters/task/make_link_spec.rb
+++ spec/presenters/task/make_link_spec.rb
@@ -4,12 +4,56 @@ RSpec.describe Task::MakeLink do
   subject(:instance) { described_class.new(application, name: "make_link") }
 
   let(:application) { create(:legal_aid_application) }
+  let(:lead_linked_application) { nil }
+
+  before { application.update!(lead_linked_application:) }
 
   describe "#path" do
     include Rails.application.routes.url_helpers
 
-    it "returns the route to first step of the task list item" do
-      expect(instance.path).to eql providers_legal_aid_application_link_application_make_link_path(application)
+    context "when there is no lead linked application" do
+      it "returns the route to first step of the task list item" do
+        expect(instance.path).to eql providers_legal_aid_application_link_application_make_link_path(application)
+      end
+    end
+
+    context "when there is a lead linked application" do
+      let(:lead_linked_application) do
+        create(:linked_application,
+               link_type_code:,
+               confirm_link:,
+               associated_application_id: application.id,
+               lead_application_id:)
+      end
+      let(:lead_application_id) { nil }
+      let(:confirm_link) { nil }
+      let(:link_type_code) { nil }
+
+      context "when the provider has not selected an application to link to" do
+        it "returns the route to first step of the task list item" do
+          expect(instance.path).to eql providers_legal_aid_application_link_application_find_link_application_path(application)
+        end
+      end
+
+      context "when the provider has not confirmed the link" do
+        let(:lead_application) { create(:legal_aid_application) }
+        let(:lead_application_id) { lead_application.id }
+
+        it "returns the route to first step of the task list item" do
+          expect(instance.path).to eql providers_legal_aid_application_link_application_confirm_link_path(application)
+        end
+      end
+
+      context "when the link has been confirmed and family linking" do
+        let(:lead_application) { create(:legal_aid_application) }
+        let(:lead_application_id) { lead_application.id }
+        let(:confirm_link) { true }
+        let(:link_type_code) { "FC_LEAD" }
+
+        it "returns the route to first step of the task list item" do
+          expect(instance.path).to eql providers_legal_aid_application_link_application_copy_path(application)
+        end
+      end
     end
   end
 end
