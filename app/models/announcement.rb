61720311565b277902f97118418f08916066f33d
ColinBruce <colin.bruce1@justice.gov.uk>
2025-10-28 12:11:54 +0000

    AP-6154: Update validations for Announcement model

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,7 +1,11 @@
 class Announcement < ApplicationRecord
   enum :display_type, { gov_uk: 0, moj: 1 }
 
-  validates :heading, :start_at, :end_at, presence: true
+  has_many :provider_dismissed_announcements, dependent: :delete_all
+
+  validates :heading, presence: true, if: ->(announcement) { announcement.display_type == "gov_uk" }
+  validates :body, presence: true, if: ->(announcement) { announcement.display_type == "moj" }
+  validates :display_type, :start_at, :end_at, presence: true
   validates :link_url, presence: true, if: ->(announcement) { announcement.link_display.present? }
   validates :link_display, presence: true, if: ->(announcement) { announcement.link_url.present? }
   validates :start_at, :end_at, date_time: ->(field) { announcement.send(field).present? }
