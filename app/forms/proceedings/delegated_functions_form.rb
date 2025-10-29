3d6ede7be3aa205a208a68737adc099074767372
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2025-10-29 16:38:58 +0000

    AP-6303: Fix delegated function reset changes
    Resetting was being done whenever checking answers, regardless, but
    the logic should be to reset if the delegated functions used question
    is answered differently only. This is because scope limitations and other
    subsequent questions fundamentally differ if delegated functions are used
    or not.

--- app/forms/proceedings/delegated_functions_form.rb
+++ app/forms/proceedings/delegated_functions_form.rb
@@ -15,7 +15,11 @@ module Proceedings
               },
               if: :used_delegated_functions_on_required?
 
+    # NOTE: position matters. We set the values of these "dirty/changed" variables before any other callbacks so that subsequent callbacks can use them reliably
+    set_callback :save, :before, :used_delegated_functions_changed, :used_delegated_functions_on_changed
+
     set_callback :save, :before, :sync_used_delegated_functions_dates
+    set_callback :save, :after, :reset_proceeding_loop, if: :used_delegated_functions_changed?
 
   private
 
@@ -26,12 +30,54 @@ module Proceedings
     end
 
     def sync_used_delegated_functions_dates
+      return unless used_delegated_functions_or_on_changed?
+
       if used_delegated_functions&.to_s == "true"
         attributes[:used_delegated_functions_reported_on] = Time.zone.today
       else
         attributes[:used_delegated_functions_reported_on] = nil
         attributes[:used_delegated_functions_on] = nil
       end
+    end
+
+    def used_delegated_functions_or_on_changed?
+      used_delegated_functions_changed? || used_delegated_functions_on_changed?
+    end
+
+    # setter
+    def used_delegated_functions_changed
+      @used_delegated_functions_changed ||= used_delegated_functions.to_s != model.used_delegated_functions.to_s
+    end
+
+    # getter
+    def used_delegated_functions_changed?
+      @used_delegated_functions_changed
+    end
+
+    # setter
+    def used_delegated_functions_on_changed
+      @used_delegated_functions_on_changed ||= used_delegated_functions_on&.to_date != model.used_delegated_functions_on&.to_date
+    end
+
+    # getter
+    def used_delegated_functions_on_changed?
+      @used_delegated_functions_on_changed
+    end
+
+    def reset_proceeding_loop
+      model.update!(
+        accepted_emergency_defaults: nil,
+        accepted_substantive_defaults: nil,
+      )
+
+      model.legal_aid_application.update!(
+        emergency_cost_override: nil,
+        emergency_cost_requested: nil,
+        emergency_cost_reasons: nil,
+        substantive_cost_override: nil,
+        substantive_cost_requested: nil,
+        substantive_cost_reasons: nil,
+      )
     end
   end
 end
