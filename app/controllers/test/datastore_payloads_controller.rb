35396cfca28f2aae75ebf2ab80288e6624598f2f
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2025-11-11 13:40:11 +0000

    AP-6337: Add viewable json payload endpoint
    For development purposes only

---
+++ app/controllers/test/datastore_payloads_controller.rb
@@ -0,0 +1,32 @@
+# :nocov:
+module Test
+  # This controller is used to render the payload that would be sent to the datastore.
+  #
+  # The test_datastore_payloads_xxx_path(s) route are defined in development only
+  #
+
+  class DatastorePayloadsController < ApplicationController
+    before_action :legal_aid_application
+
+    def application_as_json
+      respond_to do |format|
+        format.json { render json: LegalAidApplicationJsonBuilder.build(legal_aid_application).to_json }
+        format.all { render plain: "Not found", status: :not_found }
+      end
+    end
+
+    def generated_json
+      respond_to do |format|
+        format.json { render json: Datastore::PayloadGenerator.call(legal_aid_application).to_json }
+        format.all { render plain: "Not found", status: :not_found }
+      end
+    end
+
+  private
+
+    def legal_aid_application
+      @legal_aid_application ||= LegalAidApplication.find(params[:legal_aid_application_id])
+    end
+  end
+end
+# :nocov:
--- config/routes.rb
+++ config/routes.rb
@@ -456,6 +456,12 @@ Rails.application.routes.draw do
     end
   end
 
+  # development only routes for aiding development/visualisation
+  if Rails.env.development?
+    get "test/datastore_payloads/:legal_aid_application_id/application_as_json", to: "test/datastore_payloads#application_as_json", as: "test_datastore_payloads_application_as_json", defaults: { format: :json }
+    get "test/datastore_payloads/:legal_aid_application_id/generated_json", to: "test/datastore_payloads#generated_json", as: "test_datastore_payloads_generated_json", defaults: { format: :json }
+  end
+
   get "/submission_feedback/:application_ref", to: "feedback#submission"
   get "test/trapped_error", to: "test/generate_error#trapped_error"
   get "test/untrapped_error", to: "test/generate_error#untrapped_error"
