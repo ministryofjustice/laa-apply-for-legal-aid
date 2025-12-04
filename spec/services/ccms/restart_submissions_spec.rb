69d5ec9cb87b4389357444d638243e9d277d0798
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2025-12-04 20:10:00 +0000

    Fix: order restarted submissions by merits_submitted_at
    when a significant number o fapplications have been paused, including
    lead and associated applications, restarting them in created_at order
    can lead to associated applications being restarted before their leads.
    
    This change orders the restarted applications by merits_submitted_at
    to ensure that lead applications are always restarted before their
    associated applications, and applications are submitted to SOA in the
    order they were submitted on our service.

--- spec/services/ccms/restart_submissions_spec.rb
+++ spec/services/ccms/restart_submissions_spec.rb
@@ -13,16 +13,24 @@ RSpec.describe CCMS::RestartSubmissions do
   end
 
   context "when two applications are paused" do
+    let(:laa1) { create(:legal_aid_application, :submission_paused, created_at: 3.days.ago, merits_submitted_at: 2.days.ago, application_ref: "AAA111") }
+    let(:laa2) { create(:legal_aid_application, :submission_paused, created_at: 4.days.ago, merits_submitted_at: 1.day.ago, application_ref: "BBB222") }
+
     before do
-      create_list(:legal_aid_application, 2, :submission_paused)
+      laa1
+      laa2
     end
 
     it { is_expected.to eql "2 CCMS submissions restarted" }
 
     it "changes the states to submitting_assessment" do
+      expect { restart_submissions }.to change { laa1.reload.state }.from("submission_paused").to("generating_reports")
+      expect(laa2.reload.state).to eql "generating_reports"
+    end
+
+    it "processes the earliest submitted application first (despite being created later)" do
       restart_submissions
-      expect(LegalAidApplication.first.reload.state).to eql "generating_reports"
-      expect(LegalAidApplication.last.reload.state).to eql "generating_reports"
+      expect(laa1.updated_at).to be < laa2.updated_at
     end
   end
 end
