cc04c26efb7d0767f000746bf6eacd50bf7917fa
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-01-06 14:26:22 +0000

    AP-6504: rerecord bank holiday cassettes against faraday based retriever
    Since we changed the BankHolidayRetriever to use Faraday instead of Net::HTTP,
    the recorded VCR cassettes need to be updated to reflect the different request
    headers and structure.

--- spec/services/working_day_calculator_spec.rb
+++ spec/services/working_day_calculator_spec.rb
@@ -1,20 +1,22 @@
 require "rails_helper"
 
 RSpec.describe WorkingDayCalculator, vcr: { cassette_name: "gov_uk_bank_holiday_api" } do
-  let(:day) { Date.parse("17-Dec-2018") }
-  let(:three_working_days_later) { Date.parse("20-Dec-2018") } # No skip
-  let(:five_working_days_later) { Date.parse("24-Dec-2018") } # Weekend skip
-  let(:six_working_days_later) { Date.parse("27-Dec-2018") } # Weekend and bank holiday skip
-  let(:two_working_days_before) { Date.parse("13-Dec-2018") } # Go backwards by two working days and skip weekend
+  let(:day) { Date.parse("16-Dec-2025") }
+  let(:working_day) { described_class.new(day) }
+
+  let(:three_working_days_later) { Date.parse("19-Dec-2025") } # No skip
+  let(:four_working_days_later) { Date.parse("22-Dec-2025") } # Weekend skip
+  let(:seven_working_days_later) { Date.parse("29-Dec-2025") } # Weekend and bank holiday skip
+  let(:two_working_days_before) { Date.parse("12-Dec-2025") } # Go backwards by two working days and skip weekend
+
   let(:number_to_target_dates) do
     {
       3 => three_working_days_later,
-      5 => five_working_days_later,
-      6 => six_working_days_later,
+      4 => four_working_days_later,
+      7 => seven_working_days_later,
       -2 => two_working_days_before,
     }
   end
-  let(:working_day) { described_class.new(day) }
 
   describe "#working_days_from_now" do
     it "returns expected dates" do
@@ -36,7 +38,7 @@ RSpec.describe WorkingDayCalculator, vcr: { cassette_name: "gov_uk_bank_holiday_
     let(:day) { Time.zone.today }
     let(:number) { 12 }
 
-    it "returns the date `n` days form today" do
+    it "returns the date `n` days from today" do
       result = described_class.working_days_from_now(number)
       expected = working_day.add_working_days(number)
       expect(result).to eq(expected)
