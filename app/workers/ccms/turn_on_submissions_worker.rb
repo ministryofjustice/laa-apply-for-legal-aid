00d22de7392cab349886b6700cc1cda1f8ae8106
ColinBruce <6757677+colinbruce@users.noreply.github.com>
2026-01-15 07:26:06 +0000

    AP-6513: Refactor TurnOnSubmissionsWorker
    Extract the interval to a constant to reduce repetition and make
    future updates easier by making it harder to overlook the shared variable

--- app/workers/ccms/turn_on_submissions_worker.rb
+++ app/workers/ccms/turn_on_submissions_worker.rb
@@ -3,15 +3,17 @@ module CCMS
     include Sidekiq::Worker
     include Sidekiq::Status::Worker
 
+    SUBMISSION_INTERVAL = 5
+
     def perform
       # get applications where state is submission_paused
       applications = LegalAidApplication.joins(:state_machine).where(state_machine_proxies: { aasm_state: "submission_paused" }).order(:merits_submitted_at)
       if applications.empty?
         Rails.logger.info "CCMS::RestartSubmissionsWorker - No paused submissions found"
       else
-        Rails.logger.info "CCMS::RestartSubmissionsWorker - #{applications.size} submissions to restart, expected to take #{Time.zone.at(applications.size * 5).gmtime.strftime('%T')}"
+        Rails.logger.info "CCMS::RestartSubmissionsWorker - #{applications.size} submissions to restart, expected to take #{Time.zone.at(applications.size * SUBMISSION_INTERVAL).gmtime.strftime('%T')}"
         applications.each_with_index do |application, iteration|
-          RestartSubmissionWorker.perform_in(iteration * 5, application.id)
+          RestartSubmissionWorker.perform_in(iteration * SUBMISSION_INTERVAL, application.id)
         end
         Rails.logger.info "CCMS::RestartSubmissionsWorker - all submissions restarted"
       end
