1b7990bd0dcaf308dbc4668ec1ad57976e46e66f
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-01-29 17:46:17 +0000

    AP-6558: Amend datastore payload for status
    In staging it is still expecting to receive "IN_PROGRESS" and "SUBMITTED" rather than
    "APPLICATION_IN_PROGRESS" and "APPLICATION_SUBMITTED". Plus the attribute is called "status"
    not "application_status".
    
    This may change in future but for now we need to amend the payload we send to datastore to
    match what staging expects.

--- spec/services/datastore/constants_spec.rb
+++ spec/services/datastore/constants_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Datastore::Constants do
     subject(:status_value) { described_class.status_value(key) }
 
     it "returns the expected value for :in_progress" do
-      expect(described_class.status_value(:in_progress)).to eq("APPLICATION_IN_PROGRESS")
+      expect(described_class.status_value(:in_progress)).to eq("IN_PROGRESS")
     end
 
     it "returns the expected value for :submitted" do
-      expect(described_class.status_value(:submitted)).to eq("APPLICATION_SUBMITTED")
+      expect(described_class.status_value(:submitted)).to eq("SUBMITTED")
     end
 
     it "returns nil for :unknown" do
