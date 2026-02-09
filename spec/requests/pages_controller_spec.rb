e2d4a62331c2fb65aab7ba6c9221202d13123ce3
Katharine Ahern <katharine.ahern@justice.gov.uk>
2026-02-09 15:07:26 +0000

    Bring service down for maintenance

--- spec/requests/pages_controller_spec.rb
+++ spec/requests/pages_controller_spec.rb
@@ -54,8 +54,7 @@ RSpec.describe PagesController, :clamav do
 
       it "has expected content" do
         expect(page)
-          .to have_content("Sorry, there is a problem")
-          .and have_link("Online Support (opens in new tab)", href: "https://legalaidlearning.justice.gov.uk/online-support-2/")
+          .to have_content("Sorry the service is unavailable")
       end
     end
 
