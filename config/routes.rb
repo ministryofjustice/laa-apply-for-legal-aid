2dc434b3970c34ae0e7df125e67cca70f7c47009
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-03-05 13:59:02 +0000

    AP-6323: Add multiple employments interrupt

--- config/routes.rb
+++ config/routes.rb
@@ -384,6 +384,7 @@ Rails.application.routes.draw do
       namespace :partners do
         get "/employed_but_no_hmrc_data_interrupt", to: "employed_but_no_hmrc_data_interrupts#show", as: "employed_but_no_hmrc_data_interrupt"
         get "/hmrc_unavailable_interrupt", to: "hmrc_unavailable_interrupts#show", as: "hmrc_unavailable_interrupt"
+        get "/multiple_employments_interrupts", to: "multiple_employments_interrupts#show", as: "multiple_employments_interrupt"
         get "/no_nino_interrupt", to: "no_nino_interrupts#show", as: "no_nino_interrupt"
         resource :details, only: %i[show update]
         resource :about_financial_means, only: %i[show update]
