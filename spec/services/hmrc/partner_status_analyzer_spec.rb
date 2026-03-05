0cd6bb154a7ac2edb2c359cecb3a5c0fc5a37cee
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-03-05 16:35:21 +0000

    AP-6323: Update routing

--- spec/services/hmrc/partner_status_analyzer_spec.rb
+++ spec/services/hmrc/partner_status_analyzer_spec.rb
@@ -3,72 +3,90 @@ require "rails_helper"
 module HMRC
   RSpec.describe PartnerStatusAnalyzer do
     describe ".call" do
-      subject(:service_call) { described_class.call(laa) }
-      before { laa.partner.update!(employed: true) if partner_employed }
+      before do
+        laa.partner.update!(employed: true) if partner_employed
+        allow(partner).to receive(:hmrc_responses).and_return(hmrc_response)
+      end
 
-      let(:feature_flag) { true }
+      subject(:service_call) { described_class.call(laa) }
+
       let(:laa) { create(:legal_aid_application, :with_applicant_and_partner, :with_transaction_period) }
       let(:partner) { laa.partner }
-      let(:partner_employed) { true }
+      let(:hmrc_response) { [build_stubbed(:hmrc_response)] }
 
-      context "and partner not employed and no eligible payments" do
+      context "when provider says partner is not employed" do
         let(:partner_employed) { false }
 
-        it "returns not employed" do
-          expect(service_call).to eq :partner_not_employed
+        context "and no eligible payments" do
+          it { is_expected.to eq :partner_not_employed_no_payments }
+        end
+
+        context "and non-eligible employment payments exist" do
+          before { create(:employment, :with_payments_before_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
+
+          it { is_expected.to eq :partner_not_employed_no_payments }
+        end
+
+        context "and partner has no national insurance number" do
+          let(:laa) { create(:legal_aid_application, :with_partner_no_nino, :with_transaction_period) }
+
+          it { is_expected.to eq :partner_not_employed_no_nino }
+        end
+
+        context "but eligible employment payments exist" do
+          before { create(:employment, :with_payments_in_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
+
+          it { is_expected.to eq :partner_unexpected_employment_data }
+        end
+
+        context "and hmrc data is unavailable" do
+          let(:hmrc_response) { [] }
+
+          it { is_expected.to eq :partner_not_employed_hmrc_unavailable }
         end
       end
 
-      context "with partner not employed but eligible employment payments exist" do
-        let(:partner_employed) { false }
+      context "when provider says partner is employed" do
+        let(:partner_employed) { true }
 
-        before { create(:employment, :with_payments_in_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
+        context "and partner has no national insurance number" do
+          let(:laa) { create(:legal_aid_application, :with_partner_no_nino, :with_transaction_period) }
 
-        it "returns :unexpected_employment_data" do
-          expect(service_call).to eq :partner_unexpected_employment_data
+          it { is_expected.to eq :partner_employed_no_nino }
         end
-      end
 
-      context "with partner not employed and non-eligible employment payments exist" do
-        let(:partner_employed) { false }
+        context "and partner has multiple employments" do
+          before { create_list(:employment, 2, :with_payments_in_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
 
-        before { create(:employment, :with_payments_before_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
-
-        it "returns :unexpected_employment_data" do
-          expect(service_call).to eq :partner_not_employed
+          it { is_expected.to eq :partner_multiple_employments }
         end
-      end
 
-      context "and partner has multiple employments" do
-        before { create_list(:employment, 2, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
+        context "and non-eligible employment payments exist" do
+          before { create(:employment, :with_payments_before_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
 
-        it "returns partner_multiple_employments" do
-          expect(service_call).to eq :partner_multiple_employments
+          it { is_expected.to eq :partner_unexpected_no_employment_data }
         end
-      end
 
-      context "and partner has single employment" do
-        before { create(:employment, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
+        context "and partner has single eligible employment" do
+          before { create(:employment, :with_payments_in_transaction_period, legal_aid_application: laa, owner_id: partner.id, owner_type: partner.class) }
 
-        it "returns partner_hmrc_single_employment" do
-          expect(service_call).to eq :partner_single_employment
+          it { is_expected.to eq :partner_single_employment }
         end
-      end
 
-      context "but partner has no hmrc data" do
-        it "returns partner_no_hmrc_data" do
-          expect(service_call).to eq :partner_no_hmrc_data
+        context "and hmrc data is unavailable" do
+          let(:hmrc_response) { [] }
+
+          it { is_expected.to eq :partner_employed_hmrc_unavailable }
         end
       end
 
       context "and applicant has hmrc data but partner does not" do
         let(:applicant) { laa.applicant }
+        let(:partner_employed) { false }
 
-        before { create(:employment, legal_aid_application: laa, owner_id: applicant.id, owner_type: applicant.class) }
+        before { create(:employment, :with_payments_in_transaction_period, legal_aid_application: laa, owner_id: applicant.id, owner_type: applicant.class) }
 
-        it "returns partner_no_hmrc_data" do
-          expect(service_call).to eq :partner_no_hmrc_data
-        end
+        it { is_expected.to eq :partner_not_employed_no_payments }
       end
     end
   end
