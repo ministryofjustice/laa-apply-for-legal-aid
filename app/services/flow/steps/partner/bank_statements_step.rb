d50adc9c48eb197a1e37d18dd95d3ec63d72bda3
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-03-05 16:35:21 +0000

    AP-6323: Update routing

--- app/services/flow/steps/partner/bank_statements_step.rb
+++ app/services/flow/steps/partner/bank_statements_step.rb
@@ -6,14 +6,21 @@ module Flow
         forward: lambda do |application|
           status = HMRC::PartnerStatusAnalyzer.call(application)
           case status
-          when :partner_multiple_employments, :partner_no_hmrc_data
-            :partner_full_employment_details
-          when :partner_single_employment
-            :partner_employment_incomes
-          when :partner_unexpected_employment_data
-            :partner_unexpected_employment_incomes
-          when :partner_not_employed
+          when :partner_not_employed_no_nino, :partner_not_employed_hmrc_unavailable, :partner_not_employed_no_payments
             :partner_receives_state_benefits
+          when :partner_unexpected_employment_data
+            :partner_unemployed_but_hmrc_found_data_interrupts
+
+          when :partner_employed_hmrc_unavailable
+            :partner_hmrc_unavailable_interrupts
+          when :partner_employed_no_nino
+            :partner_no_nino_interrupts
+          when :partner_unexpected_no_employment_data
+            :partner_employed_but_no_hmrc_data_interrupts
+          when :partner_multiple_employments
+            :partner_multiple_employments_interrupts
+          when :partner_single_employment
+            :partner_single_employment_interrupts
           else
             raise "Unexpected hmrc status #{status.inspect}"
           end
