8f4a000a6f8b5fee6ec3c5669a15a7480fbe024b
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-03-12 12:10:31 +0000

    AP-6679: Amend Datastore/Decide opponent mappings
    Inline with agreed mappings for use by Datastore/Decide.
    
    We add an `opponent_type` field to map the `opposable_type` to a simple enum of "INDIVIDUAL" or "ORGANISATION",
    and we add `first_name`, `last_name`, and `organisation_name` fields to surface these values directly for use
    by Datastore/Decide without needing to parse the full `opposable` object.

--- app/builders/opponent_json_builder.rb
+++ app/builders/opponent_json_builder.rb
@@ -10,15 +10,45 @@ class OpponentJsonBuilder < BaseJsonBuilder
       opposable_id:,
       exists_in_ccms:,
       opposable:,
+
+      # Mappings used by Datastore/Decide
+      opponent_type:,
+      first_name:,
+      last_name:,
+      organisation_name:,
     }
   end
 
 private
 
+  # Enum for Datastore/Decide
+  def opponent_type
+    case opposable_type
+    when "ApplicationMeritsTask::Organisation"
+      "ORGANISATION"
+    when "ApplicationMeritsTask::Individual"
+      "INDIVIDUAL"
+    end
+  end
+
+  def first_name
+    object.opposable.first_name if object.opposable.respond_to?(:first_name)
+  end
+
+  def last_name
+    object.opposable.last_name if object.opposable.respond_to?(:last_name)
+  end
+
+  def organisation_name
+    object.opposable.name if object.opposable.respond_to?(:name)
+  end
+
+  # Full object for Civil Apply "rehydration", eventually
   def opposable
-    if opposable_type == "ApplicationMeritsTask::Organisation"
+    case opposable_type
+    when "ApplicationMeritsTask::Organisation"
       OrganisationJsonBuilder.build(object.opposable).as_json
-    else
+    when "ApplicationMeritsTask::Individual"
       IndividualJsonBuilder.build(object.opposable).as_json
     end
   end
