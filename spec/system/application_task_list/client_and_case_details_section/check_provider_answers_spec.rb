106312cdbfd74ce934ca2764b3837b8a812a962b
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-04-08 16:56:27 +0100

    AP-6092: Add 'not ready yet' scenario back in

--- spec/system/application_task_list/client_and_case_details_section/check_provider_answers_spec.rb
+++ spec/system/application_task_list/client_and_case_details_section/check_provider_answers_spec.rb
@@ -6,9 +6,44 @@ RSpec.describe "Client and case details section - Check your answers", :vcr do
     end
 
     scenario "I can complete the task list's Check your answers item" do
-      fill_in_client_and_case_details_until_step(:check_your_answers)
+      fill_in_client_and_case_details_until_step(:proceedings)
 
       @legal_aid_application ||= LegalAidApplication.find(id_from_current_page_url)
+      visit providers_legal_aid_application_task_list_path(@legal_aid_application)
+
+      expect_section_with_task_list_items("Client and case details") do
+        [
+          { name: "Client details", link_enabled: false, status: "Completed" },
+          { name: "Link to another application", link_enabled: false, status: "Completed" },
+          { name: "Proceedings", link_enabled: true, status: "Not started" },
+          { name: "Check your answers", link_enabled: false, status: "Not ready yet" },
+          { name: "DWP outcome", link_enabled: false, status: "Not ready yet" },
+        ]
+      end
+
+      click_on "Proceedings"
+
+      fill_in("proceeding-search-input", with: "da004")
+      govuk_choose("Non-molestation order", match: :prefer_exact)
+      click_on "Save and continue"
+
+      govuk_choose "No"
+      click_on "Save and continue"
+
+      govuk_choose "Applicant, claimant or petitioner"
+      click_on "Save and continue"
+
+      govuk_choose "No"
+      click_on "Save and continue"
+
+      govuk_choose "Yes"
+      click_on "Save and continue"
+
+      click_on "Save and continue"
+
+      govuk_choose "No"
+      click_on "Save and continue"
+
       visit providers_legal_aid_application_task_list_path(@legal_aid_application)
 
       expect_section_with_task_list_items("Client and case details") do
