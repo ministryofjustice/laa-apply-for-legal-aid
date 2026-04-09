fcfdf872f0b6634bfac6e0d04de4f767b2cfd775
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-04-09 16:46:49 +0100

    AP-6092: Add missing proceedings types spec
    Was missed off AP-6092

---
+++ spec/services/task_status/proceedings_types_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+RSpec.describe TaskStatus::ProceedingsTypes do
+  subject(:instance) { described_class.new(application) }
+
+  let(:application) { create(:application, applicant:) }
+
+  let(:applicant) do
+    create(
+      :applicant,
+      has_national_insurance_number: true,
+      national_insurance_number: "JA123456D",
+      applied_previously: false,
+      previous_reference: nil,
+      correspondence_address_choice: "home",
+      addresses: [build(:address, location: "home", lookup_used: true)],
+      employed: nil,
+    )
+  end
+
+  describe "#call" do
+    subject(:status) { instance.call }
+
+    context "with partially completed link section" do
+      before { application.update!(linked_application_completed: false) }
+
+      it { is_expected.to be_cannot_start }
+    end
+
+    context "with completed link section" do
+      before { application.update!(linked_application_completed: true) }
+
+      it { is_expected.to be_not_started }
+    end
+
+    context "with one proceeding added but details not complete", :vcr do
+      before do
+        create(:proceeding, :da004, legal_aid_application: application)
+      end
+
+      it { is_expected.to be_in_progress }
+    end
+
+    context "with one proceeding added and details complete", :vcr do
+      before do
+        create(:proceeding, :da004, legal_aid_application: application, used_delegated_functions: false, accepted_substantive_defaults: true)
+      end
+
+      it { is_expected.to be_completed }
+    end
+  end
+end
