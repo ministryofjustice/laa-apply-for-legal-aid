b0d5059393249cd3ebc1cc64099cff60b47567b8
Katharine Ahern <katharine.ahern@justice.gov.uk>
2026-04-16 12:20:51 +0100

    ap-6788: Add office address to review and print application page

--- app/controllers/providers/review_and_print_applications_controller.rb
+++ app/controllers/providers/review_and_print_applications_controller.rb
@@ -8,6 +8,8 @@ module Providers
     def show
       @source_application = @legal_aid_application.copy_case? ? LegalAidApplication.find(legal_aid_application.copy_case_id) : @legal_aid_application
       @read_only = true
+      @provider = current_provider
+      @office_address = office_address if @provider.selected_office.present?
     end
 
     def continue
@@ -16,6 +18,16 @@ module Providers
         legal_aid_application.generate_reports!
       end
       continue_or_draft
+    end
+
+  private
+
+    def office_address
+      helpers.office_address_one_line(PDA::OfficeAddressRetriever.call(@provider.selected_office.code))
+    rescue PDA::OfficeAddressRetriever::NotFoundError
+      I18n.t("providers.review_and_print_applications.show.office_address_not_found")
+    rescue PDA::OfficeAddressRetriever::ApiError
+      I18n.t("providers.review_and_print_applications.show.office_address_not_available")
     end
   end
 end
