a0777ddcf09a06293e26f9714853fe903d4d9721
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-04-23 12:01:34 +0100

    AP-6305: Amend reauthable
    Previous mechanism was not working as it did not rtigger the custom failure app
    
    NOTE: Do not sign out here as it will prvent the custom failure app triggering.
    However to prevent infinite redirect loops we set a flag in the session to indicate that we have triggered it
    then throw a message that triggers the custom failure app, in which we sign out and redirect.
    
    NOTE: initially setting the flag to symbol which will be returned, as a string. Not sure if this is a
    a good dependency, needs mote investigation.

--- lib/devise/controllers/custom_failure.rb
+++ lib/devise/controllers/custom_failure.rb
@@ -3,7 +3,13 @@ module Devise
     class CustomFailure < Devise::FailureApp
       def redirect_url
         case warden_message
-        when :timeout || :reauthenticate
+        when :timeout
+          # already signed out by timeoutable
+          session_expired_path(reason: warden_message)
+        when :reauthenticate
+          # not yet signed out by reauthauthable
+          proxy = Devise::Hooks::Proxy.new(warden)
+          Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
           session_expired_path(reason: warden_message)
         else
           super
