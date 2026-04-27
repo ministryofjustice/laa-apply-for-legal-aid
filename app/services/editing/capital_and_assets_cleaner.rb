362b6100c5ec4342e72d2bb35c676a53e6d542a6
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-04-27 17:17:38 +0100

    AP-6849: Add Capital and assets cleaner

--- app/services/editing/capital_and_assets_cleaner.rb
+++ app/services/editing/capital_and_assets_cleaner.rb
@@ -5,9 +5,33 @@ module Editing
     end
 
     def call
-      # rubocop:disable Rails/Output
-      puts "deleting capital and assets data for legal aid application #{legal_aid_application.id}"
-      # rubocop:enable Rails/Output
+      legal_aid_application.update!(
+        # "own_home" and "property_details" pages
+        own_home: nil,
+        property_value: nil,
+        outstanding_mortgage_amount: nil,
+        shared_ownership: nil,
+        percentage_home: nil,
+
+        # vehicles
+        own_vehicle: nil,
+
+        # "restrictions"
+        has_restrictions: nil,
+        restrictions_details: nil,
+      )
+
+      # "vehicle_details"
+      legal_aid_application.vehicles.each(&:destroy!)
+
+      # "offline_accounts" and "savings_and_investments" pages
+      legal_aid_application.savings_amount&.destroy!
+
+      # "other_assets" => capital_and_assets_sidebar_items,
+      legal_aid_application.other_assets_declaration&.destroy!
+
+      # "disregarded_payments" (aka mandatory disregards), "'payments_to_review" (aka discretionary disregards) and "capital_disregards/add_details" pages
+      legal_aid_application.capital_disregards.each(&:destroy!)
     end
 
   private
