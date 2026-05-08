5fb59ef23a5ac4fa3ceab84ba832f2691ce85072
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-05-08 11:01:01 +0100

    AP-6305: Add session expired page and flow
    Redirect idle timeout and reuthenticate/lifespan timeout to
    a specifc controller and page to explain the reason for the
    specific timeout/sign-out.
    
    NOTES:
    
    - The custom failure "app" is located under devise/controllersto handle zeitwerk loading issues
    
    - devise failure apps will not trigger if already signed out. Previously Reauthable expictly signed out
    and then threw an exception but the custom failure app was not triggering as they were already signed out.
    Do not sign out here as it will prevent the custom failure app triggering and redirecting to the custom
    session expired page. However, to prevent infinite redirect loops we must set a flag in the session and
    then skip the next check for reauthentication (to allow the redirect once), but then delete the flag to
    prepare for the next reauthenticate timeout for the sesion.
    IMPORTANT: Initially setting the flag to symbol which will be returned as a string. Not sure if this is a
    OK dependency, needs investigation.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,7 @@
 # Many of these configuration options can be set straight in your model.
 
 Devise.setup do |config|
+  require "devise/controllers/custom_failure"
   require "devise/models/reauthable"
 
   # ==> Configuration for custom devise extension :reauthable
@@ -309,6 +310,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
+  config.warden do |manager|
+    manager.failure_app = Devise::Controllers::CustomFailure
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
