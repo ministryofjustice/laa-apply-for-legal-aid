9b3cf2c5b68e71dd77802ad81704868b50451fa7
ColinBruce <6757677+colinbruce@users.noreply.github.com>
2026-06-03 12:13:14 +0100

    AP-6998: Update seed data
    This will be extended to test for report_only users...
    For now, lets stop breaking existing tests

--- db/seeds/admin_users.rb
+++ db/seeds/admin_users.rb
@@ -1,3 +1,4 @@
 AdminUser.find_or_create_by!(username: "apply_maintenance") do |admin_user|
   admin_user.email = "apply-for-civil-legal-aid@justice.gov.uk".freeze
+  admin_user.report_only = false
 end
--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :admin_user do
     username { Faker::Internet.username }
+    report_only { false }
   end
 end
