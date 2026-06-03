150ced0cbed02384a5445b721554dbf2fb2ff986
ColinBruce <6757677+colinbruce@users.noreply.github.com>
2026-06-03 12:13:14 +0100

    AP-6998: Update seed data
    This will be extended to test for report_only users...
    For now, lets stop breaking existing tests

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :admin_user do
     username { Faker::Internet.username }
+    role { :full }
+
+    trait :digest_only do
+      role { :digest_only }
+    end
   end
 end
