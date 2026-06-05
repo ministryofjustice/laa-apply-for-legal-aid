fba3754b99bb70fa1a0c9c035bd4e0a2fd98cbd2
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-06-05 11:33:31 +0100

    AP-7013: Remove LFA calls from task list validator

--- app/services/task_status/validators/proceeding_type.rb
+++ app/services/task_status/validators/proceeding_type.rb
@@ -5,6 +5,27 @@ module TaskStatus
 
       attr_reader :proceeding
 
+      delegate :client_involvement_type_ccms_code,
+               :client_involvement_type_description,
+               :accepted_emergency_defaults,
+               :accepted_emergency_defaults?,
+               :emergency_level_of_service,
+               :emergency_level_of_service_name,
+               :emergency_level_of_service_stage,
+               :final_hearings,
+               :emergency_final_hearing,
+               :accepted_substantive_defaults,
+               :accepted_substantive_defaults?,
+               :substantive_level_of_service,
+               :substantive_level_of_service_name,
+               :substantive_level_of_service_stage,
+               :used_delegated_functions?,
+               :special_children_act?,
+               :emergency_full_representation?,
+               :substantive_full_representation?,
+               :scope_limitations,
+               to: :proceeding
+
       def initialize(proceeding)
         @proceeding = proceeding
       end
@@ -12,6 +33,13 @@ module TaskStatus
       def valid?
         log_duration("TaskStatus::Validators::ProceedingType#valid?") do
           forms.all?(&:valid?)
+          && client_involvement_type_valid?
+          && emergency_defaults_valid?
+          && emergency_level_of_service_valid?
+          && emergency_scope_limitations_valid?
+          && substantive_defaults_valid?
+          && substantive_level_of_service_valid?
+          &&substantive_scope_limitations_valid?
         end
       end
 
@@ -19,99 +47,215 @@ module TaskStatus
 
       def forms
         [
-          client_involvement_type_form, # calls LegalFramework::ClientInvolvementTypes::Proceeding
+          # client_involvement_type_form, # calls LegalFramework::ClientInvolvementTypes::Proceeding
           delegated_functions_form,
-          emergency_defaults_form, # calls LegalFramework::ProceedingTypes::Defaults.call
-          emergency_level_of_service_form, # calls LegalFramework::ProceedingTypes::Proceeding.call
+          # emergency_defaults_form, # calls LegalFramework::ProceedingTypes::Defaults.call
+          # emergency_level_of_service_form, # calls LegalFramework::ProceedingTypes::Proceeding.call
           emergency_final_hearings_form,
-          emergency_scope_limitations_form, # calls LegalFramework::ProceedingTypes::Scopes.call
-          substantive_defaults_form, # calls LegalFramework::ProceedingTypes::Defaults.call
-          substantive_level_of_service_form, # calls LegalFramework::ProceedingTypes::Proceeding.call
+          # emergency_scope_limitations_form, # calls LegalFramework::ProceedingTypes::Scopes.call
+          # substantive_defaults_form, # calls LegalFramework::ProceedingTypes::Defaults.call
+          # substantive_level_of_service_form, # calls LegalFramework::ProceedingTypes::Proceeding.call
           substantive_final_hearings_form,
-          substantive_scope_limitations_form, # calls LegalFramework::ProceedingTypes::Scopes.call
+          # substantive_scope_limitations_form, # calls LegalFramework::ProceedingTypes::Scopes.call
         ].flatten.compact
       end
 
-      def client_involvement_type_form
-        Proceedings::ClientInvolvementTypeForm.new(model: proceeding) # calls LFA
+      # It is not the job of the task list to verify the actual code and description are valid values
+      def client_involvement_type_valid?
+        client_involvement_type_ccms_code.present?
+        && client_involvement_type_description.present?
       end
+
+      # def client_involvement_type_form
+      #   Proceedings::ClientInvolvementTypeForm.new(model: proceeding) # calls LFA
+      # end
 
       def delegated_functions_form
         Proceedings::DelegatedFunctionsForm.new(model: proceeding)
       end
 
-      def emergency_defaults_form
-        return if !proceeding.used_delegated_functions? || proceeding.special_children_act?
+      # It is not the job of the task list to verify the actual service levels or scope limitations were set correctly by the form so we only
+      # check that the emergency defaults were accepted or rejected.
+      # We don't check the validity of the hearing date as that would require an LFA query
+      # We don't check the validity of the limitation_note as the form does not (BUG?!) and
+      # would also require an LFA query
+      #
+      # TODO: we do need to check the presence of hearing date and or limitation note to avoid users seeing a potentially complete
+      # task list when they have not actually not completed the hearing date and.or limitation note, by using save and comabck later
+      #
+      def emergency_defaults_valid?
+        return true if !used_delegated_functions? || special_children_act?
 
-        Proceedings::EmergencyDefaultsForm.new(model: proceeding)
+        [true, false].include?(accepted_emergency_defaults)
       end
 
-      def emergency_level_of_service_form
-        return @emergency_level_of_service_form if defined?(@emergency_level_of_service_form)
+      # def emergency_defaults_form
+      #   return if !used_delegated_functions? || special_children_act?
 
-        @emergency_level_of_service_form =
-          if proceeding.used_delegated_functions? && !proceeding.accepted_emergency_defaults?
-            Proceedings::EmergencyLevelOfServiceForm.new(model: proceeding)
-          end
+      #   Proceedings::EmergencyDefaultsForm.new(model: proceeding)
+      # end
+
+      # It is not the job of the task list to verify the actual levels of service are valid
+      # only that the data the form will populate is present
+      def emergency_level_of_service_valid?
+        return true if used_delegated_functions? && !accepted_emergency_defaults?
+
+        emergency_level_of_service.present?
+        && emergency_level_of_service_name.present?
+        && emergency_level_of_service_stage.present?
       end
+
+      # def emergency_level_of_service_form
+      #   return @emergency_level_of_service_form if defined?(@emergency_level_of_service_form)
+
+      #   @emergency_level_of_service_form =
+      #     if used_delegated_functions? && !accepted_emergency_defaults?
+      #       Proceedings::EmergencyLevelOfServiceForm.new(model: proceeding)
+      #     end
+      # end
 
       def emergency_final_hearings_form
-        return unless proceeding.used_delegated_functions?
+        return unless used_delegated_functions?
         return unless emergency_changed_to_full_rep?
-        return unless emergency_level_of_service_form&.levels_of_service.to_a.length > 1
 
-        Proceedings::FinalHearingForm.new(model: proceeding.emergency_final_hearing)
+        # We do not need to check the validity of final hearing if one does not exist. however
+        # it is possible that a user could have left the final hearing form blank with save and
+        # come back later and the form did not create a final hearing record.
+        #
+        # TODO: We need to verify that the proceeding requires a final hearing or not based on whether the
+        # proceedings has more than one applicable service levels. if more than one and they changed to
+        # full rep/3 then they have to answer the final hearing question. However, this basic check for
+        # existence may suffice!!
+        return unless final_hearings.emergency.exists?
+
+        # REMOVED: How can we check this form just using database fields, currently it requires an LFA call.
+        # return unless emergency_level_of_service_form&.levels_of_service.to_a.length > 1
+
+        Proceedings::FinalHearingForm.new(model: emergency_final_hearing)
       end
 
       def emergency_changed_to_full_rep?
-        proceeding.emergency_full_representation? && !proceeding.accepted_emergency_defaults?
+        emergency_full_representation? && !accepted_emergency_defaults?
       end
 
-      def emergency_scopes
-        JSON.parse(LegalFramework::ProceedingTypes::Scopes.call(proceeding, true))["level_of_service"]["scope_limitations"]
+      # def emergency_scopes
+      #   JSON.parse(LegalFramework::ProceedingTypes::Scopes.call(proceeding, true))["level_of_service"]["scope_limitations"]
+      # end
+
+      def emergency_scopes_from_db
+        scope_limitations.where(scope_type: :emergency).map do |db_scope|
+          {
+            "code" => db_scope.code,
+            "meaning" => db_scope.meaning,
+            "description" => db_scope.description,
+            "additional_params" => [],
+            # "additional_params" => db_scope.additional_params,
+            # e.g.
+            #  "additional_params" => [{"name" => "hearing_date", "type" => "date", "mandatory" => true}]},
+          }
+        end
       end
 
-      def emergency_scope_limitations_form
-        return unless proceeding.used_delegated_functions?
-        return if proceeding.accepted_emergency_defaults?
-        return if proceeding.emergency_level_of_service.blank?
+      # def emergency_scope_limitations_form
+      #   return unless used_delegated_functions?
+      #   return if accepted_emergency_defaults?
+      #   return if emergency_level_of_service.blank?
 
-        Proceedings::ScopeLimitationsForm.call(emergency_scopes, model: proceeding, scope_type: :emergency)
+      #   Proceedings::ScopeLimitationsForm.call(emergency_scopes, model: proceeding, scope_type: :emergency)
+      # end
+
+      def emergency_scope_limitations_valid?
+        return true unless used_delegated_functions?
+        return true if accepted_emergency_defaults?
+        return true if emergency_level_of_service.blank?
+
+        # TODO: we need to add additional_params to scope_limitation model and save this value via the form when the user is
+        # adding scopes. This can then be passed back to the form here to validate the existing scope limitation again.
+        Proceedings::ScopeLimitationsForm.call(emergency_scopes_from_db, model: proceeding, scope_type: :emergency).valid?
       end
 
-      def substantive_defaults_form
-        Proceedings::SubstantiveDefaultsForm.new(model: proceeding)
+      def substantive_defaults_valid?
+        [true, false].include?(accepted_substantive_defaults)
       end
 
-      def substantive_level_of_service_form
-        return @substantive_level_of_service_form if defined?(@substantive_level_of_service_form)
+      # def substantive_defaults_form
+      #   Proceedings::SubstantiveDefaultsForm.new(model: proceeding)
+      # end
 
-        @substantive_level_of_service_form =
-          unless proceeding.accepted_substantive_defaults?
-            Proceedings::SubstantiveLevelOfServiceForm.new(model: proceeding)
-          end
+      # def substantive_level_of_service_form
+      #   return @substantive_level_of_service_form if defined?(@substantive_level_of_service_form)
+
+      #   @substantive_level_of_service_form =
+      #     unless accepted_substantive_defaults?
+      #       Proceedings::SubstantiveLevelOfServiceForm.new(model: proceeding)
+      #     end
+      # end
+
+      # It is not the job of the task list to verify the actual levels of service are valid
+      # only that the data the form will populate is present
+      def substantive_level_of_service_valid?
+        return true if accepted_substantive_defaults?
+
+        substantive_level_of_service.present?
+          && substantive_level_of_service_name.present?
+          && substantive_level_of_service_stage.present?
       end
 
       def substantive_changed_to_full_rep?
-        proceeding.substantive_full_representation? && !proceeding.accepted_substantive_defaults?
+        substantive_full_representation? && !accepted_substantive_defaults?
       end
 
       def substantive_final_hearings_form
         return unless substantive_changed_to_full_rep?
-        return unless substantive_level_of_service_form&.levels_of_service.to_a.length > 1
 
-        Proceedings::FinalHearingForm.new(model: proceeding.substantive_final_hearing)
+        # We do not need to check the validity of final hearing if one does not exist. however
+        # it is possible that a user could have left the final hearing form blank with save and
+        # come back later and the form did not create a final hearing record.
+        #
+        # TODO: We need to verify that the proceeding requires a final hearing or not based on whether the
+        # proceedings service levels are more than one. if more than one and they changed to full rep/3 then
+        # they have to answer the final hearing question. However, this basic check for existence may suffice!!
+        return unless final_hearings.substantive.exists?
+
+        # REMOVED: How can we check this form just using database fields, currently it requires an LFA call.
+        # return unless substantive_level_of_service_form&.levels_of_service.to_a.length > 1
+
+        Proceedings::FinalHearingForm.new(model: substantive_final_hearing)
       end
 
-      def substantive_scopes
-        JSON.parse(LegalFramework::ProceedingTypes::Scopes.call(proceeding, false))["level_of_service"]["scope_limitations"]
+      # def substantive_scopes
+      #   JSON.parse(LegalFramework::ProceedingTypes::Scopes.call(proceeding, false))["level_of_service"]["scope_limitations"]
+      # end
+
+      def substantive_scopes_from_db
+        scope_limitations.where(scope_type: :substantive).map do |sl|
+          {
+            "code" => sl.code,
+            "meaning" => sl.meaning,
+            "description" => sl.description,
+            "additional_params" => [],
+            # TODO: as below
+            # "additional_params" => sl.additional_params,
+            # e.g.
+            #  "additional_params" => [{"name" => "hearing_date", "type" => "date", "mandatory" => true}]},
+          }
+        end
       end
 
-      def substantive_scope_limitations_form
-        return if proceeding.accepted_substantive_defaults?
-        return if proceeding.substantive_level_of_service.blank?
+      # def substantive_scope_limitations_form
+      #   return if accepted_substantive_defaults?
+      #   return if substantive_level_of_service.blank?
 
-        Proceedings::ScopeLimitationsForm.call(substantive_scopes, model: proceeding, scope_type: :substantive)
+      #   Proceedings::ScopeLimitationsForm.call(substantive_scopes, model: proceeding, scope_type: :substantive)
+      # end
+
+      def substantive_scope_limitations_valid?
+        return true if accepted_substantive_defaults?
+        return true if substantive_level_of_service.blank?
+
+        # TODO: we need to add additional_params to scope_limitation model and save this value via the form when the user is
+        # adding scopes. This can then be passed back to the form here to validate the existing scope limitation again.
+        Proceedings::ScopeLimitationsForm.call(substantive_scopes_from_db, model: proceeding, scope_type: :substantive).valid?
       end
     end
   end
