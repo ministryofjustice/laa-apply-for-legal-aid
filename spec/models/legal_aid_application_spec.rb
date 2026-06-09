695113b914bc1d0339b52666100460f5dd682368
Katharine Ahern <katharine.ahern@justice.gov.uk>
2026-06-09 08:02:23 +0100

    ap-7019: Add new rules around validation of proceeding types for autogranting

--- spec/models/legal_aid_application_spec.rb
+++ spec/models/legal_aid_application_spec.rb
@@ -2511,13 +2511,40 @@ RSpec.describe LegalAidApplication do
     before { legal_aid_application.proceedings << proceeding }
 
     context "when there are special children act proceedings" do
-      it { is_expected.to be true }
+      context "when there is a valid combination of proceedings" do
+        context "when the application is a supervision order application" do
+          it { is_expected.to be true }
+        end
+
+        context "when the application has a care order and a supervision order proceeding" do
+          let(:care_order_proceeding) { create(:proceeding, :pb057, legal_aid_application:) }
+
+          before { legal_aid_application.proceedings << care_order_proceeding }
+
+          it { is_expected.to be true }
+        end
+      end
+
+      context "when the application does not have a valid combination of proceedings to be autogranted" do
+        let(:proceeding) { create(:proceeding, :pb057, legal_aid_application:) }
+        let(:emergency_protection_order_proceeding) { create(:proceeding, :pb026, legal_aid_application:) }
+
+        before { legal_aid_application.proceedings << emergency_protection_order_proceeding }
+
+        it { is_expected.to be false }
+      end
 
       context "when the applicant is a child subject with a supervision order proceeding and over 17" do
         let(:applicant) { create(:applicant, age_for_means_test_purposes: 17) }
 
         it { is_expected.to be false }
       end
+    end
+
+    context "when there are no special children act proceedings" do
+      let(:proceeding) { create(:proceeding, :da001, legal_aid_application:) }
+
+      it { is_expected.to be false }
     end
   end
 
