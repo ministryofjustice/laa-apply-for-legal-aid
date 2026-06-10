97b1a1d2fcf0522901b74b10c970e50e61421ef8
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-06-10 16:06:22 +0100

    AP-6909: Add citizen_completed_at column
    This will replace the exisiting completed_at column as it is more
    descriptive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_04_124023) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_10_150257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -669,6 +669,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_04_124023) do
     t.boolean "applicant_in_receipt_of_housing_benefit"
     t.string "application_ref"
     t.boolean "case_cloned"
+    t.datetime "citizen_completed_at", precision: nil
     t.datetime "client_declaration_confirmed_at", precision: nil
     t.datetime "completed_at", precision: nil
     t.boolean "copy_case"
