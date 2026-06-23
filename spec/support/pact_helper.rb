f9c126e60d4d05af0036a615545044eb53b75f1b
Joel Sugarman <7016425+jsugarman@users.noreply.github.com>
2026-06-23 15:09:01 +0100

    AP-7050: Add pact-ruby gem and generate pact file
    Add two basic pact file generating tests for the client involvement types
    and countries endpoints to see how it would be implement and
    to share config and context for further extension of the pact testing.

---
+++ spec/support/pact_helper.rb
@@ -0,0 +1,6 @@
+require "pact"
+require "pact/rspec"
+
+RSpec.shared_context "with legal framework api consumer pact" do
+  has_http_pact_between "laa-apply-for-legal-aid", "legal-framework-api", opts: { pact_dir: "spec/pacts/legal_framework_api" }
+end
